717582521c4041817edf8687777caf558ec09abe
dginovker <danielginovker@gmail.com>
2026-06-29 03:01:41 -0700

    Match FXParticleSystem category module template accessors and assignments

--- src/game/fx_particle_system.cpp
+++ src/game/fx_particle_system.cpp
@@ -2,6 +2,45 @@
 
 namespace FXParticleSystem {
 
+template <int Category, int SubCategory = 1>
+class CategoryModuleClassBase {
+public:
+    virtual ~CategoryModuleClassBase() {}
+    CategoryModuleClassBase &operator=(const CategoryModuleClassBase &that) { return *this; }
+};
+
+template <int Category>
+class CategoryModuleClass : public CategoryModuleClassBase<Category> {
+public:
+    const char *getKey() const { return m_key; }
+    const char *getName() const { return m_name; }
+
+private:
+    const char *m_key;
+    const char *m_name;
+};
+
+template <int Category>
+class CategoryModuleTemplateBase {
+public:
+    CategoryModuleTemplateBase &operator=(const CategoryModuleTemplateBase &that) { return *this; }
+};
+
+template <int Category>
+class CategoryModuleTemplate : public CategoryModuleTemplateBase<Category> {
+public:
+    CategoryModuleTemplate &operator=(const CategoryModuleTemplate &that) { return *this; }
+};
+
+template CategoryModuleClassBase<0, 1> &CategoryModuleClassBase<0, 1>::operator=(const CategoryModuleClassBase<0, 1> &);
+template CategoryModuleClassBase<3, 1> &CategoryModuleClassBase<3, 1>::operator=(const CategoryModuleClassBase<3, 1> &);
+template const char *CategoryModuleClass<0>::getKey() const;
+template const char *CategoryModuleClass<5>::getKey() const;
+template const char *CategoryModuleClass<8>::getName() const;
+template CategoryModuleTemplateBase<1> &CategoryModuleTemplateBase<1>::operator=(const CategoryModuleTemplateBase<1> &);
+template CategoryModuleTemplate<1> &CategoryModuleTemplate<1>::operator=(const CategoryModuleTemplate<1> &);
+template CategoryModuleTemplate<3> &CategoryModuleTemplate<3>::operator=(const CategoryModuleTemplate<3> &);
+
 ButterflyDrawModuleInfo::~ButterflyDrawModuleInfo()
 {
 }

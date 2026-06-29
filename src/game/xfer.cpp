1145022ee9ef5a47b54f597ac6d9ad235aa14e33
dginovker <danielginovker@gmail.com>
2026-06-28 23:37:48 -0700

    Match additional Xfer no-op virtuals

--- src/game/xfer.cpp
+++ src/game/xfer.cpp
@@ -4,3 +4,22 @@ bool Xfer::IsLoading() const
 {
     return false;
 }
+
+bool Xfer::IsStoring() const
+{
+    return false;
+}
+
+bool Xfer::IsCRC() const
+{
+    return false;
+}
+
+bool Xfer::IsLightCRC() const
+{
+    return false;
+}
+
+void Xfer::SkipBadBlock(Snapshot &snapshot, unsigned int size)
+{
+}
--- src/game/xfer.h
+++ src/game/xfer.h
@@ -1,6 +1,12 @@
 #pragma once
 
+class Snapshot;
+
 class Xfer {
 public:
     virtual bool IsLoading() const;
+    virtual bool IsStoring() const;
+    virtual bool IsCRC() const;
+    virtual bool IsLightCRC() const;
+    virtual void SkipBadBlock(Snapshot &snapshot, unsigned int size);
 };

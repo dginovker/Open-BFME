600bbc5d2b656cfe392fc78d54fde0dba70b4d7d
dginovker <danielginovker@gmail.com>
2026-06-29 04:21:27 -0700

    Match more FXParticleSystem template empty assignments

--- src/game/fx_particle_system.cpp
+++ src/game/fx_particle_system.cpp
@@ -43,14 +43,45 @@ struct DefaultModuleKey {
     DefaultModuleKey &operator=(const DefaultModuleKey &that) { return *this; }
 };
 
+template <int Category>
+struct DefaultModuleName {
+    DefaultModuleName &operator=(const DefaultModuleName &that) { return *this; }
+};
+
+template <int Category>
+class DefaultModuleTemplate {
+public:
+    DefaultModuleTemplate &operator=(const DefaultModuleTemplate &that) { return *this; }
+};
+
 template CategoryModuleClassBase<0, 1> &CategoryModuleClassBase<0, 1>::operator=(const CategoryModuleClassBase<0, 1> &);
+template CategoryModuleClassBase<1, 1> &CategoryModuleClassBase<1, 1>::operator=(const CategoryModuleClassBase<1, 1> &);
+template CategoryModuleClassBase<2, 1> &CategoryModuleClassBase<2, 1>::operator=(const CategoryModuleClassBase<2, 1> &);
 template CategoryModuleClassBase<3, 1> &CategoryModuleClassBase<3, 1>::operator=(const CategoryModuleClassBase<3, 1> &);
+template CategoryModuleClassBase<4, 1> &CategoryModuleClassBase<4, 1>::operator=(const CategoryModuleClassBase<4, 1> &);
+template CategoryModuleClassBase<5, 1> &CategoryModuleClassBase<5, 1>::operator=(const CategoryModuleClassBase<5, 1> &);
+template CategoryModuleClassBase<6, 1> &CategoryModuleClassBase<6, 1>::operator=(const CategoryModuleClassBase<6, 1> &);
+template CategoryModuleClassBase<7, 1> &CategoryModuleClassBase<7, 1>::operator=(const CategoryModuleClassBase<7, 1> &);
+template CategoryModuleClassBase<8, 0> &CategoryModuleClassBase<8, 0>::operator=(const CategoryModuleClassBase<8, 0> &);
 template const char *CategoryModuleClass<0>::getKey() const;
 template const char *CategoryModuleClass<5>::getKey() const;
 template const char *CategoryModuleClass<8>::getName() const;
+template CategoryModuleTemplateBase<0> &CategoryModuleTemplateBase<0>::operator=(const CategoryModuleTemplateBase<0> &);
 template CategoryModuleTemplateBase<1> &CategoryModuleTemplateBase<1>::operator=(const CategoryModuleTemplateBase<1> &);
+template CategoryModuleTemplateBase<2> &CategoryModuleTemplateBase<2>::operator=(const CategoryModuleTemplateBase<2> &);
+template CategoryModuleTemplateBase<3> &CategoryModuleTemplateBase<3>::operator=(const CategoryModuleTemplateBase<3> &);
+template CategoryModuleTemplateBase<4> &CategoryModuleTemplateBase<4>::operator=(const CategoryModuleTemplateBase<4> &);
+template CategoryModuleTemplateBase<5> &CategoryModuleTemplateBase<5>::operator=(const CategoryModuleTemplateBase<5> &);
+template CategoryModuleTemplateBase<6> &CategoryModuleTemplateBase<6>::operator=(const CategoryModuleTemplateBase<6> &);
+template CategoryModuleTemplateBase<7> &CategoryModuleTemplateBase<7>::operator=(const CategoryModuleTemplateBase<7> &);
+template CategoryModuleTemplateBase<8> &CategoryModuleTemplateBase<8>::operator=(const CategoryModuleTemplateBase<8> &);
+template CategoryModuleTemplate<0> &CategoryModuleTemplate<0>::operator=(const CategoryModuleTemplate<0> &);
 template CategoryModuleTemplate<1> &CategoryModuleTemplate<1>::operator=(const CategoryModuleTemplate<1> &);
+template CategoryModuleTemplate<2> &CategoryModuleTemplate<2>::operator=(const CategoryModuleTemplate<2> &);
 template CategoryModuleTemplate<3> &CategoryModuleTemplate<3>::operator=(const CategoryModuleTemplate<3> &);
+template CategoryModuleTemplate<4> &CategoryModuleTemplate<4>::operator=(const CategoryModuleTemplate<4> &);
+template CategoryModuleTemplate<5> &CategoryModuleTemplate<5>::operator=(const CategoryModuleTemplate<5> &);
+template CategoryModuleTemplate<6> &CategoryModuleTemplate<6>::operator=(const CategoryModuleTemplate<6> &);
 template CategoryModuleInfo<0> &CategoryModuleInfo<0>::operator=(const CategoryModuleInfo<0> &);
 template CategoryModuleInfo<1> &CategoryModuleInfo<1>::operator=(const CategoryModuleInfo<1> &);
 template CategoryModuleInfo<2> &CategoryModuleInfo<2>::operator=(const CategoryModuleInfo<2> &);
@@ -66,6 +97,13 @@ template DefaultModuleKey<2> &DefaultModuleKey<2>::operator=(const DefaultModule
 template DefaultModuleKey<3> &DefaultModuleKey<3>::operator=(const DefaultModuleKey<3> &);
 template DefaultModuleKey<6> &DefaultModuleKey<6>::operator=(const DefaultModuleKey<6> &);
 template DefaultModuleKey<7> &DefaultModuleKey<7>::operator=(const DefaultModuleKey<7> &);
+template DefaultModuleName<0> &DefaultModuleName<0>::operator=(const DefaultModuleName<0> &);
+template DefaultModuleName<1> &DefaultModuleName<1>::operator=(const DefaultModuleName<1> &);
+template DefaultModuleName<2> &DefaultModuleName<2>::operator=(const DefaultModuleName<2> &);
+template DefaultModuleName<3> &DefaultModuleName<3>::operator=(const DefaultModuleName<3> &);
+template DefaultModuleName<6> &DefaultModuleName<6>::operator=(const DefaultModuleName<6> &);
+template DefaultModuleName<7> &DefaultModuleName<7>::operator=(const DefaultModuleName<7> &);
+template DefaultModuleTemplate<6> &DefaultModuleTemplate<6>::operator=(const DefaultModuleTemplate<6> &);
 
 ButterflyDrawModuleInfo::~ButterflyDrawModuleInfo()
 {

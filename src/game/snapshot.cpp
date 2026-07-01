28be5397dd00454f318d6a5c1312e6bacb7ad6a2
dginovker <danielginovker@gmail.com>
2026-07-01 00:00:29 -0700

    Emit Snapshot functions from source

--- src/game/snapshot.cpp
+++ src/game/snapshot.cpp
@@ -1,1 +1,18 @@
 #include "snapshot.h"
+
+Snapshot::Snapshot()
+{
+}
+
+Snapshot::Snapshot(const Snapshot &that)
+{
+}
+
+Snapshot::~Snapshot()
+{
+}
+
+Snapshot &Snapshot::operator=(const Snapshot &that)
+{
+    return *this;
+}
--- src/game/snapshot.h
+++ src/game/snapshot.h
@@ -4,10 +4,10 @@ class Xfer;
 
 class Snapshot {
 public:
-    Snapshot() {}
-    Snapshot(const Snapshot &that) {}
-    virtual ~Snapshot() {}
-    Snapshot &operator=(const Snapshot &that) { return *this; }
+    Snapshot();
+    Snapshot(const Snapshot &that);
+    virtual ~Snapshot();
+    Snapshot &operator=(const Snapshot &that);
 
     virtual const char *GetSnapshotName() = 0;
     virtual void LoadPostProcess() = 0;

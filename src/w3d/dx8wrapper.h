722d0f79f7862ace8729b2650db776af76ba9201
dginovker <danielginovker@gmail.com>
2026-07-05 17:41:50 -0700

    Add line-renderer vertex format to dx8 shim

--- src/w3d/dx8wrapper.h
+++ src/w3d/dx8wrapper.h
@@ -5,4 +5,16 @@
 // DX8Wrapper statics must declare exactly what it uses (see ww3d.h precedent).
 #ifndef DX8WRAPPER_H
 #define DX8WRAPPER_H
+
+// Fixed vertex format used by the line renderers (verbatim from WW3D2 dx8fvf.h).
+struct VertexFormatXYZDUV1
+{
+	float x;
+	float y;
+	float z;
+	unsigned diffuse;
+	float u1;
+	float v1;
+};
+
 #endif

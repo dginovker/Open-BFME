be615b74ebe79571a9763af429bdb54608a57a9c
dginovker <danielginovker@gmail.com>
2026-07-05 21:04:02 -0700

    Bring up hlod.cpp (+18) and hcanim.cpp (+1): HLod/compressed-anim functions

--- src/w3d/winbase_shim.h
+++ src/w3d/winbase_shim.h
@@ -5,10 +5,16 @@
 // is identical.
 #ifndef WINBASE_SHIM_H
 #define WINBASE_SHIM_H
+#ifndef TRUE
+#define TRUE 1
+#define FALSE 0
+#endif
 extern "C" __declspec(dllimport) char * __stdcall lstrcpyA(char *, const char *);
 extern "C" __declspec(dllimport) int    __stdcall lstrlenA(const char *);
 extern "C" __declspec(dllimport) int    __stdcall lstrcmpiA(const char *, const char *);
+extern "C" __declspec(dllimport) char * __stdcall lstrcpynA(char *, const char *, int);
 #define lstrcpy  lstrcpyA
 #define lstrlen  lstrlenA
 #define lstrcmpi lstrcmpiA
+#define lstrcpyn lstrcpynA
 #endif

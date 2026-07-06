47cad26b20cd3e7d09eb6e4439a0903f4f4f7b69
dginovker <danielginovker@gmail.com>
2026-07-05 21:25:01 -0700

    Match distlod/line3d: 4 render-object functions

--- src/w3d/ww3d.h
+++ src/w3d/ww3d.h
@@ -13,12 +13,14 @@ class WW3D
 	static unsigned int SyncTime;
 	static int FrameCount;
 	static float DefaultNativeScreenSize;
+	static unsigned int FrameTime;
 	static bool IsSortingEnabled;
 	static bool AreStaticSortListsEnabled;
 public:
 	static unsigned int Get_Sync_Time(void) { return SyncTime; }
 	static unsigned int Get_Frame_Count(void) { return FrameCount; }
 	static float Get_Default_Native_Screen_Size(void) { return DefaultNativeScreenSize; }
+	static unsigned int Get_Frame_Time(void) { return FrameTime; }
 	static bool Is_Sorting_Enabled(void) { return IsSortingEnabled; }
 	static bool Are_Static_Sort_Lists_Enabled(void) { return AreStaticSortListsEnabled; }
 	static void Add_To_Static_Sort_List(class RenderObjClass *robj, unsigned int sort_level);

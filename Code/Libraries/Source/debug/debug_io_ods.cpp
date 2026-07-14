d444bfd70d3738f46a11051a54724414d6079795
dginovker <danielginovker@gmail.com>
2026-07-13 18:20:49 -0700

    Land GameEngineDevice batch (W3DTankDraw/TruckDraw, FlatHeightMap, W3DBufferManager, W3DAssetManager, W3DShroud/Snow), dib, debug_io_con/ods — 16 rows; remove zero-row texturefilter/RandomValue

---
+++ Code/Libraries/Source/debug/debug_io_ods.cpp
@@ -0,0 +1,54 @@
+// cl: /DNDEBUG /DWIN32 /D_WINDOWS /MD /EHsc /Ireference/shims/sweep /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngine/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/Compression /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/debug /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWLib /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/GameEngineDevice/Include /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WW3D2 /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWMath /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWDebug /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Libraries/Source/WWVegas/WWSaveLoad /Ireference/CnC_Generals_Zero_Hour/GeneralsMD/Code/Main
+// stlport
+#define Matrix4x4 Matrix4  // BFME renamed it
+#define __PLACEMENT_VEC_NEW_INLINE  // always.h/GameMemory.h define array placement-new themselves
+// stlport
+/*
+**	Command & Conquer Generals Zero Hour(tm)
+**	Copyright 2025 Electronic Arts Inc.
+**
+**	This program is free software: you can redistribute it and/or modify
+**	it under the terms of the GNU General Public License as published by
+**	the Free Software Foundation, either version 3 of the License, or
+**	(at your option) any later version.
+**
+**	This program is distributed in the hope that it will be useful,
+**	but WITHOUT ANY WARRANTY; without even the implied warranty of
+**	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**	GNU General Public License for more details.
+**
+**	You should have received a copy of the GNU General Public License
+**	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/////////////////////////////////////////////////////////////////////////EA-V1
+// $File: //depot/GeneralsMD/Staging/code/Libraries/Source/debug/debug_io_ods.cpp $
+// $Author: mhoffe $
+// $Revision: #1 $
+// $DateTime: 2003/07/03 11:55:26 $
+//
+// �2003 Electronic Arts
+//
+// Debug I/O class ods (OutputDebugString, for use in debugger)
+//////////////////////////////////////////////////////////////////////////////
+#include "_pch.h"
+#include <new>      // needed for placement new prototype
+
+// ?Write@DebugIOOds@@UAEXW4StringType@DebugIOInterface@@PBD1@Z present-unmatched
+void DebugIOOds::Write(StringType type, const char *src, const char *str)
+{
+  if (type!=StringType::StructuredCmdReply&&str)
+    OutputDebugString(str);
+}
+
+// ?Create@DebugIOOds@@SAPAVDebugIOInterface@@XZ present-unmatched
+DebugIOInterface *DebugIOOds::Create(void)
+{
+  return new (DebugAllocMemory(sizeof(DebugIOOds))) DebugIOOds();
+}
+
+void DebugIOOds::Delete(void)
+{
+  this->~DebugIOOds();
+  DebugFreeMemory(this);
+}

b16b7bcdca61ef9dc0ca978e7baebf67990b86bc
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-15 18:13:52 -0400

    Match terrain road transition parsers

---
+++ Code/masm_dumps/_parseTransitionToOCL_TerrainRoadType__KAXPAVINI__PAX1PBX_Z_601D40.asm
@@ -0,0 +1,44 @@
+.386
+.model flat
+
+; ?parseTransitionToOCL@TerrainRoadType@@KAXPAVINI@@PAX1PBX@Z
+; Retail @ 0x00601D40 size 263
+_TEXT SEGMENT
+public ?parseTransitionToOCL@TerrainRoadType@@KAXPAVINI@@PAX1PBX@Z
+?parseTransitionToOCL@TerrainRoadType@@KAXPAVINI@@PAX1PBX@Z PROC
+    db 83h,0ECh,8h,53h,55h,56h,57h,8Bh
+    db 7Ch,24h,1Ch,68h,0B4h,97h,0Fh,1h
+    db 8Bh,0CFh,0E8h,0A9h,0EDh,24h,0h,8Bh
+    db 1Dh,3Ch,93h,35h,1h,8Bh,0F0h,68h
+    db 14h,98h,0Ah,1h,56h,0FFh,0D3h,83h
+    db 0C4h,8h,85h,0C0h,75h,4h,0B3h,1h
+    db 0EBh,15h,68h,88h,50h,11h,1h,56h
+    db 0FFh,0D3h,83h,0C4h,8h,85h,0C0h,0Fh
+    db 85h,9Fh,0h,0h,0h,32h,0DBh,68h
+    db 7Ch,50h,11h,1h,8Bh,0CFh,0E8h,6Dh
+    db 0EDh,24h,0h,68h,30h,8Eh,2Bh,1h
+    db 50h,0E8h,42h,0ECh,24h,0h,83h,0C4h
+    db 8h,68h,70h,50h,11h,1h,8Bh,0CFh
+    db 8Bh,0E8h,0E8h,51h,0EDh,24h,0h,50h
+    db 0E8h,6Bh,8h,25h,0h,8Bh,0F0h,83h
+    db 0C4h,4h,4Eh,78h,42h,83h,0FEh,3h
+    db 7Dh,3Dh,68h,50h,28h,0Ah,1h,8Bh
+    db 0CFh,0E8h,32h,0EDh,24h,0h,51h,8Bh
+    db 0CCh,89h,64h,24h,14h,50h,0E8h,0E5h
+    db 6Dh,28h,0h,84h,0DBh,8Bh,4Ch,24h
+    db 24h,56h,55h,74h,0Dh,0E8h,0FEh,0EEh
+    db 0A1h,0FFh,5Fh,5Eh,5Dh,5Bh,83h,0C4h
+    db 8h,0C3h,0E8h,0D6h,6Ch,0A3h,0FFh,5Fh
+    db 5Eh,5Dh,5Bh,83h,0C4h,8h,0C3h,6Ah
+    db 3h,68h,34h,50h,11h,1h,8Dh,44h
+    db 24h,18h,6Ah,3h,50h,0E8h,0EEh,0E7h
+    db 24h,0h,83h,0C4h,10h,68h,30h,0FCh
+    db 1Dh,1h,8Dh,4Ch,24h,14h,51h,0E8h
+    db 0DCh,4Eh,3Fh,0h,68h,0h,50h,11h
+    db 1h,8Dh,54h,24h,14h,6Ah,3h,52h
+    db 0E8h,0CBh,0E7h,24h,0h,83h,0C4h,0Ch
+    db 68h,30h,0FCh,1Dh,1h,8Dh,44h,24h
+    db 14h,50h,0E8h,0B9h,4Eh,3Fh,0h
+?parseTransitionToOCL@TerrainRoadType@@KAXPAVINI@@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

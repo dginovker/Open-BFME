949b52de7dd2006ba62634256700e5061a044a00
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-16 11:04:31 -0400

    Land retail export 005BFDD0

---
+++ Code/masm_dumps/1_CategoryModuleTemplate_05_FXParticleSystem_UAE_XZ_005BFDD0_a2b834c8.asm
@@ -0,0 +1,16 @@
+.386
+.model flat
+
+; ??1?$CategoryModuleTemplate@$05@FXParticleSystem@@UAE@XZ
+; Retail @ 0x005BFDD0 size 33
+_TEXT SEGMENT
+public ??1?$CategoryModuleTemplate@$05@FXParticleSystem@@UAE@XZ
+??1?$CategoryModuleTemplate@$05@FXParticleSystem@@UAE@XZ PROC
+    db 85h,0C9h,74h,0Eh,0C7h,41h,04h,7Ch
+    db 0F9h,10h,01h,0C7h,01h,58h,37h,07h
+    db 01h,0C3h,33h,0C0h,0C7h,00h,7Ch,0F9h
+    db 10h,01h,0C7h,01h,58h,37h,07h,01h
+    db 0C3h
+??1?$CategoryModuleTemplate@$05@FXParticleSystem@@UAE@XZ ENDP
+_TEXT ENDS
+END

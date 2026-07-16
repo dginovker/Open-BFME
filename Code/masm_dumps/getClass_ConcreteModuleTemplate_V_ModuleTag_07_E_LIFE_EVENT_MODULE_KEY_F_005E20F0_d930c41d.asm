d6f7c57c302e432a6482d972890026924a5815a3
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-16 15:09:20 -0400

    Land retail export 005E20F0

---
+++ Code/masm_dumps/getClass_ConcreteModuleTemplate_V_ModuleTag_07_E_LIFE_EVENT_MODULE_KEY_F_005E20F0_d930c41d.asm
@@ -0,0 +1,16 @@
+.386
+.model flat
+
+; ?getClass@?$ConcreteModuleTemplate@V?$ModuleTag@$07$E?LIFE_EVENT_MODULE_KEY@FXParticleSystem@@3QBDB$E?LIFE_EVENT_MODULE_
+; NAME@2@3QBDBVLifeEventModule@2@VLifeEventModuleTemplate@2@VParticleLifeEventModule@2@VParticleLifeEventModuleTemplate@2@
+; @FXParticleSystem@@@FXParticleSystem@@UBEABV?$ConcreteModuleClass@V?$ModuleTag@$07$E?LIFE_EVENT_MODULE_KEY@FXParticleSys
+; tem@@3QBDB$E?LIFE_EVENT_MODULE_NAME@2@3QBDBVLifeEventModule@2@VLifeEventModuleTemplate@2@VParticleLifeEventModule@2@VPar
+; ticleLifeEventModuleTemplate@2@@FXParticleSystem@@@2@XZ
+; Retail @ 0x005E20F0 size 5
+_TEXT SEGMENT
+public _bfme_export_d930c41d
+_bfme_export_d930c41d PROC
+    db 0E9h,07h,2Bh,0A6h,0FFh
+_bfme_export_d930c41d ENDP
+_TEXT ENDS
+END

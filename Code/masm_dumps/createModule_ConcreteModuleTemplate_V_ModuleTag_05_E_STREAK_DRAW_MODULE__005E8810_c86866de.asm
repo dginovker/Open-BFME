4ae5d307bea2f596359a3c619610dbe9280aaad1
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-16 15:23:25 -0400

    Land retail export 005E8810

---
+++ Code/masm_dumps/createModule_ConcreteModuleTemplate_V_ModuleTag_05_E_STREAK_DRAW_MODULE__005E8810_c86866de.asm
@@ -0,0 +1,28 @@
+.386
+.model flat
+
+; ?createModule@?$ConcreteModuleTemplate@V?$ModuleTag@$05$E?STREAK_DRAW_MODULE_KEY@FXParticleSystem@@3QBDB$E?STREAK_DRAW_M
+; ODULE_NAME@2@3QBDBVStreakDrawModule@2@VStreakDrawModuleTemplate@2@V?$DefaultParticleModule@$05@2@V?$DefaultParticleModul
+; eTemplate@$05@2@@FXParticleSystem@@@FXParticleSystem@@UAEPAVStreakDrawModule@2@AAV?$TrackingPtr@VParticleSystem@FXPartic
+; leSystem@@@@@Z
+; Retail @ 0x005E8810 size 106
+_TEXT SEGMENT
+public _bfme_export_c86866de
+_bfme_export_c86866de PROC
+    db 6Ah,0FFh,68h,0FBh,0BAh,03h,01h,64h
+    db 0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,51h,56h,57h
+    db 6Ah,1Ch,8Bh,0F9h,0E8h,0FFh,96h,29h
+    db 00h,8Bh,0F0h,83h,0C4h,04h,89h,74h
+    db 24h,08h,33h,0C0h,3Bh,0F0h,89h,44h
+    db 24h,14h,74h,23h,8Bh,44h,24h,1Ch
+    db 57h,50h,8Bh,0CEh,0E8h,0D1h,0E3h,0A5h
+    db 0FFh,0C7h,06h,0F0h,1Dh,11h,01h,0C7h
+    db 46h,14h,0ECh,1Dh,11h,01h,0C7h,46h
+    db 18h,0D8h,1Dh,11h,01h,8Bh,0C6h,8Bh
+    db 4Ch,24h,0Ch,5Fh,5Eh,64h,89h,0Dh
+    db 00h,00h,00h,00h,83h,0C4h,10h,0C2h
+    db 04h,00h
+_bfme_export_c86866de ENDP
+_TEXT ENDS
+END

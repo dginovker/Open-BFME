0f0cff83feef8736ed02d4545376e3181e2ce5ba
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-16 11:26:22 -0400

    Land retail export 005DCD30

---
+++ Code/masm_dumps/clone_ConcreteModuleTemplate_V_ModuleTag_01_E_RENDEROBJECT_UPDATE_MODULE_005DCD30_31312602.asm
@@ -0,0 +1,16 @@
+.386
+.model flat
+
+; ?clone@?$ConcreteModuleTemplate@V?$ModuleTag@$01$E?RENDEROBJECT_UPDATE_MODULE_KEY@FXParticleSystem@@3QBDB$E?RENDEROBJECT_UPDATE_MODULE_NAME@2@3QBDBVRenderObjectUpdateModule@2@VRenderObjectUpdateModuleTemplate@2@VRenderObjectParticleUpdateModule@2@VRenderObjectParticleUpdateModuleTemplate@2@@FXParticleSystem@@@FXParticleSystem@@UBEPAVRenderObjectUpdateModuleTemplate@2@XZ
+; Retail @ 0x005DCD30 size 34
+_TEXT SEGMENT
+public _bfme_export_31312602
+_bfme_export_31312602 PROC
+    db 56h,68h,0A0h,00h,00h,00h,8Bh,0F1h
+    db 0E8h,0F3h,51h,2Ah,00h,83h,0C4h,04h
+    db 85h,0C0h,74h,0Ah,56h,8Bh,0C8h,0E8h
+    db 03h,43h,0A3h,0FFh,5Eh,0C3h,33h,0C0h
+    db 5Eh,0C3h
+_bfme_export_31312602 ENDP
+_TEXT ENDS
+END

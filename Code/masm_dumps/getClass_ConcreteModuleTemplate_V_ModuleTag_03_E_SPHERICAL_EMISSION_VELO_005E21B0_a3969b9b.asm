e3d5cafa8c5a5cd9c0a71270815bf5a4fc7fee7e
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-16 15:10:16 -0400

    Land retail export 005E21B0

---
+++ Code/masm_dumps/getClass_ConcreteModuleTemplate_V_ModuleTag_03_E_SPHERICAL_EMISSION_VELO_005E21B0_a3969b9b.asm
@@ -0,0 +1,17 @@
+.386
+.model flat
+
+; ?getClass@?$ConcreteModuleTemplate@V?$ModuleTag@$03$E?SPHERICAL_EMISSION_VELOCITY_MODULE_KEY@FXParticleSystem@@3QBDB$E?S
+; PHERICAL_EMISSION_VELOCITY_MODULE_NAME@2@3QBDBVSphericalEmissionVelocityModule@2@VSphericalEmissionVelocityModuleTemplat
+; e@2@V?$DefaultParticleModule@$03@2@V?$DefaultParticleModuleTemplate@$03@2@@FXParticleSystem@@@FXParticleSystem@@UBEABV?$
+; ConcreteModuleClass@V?$ModuleTag@$03$E?SPHERICAL_EMISSION_VELOCITY_MODULE_KEY@FXParticleSystem@@3QBDB$E?SPHERICAL_EMISSI
+; ON_VELOCITY_MODULE_NAME@2@3QBDBVSphericalEmissionVelocityModule@2@VSphericalEmissionVelocityModuleTemplate@2@V?$DefaultP
+; articleModule@$03@2@V?$DefaultParticleModuleTemplate@$03@2@@FXParticleSystem@@@2@XZ
+; Retail @ 0x005E21B0 size 5
+_TEXT SEGMENT
+public _bfme_export_a3969b9b
+_bfme_export_a3969b9b PROC
+    db 0E9h,0ECh,56h,0A2h,0FFh
+_bfme_export_a3969b9b ENDP
+_TEXT ENDS
+END

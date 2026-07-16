c8e35c42034b5b767513ac503ca88d21f75788f7
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-16 11:02:47 -0400

    Land retail export 0009B530

---
+++ Code/masm_dumps/9G_YA_NABV_StringBase_G_0_Z_0009B530_b5aa27e0.asm
@@ -0,0 +1,14 @@
+.386
+.model flat
+
+; ??$?9G@@YA_NABV?$StringBase@G@@0@Z
+; Retail @ 0x0009B530 size 20
+_TEXT SEGMENT
+public ??$?9G@@YA_NABV?$StringBase@G@@0@Z
+??$?9G@@YA_NABV?$StringBase@G@@0@Z PROC
+    db 8Bh,44h,24h,08h,8Bh,4Ch,24h,04h
+    db 50h,0E8h,0AEh,71h,0F8h,0FFh,0F7h,0D8h
+    db 1Bh,0C0h,0F7h,0D8h
+??$?9G@@YA_NABV?$StringBase@G@@0@Z ENDP
+_TEXT ENDS
+END

0ef088ebdf04784d318b4109f35c4e8d6b69460e
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-16 15:11:00 -0400

    Land retail export 005E21E0

---
+++ Code/masm_dumps/getClass_ConcreteModuleTemplate_V_ModuleTag_03_E_OUTWARD_EMISSION_VELOCI_005E21E0_6cabc4e5.asm
@@ -0,0 +1,17 @@
+.386
+.model flat
+
+; ?getClass@?$ConcreteModuleTemplate@V?$ModuleTag@$03$E?OUTWARD_EMISSION_VELOCITY_MODULE_KEY@FXParticleSystem@@3QBDB$E?OUT
+; WARD_EMISSION_VELOCITY_MODULE_NAME@2@3QBDBVOutwardEmissionVelocityModule@2@VOutwardEmissionVelocityModuleTemplate@2@V?$D
+; efaultParticleModule@$03@2@V?$DefaultParticleModuleTemplate@$03@2@@FXParticleSystem@@@FXParticleSystem@@UBEABV?$Concrete
+; ModuleClass@V?$ModuleTag@$03$E?OUTWARD_EMISSION_VELOCITY_MODULE_KEY@FXParticleSystem@@3QBDB$E?OUTWARD_EMISSION_VELOCITY_
+; MODULE_NAME@2@3QBDBVOutwardEmissionVelocityModule@2@VOutwardEmissionVelocityModuleTemplate@2@V?$DefaultParticleModule@$0
+; 3@2@V?$DefaultParticleModuleTemplate@$03@2@@FXParticleSystem@@@2@XZ
+; Retail @ 0x005E21E0 size 5
+_TEXT SEGMENT
+public _bfme_export_6cabc4e5
+_bfme_export_6cabc4e5 PROC
+    db 0E9h,73h,42h,0A5h,0FFh
+_bfme_export_6cabc4e5 ENDP
+_TEXT ENDS
+END

9c94b5fec00d699650774d06f89ac5a148aaf58a
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-16 11:04:15 -0400

    Land retail export 0054EB50

---
+++ Code/masm_dumps/MG_YA_NABV_StringBase_G_0_Z_0054EB50_19c1eacb.asm
@@ -0,0 +1,14 @@
+.386
+.model flat
+
+; ??$?MG@@YA_NABV?$StringBase@G@@0@Z
+; Retail @ 0x0054EB50 size 24
+_TEXT SEGMENT
+public ??$?MG@@YA_NABV?$StringBase@G@@0@Z
+??$?MG@@YA_NABV?$StringBase@G@@0@Z PROC
+    db 8Bh,44h,24h,08h,8Bh,4Ch,24h,04h
+    db 50h,0E8h,8Eh,3Bh,0ADh,0FFh,33h,0C9h
+    db 85h,0C0h,0Fh,9Ch,0C1h,8Ah,0C1h,0C3h
+??$?MG@@YA_NABV?$StringBase@G@@0@Z ENDP
+_TEXT ENDS
+END

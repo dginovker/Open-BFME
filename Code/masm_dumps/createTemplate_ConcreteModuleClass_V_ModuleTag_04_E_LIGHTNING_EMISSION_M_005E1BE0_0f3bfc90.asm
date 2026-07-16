7764e66a7e28da09f9eaf66e3bfd415447019218
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-16 11:41:34 -0400

    Land retail export 005E1BE0

---
+++ Code/masm_dumps/createTemplate_ConcreteModuleClass_V_ModuleTag_04_E_LIGHTNING_EMISSION_M_005E1BE0_0f3bfc90.asm
@@ -0,0 +1,31 @@
+.386
+.model flat
+
+; ?createTemplate@?$ConcreteModuleClass@V?$ModuleTag@$04$E?LIGHTNING_EMISSION_MODULE_KEY@FXParticleSystem@@3QBDB$E?LIGHTNI
+; NG_EMISSION_MODULE_NAME@2@3QBDBVLightningEmissionModule@2@VLightningEmissionModuleTemplate@2@V?$DefaultParticleModule@$0
+; 4@2@V?$DefaultParticleModuleTemplate@$04@2@@FXParticleSystem@@@FXParticleSystem@@UBEPAVLightningEmissionModuleTemplate@2
+; @PAVINI@@@Z
+; Retail @ 0x005E1BE0 size 129
+_TEXT SEGMENT
+public _bfme_export_0f3bfc90
+_bfme_export_0f3bfc90 PROC
+    db 6Ah,0FFh,68h,23h,0B5h,03h,01h,64h
+    db 0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,51h,56h,68h
+    db 94h,00h,00h,00h,0E8h,2Fh,03h,2Ah
+    db 00h,8Bh,0F0h,83h,0C4h,04h,89h,74h
+    db 24h,04h,85h,0F6h,0C7h,44h,24h,10h
+    db 00h,00h,00h,00h,74h,1Dh,8Bh,0CEh
+    db 0E8h,1Ch,0B0h,0A2h,0FFh,0C7h,06h,58h
+    db 12h,11h,01h,0C7h,46h,04h,54h,12h
+    db 11h,01h,0C7h,46h,08h,40h,12h,11h
+    db 01h,0EBh,02h,33h,0F6h,89h,74h,24h
+    db 04h,8Bh,44h,24h,18h,50h,8Bh,0CEh
+    db 0C7h,44h,24h,14h,01h,00h,00h,00h
+    db 0E8h,0B9h,43h,0A6h,0FFh,8Bh,4Ch,24h
+    db 08h,8Bh,0C6h,5Eh,64h,89h,0Dh,00h
+    db 00h,00h,00h,83h,0C4h,10h,0C2h,04h
+    db 00h
+_bfme_export_0f3bfc90 ENDP
+_TEXT ENDS
+END

d74a06e1bbbcc9c91c04a942c47dcaf5c8be3680
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-16 11:13:52 -0400

    Land retail export 005E42E0

---
+++ Code/masm_dumps/createTemplate_ConcreteModuleClass_V_DefaultModuleTag_0A_FXParticleSyste_005E42E0_31569bde.asm
@@ -0,0 +1,24 @@
+.386
+.model flat
+
+; ?createTemplate@?$ConcreteModuleClass@V?$DefaultModuleTag@$0A@@FXParticleSystem@@@FXParticleSystem@@UBEPAV?$DefaultModuleTemplate@$0A@@2@XZ
+; Retail @ 0x005E42E0 size 97
+_TEXT SEGMENT
+public ?createTemplate@?$ConcreteModuleClass@V?$DefaultModuleTag@$0A@@FXParticleSystem@@@FXParticleSystem@@UBEPAV?$DefaultModuleTemplate@$0A@@2@XZ
+?createTemplate@?$ConcreteModuleClass@V?$DefaultModuleTag@$0A@@FXParticleSystem@@@FXParticleSystem@@UBEPAV?$DefaultModuleTemplate@$0A@@2@XZ PROC
+    db 6Ah,0FFh,68h,2Bh,0B7h,03h,01h,64h
+    db 0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,51h,56h,68h
+    db 98h,00h,00h,00h,0E8h,2Fh,0DCh,29h
+    db 00h,8Bh,0F0h,83h,0C4h,04h,89h,74h
+    db 24h,04h,33h,0C0h,3Bh,0F0h,89h,44h
+    db 24h,10h,74h,1Dh,8Bh,0CEh,0E8h,9Ah
+    db 0D7h,0A5h,0FFh,0C7h,06h,98h,0Fh,11h
+    db 01h,0C7h,46h,04h,94h,0Fh,11h,01h
+    db 0C7h,46h,08h,80h,0Fh,11h,01h,8Bh
+    db 0C6h,8Bh,4Ch,24h,08h,5Eh,64h,89h
+    db 0Dh,00h,00h,00h,00h,83h,0C4h,10h
+    db 0C3h
+?createTemplate@?$ConcreteModuleClass@V?$DefaultModuleTag@$0A@@FXParticleSystem@@@FXParticleSystem@@UBEPAV?$DefaultModuleTemplate@$0A@@2@XZ ENDP
+_TEXT ENDS
+END

6d053fd99875fab1a8e9418cc9c5cc69692e023f
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-16 11:21:18 -0400

    Land retail export 005DAD80

---
+++ Code/masm_dumps/clone_ConcreteModuleTemplate_V_ModuleTag_05_E_BUTTERFLY_DRAW_MODULE_KEY__005DAD80_de8eb043.asm
@@ -0,0 +1,15 @@
+.386
+.model flat
+
+; ?clone@?$ConcreteModuleTemplate@V?$ModuleTag@$05$E?BUTTERFLY_DRAW_MODULE_KEY@FXParticleSystem@@3QBDB$E?BUTTERFLY_DRAW_MODULE_NAME@2@3QBDBVButterflyDrawModule@2@VButterflyDrawModuleTemplate@2@V?$DefaultParticleModule@$05@2@V?$DefaultParticleModuleTemplate@$05@2@@FXParticleSystem@@@FXParticleSystem@@UBEPAVButterflyDrawModuleTemplate@2@XZ
+; Retail @ 0x005DAD80 size 31
+_TEXT SEGMENT
+public _bfme_export_de8eb043
+_bfme_export_de8eb043 PROC
+    db 56h,6Ah,0Ch,8Bh,0F1h,0E8h,0A6h,71h
+    db 2Ah,00h,83h,0C4h,04h,85h,0C0h,74h
+    db 0Ah,56h,8Bh,0C8h,0E8h,06h,15h,0A6h
+    db 0FFh,5Eh,0C3h,33h,0C0h,5Eh,0C3h
+_bfme_export_de8eb043 ENDP
+_TEXT ENDS
+END

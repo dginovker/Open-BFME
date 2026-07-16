6a9e65dab13def4132f79a359600e77b962d8b0a
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-16 11:35:18 -0400

    Land retail export 005DFB30

---
+++ Code/masm_dumps/getClass_ConcreteModuleTemplate_V_ModuleTag_05_E_RENDEROBJECT_DRAW_MODUL_005DFB30_b5657c36.asm
@@ -0,0 +1,17 @@
+.386
+.model flat
+
+; ?getClass@?$ConcreteModuleTemplate@V?$ModuleTag@$05$E?RENDEROBJECT_DRAW_MODULE_KEY@FXParticleSystem@@3QBDB$E?RENDEROBJEC
+; T_DRAW_MODULE_NAME@2@3QBDBVRenderObjectDrawModule@2@VRenderObjectDrawModuleTemplate@2@V?$DefaultParticleModule@$05@2@V?$
+; DefaultParticleModuleTemplate@$05@2@@FXParticleSystem@@@FXParticleSystem@@UBEABV?$ConcreteModuleClass@V?$ModuleTag@$05$E
+; ?RENDEROBJECT_DRAW_MODULE_KEY@FXParticleSystem@@3QBDB$E?RENDEROBJECT_DRAW_MODULE_NAME@2@3QBDBVRenderObjectDrawModule@2@V
+; RenderObjectDrawModuleTemplate@2@V?$DefaultParticleModule@$05@2@V?$DefaultParticleModuleTemplate@$05@2@@FXParticleSystem
+; @@@2@XZ
+; Retail @ 0x005DFB30 size 5
+_TEXT SEGMENT
+public _bfme_export_b5657c36
+_bfme_export_b5657c36 PROC
+    db 0E9h,4Fh,6Bh,0A4h,0FFh
+_bfme_export_b5657c36 ENDP
+_TEXT ENDS
+END

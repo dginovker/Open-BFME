3e051ad4ef6285ff7bcd0f092c3f18d5398be1ce
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-16 15:11:29 -0400

    Land retail export 005E2210

---
+++ Code/masm_dumps/getClass_ConcreteModuleTemplate_V_ModuleTag_04_E_BOX_EMISSION_VOLUME_MOD_005E2210_81e09dfb.asm
@@ -0,0 +1,17 @@
+.386
+.model flat
+
+; ?getClass@?$ConcreteModuleTemplate@V?$ModuleTag@$04$E?BOX_EMISSION_VOLUME_MODULE_KEY@FXParticleSystem@@3QBDB$E?BOX_EMISS
+; ION_VOLUME_MODULE_NAME@2@3QBDBVBoxEmissionVolumeModule@2@VBoxEmissionVolumeModuleTemplate@2@V?$DefaultParticleModule@$04
+; @2@V?$DefaultParticleModuleTemplate@$04@2@@FXParticleSystem@@@FXParticleSystem@@UBEABV?$ConcreteModuleClass@V?$ModuleTag
+; @$04$E?BOX_EMISSION_VOLUME_MODULE_KEY@FXParticleSystem@@3QBDB$E?BOX_EMISSION_VOLUME_MODULE_NAME@2@3QBDBVBoxEmissionVolum
+; eModule@2@VBoxEmissionVolumeModuleTemplate@2@V?$DefaultParticleModule@$04@2@V?$DefaultParticleModuleTemplate@$04@2@@FXPa
+; rticleSystem@@@2@XZ
+; Retail @ 0x005E2210 size 5
+_TEXT SEGMENT
+public _bfme_export_81e09dfb
+_bfme_export_81e09dfb PROC
+    db 0E9h,0Ah,14h,0A3h,0FFh
+_bfme_export_81e09dfb ENDP
+_TEXT ENDS
+END

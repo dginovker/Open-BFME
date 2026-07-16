2e0256d0b0ec7062b42649390142a9c3462308f9
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-16 11:11:06 -0400

    Land retail export 005DE650

---
+++ Code/masm_dumps/1TerrainCollisionModuleInfo_FXParticleSystem_UAE_XZ_005DE650_db9494df.asm
@@ -0,0 +1,20 @@
+.386
+.model flat
+
+; ??1TerrainCollisionModuleInfo@FXParticleSystem@@UAE@XZ
+; Retail @ 0x005DE650 size 67
+_TEXT SEGMENT
+public ??1TerrainCollisionModuleInfo@FXParticleSystem@@UAE@XZ
+??1TerrainCollisionModuleInfo@FXParticleSystem@@UAE@XZ PROC
+    db 6Ah,0FFh,68h,18h,0AFh,03h,01h,64h
+    db 0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,51h,56h,8Bh
+    db 0F1h,89h,74h,24h,04h,8Dh,4Eh,04h
+    db 0C7h,44h,24h,10h,00h,00h,00h,00h
+    db 0E8h,0C3h,92h,2Ah,00h,8Bh,4Ch,24h
+    db 08h,0C7h,06h,44h,37h,07h,01h,5Eh
+    db 64h,89h,0Dh,00h,00h,00h,00h,83h
+    db 0C4h,10h,0C3h
+??1TerrainCollisionModuleInfo@FXParticleSystem@@UAE@XZ ENDP
+_TEXT ENDS
+END

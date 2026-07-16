83f95f16299a34242f70cbd582cd86025492596b
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-16 15:23:52 -0400

    Land retail export 005E8930

---
+++ Code/masm_dumps/createModule_ConcreteModuleTemplate_V_ModuleTag_05_E_BUTTERFLY_DRAW_MODU_005E8930_2ade81e4.asm
@@ -0,0 +1,28 @@
+.386
+.model flat
+
+; ?createModule@?$ConcreteModuleTemplate@V?$ModuleTag@$05$E?BUTTERFLY_DRAW_MODULE_KEY@FXParticleSystem@@3QBDB$E?BUTTERFLY_
+; DRAW_MODULE_NAME@2@3QBDBVButterflyDrawModule@2@VButterflyDrawModuleTemplate@2@V?$DefaultParticleModule@$05@2@V?$DefaultP
+; articleModuleTemplate@$05@2@@FXParticleSystem@@@FXParticleSystem@@UAEPAVButterflyDrawModule@2@AAV?$TrackingPtr@VParticle
+; System@FXParticleSystem@@@@@Z
+; Retail @ 0x005E8930 size 106
+_TEXT SEGMENT
+public _bfme_export_2ade81e4
+_bfme_export_2ade81e4 PROC
+    db 6Ah,0FFh,68h,3Bh,0BBh,03h,01h,64h
+    db 0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,51h,56h,57h
+    db 6Ah,1Ch,8Bh,0F9h,0E8h,0DFh,95h,29h
+    db 00h,8Bh,0F0h,83h,0C4h,04h,89h,74h
+    db 24h,08h,33h,0C0h,3Bh,0F0h,89h,44h
+    db 24h,14h,74h,23h,8Bh,44h,24h,1Ch
+    db 57h,50h,8Bh,0CEh,0E8h,0AEh,25h,0A2h
+    db 0FFh,0C7h,06h,68h,1Eh,11h,01h,0C7h
+    db 46h,14h,64h,1Eh,11h,01h,0C7h,46h
+    db 18h,50h,1Eh,11h,01h,8Bh,0C6h,8Bh
+    db 4Ch,24h,0Ch,5Fh,5Eh,64h,89h,0Dh
+    db 00h,00h,00h,00h,83h,0C4h,10h,0C2h
+    db 04h,00h
+_bfme_export_2ade81e4 ENDP
+_TEXT ENDS
+END

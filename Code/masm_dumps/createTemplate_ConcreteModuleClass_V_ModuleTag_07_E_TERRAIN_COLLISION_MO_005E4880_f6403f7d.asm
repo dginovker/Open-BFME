4948c1409aff429004ec2e580cf258cbb974e71c
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-16 15:23:11 -0400

    Land retail export 005E4880

---
+++ Code/masm_dumps/createTemplate_ConcreteModuleClass_V_ModuleTag_07_E_TERRAIN_COLLISION_MO_005E4880_f6403f7d.asm
@@ -0,0 +1,26 @@
+.386
+.model flat
+
+; ?createTemplate@?$ConcreteModuleClass@V?$ModuleTag@$07$E?TERRAIN_COLLISION_MODULE_KEY@FXParticleSystem@@3QBDB$E?TERRAIN_
+; COLLISION_MODULE_NAME@2@3QBDBVTerrainCollisionModule@2@VTerrainCollisionModuleTemplate@2@VParticleTerrainCollisionModule
+; @2@VParticleTerrainCollisionModuleTemplate@2@@FXParticleSystem@@@FXParticleSystem@@UBEPAVTerrainCollisionModuleTemplate@
+; 2@XZ
+; Retail @ 0x005E4880 size 94
+_TEXT SEGMENT
+public _bfme_export_f6403f7d
+_bfme_export_f6403f7d PROC
+    db 6Ah,0FFh,68h,0BBh,0B8h,03h,01h,64h
+    db 0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,51h,56h,6Ah
+    db 28h,0E8h,92h,0D6h,29h,00h,8Bh,0F0h
+    db 83h,0C4h,04h,89h,74h,24h,04h,33h
+    db 0C0h,3Bh,0F0h,89h,44h,24h,10h,74h
+    db 1Dh,8Bh,0CEh,0E8h,4Ah,0DDh,0A2h,0FFh
+    db 0C7h,06h,50h,14h,11h,01h,0C7h,46h
+    db 04h,4Ch,14h,11h,01h,0C7h,46h,0Ch
+    db 38h,14h,11h,01h,8Bh,0C6h,8Bh,4Ch
+    db 24h,08h,5Eh,64h,89h,0Dh,00h,00h
+    db 00h,00h,83h,0C4h,10h,0C3h
+_bfme_export_f6403f7d ENDP
+_TEXT ENDS
+END

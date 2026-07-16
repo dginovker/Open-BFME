2f9adc870ba33a00b1e0ccda261ec67bca016423
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-16 11:12:48 -0400

    Land retail export 005E14A0

---
+++ Code/masm_dumps/createTemplate_ConcreteModuleClass_UPointEmissionVolumeModuleTag_FXParti_005E14A0_53be4666.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+
+; ?createTemplate@?$ConcreteModuleClass@UPointEmissionVolumeModuleTag@FXParticleSystem@@@FXParticleSystem@@UBEPAVPointEmissionVolumeModuleTemplate@2@XZ
+; Retail @ 0x005E14A0 size 56
+_TEXT SEGMENT
+public ?createTemplate@?$ConcreteModuleClass@UPointEmissionVolumeModuleTag@FXParticleSystem@@@FXParticleSystem@@UBEPAVPointEmissionVolumeModuleTemplate@2@XZ
+?createTemplate@?$ConcreteModuleClass@UPointEmissionVolumeModuleTag@FXParticleSystem@@@FXParticleSystem@@UBEPAVPointEmissionVolumeModuleTemplate@2@XZ PROC
+    db 6Ah,10h,0E8h,89h,0Ah,2Ah,00h,83h
+    db 0C4h,04h,85h,0C0h,74h,27h,0C7h,40h
+    db 04h,0ACh,0F9h,10h,01h,0C7h,40h,08h
+    db 80h,0F9h,10h,01h,0C6h,40h,0Ch,00h
+    db 0C7h,00h,7Ch,11h,11h,01h,0C7h,40h
+    db 04h,78h,11h,11h,01h,0C7h,40h,08h
+    db 64h,11h,11h,01h,0C3h,33h,0C0h,0C3h
+?createTemplate@?$ConcreteModuleClass@UPointEmissionVolumeModuleTag@FXParticleSystem@@@FXParticleSystem@@UBEPAVPointEmissionVolumeModuleTemplate@2@XZ ENDP
+_TEXT ENDS
+END

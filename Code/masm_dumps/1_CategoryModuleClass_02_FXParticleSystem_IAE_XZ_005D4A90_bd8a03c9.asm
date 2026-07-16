868c5bca0fe9a7e7871a6f0a69bd318ab7767206
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-16 11:06:18 -0400

    Land retail export 005D4A90

---
+++ Code/masm_dumps/1_CategoryModuleClass_02_FXParticleSystem_IAE_XZ_005D4A90_bd8a03c9.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ??1?$CategoryModuleClass@$02@FXParticleSystem@@IAE@XZ
+; Retail @ 0x005D4A90 size 7
+_TEXT SEGMENT
+public ??1?$CategoryModuleClass@$02@FXParticleSystem@@IAE@XZ
+??1?$CategoryModuleClass@$02@FXParticleSystem@@IAE@XZ PROC
+    db 0C7h,01h,50h,08h,11h,01h,0C3h
+??1?$CategoryModuleClass@$02@FXParticleSystem@@IAE@XZ ENDP
+_TEXT ENDS
+END

3139fca674b38d21f577ca93b38fbe9d5fe0118b
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-16 11:33:54 -0400

    Land retail export 005DFA80

---
+++ Code/masm_dumps/getClass_ConcreteModuleTemplate_V_ModuleTag_05_E_STREAK_DRAW_MODULE_KEY__005DFA80_042c9649.asm
@@ -0,0 +1,16 @@
+.386
+.model flat
+
+; ?getClass@?$ConcreteModuleTemplate@V?$ModuleTag@$05$E?STREAK_DRAW_MODULE_KEY@FXParticleSystem@@3QBDB$E?STREAK_DRAW_MODUL
+; E_NAME@2@3QBDBVStreakDrawModule@2@VStreakDrawModuleTemplate@2@V?$DefaultParticleModule@$05@2@V?$DefaultParticleModuleTem
+; plate@$05@2@@FXParticleSystem@@@FXParticleSystem@@UBEABV?$ConcreteModuleClass@V?$ModuleTag@$05$E?STREAK_DRAW_MODULE_KEY@
+; FXParticleSystem@@3QBDB$E?STREAK_DRAW_MODULE_NAME@2@3QBDBVStreakDrawModule@2@VStreakDrawModuleTemplate@2@V?$DefaultParti
+; cleModule@$05@2@V?$DefaultParticleModuleTemplate@$05@2@@FXParticleSystem@@@2@XZ
+; Retail @ 0x005DFA80 size 5
+_TEXT SEGMENT
+public _bfme_export_042c9649
+_bfme_export_042c9649 PROC
+    db 0E9h,0C7h,84h,0A5h,0FFh
+_bfme_export_042c9649 ENDP
+_TEXT ENDS
+END

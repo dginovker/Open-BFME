cb8b890b5062083549f6075dd9bd8318e978ffb5
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-16 15:10:02 -0400

    Land retail export 005E2190

---
+++ Code/masm_dumps/getClass_ConcreteModuleTemplate_V_ModuleTag_07_E_TERRAIN_COLLISION_MODUL_005E2190_329f4c7d.asm
@@ -0,0 +1,17 @@
+.386
+.model flat
+
+; ?getClass@?$ConcreteModuleTemplate@V?$ModuleTag@$07$E?TERRAIN_COLLISION_MODULE_KEY@FXParticleSystem@@3QBDB$E?TERRAIN_COL
+; LISION_MODULE_NAME@2@3QBDBVTerrainCollisionModule@2@VTerrainCollisionModuleTemplate@2@VParticleTerrainCollisionModule@2@
+; VParticleTerrainCollisionModuleTemplate@2@@FXParticleSystem@@@FXParticleSystem@@UBEABV?$ConcreteModuleClass@V?$ModuleTag
+; @$07$E?TERRAIN_COLLISION_MODULE_KEY@FXParticleSystem@@3QBDB$E?TERRAIN_COLLISION_MODULE_NAME@2@3QBDBVTerrainCollisionModu
+; le@2@VTerrainCollisionModuleTemplate@2@VParticleTerrainCollisionModule@2@VParticleTerrainCollisionModuleTemplate@2@@FXPa
+; rticleSystem@@@2@XZ
+; Retail @ 0x005E2190 size 5
+_TEXT SEGMENT
+public _bfme_export_329f4c7d
+_bfme_export_329f4c7d PROC
+    db 0E9h,19h,03h,0A3h,0FFh
+_bfme_export_329f4c7d ENDP
+_TEXT ENDS
+END

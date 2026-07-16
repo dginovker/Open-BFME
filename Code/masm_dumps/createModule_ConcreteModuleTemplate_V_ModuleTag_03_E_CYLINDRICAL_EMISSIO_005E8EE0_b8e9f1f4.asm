a4fed0f57553c28c60807412e27ec6c352b67a00
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-16 15:40:20 -0400

    Land retail export 005E8EE0

---
+++ Code/masm_dumps/createModule_ConcreteModuleTemplate_V_ModuleTag_03_E_CYLINDRICAL_EMISSIO_005E8EE0_b8e9f1f4.asm
@@ -0,0 +1,23 @@
+.386
+.model flat
+
+; ?createModule@?$ConcreteModuleTemplate@V?$ModuleTag@$03$E?CYLINDRICAL_EMISSION_VELOCITY_MODULE_KEY@FXParticleSystem@@3QB
+; DB$E?CYLINDRICAL_EMISSION_VELOCITY_MODULE_NAME@2@3QBDBVCylindricalEmissionVelocityModule@2@VCylindricalEmissionVelocityM
+; oduleTemplate@2@V?$DefaultParticleModule@$03@2@V?$DefaultParticleModuleTemplate@$03@2@@FXParticleSystem@@@FXParticleSyst
+; em@@UAEPAVCylindricalEmissionVelocityModule@2@AAV?$TrackingPtr@VParticleSystem@FXParticleSystem@@@@@Z
+; Retail @ 0x005E8EE0 size 67
+_TEXT SEGMENT
+public _bfme_export_b8e9f1f4
+_bfme_export_b8e9f1f4 PROC
+    db 56h,57h,6Ah,34h,8Bh,0F9h,0E8h,45h
+    db 90h,29h,00h,8Bh,0F0h,83h,0C4h,04h
+    db 85h,0F6h,74h,28h,8Bh,44h,24h,0Ch
+    db 57h,50h,8Bh,0CEh,0E8h,14h,0E3h,0A2h
+    db 0FFh,5Fh,0C7h,06h,7Ch,22h,11h,01h
+    db 0C7h,46h,14h,78h,22h,11h,01h,0C7h
+    db 46h,18h,64h,22h,11h,01h,8Bh,0C6h
+    db 5Eh,0C2h,04h,00h,5Fh,33h,0C0h,5Eh
+    db 0C2h,04h,00h
+_bfme_export_b8e9f1f4 ENDP
+_TEXT ENDS
+END

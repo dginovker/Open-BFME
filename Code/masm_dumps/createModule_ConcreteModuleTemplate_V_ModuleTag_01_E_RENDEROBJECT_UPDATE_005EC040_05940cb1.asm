2e03dacac5b60f3336a5ccdc93c0f1862f71cf81
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-16 15:42:30 -0400

    Land retail export 005EC040

---
+++ Code/masm_dumps/createModule_ConcreteModuleTemplate_V_ModuleTag_01_E_RENDEROBJECT_UPDATE_005EC040_05940cb1.asm
@@ -0,0 +1,29 @@
+.386
+.model flat
+
+; ?createModule@?$ConcreteModuleTemplate@V?$ModuleTag@$01$E?RENDEROBJECT_UPDATE_MODULE_KEY@FXParticleSystem@@3QBDB$E?RENDE
+; ROBJECT_UPDATE_MODULE_NAME@2@3QBDBVRenderObjectUpdateModule@2@VRenderObjectUpdateModuleTemplate@2@VRenderObjectParticleU
+; pdateModule@2@VRenderObjectParticleUpdateModuleTemplate@2@@FXParticleSystem@@@FXParticleSystem@@UAEPAVRenderObjectUpdate
+; Module@2@AAV?$TrackingPtr@VParticleSystem@FXParticleSystem@@@@@Z
+; Retail @ 0x005EC040 size 116
+_TEXT SEGMENT
+public _bfme_export_05940cb1
+_bfme_export_05940cb1 PROC
+    db 6Ah,0FFh,68h,7Bh,0BDh,03h,01h,64h
+    db 0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,51h,56h,57h
+    db 68h,0B4h,00h,00h,00h,8Bh,0F9h,0E8h
+    db 0CCh,5Eh,29h,00h,8Bh,0F0h,83h,0C4h
+    db 04h,89h,74h,24h,08h,33h,0C0h,3Bh
+    db 0F0h,89h,44h,24h,14h,74h,2Ah,8Bh
+    db 44h,24h,1Ch,57h,50h,8Bh,0CEh,0E8h
+    db 0CCh,3Ah,0A2h,0FFh,0C7h,06h,4Ch,2Eh
+    db 11h,01h,0C7h,46h,14h,48h,2Eh,11h
+    db 01h,0C7h,46h,18h,44h,2Eh,11h,01h
+    db 0C7h,46h,1Ch,30h,2Eh,11h,01h,8Bh
+    db 0C6h,8Bh,4Ch,24h,0Ch,5Fh,5Eh,64h
+    db 89h,0Dh,00h,00h,00h,00h,83h,0C4h
+    db 10h,0C2h,04h,00h
+_bfme_export_05940cb1 ENDP
+_TEXT ENDS
+END

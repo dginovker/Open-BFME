ff608753f69d78b6a7e1dd7769cd27a89d163b4a
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 05:34:55 -0400

    Correct INI dynamic audio event parser identity

--- Code/masm_dumps/parseDynamicAudioEventRTS_INI_SAXPAV1_PAX1PBX_Z_2E2DA.asm
+++ Code/masm_dumps/parseDynamicAudioEventRTS_INI_SAXPAV1_PAX1PBX_Z_2E2DA.asm
@@ -2,11 +2,11 @@
 .model flat
 
 ; ?parseDynamicAudioEventRTS@INI@@SAXPAV1@PAX1PBX@Z
-; Retail public thunk @ 0040C7CF size 5; canonical body @ 004BD640
+; Retail public thunk @ 0042E2DA size 5; canonical body @ 004BB9A0
 _TEXT SEGMENT
 public ?parseDynamicAudioEventRTS@INI@@SAXPAV1@PAX1PBX@Z
 ?parseDynamicAudioEventRTS@INI@@SAXPAV1@PAX1PBX@Z PROC
-    db 0e9h, 06ch, 00eh, 00bh, 000h
+    db 0e9h, 0c1h, 0d6h, 008h, 000h
 ?parseDynamicAudioEventRTS@INI@@SAXPAV1@PAX1PBX@Z ENDP
 _TEXT ENDS
 END

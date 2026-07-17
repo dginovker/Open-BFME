a94111efbab6133f4550acd63e5612d4973b57d4
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 06:00:55 -0400

    Match BoneFX OCL parser

---
+++ Code/masm_dumps/parseObjectCreationList_BoneFXUpdateModuleData_SAXPAVINI_PAX1PBX_Z_3625.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ?parseObjectCreationList@BoneFXUpdateModuleData@@SAXPAVINI@@PAX1PBX@Z
+; Retail public thunk @ 00403625 size 5; Ghidra body FUN_00687D00
+_TEXT SEGMENT
+public ?parseObjectCreationList@BoneFXUpdateModuleData@@SAXPAVINI@@PAX1PBX@Z
+?parseObjectCreationList@BoneFXUpdateModuleData@@SAXPAVINI@@PAX1PBX@Z PROC
+    db 0e9h, 0d6h, 046h, 028h, 000h
+?parseObjectCreationList@BoneFXUpdateModuleData@@SAXPAVINI@@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

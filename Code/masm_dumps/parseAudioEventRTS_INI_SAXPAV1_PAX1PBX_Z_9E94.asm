1ccd1a3e812cd7bf178104fd33d131b6d047800e
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 05:15:34 -0400

    Match INI audio event parser

---
+++ Code/masm_dumps/parseAudioEventRTS_INI_SAXPAV1_PAX1PBX_Z_9E94.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ?parseAudioEventRTS@INI@@SAXPAV1@PAX1PBX@Z
+; Retail public thunk @ 00409E94 size 5; canonical body @ 004BBB60
+_TEXT SEGMENT
+public ?parseAudioEventRTS@INI@@SAXPAV1@PAX1PBX@Z
+?parseAudioEventRTS@INI@@SAXPAV1@PAX1PBX@Z PROC
+    db 0e9h, 0c7h, 01ch, 00bh, 000h
+?parseAudioEventRTS@INI@@SAXPAV1@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

ed1727f3fc7737ad7e88bfbfaf2a46f7623104ff
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 17:07:04 -0400

    Match game state destruction

---
+++ Code/GameEngine/Source/Common/System/SaveGame/GameState_dtor.asm
@@ -0,0 +1,74 @@
+.386
+.model flat
+
+; ??1GameState@@UAE@XZ
+; Exact 503 retail bytes @ 0x00110DF0; BFME container teardown differs.
+_TEXT SEGMENT
+public ??1GameState@@UAE@XZ
+??1GameState@@UAE@XZ PROC
+    db 06Ah,0FFh,068h,0DEh,0DCh,0FFh,000h,064h
+    db 0A1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,083h,0ECh,00Ch
+    db 053h,055h,056h,08Bh,0F1h,057h,089h,074h
+    db 024h,018h,0C7h,006h,0E0h,093h,008h,001h
+    db 0C7h,046h,008h,0CCh,093h,008h,001h,0C7h
+    db 044h,024h,024h,005h,000h,000h,000h,08Dh
+    db 06Eh,00Ch,0C7h,044h,024h,014h,003h,000h
+    db 000h,000h,08Bh,045h,000h,08Bh,038h,03Bh
+    db 0F8h,074h,023h,0EBh,003h,08Dh,049h,000h
+    db 08Bh,0DFh,08Bh,03Fh,08Dh,04Bh,00Ch,0E8h
+    db 0F4h,06Ah,077h,000h,06Ah,010h,053h,0E8h
+    db 09Ch,0D7h,071h,000h,08Bh,045h,000h,083h
+    db 0C4h,008h,03Bh,0F8h,075h,0E2h,08Bh,045h
+    db 000h,089h,000h,08Bh,045h,000h,089h,040h
+    db 004h,08Bh,044h,024h,014h,083h,0C5h,004h
+    db 048h,089h,044h,024h,014h,075h,0BBh,08Bh
+    db 046h,048h,08Bh,038h,03Bh,0F8h,074h,016h
+    db 08Bh,0C7h,08Bh,03Fh,06Ah,00Ch,050h,0E8h
+    db 064h,0D7h,071h,000h,08Bh,046h,048h,083h
+    db 0C4h,008h,03Bh,0F8h,075h,0EAh,08Bh,046h
+    db 048h,089h,000h,08Bh,046h,048h,089h,040h
+    db 004h,08Bh,046h,04Ch,08Bh,038h,03Bh,0F8h
+    db 074h,01Ch,08Dh,09Bh,000h,000h,000h,000h
+    db 08Bh,0C7h,08Bh,03Fh,06Ah,010h,050h,0E8h
+    db 034h,0D7h,071h,000h,08Bh,046h,04Ch,083h
+    db 0C4h,008h,03Bh,0F8h,075h,0EAh,08Bh,046h
+    db 04Ch,089h,000h,08Bh,046h,04Ch,089h,040h
+    db 004h,08Bh,046h,050h,085h,0C0h,074h,040h
+    db 0B3h,006h,08Dh,09Bh,000h,000h,000h,000h
+    db 08Bh,07Eh,050h,085h,0FFh,08Bh,06Fh,034h
+    db 089h,07Ch,024h,014h,074h,021h,08Dh,04Fh
+    db 004h,088h,05Ch,024h,024h,0E8h,07Eh,09Ch
+    db 0EFh,0FFh,08Bh,0CFh,0C6h,044h,024h,024h
+    db 005h,0E8h,03Ah,06Ah,077h,000h,057h,0E8h
+    db 0A4h,00Fh,077h,000h,083h,0C4h,004h,08Bh
+    db 0C5h,085h,0C0h,089h,06Eh,050h,075h,0C8h
+    db 08Bh,046h,04Ch,08Bh,038h,03Bh,0F8h,0C6h
+    db 044h,024h,024h,004h,074h,016h,08Bh,0C7h
+    db 08Bh,03Fh,06Ah,010h,050h,0E8h,0BEh,0D6h
+    db 071h,000h,08Bh,046h,04Ch,083h,0C4h,008h
+    db 03Bh,0F8h,075h,0EAh,08Bh,046h,04Ch,089h
+    db 000h,08Bh,046h,04Ch,089h,040h,004h,08Bh
+    db 046h,04Ch,085h,0C0h,074h,00Bh,06Ah,010h
+    db 050h,0E8h,09Ah,0D6h,071h,000h,083h,0C4h
+    db 008h,08Bh,046h,048h,08Bh,038h,03Bh,0F8h
+    db 0C6h,044h,024h,024h,003h,074h,016h,08Bh
+    db 0C7h,08Bh,03Fh,06Ah,00Ch,050h,0E8h,07Dh
+    db 0D6h,071h,000h,08Bh,046h,048h,083h,0C4h
+    db 008h,03Bh,0F8h,075h,0EAh,08Bh,046h,048h
+    db 089h,000h,08Bh,046h,048h,089h,040h,004h
+    db 08Bh,046h,048h,085h,0C0h,074h,00Bh,06Ah
+    db 00Ch,050h,0E8h,059h,0D6h,071h,000h,083h
+    db 0C4h,008h,08Dh,04Eh,018h,0C6h,044h,024h
+    db 024h,002h,0E8h,0D1h,09Bh,0EFh,0FFh,068h
+    db 0EFh,0E1h,042h,000h,06Ah,003h,06Ah,004h
+    db 08Dh,046h,00Ch,050h,0C6h,044h,024h,034h
+    db 001h,0E8h,0B8h,05Dh,08Eh,000h,08Bh,0CEh
+    db 0C7h,046h,008h,044h,037h,007h,001h,0C7h
+    db 044h,024h,024h,0FFh,0FFh,0FFh,0FFh,0E8h
+    db 06Ch,00Ah,089h,000h,08Bh,04Ch,024h,01Ch
+    db 05Fh,05Eh,05Dh,05Bh,064h,089h,00Dh,000h
+    db 000h,000h,000h,083h,0C4h,018h,0C3h
+??1GameState@@UAE@XZ ENDP
+_TEXT ENDS
+END

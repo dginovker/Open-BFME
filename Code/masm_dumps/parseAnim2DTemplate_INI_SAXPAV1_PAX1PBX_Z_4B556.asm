9004794800bfac340efd12cead5c1ac6a93b6fd0
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 05:22:46 -0400

    Match INI Anim2D template parser

---
+++ Code/masm_dumps/parseAnim2DTemplate_INI_SAXPAV1_PAX1PBX_Z_4B556.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ?parseAnim2DTemplate@INI@@SAXPAV1@PAX1PBX@Z
+; Retail public thunk @ 0044B556 size 5; string-anchored body @ 004BAB30
+_TEXT SEGMENT
+public ?parseAnim2DTemplate@INI@@SAXPAV1@PAX1PBX@Z
+?parseAnim2DTemplate@INI@@SAXPAV1@PAX1PBX@Z PROC
+    db 0e9h, 0d5h, 0f5h, 006h, 000h
+?parseAnim2DTemplate@INI@@SAXPAV1@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

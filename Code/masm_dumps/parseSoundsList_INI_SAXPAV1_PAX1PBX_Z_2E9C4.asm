07616443d41656749abe9a919453decd08c65948
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 05:06:58 -0400

    Match INI sounds list parser

---
+++ Code/masm_dumps/parseSoundsList_INI_SAXPAV1_PAX1PBX_Z_2E9C4.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ?parseSoundsList@INI@@SAXPAV1@PAX1PBX@Z
+; Retail public thunk @ 0042E9C4 size 5; canonical BFME body @ 004B1DF0
+_TEXT SEGMENT
+public ?parseSoundsList@INI@@SAXPAV1@PAX1PBX@Z
+?parseSoundsList@INI@@SAXPAV1@PAX1PBX@Z PROC
+    db 0e9h, 027h, 034h, 008h, 000h
+?parseSoundsList@INI@@SAXPAV1@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

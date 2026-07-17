af96b320a532a88882b1b7f2eb799d00d4da7fae
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 05:41:26 -0400

    Match KindOf mask INI parser

---
+++ Code/masm_dumps/parseFromINI_BitFlags_0HE_SAXPAVINI_PAX1PBX_Z_2032E.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ?parseFromINI@?$BitFlags@$0HE@@@SAXPAVINI@@PAX1PBX@Z
+; Retail public thunk @ 0042032E size 5; Ghidra body FUN_00529810
+_TEXT SEGMENT
+public ?parseFromINI@?$BitFlags@$0HE@@@SAXPAVINI@@PAX1PBX@Z
+?parseFromINI@?$BitFlags@$0HE@@@SAXPAVINI@@PAX1PBX@Z PROC
+    db 0e9h, 0ddh, 094h, 010h, 000h
+?parseFromINI@?$BitFlags@$0HE@@@SAXPAVINI@@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

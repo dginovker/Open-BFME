c69efad3e364691c1ea8169f504fbdc5518c1c7a
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 04:26:14 -0400

    Match INI FX list parser

---
+++ Code/masm_dumps/parseFXList_INI_SAXPAV1_PAX1PBX_Z_4B47.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ?parseFXList@INI@@SAXPAV1@PAX1PBX@Z
+; Retail public thunk @ 00404B47 size 5; canonical body @ 004B8E80
+_TEXT SEGMENT
+public ?parseFXList@INI@@SAXPAV1@PAX1PBX@Z
+?parseFXList@INI@@SAXPAV1@PAX1PBX@Z PROC
+    db 0e9h, 34h, 43h, 0bh, 00h
+?parseFXList@INI@@SAXPAV1@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

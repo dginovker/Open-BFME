2e60fe62e5385acb27297dee12cd36caa22dd147
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 03:37:55 -0400

    Match INI angle parser

---
+++ Code/masm_dumps/parseAngleReal_INI_SAXPAV1_PAX1PBX_Z_852D30.asm
@@ -0,0 +1,17 @@
+.386
+.model flat
+
+; ?parseAngleReal@INI@@SAXPAV1@PAX1PBX@Z
+; Retail @ 00C52D30 size 92
+_TEXT SEGMENT
+public ?parseAngleReal@INI@@SAXPAV1@PAX1PBX@Z
+?parseAngleReal@INI@@SAXPAV1@PAX1PBX@Z PROC
+    db 8bh, 44h, 24h, 04h, 83h, 0ech, 08h, 56h, 8bh, 0b0h, 14h, 04h, 00h, 00h, 56h, 6ah
+    db 00h, 0ffh, 15h, 0d8h, 94h, 35h, 01h, 83h, 0c4h, 08h, 85h, 0c0h, 75h, 24h, 56h, 68h
+    db 50h, 03h, 13h, 01h, 8dh, 4ch, 24h, 0ch, 6ah, 03h, 51h, 0e8h, 0a0h, 0d8h, 0ffh, 0ffh
+    db 83h, 0c4h, 10h, 68h, 30h, 0fch, 1dh, 01h, 8dh, 54h, 24h, 08h, 52h, 0e8h, 8eh, 3fh
+    db 1ah, 00h, 50h, 0e8h, 68h, 0f9h, 0ffh, 0ffh, 0d8h, 0dh, 54h, 59h, 07h, 01h, 8bh, 44h
+    db 24h, 1ch, 83h, 0c4h, 04h, 0d9h, 18h, 5eh, 83h, 0c4h, 08h, 0c3h
+?parseAngleReal@INI@@SAXPAV1@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

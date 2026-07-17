395999578473ca15d3168205a9b02f32919d8e5c
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 04:32:50 -0400

    Match INI damage FX parser

---
+++ Code/masm_dumps/parseDamageFX_INI_SAXPAV1_PAX1PBX_Z_12026.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ?parseDamageFX@INI@@SAXPAV1@PAX1PBX@Z
+; Retail public thunk @ 00412026 size 5; canonical body @ 004BAFD0
+_TEXT SEGMENT
+public ?parseDamageFX@INI@@SAXPAV1@PAX1PBX@Z
+?parseDamageFX@INI@@SAXPAV1@PAX1PBX@Z PROC
+    db 0e9h, 0a5h, 8fh, 0ah, 00h
+?parseDamageFX@INI@@SAXPAV1@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

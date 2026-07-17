af9e261e38c312d55150171874f3ebc5d06a889e
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 04:43:34 -0400

    Match INI special power template parser

---
+++ Code/masm_dumps/parseSpecialPowerTemplate_INI_SAXPAV1_PAX1PBX_Z_1D77D.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ?parseSpecialPowerTemplate@INI@@SAXPAV1@PAX1PBX@Z
+; Retail public thunk @ 0041D77D size 5; canonical body @ 004BB210
+_TEXT SEGMENT
+public ?parseSpecialPowerTemplate@INI@@SAXPAV1@PAX1PBX@Z
+?parseSpecialPowerTemplate@INI@@SAXPAV1@PAX1PBX@Z PROC
+    db 0e9h, 08eh, 0dah, 09h, 00h
+?parseSpecialPowerTemplate@INI@@SAXPAV1@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

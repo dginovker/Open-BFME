5b3ae9e6f242229fc0e2a7212fa68cbc5a0b9078
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 04:47:47 -0400

    Match INI science vector parser

---
+++ Code/masm_dumps/parseScienceVector_INI_SAXPAV1_PAX1PBX_Z_42609.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ?parseScienceVector@INI@@SAXPAV1@PAX1PBX@Z
+; Retail public thunk @ 00442609 size 5; canonical body @ 004BCD80
+_TEXT SEGMENT
+public ?parseScienceVector@INI@@SAXPAV1@PAX1PBX@Z
+?parseScienceVector@INI@@SAXPAV1@PAX1PBX@Z PROC
+    db 0e9h, 072h, 0a7h, 07h, 00h
+?parseScienceVector@INI@@SAXPAV1@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

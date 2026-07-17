4a24b43c50153da6a81ef135b6cafc18a0a2e9b7
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 05:54:40 -0400

    Match TransitionDamageFX particle parser

---
+++ Code/masm_dumps/parseParticleSystem_TransitionDamageFXModuleData_SAXPAVINI_PAX1PBX_Z_20C43.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ?parseParticleSystem@TransitionDamageFXModuleData@@SAXPAVINI@@PAX1PBX@Z
+; Retail public thunk @ 00420C43 size 5; Ghidra body FUN_00652010
+_TEXT SEGMENT
+public ?parseParticleSystem@TransitionDamageFXModuleData@@SAXPAVINI@@PAX1PBX@Z
+?parseParticleSystem@TransitionDamageFXModuleData@@SAXPAVINI@@PAX1PBX@Z PROC
+    db 0e9h, 0c8h, 013h, 023h, 000h
+?parseParticleSystem@TransitionDamageFXModuleData@@SAXPAVINI@@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

b5d9fc56333ef63854de8a7ab01c379ed5348dac
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 04:04:49 -0400

    Match INI acceleration parser

---
+++ Code/masm_dumps/parseAccelerationReal_INI_SAXPAV1_PAX1PBX_Z_34969.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ?parseAccelerationReal@INI@@SAXPAV1@PAX1PBX@Z
+; Retail public thunk @ 00434969 size 5; canonical body @ 004B9D50
+_TEXT SEGMENT
+public ?parseAccelerationReal@INI@@SAXPAV1@PAX1PBX@Z
+?parseAccelerationReal@INI@@SAXPAV1@PAX1PBX@Z PROC
+    db 0e9h, 0e2h, 53h, 08h, 00h
+?parseAccelerationReal@INI@@SAXPAV1@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

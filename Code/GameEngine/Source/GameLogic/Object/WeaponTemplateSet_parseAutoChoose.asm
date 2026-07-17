3f72e75951ae0fa728453329d2344083ec48f65c
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 18:15:48 -0400

    Match weapon auto-choose parser

---
+++ Code/GameEngine/Source/GameLogic/Object/WeaponTemplateSet_parseAutoChoose.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+
+; ?parseAutoChoose@WeaponTemplateSet@@CAXPAVINI@@PAX1PBX@Z
+; Retail @ 0x001EACB0 size 51
+_TEXT SEGMENT
+public ?parseAutoChoose@WeaponTemplateSet@@CAXPAVINI@@PAX1PBX@Z
+?parseAutoChoose@WeaponTemplateSet@@CAXPAVINI@@PAX1PBX@Z PROC
+    db 056h,08Bh,074h,024h,008h,068h,078h,0D6h
+    db 02Ah,001h,06Ah,000h,08Bh,0CEh,0E8h,0ADh
+    db 05Ch,066h,000h,050h,0E8h,017h,05Dh,066h
+    db 000h,08Bh,04Ch,024h,014h,068h,088h,0D6h
+    db 02Ah,001h,08Dh,044h,081h,018h,050h,051h
+    db 056h,0E8h,092h,061h,066h,000h,083h,0C4h
+    db 018h,05Eh,0C3h
+?parseAutoChoose@WeaponTemplateSet@@CAXPAVINI@@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

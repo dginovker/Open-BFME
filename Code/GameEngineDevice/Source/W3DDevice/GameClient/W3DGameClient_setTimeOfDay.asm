6fe3f6e994de6e95e2617b53d772f56ffa1f7d3e
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 17:44:50 -0400

    Match W3D time of day update

---
+++ Code/GameEngineDevice/Source/W3DDevice/GameClient/W3DGameClient_setTimeOfDay.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+
+; ?setTimeOfDay@W3DGameClient@@UAEXW4TimeOfDay@@@Z
+; Retail @ 0x006FBCB0 size 56
+_TEXT SEGMENT
+public ?setTimeOfDay@W3DGameClient@@UAEXW4TimeOfDay@@@Z
+?setTimeOfDay@W3DGameClient@@UAEXW4TimeOfDay@@@Z PROC
+    db 08Bh,00Dh,07Ch,06Dh,030h,001h,085h,0C9h
+    db 056h,08Bh,074h,024h,008h,074h,006h,056h
+    db 0E8h,063h,05Ah,093h,0FFh,08Bh,00Dh,0ECh
+    db 06Eh,030h,001h,085h,0C9h,074h,006h,056h
+    db 0E8h,058h,027h,094h,0FFh,08Bh,00Dh,070h
+    db 012h,02Fh,001h,08Bh,001h,056h,0FFh,090h
+    db 094h,000h,000h,000h,05Eh,0C2h,004h,000h
+?setTimeOfDay@W3DGameClient@@UAEXW4TimeOfDay@@@Z ENDP
+_TEXT ENDS
+END

454cfc2cc4d47d69a8f43401356a9c9d97a48a88
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 05:44:51 -0400

    Match GlobalLanguage font descriptor parser

---
+++ Code/masm_dumps/parseFontDesc_GlobalLanguage_SAXPAVINI_PAX1PBX_Z_B41F.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ?parseFontDesc@GlobalLanguage@@SAXPAVINI@@PAX1PBX@Z
+; Retail public thunk @ 0040B41F size 5; Ghidra body FUN_00839150
+_TEXT SEGMENT
+public ?parseFontDesc@GlobalLanguage@@SAXPAVINI@@PAX1PBX@Z
+?parseFontDesc@GlobalLanguage@@SAXPAVINI@@PAX1PBX@Z PROC
+    db 0e9h, 02ch, 0ddh, 042h, 000h
+?parseFontDesc@GlobalLanguage@@SAXPAVINI@@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

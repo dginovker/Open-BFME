f8aa0b2a29eff54308287b8e1585401dde05cab9
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 04:55:23 -0400

    Match INI ASCII string vector parser

---
+++ Code/masm_dumps/parseAsciiStringVector_INI_SAXPAV1_PAX1PBX_Z_8542B0.asm
@@ -0,0 +1,16 @@
+.386
+.model flat
+
+; ?parseAsciiStringVector@INI@@SAXPAV1@PAX1PBX@Z
+; Ghidra FUN_00c542b0 @ RVA 008542B0 size 39
+_TEXT SEGMENT
+public ?parseAsciiStringVector@INI@@SAXPAV1@PAX1PBX@Z
+?parseAsciiStringVector@INI@@SAXPAV1@PAX1PBX@Z PROC
+    db 056h, 08bh, 074h, 024h, 010h, 08bh, 046h, 004h
+    db 08bh, 00eh, 050h, 051h, 08bh, 0ceh, 0e8h, 054h
+    db 009h, 07dh, 0ffh, 08bh, 054h, 024h, 008h, 06ah
+    db 000h, 056h, 06ah, 000h, 052h, 0e8h, 00eh, 0ffh
+    db 0ffh, 0ffh, 083h, 0c4h, 010h, 05eh, 0c3h
+?parseAsciiStringVector@INI@@SAXPAV1@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

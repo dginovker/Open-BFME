151bd9623de0bb4f5fcc68d72e91ac16287db71c
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 17:57:24 -0400

    Match veterancy upgrade name helper

---
+++ Code/GameEngine/Source/Common/System/Upgrade_getVetUpgradeName.asm
@@ -0,0 +1,33 @@
+.386
+.model flat
+
+; ?getVetUpgradeName@@YA?AVAsciiString@@W4VeterancyLevel@@@Z
+; Retail @ 0x0010AD90 size 173
+_TEXT SEGMENT
+public ?getVetUpgradeName@@YA?AVAsciiString@@W4VeterancyLevel@@@Z
+?getVetUpgradeName@@YA?AVAsciiString@@W4VeterancyLevel@@@Z PROC
+    db 06Ah,0FFh,068h,0D1h,0D3h,0FFh,000h,064h
+    db 0A1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,083h,0ECh,008h
+    db 057h,08Bh,07Ch,024h,01Ch,0C7h,044h,024h
+    db 008h,000h,000h,000h,000h,0C7h,044h,024h
+    db 004h,000h,000h,000h,000h,06Ah,012h,068h
+    db 068h,08Dh,008h,001h,08Dh,04Ch,024h,00Ch
+    db 0C7h,044h,024h,01Ch,001h,000h,000h,000h
+    db 0E8h,04Bh,0CFh,077h,000h,08Bh,044h,024h
+    db 020h,08Bh,00Ch,085h,0B4h,09Fh,02Ah,001h
+    db 085h,0C9h,074h,018h,08Bh,0C1h,056h,08Dh
+    db 070h,001h,08Dh,09Bh,000h,000h,000h,000h
+    db 08Ah,010h,040h,084h,0D2h,075h,0F9h,02Bh
+    db 0C6h,05Eh,0EBh,002h,033h,0C0h,050h,051h
+    db 08Dh,04Ch,024h,00Ch,0E8h,057h,0CFh,077h
+    db 000h,08Dh,04Ch,024h,004h,051h,08Bh,0CFh
+    db 0E8h,04Bh,0CDh,077h,000h,08Dh,04Ch,024h
+    db 004h,0C7h,044h,024h,008h,001h,000h,000h
+    db 000h,0C6h,044h,024h,014h,000h,0E8h,015h
+    db 0CBh,077h,000h,08Bh,04Ch,024h,00Ch,08Bh
+    db 0C7h,05Fh,064h,089h,00Dh,000h,000h,000h
+    db 000h,083h,0C4h,014h,0C3h
+?getVetUpgradeName@@YA?AVAsciiString@@W4VeterancyLevel@@@Z ENDP
+_TEXT ENDS
+END

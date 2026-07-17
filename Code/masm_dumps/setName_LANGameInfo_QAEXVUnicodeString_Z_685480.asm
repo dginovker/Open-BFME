060330d76d488ea6fd712f6422e7601ecddd56e0
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 01:32:07 -0400

    Match LAN game name setter

---
+++ Code/masm_dumps/setName_LANGameInfo_QAEXVUnicodeString_Z_685480.asm
@@ -0,0 +1,16 @@
+.386
+.model flat
+
+; ?setName@LANGameInfo@@QAEXVUnicodeString@@@Z
+; Retail @ 0x00A85480 size 78
+_TEXT SEGMENT
+public ?setName@LANGameInfo@@QAEXVUnicodeString@@@Z
+?setName@LANGameInfo@@QAEXVUnicodeString@@@Z PROC
+    db 064h,0A1h,000h,000h,000h,000h,06Ah,0FFh,068h,058h,062h,004h,001h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,08Dh,044h,024h,010h,081h,0C1h,0A0h,003h,000h,000h,050h
+    db 0C7h,044h,024h,00Ch,000h,000h,000h,000h,0E8h,083h,030h,020h,000h,08Dh,04Ch,024h
+    db 010h,0C7h,044h,024h,008h,0FFh,0FFh,0FFh,0FFh,0E8h,012h,02Dh,020h,000h,08Bh,00Ch
+    db 024h,064h,089h,00Dh,000h,000h,000h,000h,083h,0C4h,00Ch,0C2h,004h,000h
+?setName@LANGameInfo@@QAEXVUnicodeString@@@Z ENDP
+_TEXT ENDS
+END

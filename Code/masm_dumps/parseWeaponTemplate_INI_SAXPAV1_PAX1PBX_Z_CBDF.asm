0a0f90195a0eeceb1b2bf77a2c04dba585d54733
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 04:24:54 -0400

    Match INI weapon template parser

---
+++ Code/masm_dumps/parseWeaponTemplate_INI_SAXPAV1_PAX1PBX_Z_CBDF.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ?parseWeaponTemplate@INI@@SAXPAV1@PAX1PBX@Z
+; Retail public thunk @ 0040CBDF size 5; canonical body @ 004BAEC0
+_TEXT SEGMENT
+public ?parseWeaponTemplate@INI@@SAXPAV1@PAX1PBX@Z
+?parseWeaponTemplate@INI@@SAXPAV1@PAX1PBX@Z PROC
+    db 0e9h, 0dch, 0e2h, 0ah, 00h
+?parseWeaponTemplate@INI@@SAXPAV1@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

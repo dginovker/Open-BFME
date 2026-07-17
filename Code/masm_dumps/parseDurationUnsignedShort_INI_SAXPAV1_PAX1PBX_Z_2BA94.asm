79c5fc0f95b68a8c215d64b3af4b7bd3674238a6
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 03:56:07 -0400

    Match INI unsigned short duration parser

---
+++ Code/masm_dumps/parseDurationUnsignedShort_INI_SAXPAV1_PAX1PBX_Z_2BA94.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ?parseDurationUnsignedShort@INI@@SAXPAV1@PAX1PBX@Z
+; Retail public thunk @ 0042BA94 size 5; canonical body @ 004B9CC0
+_TEXT SEGMENT
+public ?parseDurationUnsignedShort@INI@@SAXPAV1@PAX1PBX@Z
+?parseDurationUnsignedShort@INI@@SAXPAV1@PAX1PBX@Z PROC
+    db 0e9h, 27h, 0e2h, 08h, 00h
+?parseDurationUnsignedShort@INI@@SAXPAV1@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

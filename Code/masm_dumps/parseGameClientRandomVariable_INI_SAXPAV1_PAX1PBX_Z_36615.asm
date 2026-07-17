127a3be05496565ba9ede8ab4a9facddb0efc7f9
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 04:06:10 -0400

    Match INI random variable parser

---
+++ Code/masm_dumps/parseGameClientRandomVariable_INI_SAXPAV1_PAX1PBX_Z_36615.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ?parseGameClientRandomVariable@INI@@SAXPAV1@PAX1PBX@Z
+; Retail public thunk @ 00436615 size 5; canonical body @ 004B8F60
+_TEXT SEGMENT
+public ?parseGameClientRandomVariable@INI@@SAXPAV1@PAX1PBX@Z
+?parseGameClientRandomVariable@INI@@SAXPAV1@PAX1PBX@Z PROC
+    db 0e9h, 46h, 29h, 08h, 00h
+?parseGameClientRandomVariable@INI@@SAXPAV1@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

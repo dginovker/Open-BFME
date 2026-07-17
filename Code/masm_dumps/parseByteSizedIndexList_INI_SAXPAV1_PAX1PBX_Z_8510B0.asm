e2edf6a2523be6600c2ad38a24f0e4cb60441358
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 05:04:41 -0400

    Match INI byte-sized index list parser

---
+++ Code/masm_dumps/parseByteSizedIndexList_INI_SAXPAV1_PAX1PBX_Z_8510B0.asm
@@ -0,0 +1,27 @@
+.386
+.model flat
+
+; ?parseByteSizedIndexList@INI@@SAXPAV1@PAX1PBX@Z
+; Retail body @ RVA 008510B0 size 125; padding-bounded in a Ghidra recovery gap
+_TEXT SEGMENT
+public ?parseByteSizedIndexList@INI@@SAXPAV1@PAX1PBX@Z
+?parseByteSizedIndexList@INI@@SAXPAV1@PAX1PBX@Z PROC
+    db 08bh, 044h, 024h, 004h, 083h, 0ech, 008h, 056h
+    db 08bh, 0b0h, 014h, 004h, 000h, 000h, 056h, 06ah
+    db 000h, 0ffh, 015h, 0d8h, 094h, 035h, 001h, 083h
+    db 0c4h, 008h, 085h, 0c0h, 075h, 024h, 056h, 068h
+    db 050h, 003h, 013h, 001h, 08dh, 04ch, 024h, 00ch
+    db 06ah, 003h, 051h, 0e8h, 020h, 0f5h, 0ffh, 0ffh
+    db 083h, 0c4h, 010h, 068h, 030h, 0fch, 01dh, 001h
+    db 08dh, 054h, 024h, 008h, 052h, 0e8h, 00eh, 05ch
+    db 01ah, 000h, 08bh, 04ch, 024h, 01ch, 051h, 050h
+    db 0e8h, 0e3h, 0f8h, 0ffh, 0ffh, 083h, 0c4h, 008h
+    db 085h, 0c0h, 07ch, 012h, 03dh, 0ffh, 000h, 000h
+    db 000h, 07fh, 00bh, 08bh, 054h, 024h, 018h, 088h
+    db 002h, 05eh, 083h, 0c4h, 008h, 0c3h, 068h, 0c0h
+    db 054h, 024h, 001h, 08dh, 044h, 024h, 020h, 050h
+    db 0c7h, 044h, 024h, 024h, 001h, 000h, 000h, 000h
+    db 0e8h, 0d3h, 05bh, 01ah, 000h
+?parseByteSizedIndexList@INI@@SAXPAV1@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

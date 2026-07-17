1da7d3008b0aa0f4af7766ff03621afb83a25d6a
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 03:55:01 -0400

    Match INI angular velocity parser

---
+++ Code/masm_dumps/parseAngularVelocityReal_INI_SAXPAV1_PAX1PBX_Z_114D7.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ?parseAngularVelocityReal@INI@@SAXPAV1@PAX1PBX@Z
+; Retail public thunk @ 004114D7 size 5; canonical body @ 004B9BA0
+_TEXT SEGMENT
+public ?parseAngularVelocityReal@INI@@SAXPAV1@PAX1PBX@Z
+?parseAngularVelocityReal@INI@@SAXPAV1@PAX1PBX@Z PROC
+    db 0e9h, 0c4h, 86h, 0ah, 00h
+?parseAngularVelocityReal@INI@@SAXPAV1@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

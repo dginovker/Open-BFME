3d7b51d6fd2e7abab7bfcd5b55ea3dc5b155526a
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 05:52:33 -0400

    Match TransitionDamageFX list parser

---
+++ Code/masm_dumps/parseFXList_TransitionDamageFXModuleData_SAXPAVINI_PAX1PBX_Z_A5AB.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ?parseFXList@TransitionDamageFXModuleData@@SAXPAVINI@@PAX1PBX@Z
+; Retail public thunk @ 0040A5AB size 5; Ghidra body FUN_00651ED0
+_TEXT SEGMENT
+public ?parseFXList@TransitionDamageFXModuleData@@SAXPAVINI@@PAX1PBX@Z
+?parseFXList@TransitionDamageFXModuleData@@SAXPAVINI@@PAX1PBX@Z PROC
+    db 0e9h, 020h, 079h, 024h, 000h
+?parseFXList@TransitionDamageFXModuleData@@SAXPAVINI@@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

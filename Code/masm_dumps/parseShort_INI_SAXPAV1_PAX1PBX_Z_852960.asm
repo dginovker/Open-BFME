7ad57b9cde176c41c9e69ad63dd6fed988ce9430
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 03:45:57 -0400

    Match INI short parser

---
+++ Code/masm_dumps/parseShort_INI_SAXPAV1_PAX1PBX_Z_852960.asm
@@ -0,0 +1,19 @@
+.386
+.model flat
+
+; ?parseShort@INI@@SAXPAV1@PAX1PBX@Z
+; Retail @ 00C52960 size 124
+_TEXT SEGMENT
+public ?parseShort@INI@@SAXPAV1@PAX1PBX@Z
+?parseShort@INI@@SAXPAV1@PAX1PBX@Z PROC
+    db 8bh, 44h, 24h, 04h, 83h, 0ech, 08h, 56h, 8bh, 0b0h, 14h, 04h, 00h, 00h, 56h, 6ah
+    db 00h, 0ffh, 15h, 0d8h, 94h, 35h, 01h, 83h, 0c4h, 08h, 85h, 0c0h, 75h, 24h, 56h, 68h
+    db 50h, 03h, 13h, 01h, 8dh, 4ch, 24h, 0ch, 6ah, 03h, 51h, 0e8h, 70h, 0dch, 0ffh, 0ffh
+    db 83h, 0c4h, 10h, 68h, 30h, 0fch, 1dh, 01h, 8dh, 54h, 24h, 08h, 52h, 0e8h, 5eh, 43h
+    db 1ah, 00h, 50h, 0e8h, 78h, 0fch, 0ffh, 0ffh, 83h, 0c4h, 04h, 3dh, 00h, 80h, 0ffh, 0ffh
+    db 7ch, 13h, 3dh, 0ffh, 7fh, 00h, 00h, 7fh, 0ch, 8bh, 4ch, 24h, 18h, 66h, 89h, 01h
+    db 5eh, 83h, 0c4h, 08h, 0c3h, 68h, 0c0h, 54h, 24h, 01h, 8dh, 54h, 24h, 08h, 52h, 0c7h
+    db 44h, 24h, 0ch, 01h, 00h, 00h, 00h, 0e8h, 24h, 43h, 1ah, 00h
+?parseShort@INI@@SAXPAV1@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

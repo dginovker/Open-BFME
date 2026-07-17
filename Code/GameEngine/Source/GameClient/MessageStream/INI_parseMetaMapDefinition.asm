aaae7e8b5e58cc608260aa5a4c2394ba25811009
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 18:01:06 -0400

    Match meta map definition parser

---
+++ Code/GameEngine/Source/GameClient/MessageStream/INI_parseMetaMapDefinition.asm
@@ -0,0 +1,29 @@
+.386
+.model flat
+
+; ?parseMetaMapDefinition@INI@@SAXPAV1@@Z
+; Retail @ 0x005B71A0 size 143
+_TEXT SEGMENT
+public ?parseMetaMapDefinition@INI@@SAXPAV1@@Z
+?parseMetaMapDefinition@INI@@SAXPAV1@@Z PROC
+    db 083h,0ECh,008h,056h,057h,08Bh,07Ch,024h
+    db 014h,06Ah,000h,08Bh,0CFh,0E8h,0BEh,097h
+    db 029h,000h,08Bh,00Dh,098h,04Ch,02Fh,001h
+    db 08Bh,0F0h,056h,0E8h,072h,081h,0A8h,0FFh
+    db 085h,0C0h,075h,024h,056h,068h,084h,0F0h
+    db 010h,001h,08Dh,044h,024h,010h,06Ah,003h
+    db 050h,0E8h,02Ah,094h,029h,000h,083h,0C4h
+    db 010h,068h,030h,0FCh,01Dh,001h,08Dh,04Ch
+    db 024h,00Ch,051h,0E8h,018h,0FBh,043h,000h
+    db 08Bh,00Dh,098h,04Ch,02Fh,001h,050h,0E8h
+    db 0EDh,0C5h,0A5h,0FFh,085h,0C0h,075h,024h
+    db 056h,068h,05Ch,0F0h,010h,001h,08Dh,054h
+    db 024h,010h,06Ah,003h,052h,0E8h,0F6h,093h
+    db 029h,000h,083h,0C4h,010h,068h,030h,0FCh
+    db 01Dh,001h,08Dh,044h,024h,00Ch,050h,0E8h
+    db 0E4h,0FAh,043h,000h,068h,070h,0E5h,010h
+    db 001h,050h,08Bh,0CFh,0E8h,077h,0AEh,029h
+    db 000h,05Fh,05Eh,083h,0C4h,008h,0C3h
+?parseMetaMapDefinition@INI@@SAXPAV1@@Z ENDP
+_TEXT ENDS
+END

afcfbf82938955eb9665481bc66cd4a7c384ff5e
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 04:46:24 -0400

    Match INI damage type flags parser

---
+++ Code/masm_dumps/parseDamageTypeFlags_INI_SAXPAV1_PAX1PBX_Z_1096F.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ?parseDamageTypeFlags@INI@@SAXPAV1@PAX1PBX@Z
+; Retail public thunk @ 0041096F size 5; canonical body @ 004B8FF0
+_TEXT SEGMENT
+public ?parseDamageTypeFlags@INI@@SAXPAV1@PAX1PBX@Z
+?parseDamageTypeFlags@INI@@SAXPAV1@PAX1PBX@Z PROC
+    db 0e9h, 07ch, 086h, 0ah, 00h
+?parseDamageTypeFlags@INI@@SAXPAV1@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

ba564600d9808bad938d3347068a03071ef7cce9
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 18:06:06 -0400

    Match weapon status helper constructor

---
+++ Code/GameEngine/Source/GameLogic/Object/ObjectWeaponStatusHelper_ctor.asm
@@ -0,0 +1,25 @@
+.386
+.model flat
+
+; ??0ObjectWeaponStatusHelper@@QAE@PAVThing@@PBVModuleData@@@Z
+; Retail @ 0x001C1340 size 107
+_TEXT SEGMENT
+public ??0ObjectWeaponStatusHelper@@QAE@PAVThing@@PBVModuleData@@@Z
+??0ObjectWeaponStatusHelper@@QAE@PAVThing@@PBVModuleData@@@Z PROC
+    db 06Ah,0FFh,068h,0B8h,08Dh,000h,001h,064h
+    db 0A1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,051h,08Bh,044h
+    db 024h,018h,056h,08Bh,0F1h,08Bh,04Ch,024h
+    db 018h,050h,051h,08Bh,0CEh,089h,074h,024h
+    db 00Ch,0E8h,07Ah,008h,0E6h,0FFh,08Bh,056h
+    db 008h,06Ah,001h,052h,08Bh,0CEh,0C7h,044h
+    db 024h,018h,000h,000h,000h,000h,0C7h,006h
+    db 0D4h,0E8h,009h,001h,0C7h,046h,00Ch,010h
+    db 0E8h,009h,001h,0C7h,046h,010h,004h,0E8h
+    db 009h,001h,0E8h,043h,044h,0E5h,0FFh,08Bh
+    db 04Ch,024h,008h,08Bh,0C6h,05Eh,064h,089h
+    db 00Dh,000h,000h,000h,000h,083h,0C4h,010h
+    db 0C2h,008h,000h
+??0ObjectWeaponStatusHelper@@QAE@PAVThing@@PBVModuleData@@@Z ENDP
+_TEXT ENDS
+END

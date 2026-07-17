f0b7292a68ce418e6d6577ae0080b45ff61aecd1
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 16:33:10 -0400

    Match BoneFX object creation

---
+++ Code/GameEngine/Source/GameLogic/Object/Damage/BoneFXDamage_onObjectCreated.asm
@@ -0,0 +1,31 @@
+.386
+.model flat
+
+; ?onObjectCreated@BoneFXDamage@@MAEXXZ
+; Exact 154 retail bytes @ 0x00250880; SEH + BFME module layout; exact via MASM
+_TEXT SEGMENT
+public ?onObjectCreated@BoneFXDamage@@MAEXXZ
+?onObjectCreated@BoneFXDamage@@MAEXXZ PROC
+    db 064h,0A1h,000h,000h,000h,000h,06Ah,0FFh
+    db 068h,08Eh,0E6h,000h,001h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,083h,0ECh,008h
+    db 056h,08Bh,0F1h,08Ah,00Dh,0C8h,0FBh,02Eh
+    db 001h,0B8h,001h,000h,000h,000h,084h,0C8h
+    db 075h,02Bh,009h,005h,0C8h,0FBh,02Eh,001h
+    db 08Bh,00Dh,000h,0D6h,02Eh,001h,068h,024h
+    db 001h,009h,001h,0C7h,044h,024h,018h,000h
+    db 000h,000h,000h,0E8h,00Fh,0A5h,0DEh,0FFh
+    db 0A3h,0C4h,0FBh,02Eh,001h,0C7h,044h,024h
+    db 014h,0FFh,0FFh,0FFh,0FFh,0A1h,0C4h,0FBh
+    db 02Eh,001h,08Bh,04Eh,008h,050h,0E8h,040h
+    db 0A5h,0DDh,0FFh,085h,0C0h,05Eh,075h,023h
+    db 068h,0E4h,01Fh,00Bh,001h,08Dh,04Ch,024h
+    db 004h,06Ah,003h,051h,0E8h,007h,0FDh,05Fh
+    db 000h,083h,0C4h,00Ch,068h,030h,0FCh,01Dh
+    db 001h,08Dh,054h,024h,004h,052h,0E8h,0F5h
+    db 063h,07Ah,000h,08Bh,04Ch,024h,008h,064h
+    db 089h,00Dh,000h,000h,000h,000h,083h,0C4h
+    db 014h,0C3h
+?onObjectCreated@BoneFXDamage@@MAEXXZ ENDP
+_TEXT ENDS
+END

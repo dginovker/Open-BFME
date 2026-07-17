da37fa61232a6c1948d9f878a15ba1ef05bff27c
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 03:38:49 -0400

    Match INI bit-mask parser

---
+++ Code/masm_dumps/parseBitInInt32_INI_SAXPAV1_PAX1PBX_Z_852E60.asm
@@ -0,0 +1,19 @@
+.386
+.model flat
+
+; ?parseBitInInt32@INI@@SAXPAV1@PAX1PBX@Z
+; Retail @ 00C52E60 size 115
+_TEXT SEGMENT
+public ?parseBitInInt32@INI@@SAXPAV1@PAX1PBX@Z
+?parseBitInInt32@INI@@SAXPAV1@PAX1PBX@Z PROC
+    db 8bh, 44h, 24h, 04h, 83h, 0ech, 08h, 56h, 8bh, 0b0h, 14h, 04h, 00h, 00h, 56h, 6ah
+    db 00h, 0ffh, 15h, 0d8h, 94h, 35h, 01h, 83h, 0c4h, 08h, 85h, 0c0h, 75h, 24h, 56h, 68h
+    db 50h, 03h, 13h, 01h, 8dh, 4ch, 24h, 0ch, 6ah, 03h, 51h, 0e8h, 70h, 0d7h, 0ffh, 0ffh
+    db 83h, 0c4h, 10h, 68h, 30h, 0fch, 1dh, 01h, 8dh, 54h, 24h, 08h, 52h, 0e8h, 5eh, 3eh
+    db 1ah, 00h, 50h, 0e8h, 0a8h, 0f6h, 0ffh, 0ffh, 83h, 0c4h, 04h, 84h, 0c0h, 8bh, 44h, 24h
+    db 18h, 74h, 0fh, 8bh, 10h, 8bh, 4ch, 24h, 1ch, 0bh, 0d1h, 89h, 10h, 5eh, 83h, 0c4h
+    db 08h, 0c3h, 8bh, 54h, 24h, 1ch, 8bh, 08h, 0f7h, 0d2h, 23h, 0cah, 89h, 08h, 5eh, 83h
+    db 0c4h, 08h, 0c3h
+?parseBitInInt32@INI@@SAXPAV1@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

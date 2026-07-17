e2e97cd74867513cc2996b2b94dafd0992279733
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 04:33:55 -0400

    Match INI thing template parser

---
+++ Code/masm_dumps/parseThingTemplate_INI_SAXPAV1_PAX1PBX_Z_39969.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ?parseThingTemplate@INI@@SAXPAV1@PAX1PBX@Z
+; Retail public thunk @ 00439969 size 5; canonical body @ 004BAD60
+_TEXT SEGMENT
+public ?parseThingTemplate@INI@@SAXPAV1@PAX1PBX@Z
+?parseThingTemplate@INI@@SAXPAV1@PAX1PBX@Z PROC
+    db 0e9h, 0f2h, 13h, 08h, 00h
+?parseThingTemplate@INI@@SAXPAV1@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

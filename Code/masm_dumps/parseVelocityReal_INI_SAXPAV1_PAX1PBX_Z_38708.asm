aee911f55e5b7f73ead3c09066bbcb94a08cde3c
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 03:57:08 -0400

    Match INI velocity parser

---
+++ Code/masm_dumps/parseVelocityReal_INI_SAXPAV1_PAX1PBX_Z_38708.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ?parseVelocityReal@INI@@SAXPAV1@PAX1PBX@Z
+; Retail public thunk @ 00438708 size 5; canonical body @ 004B9D20
+_TEXT SEGMENT
+public ?parseVelocityReal@INI@@SAXPAV1@PAX1PBX@Z
+?parseVelocityReal@INI@@SAXPAV1@PAX1PBX@Z PROC
+    db 0e9h, 13h, 16h, 08h, 00h
+?parseVelocityReal@INI@@SAXPAV1@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

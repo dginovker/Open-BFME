f7261e86538a129b82445ad5851fbe609b6da7bb
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 05:14:27 -0400

    Match INI death type flags parser

---
+++ Code/masm_dumps/parseDeathTypeFlags_INI_SAXPAV1_PAX1PBX_Z_29EEC.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ?parseDeathTypeFlags@INI@@SAXPAV1@PAX1PBX@Z
+; Retail public thunk @ 00429EEC size 5; Ghidra body FUN_004b9100
+_TEXT SEGMENT
+public ?parseDeathTypeFlags@INI@@SAXPAV1@PAX1PBX@Z
+?parseDeathTypeFlags@INI@@SAXPAV1@PAX1PBX@Z PROC
+    db 0e9h, 00fh, 0f2h, 008h, 000h
+?parseDeathTypeFlags@INI@@SAXPAV1@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

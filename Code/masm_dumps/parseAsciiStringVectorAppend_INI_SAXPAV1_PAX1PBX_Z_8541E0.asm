95ceff65c06dcb457227c5f320c0bc438bf726e4
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 04:56:59 -0400

    Match INI ASCII string vector append parser

---
+++ Code/masm_dumps/parseAsciiStringVectorAppend_INI_SAXPAV1_PAX1PBX_Z_8541E0.asm
@@ -0,0 +1,36 @@
+.386
+.model flat
+
+; ?parseAsciiStringVectorAppend@INI@@SAXPAV1@PAX1PBX@Z
+; Ghidra FUN_00c541e0 @ RVA 008541E0 size 200
+_TEXT SEGMENT
+public ?parseAsciiStringVectorAppend@INI@@SAXPAV1@PAX1PBX@Z
+?parseAsciiStringVectorAppend@INI@@SAXPAV1@PAX1PBX@Z PROC
+    db 064h, 0a1h, 000h, 000h, 000h, 000h, 06ah, 0ffh
+    db 068h, 059h, 066h, 005h, 001h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 083h, 0ech, 008h
+    db 055h, 08bh, 02dh, 0d8h, 094h, 035h, 001h, 057h
+    db 08bh, 07ch, 024h, 020h, 08bh, 087h, 014h, 004h
+    db 000h, 000h, 050h, 06ah, 000h, 0ffh, 0d5h, 083h
+    db 0c4h, 008h, 085h, 0c0h, 00fh, 084h, 07dh, 000h
+    db 000h, 000h, 056h, 08bh, 074h, 024h, 02ch, 090h
+    db 050h, 08dh, 04ch, 024h, 030h, 0e8h, 096h, 049h
+    db 003h, 000h, 08bh, 046h, 004h, 03bh, 046h, 008h
+    db 0c7h, 044h, 024h, 01ch, 000h, 000h, 000h, 000h
+    db 074h, 023h, 089h, 044h, 024h, 00ch, 089h, 044h
+    db 024h, 010h, 085h, 0c0h, 0c6h, 044h, 024h, 01ch
+    db 001h, 074h, 00ch, 08dh, 04ch, 024h, 02ch, 051h
+    db 08bh, 0c8h, 0e8h, 009h, 039h, 003h, 000h, 083h
+    db 046h, 004h, 004h, 0ebh, 016h, 06ah, 001h, 06ah
+    db 001h, 08dh, 054h, 024h, 034h, 052h, 08dh, 04ch
+    db 024h, 038h, 051h, 050h, 08bh, 0ceh, 0e8h, 008h
+    db 040h, 07eh, 0ffh, 08dh, 04ch, 024h, 02ch, 0c7h
+    db 044h, 024h, 01ch, 0ffh, 0ffh, 0ffh, 0ffh, 0e8h
+    db 0bch, 036h, 003h, 000h, 08bh, 087h, 014h, 004h
+    db 000h, 000h, 050h, 06ah, 000h, 0ffh, 0d5h, 083h
+    db 0c4h, 008h, 085h, 0c0h, 075h, 08ah, 05eh, 08bh
+    db 04ch, 024h, 010h, 05fh, 05dh, 064h, 089h, 00dh
+    db 000h, 000h, 000h, 000h, 083h, 0c4h, 014h, 0c3h
+?parseAsciiStringVectorAppend@INI@@SAXPAV1@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

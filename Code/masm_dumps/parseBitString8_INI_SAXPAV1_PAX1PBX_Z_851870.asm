4e06661969852a568fdca313fd48fbea0a268eef
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 04:07:15 -0400

    Match INI 8-bit flag parser

---
+++ Code/masm_dumps/parseBitString8_INI_SAXPAV1_PAX1PBX_Z_851870.asm
@@ -0,0 +1,16 @@
+.386
+.model flat
+
+; ?parseBitString8@INI@@SAXPAV1@PAX1PBX@Z
+; Retail @ 00C51870 size 68
+_TEXT SEGMENT
+public ?parseBitString8@INI@@SAXPAV1@PAX1PBX@Z
+?parseBitString8@INI@@SAXPAV1@PAX1PBX@Z PROC
+    db 51h, 8bh, 44h, 24h, 14h, 8bh, 54h, 24h, 08h, 50h, 8dh, 4ch, 24h, 04h, 51h, 6ah
+    db 00h, 52h, 0e8h, 0e9h, 0f5h, 0ffh, 0ffh, 8bh, 44h, 24h, 10h, 83h, 0c4h, 10h, 0a9h, 00h
+    db 0ffh, 0ffh, 0ffh, 74h, 17h, 68h, 0c0h, 54h, 24h, 01h, 8dh, 44h, 24h, 04h, 50h, 0c7h
+    db 44h, 24h, 08h, 01h, 00h, 00h, 00h, 0e8h, 54h, 54h, 1ah, 00h, 8bh, 4ch, 24h, 10h
+    db 88h, 01h, 59h, 0c3h
+?parseBitString8@INI@@SAXPAV1@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

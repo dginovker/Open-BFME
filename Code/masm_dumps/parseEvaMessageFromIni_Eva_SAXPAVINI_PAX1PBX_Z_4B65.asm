9b9fdb64da21ce744388d8b1ea34330f9216cf3e
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 06:12:57 -0400

    Match Eva message field parser

---
+++ Code/masm_dumps/parseEvaMessageFromIni_Eva_SAXPAVINI_PAX1PBX_Z_4B65.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ?parseEvaMessageFromIni@Eva@@SAXPAVINI@@PAX1PBX@Z
+; Retail public thunk @ 00404B65 size 5; canonical body @ 00825B80 (Ghidra recovery gap)
+_TEXT SEGMENT
+public ?parseEvaMessageFromIni@Eva@@SAXPAVINI@@PAX1PBX@Z
+?parseEvaMessageFromIni@Eva@@SAXPAVINI@@PAX1PBX@Z PROC
+    db 0e9h, 016h, 010h, 042h, 000h
+?parseEvaMessageFromIni@Eva@@SAXPAVINI@@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

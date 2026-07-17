4213d2d6b4bdbbe597c4d26dcff78cd4ac75e272
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 02:57:10 -0400

    Match INI percent scanner

---
+++ Code/masm_dumps/scanPercentToReal_INI_SAMPBD_Z_852740.asm
@@ -0,0 +1,13 @@
+.386
+.model flat
+
+; ?scanPercentToReal@INI@@SAMPBD@Z
+; Retail @ 0x00C52740 size 20
+_TEXT SEGMENT
+public ?scanPercentToReal@INI@@SAMPBD@Z
+?scanPercentToReal@INI@@SAMPBD@Z PROC
+    db 08Bh,044h,024h,004h,050h,0E8h,096h,0FFh,0FFh,0FFh,0D8h,00Dh,024h,06Ch,007h,001h
+    db 083h,0C4h,004h,0C3h
+?scanPercentToReal@INI@@SAMPBD@Z ENDP
+_TEXT ENDS
+END

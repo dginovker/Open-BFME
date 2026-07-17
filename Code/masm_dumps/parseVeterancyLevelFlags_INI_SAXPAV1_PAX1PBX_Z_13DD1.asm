13def7c84274ebe41b5d03e5e192b502049601c3
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 05:13:17 -0400

    Match INI veterancy level flags parser

---
+++ Code/masm_dumps/parseVeterancyLevelFlags_INI_SAXPAV1_PAX1PBX_Z_13DD1.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ?parseVeterancyLevelFlags@INI@@SAXPAV1@PAX1PBX@Z
+; Retail public thunk @ 00413DD1 size 5; Ghidra body FUN_004b9d90
+_TEXT SEGMENT
+public ?parseVeterancyLevelFlags@INI@@SAXPAV1@PAX1PBX@Z
+?parseVeterancyLevelFlags@INI@@SAXPAV1@PAX1PBX@Z PROC
+    db 0e9h, 0bah, 05fh, 00ah, 000h
+?parseVeterancyLevelFlags@INI@@SAXPAV1@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

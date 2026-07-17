f1e20c8117d3dca22aa43af7404671b33c1b6d81
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 05:53:38 -0400

    Match TransitionDamageFX OCL parser

---
+++ Code/masm_dumps/parseObjectCreationList_TransitionDamageFXModuleData_SAXPAVINI_PAX1PBX_Z_41FB.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ?parseObjectCreationList@TransitionDamageFXModuleData@@SAXPAVINI@@PAX1PBX@Z
+; Retail public thunk @ 004041FB size 5; Ghidra body FUN_00651F70
+_TEXT SEGMENT
+public ?parseObjectCreationList@TransitionDamageFXModuleData@@SAXPAVINI@@PAX1PBX@Z
+?parseObjectCreationList@TransitionDamageFXModuleData@@SAXPAVINI@@PAX1PBX@Z PROC
+    db 0e9h, 070h, 0ddh, 024h, 000h
+?parseObjectCreationList@TransitionDamageFXModuleData@@SAXPAVINI@@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

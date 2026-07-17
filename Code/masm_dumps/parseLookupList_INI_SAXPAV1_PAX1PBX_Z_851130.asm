bea38eaa5f2e071959127c8eeab20f05d6317330
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 05:06:01 -0400

    Match INI lookup list parser

---
+++ Code/masm_dumps/parseLookupList_INI_SAXPAV1_PAX1PBX_Z_851130.asm
@@ -0,0 +1,23 @@
+.386
+.model flat
+
+; ?parseLookupList@INI@@SAXPAV1@PAX1PBX@Z
+; Retail body @ RVA 00851130 size 91; padding-bounded in a Ghidra recovery gap
+_TEXT SEGMENT
+public ?parseLookupList@INI@@SAXPAV1@PAX1PBX@Z
+?parseLookupList@INI@@SAXPAV1@PAX1PBX@Z PROC
+    db 08bh, 044h, 024h, 004h, 083h, 0ech, 008h, 056h
+    db 08bh, 0b0h, 014h, 004h, 000h, 000h, 056h, 06ah
+    db 000h, 0ffh, 015h, 0d8h, 094h, 035h, 001h, 083h
+    db 0c4h, 008h, 085h, 0c0h, 075h, 024h, 056h, 068h
+    db 050h, 003h, 013h, 001h, 08dh, 04ch, 024h, 00ch
+    db 06ah, 003h, 051h, 0e8h, 0a0h, 0f4h, 0ffh, 0ffh
+    db 083h, 0c4h, 010h, 068h, 030h, 0fch, 01dh, 001h
+    db 08dh, 054h, 024h, 008h, 052h, 0e8h, 08eh, 05bh
+    db 01ah, 000h, 08bh, 04ch, 024h, 01ch, 051h, 050h
+    db 0e8h, 0f3h, 0f8h, 0ffh, 0ffh, 08bh, 054h, 024h
+    db 020h, 083h, 0c4h, 008h, 089h, 002h, 05eh, 083h
+    db 0c4h, 008h, 0c3h
+?parseLookupList@INI@@SAXPAV1@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

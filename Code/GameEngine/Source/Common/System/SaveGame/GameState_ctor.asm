2fb3bb3a0ad2cf6672df6ecfb7c56e222ed73601
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 17:03:39 -0400

    Match game state construction

---
+++ Code/GameEngine/Source/Common/System/SaveGame/GameState_ctor.asm
@@ -0,0 +1,33 @@
+.386
+.model flat
+
+; ??0GameState@@QAE@XZ
+; Exact 171 retail bytes @ 0x001111C0; BFME container construction differs.
+_TEXT SEGMENT
+public ??0GameState@@QAE@XZ
+??0GameState@@QAE@XZ PROC
+    db 06Ah,0FFh,068h,08Fh,0DDh,0FFh,000h,064h
+    db 0A1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,051h,053h,056h
+    db 08Bh,0F1h,089h,074h,024h,008h,0E8h,04Dh
+    db 008h,089h,000h,033h,0DBh,089h,05Ch,024h
+    db 014h,0C7h,046h,008h,044h,037h,007h,001h
+    db 068h,0EFh,0E1h,042h,000h,068h,0F8h,098h
+    db 041h,000h,06Ah,003h,06Ah,004h,08Dh,046h
+    db 00Ch,050h,0C6h,044h,024h,028h,001h,0C7h
+    db 006h,0E0h,093h,008h,001h,0C7h,046h,008h
+    db 0CCh,093h,008h,001h,0E8h,0CBh,05Ch,08Eh
+    db 000h,08Dh,04Eh,018h,0E8h,0F7h,0DFh,0EFh
+    db 0FFh,06Ah,00Ch,0C6h,044h,024h,018h,003h
+    db 089h,05Eh,048h,0E8h,010h,0D3h,071h,000h
+    db 089h,000h,089h,040h,004h,089h,046h,048h
+    db 06Ah,010h,0C6h,044h,024h,01Ch,004h,089h
+    db 05Eh,04Ch,0E8h,0F9h,0D2h,071h,000h,08Bh
+    db 04Ch,024h,014h,089h,000h,089h,040h,004h
+    db 089h,046h,04Ch,083h,0C4h,008h,089h,05Eh
+    db 050h,088h,05Eh,054h,08Bh,0C6h,05Eh,05Bh
+    db 064h,089h,00Dh,000h,000h,000h,000h,083h
+    db 0C4h,010h,0C3h
+??0GameState@@QAE@XZ ENDP
+_TEXT ENDS
+END

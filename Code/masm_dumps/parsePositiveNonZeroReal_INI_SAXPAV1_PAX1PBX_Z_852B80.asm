32a7e86da52230e3b3a608dedac341403aecdd7e
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 03:35:45 -0400

    Match INI positive real parser

---
+++ Code/masm_dumps/parsePositiveNonZeroReal_INI_SAXPAV1_PAX1PBX_Z_852B80.asm
@@ -0,0 +1,20 @@
+.386
+.model flat
+
+; ?parsePositiveNonZeroReal@INI@@SAXPAV1@PAX1PBX@Z
+; Retail @ 00C52B80 size 144
+_TEXT SEGMENT
+public ?parsePositiveNonZeroReal@INI@@SAXPAV1@PAX1PBX@Z
+?parsePositiveNonZeroReal@INI@@SAXPAV1@PAX1PBX@Z PROC
+    db 8bh, 44h, 24h, 04h, 83h, 0ech, 08h, 56h, 8bh, 0b0h, 14h, 04h, 00h, 00h, 56h, 6ah
+    db 00h, 0ffh, 15h, 0d8h, 94h, 35h, 01h, 83h, 0c4h, 08h, 85h, 0c0h, 75h, 24h, 56h, 68h
+    db 50h, 03h, 13h, 01h, 8dh, 4ch, 24h, 0ch, 6ah, 03h, 51h, 0e8h, 50h, 0dah, 0ffh, 0ffh
+    db 83h, 0c4h, 10h, 68h, 30h, 0fch, 1dh, 01h, 8dh, 54h, 24h, 08h, 52h, 0e8h, 3eh, 41h
+    db 1ah, 00h, 50h, 0e8h, 18h, 0fbh, 0ffh, 0ffh, 0d9h, 0c0h, 8bh, 44h, 24h, 1ch, 0d9h, 18h
+    db 83h, 0c4h, 04h, 0d8h, 15h, 50h, 53h, 07h, 01h, 0dfh, 0e0h, 0f6h, 0c4h, 41h, 7ah, 29h
+    db 83h, 0ech, 08h, 0ddh, 1ch, 24h, 68h, 0f4h, 07h, 13h, 01h, 8dh, 4ch, 24h, 10h, 6ah
+    db 03h, 51h, 0e8h, 09h, 0dah, 0ffh, 0ffh, 83h, 0c4h, 14h, 68h, 30h, 0fch, 1dh, 01h, 8dh
+    db 54h, 24h, 08h, 52h, 0e8h, 0f7h, 40h, 1ah, 00h, 0ddh, 0d8h, 5eh, 83h, 0c4h, 08h, 0c3h
+?parsePositiveNonZeroReal@INI@@SAXPAV1@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

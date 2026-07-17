41a0ce3b08368696b425e2d01dc3d8d6178bcea8
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 05:59:43 -0400

    Match BoneFX list parser

---
+++ Code/masm_dumps/parseFXList_BoneFXUpdateModuleData_SAXPAVINI_PAX1PBX_Z_33C2B.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ?parseFXList@BoneFXUpdateModuleData@@SAXPAVINI@@PAX1PBX@Z
+; Retail public thunk @ 00433C2B size 5; Ghidra body FUN_00687BC0
+_TEXT SEGMENT
+public ?parseFXList@BoneFXUpdateModuleData@@SAXPAVINI@@PAX1PBX@Z
+?parseFXList@BoneFXUpdateModuleData@@SAXPAVINI@@PAX1PBX@Z PROC
+    db 0e9h, 090h, 03fh, 025h, 000h
+?parseFXList@BoneFXUpdateModuleData@@SAXPAVINI@@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

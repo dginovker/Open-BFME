07f53d28d815ee53bd864d34f428fdfb245755c5
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-17 04:23:22 -0400

    Match INI armor template parser

---
+++ Code/masm_dumps/parseArmorTemplate_INI_SAXPAV1_PAX1PBX_Z_3EF6D.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ?parseArmorTemplate@INI@@SAXPAV1@PAX1PBX@Z
+; Retail public thunk @ 0043EF6D size 5; canonical body @ 004BAE50
+_TEXT SEGMENT
+public ?parseArmorTemplate@INI@@SAXPAV1@PAX1PBX@Z
+?parseArmorTemplate@INI@@SAXPAV1@PAX1PBX@Z PROC
+    db 0e9h, 0deh, 0beh, 07h, 00h
+?parseArmorTemplate@INI@@SAXPAV1@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

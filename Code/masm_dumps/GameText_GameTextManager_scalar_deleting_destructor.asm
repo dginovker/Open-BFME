f4616a860eaa19fe710012148cfa794d4dfdeaf5
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-18 05:53:35 -0400

    Match GameTextManager deleting destructor

---
+++ Code/masm_dumps/GameText_GameTextManager_scalar_deleting_destructor.asm
@@ -0,0 +1,15 @@
+.386
+.model flat
+
+; ??_GGameTextManager@@UAEPAXI@Z
+; Exact 30 retail bytes @ 0x00438610; vtable scalar-deleting destructor.
+_TEXT SEGMENT
+public ??_GGameTextManager@@UAEPAXI@Z
+??_GGameTextManager@@UAEPAXI@Z PROC
+    db 56h,8Bh,0F1h,0E8h,0Eh,0AEh,0BDh,0FFh
+    db 0F6h,44h,24h,08h,01h,74h,09h,56h
+    db 0E8h,8Bh,98h,44h,00h,83h,0C4h,04h
+    db 8Bh,0C6h,5Eh,0C2h,04h,00h
+??_GGameTextManager@@UAEPAXI@Z ENDP
+_TEXT ENDS
+END

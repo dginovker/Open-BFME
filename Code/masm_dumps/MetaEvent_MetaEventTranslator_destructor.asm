fa7c33904e65c2cc701269268bb039660b513b5c
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-18 07:34:51 -0400

    Match MetaEventTranslator destructor

---
+++ Code/masm_dumps/MetaEvent_MetaEventTranslator_destructor.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ??1MetaEventTranslator@@UAE@XZ
+; Exact 7 retail bytes @ 0x009B6A70; restores the GameMessageTranslator base vtable.
+_TEXT SEGMENT
+public ??1MetaEventTranslator@@UAE@XZ
+??1MetaEventTranslator@@UAE@XZ PROC
+    db 0C7h,01h,0D4h,35h,0Fh,01h,0C3h
+??1MetaEventTranslator@@UAE@XZ ENDP
+_TEXT ENDS
+END

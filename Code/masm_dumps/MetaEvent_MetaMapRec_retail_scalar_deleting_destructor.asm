59c78bdd1a024e177b88fe659ab21eb3ae6f7808
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-18 07:14:47 -0400

    Match true MetaMapRec deleting destructor

---
+++ Code/masm_dumps/MetaEvent_MetaMapRec_retail_scalar_deleting_destructor.asm
@@ -0,0 +1,15 @@
+.386
+.model flat
+
+; ??_GMetaMapRec@@QAEPAXI@Z
+; Exact 30 retail bytes @ 0x009B70E0; nonvirtual scalar-deleting destructor for the 0x24-byte record.
+_TEXT SEGMENT
+public ??_GMetaMapRec@@QAEPAXI@Z
+??_GMetaMapRec@@QAEPAXI@Z PROC
+    db 56h,8Bh,0F1h,0E8h,9Fh,13h,0A9h,0FFh
+    db 0F6h,44h,24h,08h,01h,74h,09h,56h
+    db 0E8h,0BBh,0ADh,2Ch,00h,83h,0C4h,04h
+    db 8Bh,0C6h,5Eh,0C2h,04h,00h
+??_GMetaMapRec@@QAEPAXI@Z ENDP
+_TEXT ENDS
+END

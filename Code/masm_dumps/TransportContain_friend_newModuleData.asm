fd3c8c02e156ed9b23b049e7967a1792df2c1b12
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-18 11:06:52 -0400

    Match transport-contain module-data factory

---
+++ Code/masm_dumps/TransportContain_friend_newModuleData.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+
+; ?friend_newModuleData@TransportContain@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 110 retail bytes @ 0x00525470; allocates, constructs, and parses TransportContain module data.
+_TEXT SEGMENT
+public ?friend_newModuleData@TransportContain@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@TransportContain@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 06Ah,0FFh,068h,0ABh,016h,000h,001h,064h,0A1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,051h,056h,068h,07Ch,001h,000h,000h,0E8h,09Fh,0CAh,075h
+    db 000h,083h,0C4h,004h,089h,044h,024h,004h,085h,0C0h,0C7h,044h,024h,010h,000h,000h
+    db 000h,000h,074h,00Bh,08Bh,0C8h,0E8h,0BAh,0F0h,0EEh,0FFh,08Bh,0F0h,0EBh,002h,033h
+    db 0F6h,08Bh,04Ch,024h,018h,085h,0C9h,0C7h,044h,024h,010h,0FFh,0FFh,0FFh,0FFh,074h
+    db 00Bh,068h,0E7h,092h,044h,000h,056h,0E8h,064h,0CCh,072h,000h,08Bh,04Ch,024h,008h
+    db 08Bh,0C6h,05Eh,064h,089h,00Dh,000h,000h,000h,000h,083h,0C4h,010h,0C3h
+?friend_newModuleData@TransportContain@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END

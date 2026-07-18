45dc3390fd61fc03d7439be1b54aade77c2f174a
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-18 15:10:18 -0400

    Match unit-crate collide data factory

---
+++ Code/masm_dumps/UnitCrateCollide_friend_newModuleData.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+
+; ?friend_newModuleData@UnitCrateCollide@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 107 retail bytes @ RVA 0x00125F10; crate module-data registration factory
+_TEXT SEGMENT
+public ?friend_newModuleData@UnitCrateCollide@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@UnitCrateCollide@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 06Ah,0FFh,068h,08Bh,018h,000h,001h,064h,0A1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,051h,056h,06Ah,05Ch,0E8h,002h,0C0h,075h,000h,083h,0C4h
+    db 004h,089h,044h,024h,004h,085h,0C0h,0C7h,044h,024h,010h,000h,000h,000h,000h,074h
+    db 00Bh,08Bh,0C8h,0E8h,006h,07Bh,0EEh,0FFh,08Bh,0F0h,0EBh,002h,033h,0F6h,08Bh,04Ch
+    db 024h,018h,085h,0C9h,0C7h,044h,024h,010h,0FFh,0FFh,0FFh,0FFh,074h,00Bh,068h,05Ah
+    db 021h,043h,000h,056h,0E8h,0C7h,0C1h,072h,000h,08Bh,04Ch,024h,008h,08Bh,0C6h,05Eh
+    db 064h,089h,00Dh,000h,000h,000h,000h,083h,0C4h,010h,0C3h
+?friend_newModuleData@UnitCrateCollide@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END

2be211f305ea3835b6df023895a0afa34c4f1c9d
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-18 07:17:16 -0400

    Match ScriptActions local defeat flow

---
+++ Code/masm_dumps/ScriptActions_doLocalDefeat.asm
@@ -0,0 +1,42 @@
+.386
+.model flat
+
+; ?doLocalDefeat@ScriptActions@@IAEXXZ
+; Exact 246 retail bytes @ 0x006F1EC0; BFME local-defeat flow differs materially from ZH.
+_TEXT SEGMENT
+public ?doLocalDefeat@ScriptActions@@IAEXXZ
+?doLocalDefeat@ScriptActions@@IAEXXZ PROC
+    db 64h,0A1h,00h,00h,00h,00h,6Ah,0FFh
+    db 68h,20h,5Fh,01h,01h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,83h,0ECh,08h
+    db 53h,56h,8Bh,0F1h,8Bh,0Dh,6Ch,07h
+    db 2Fh,01h,0E8h,0E5h,0Bh,0D1h,0FFh,33h
+    db 0DBh,88h,5Eh,08h,8Bh,0Dh,98h,08h
+    db 2Fh,01h,0E8h,8Fh,15h,0D1h,0FFh,38h
+    db 5Eh,08h,0Fh,85h,91h,00h,00h,00h
+    db 8Bh,0Dh,9Ch,07h,2Fh,01h,3Bh,0CBh
+    db 0Fh,84h,83h,00h,00h,00h,8Bh,01h
+    db 0FFh,50h,40h,84h,0C0h,75h,7Ah,8Bh
+    db 0Dh,48h,0D7h,2Eh,01h,8Bh,71h,0Ch
+    db 3Bh,0F3h,74h,6Dh,68h,0E0h,00h,0Dh
+    db 01h,8Dh,4Ch,24h,10h,0E8h,8Eh,6Ch
+    db 59h,00h,68h,0D0h,00h,0Dh,01h,8Dh
+    db 4Ch,24h,0Ch,89h,5Ch,24h,1Ch,0E8h
+    db 7Ch,6Ch,59h,00h,8Bh,46h,04h,3Bh
+    db 0C3h,0C6h,44h,24h,18h,01h,74h,08h
+    db 8Ah,80h,18h,01h,00h,00h,0EBh,02h
+    db 32h,0C0h,8Bh,0Dh,9Ch,07h,2Fh,01h
+    db 8Bh,11h,68h,50h,6Eh,33h,01h,8Dh
+    db 74h,24h,10h,56h,50h,8Dh,44h,24h
+    db 14h,50h,0FFh,52h,48h,8Dh,4Ch,24h
+    db 08h,88h,5Ch,24h,18h,0E8h,0BEh,59h
+    db 59h,00h,8Bh,0CEh,0C7h,44h,24h,18h
+    db 0FFh,0FFh,0FFh,0FFh,0E8h,0AFh,59h,59h
+    db 00h,8Bh,0Dh,24h,10h,2Fh,01h,88h
+    db 59h,1Dh,8Bh,0Dh,6Ch,07h,2Fh,01h
+    db 8Bh,11h,0FFh,52h,38h,8Bh,4Ch,24h
+    db 10h,5Eh,5Bh,64h,89h,0Dh,00h,00h
+    db 00h,00h,83h,0C4h,14h,0C3h
+?doLocalDefeat@ScriptActions@@IAEXXZ ENDP
+_TEXT ENDS
+END

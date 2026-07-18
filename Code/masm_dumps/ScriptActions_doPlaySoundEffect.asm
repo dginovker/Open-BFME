20896f598b55066a01ecd1207c6a1d1601e43dec
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-18 07:37:26 -0400

    Match ScriptActions sound-effect flow

---
+++ Code/masm_dumps/ScriptActions_doPlaySoundEffect.asm
@@ -0,0 +1,45 @@
+.386
+.model flat
+
+; ?doPlaySoundEffect@ScriptActions@@IAEXABVAsciiString@@@Z
+; Exact 266 retail bytes @ 0x006F22A0; BFME sound dispatch differs materially from ZH.
+_TEXT SEGMENT
+public ?doPlaySoundEffect@ScriptActions@@IAEXABVAsciiString@@@Z
+?doPlaySoundEffect@ScriptActions@@IAEXABVAsciiString@@@Z PROC
+    db 83h,0ECh,20h,8Bh,44h,24h,24h,53h
+    db 55h,56h,57h,6Ah,00h,51h,89h,64h
+    db 24h,18h,8Bh,0CCh,50h,0E8h,0A6h,58h
+    db 59h,00h,8Bh,0Dh,6Ch,07h,2Fh,01h
+    db 8Bh,11h,0FFh,52h,44h,8Bh,0F0h,85h
+    db 0F6h,0Fh,84h,0D1h,00h,00h,00h,8Bh
+    db 0Dh,14h,0F2h,2Eh,01h,0E8h,96h,92h
+    db 0D4h,0FFh,85h,0C0h,89h,44h,24h,34h
+    db 0Fh,84h,0BAh,00h,00h,00h,50h,8Bh
+    db 0CEh,0E8h,0D3h,0Bh,0D3h,0FFh,0A1h,0B8h
+    db 0D8h,2Eh,01h,8Bh,0Dh,0BCh,0D8h,2Eh
+    db 01h,8Bh,15h,0C0h,0D8h,2Eh,01h,8Bh
+    db 3Dh,0C4h,0D8h,2Eh,01h,8Bh,1Dh,0C8h
+    db 0D8h,2Eh,01h,83h,0ECh,18h,8Bh,0ECh
+    db 89h,45h,00h,89h,4Dh,04h,89h,55h
+    db 08h,89h,7Dh,0Ch,89h,5Dh,10h,8Bh
+    db 1Dh,0CCh,0D8h,2Eh,01h,89h,5Dh,14h
+    db 83h,0ECh,18h,8Bh,0ECh,89h,45h,00h
+    db 0A1h,0C8h,0D8h,2Eh,01h,89h,4Dh,04h
+    db 89h,55h,08h,89h,7Dh,0Ch,89h,45h
+    db 10h,8Bh,0CEh,89h,5Dh,14h,0E8h,27h
+    db 4Eh,0D5h,0FFh,8Dh,4Ch,24h,14h,51h
+    db 8Bh,0CEh,8Bh,0F8h,0E8h,46h,68h,0D4h
+    db 0FFh,83h,0FFh,01h,6Ah,01h,7Eh,1Ch
+    db 8Bh,4Ch,24h,38h,6Ah,00h,6Ah,01h
+    db 8Dh,54h,24h,20h,52h,0E8h,0BAh,0ECh
+    db 0D1h,0FFh,5Fh,5Eh,5Dh,5Bh,83h,0C4h
+    db 20h,0C2h,04h,00h,8Dh,44h,24h,18h
+    db 8Dh,4Ch,24h,24h,89h,44h,24h,24h
+    db 33h,0C0h,51h,8Bh,4Ch,24h,3Ch,88h
+    db 44h,24h,2Ch,89h,44h,24h,30h,89h
+    db 44h,24h,34h,0E8h,0A3h,68h,0D5h,0FFh
+    db 5Fh,5Eh,5Dh,5Bh,83h,0C4h,20h,0C2h
+    db 04h,00h
+?doPlaySoundEffect@ScriptActions@@IAEXABVAsciiString@@@Z ENDP
+_TEXT ENDS
+END

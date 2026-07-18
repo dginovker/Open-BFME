8833d0c1e4a847ac487f76278ea010720070676b
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-18 06:43:33 -0400

    Match ScienceStore lookup helper

---
+++ Code/masm_dumps/Science_ScienceStore_friend_lookupScience.asm
@@ -0,0 +1,22 @@
+.386
+.model flat
+
+; ?friend_lookupScience@ScienceStore@@QBE?AW4ScienceType@@PBD@Z
+; Exact 85 retail bytes @ 0x004E7240; retail INIException ABI differs from the shared ZH header.
+_TEXT SEGMENT
+public ?friend_lookupScience@ScienceStore@@QBE?AW4ScienceType@@PBD@Z
+?friend_lookupScience@ScienceStore@@QBE?AW4ScienceType@@PBD@Z PROC
+    db 83h,0ECh,08h,53h,8Bh,5Ch,24h,10h
+    db 56h,57h,8Bh,0F9h,8Bh,0Dh,00h,0D6h
+    db 2Eh,01h,53h,0E8h,7Fh,3Bh,0F5h,0FFh
+    db 8Bh,0F0h,56h,8Bh,0CFh,0E8h,0ABh,1Bh
+    db 0F3h,0FFh,85h,0C0h,75h,24h,53h,68h
+    db 74h,4Ch,08h,01h,8Dh,44h,24h,14h
+    db 6Ah,03h,50h,0E8h,88h,93h,76h,00h
+    db 83h,0C4h,10h,68h,30h,0FCh,1Dh,01h
+    db 8Dh,4Ch,24h,10h,51h,0E8h,76h,0FAh
+    db 90h,00h,5Fh,8Bh,0C6h,5Eh,5Bh,83h
+    db 0C4h,08h,0C2h,04h,00h
+?friend_lookupScience@ScienceStore@@QBE?AW4ScienceType@@PBD@Z ENDP
+_TEXT ENDS
+END

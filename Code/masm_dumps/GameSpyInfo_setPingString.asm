f3b78526d8db8f900861c802765bf5710581852e
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-18 14:57:02 -0400

    Match GameSpy ping-string setter

---
+++ Code/masm_dumps/GameSpyInfo_setPingString.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ?setPingString@GameSpyInfo@@UAEXABVAsciiString@@@Z
+; Exact 8 retail bytes @ RVA 0x00637230; vtable-owned StringBase tail stub
+_TEXT SEGMENT
+public ?setPingString@GameSpyInfo@@UAEXABVAsciiString@@@Z
+?setPingString@GameSpyInfo@@UAEXABVAsciiString@@@Z PROC
+    db 083h,0C1h,014h,0E9h,058h,00Ah,025h,000h
+?setPingString@GameSpyInfo@@UAEXABVAsciiString@@@Z ENDP
+_TEXT ENDS
+END

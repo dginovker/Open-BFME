5c93079b09eb95f8de7d13d13fe05d026a495ad0
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-18 15:02:38 -0400

    Match money-crate collide data factory

---
+++ Code/masm_dumps/MoneyCrateCollide_friend_newModuleData.asm
@@ -0,0 +1,19 @@
+.386
+.model flat
+
+; ?friend_newModuleData@MoneyCrateCollide@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 120 retail bytes @ RVA 0x00127020; crate module-data registration factory
+_TEXT SEGMENT
+public ?friend_newModuleData@MoneyCrateCollide@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@MoneyCrateCollide@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 06Ah,0FFh,068h,01Bh,01Bh,000h,001h,064h,0A1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,051h,056h,06Ah,058h,0E8h,0F2h,0AEh,075h,000h,08Bh,0F0h
+    db 083h,0C4h,004h,089h,074h,024h,004h,085h,0F6h,0C7h,044h,024h,010h,000h,000h,000h
+    db 000h,074h,016h,08Bh,0CEh,0E8h,070h,0D1h,0F1h,0FFh,0C7h,006h,0A0h,0EAh,008h,001h
+    db 0C7h,046h,054h,000h,000h,000h,000h,0EBh,002h,033h,0F6h,08Bh,04Ch,024h,018h,085h
+    db 0C9h,0C7h,044h,024h,010h,0FFh,0FFh,0FFh,0FFh,074h,00Bh,068h,0EEh,0BAh,042h,000h
+    db 056h,0E8h,0AAh,0B0h,072h,000h,08Bh,04Ch,024h,008h,08Bh,0C6h,05Eh,064h,089h,00Dh
+    db 000h,000h,000h,000h,083h,0C4h,010h,0C3h
+?friend_newModuleData@MoneyCrateCollide@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END

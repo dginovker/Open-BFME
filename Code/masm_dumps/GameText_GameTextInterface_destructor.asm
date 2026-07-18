96bfcee1399992fe8c9022bd1979bab82c514e91
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-18 06:09:33 -0400

    Match GameTextInterface destructor

---
+++ Code/masm_dumps/GameText_GameTextInterface_destructor.asm
@@ -0,0 +1,13 @@
+.386
+.model flat
+
+; ??1GameTextInterface@@UAE@XZ
+; Exact 14 retail bytes @ 0x009A1A40; ICF-folded with Snapshot::~Snapshot.
+_TEXT SEGMENT
+public ??1GameTextInterface@@UAE@XZ
+??1GameTextInterface@@UAE@XZ PROC
+    db 0C7h,01h,40h,16h,14h,01h,83h,0C1h
+    db 04h,0E9h,0F2h,5Eh,0EEh,0FFh
+??1GameTextInterface@@UAE@XZ ENDP
+_TEXT ENDS
+END

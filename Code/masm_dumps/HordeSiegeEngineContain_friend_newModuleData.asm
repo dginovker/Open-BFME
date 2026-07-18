deddae748dfb90494f29ba77efc71a02716c1fe9
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-18 11:28:16 -0400

    Match horde-siege-contain module-data factory

---
+++ Code/masm_dumps/HordeSiegeEngineContain_friend_newModuleData.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+
+; ?friend_newModuleData@HordeSiegeEngineContain@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 110 retail bytes @ 0x00516360; allocates, constructs, and parses HordeSiegeEngineContain module data.
+_TEXT SEGMENT
+public ?friend_newModuleData@HordeSiegeEngineContain@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@HordeSiegeEngineContain@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 06Ah,0FFh,068h,0ABh,0E8h,0FFh,000h,064h,0A1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,051h,056h,068h,04Ch,002h,000h,000h,0E8h,0AFh,0BBh,076h
+    db 000h,083h,0C4h,004h,089h,044h,024h,004h,085h,0C0h,0C7h,044h,024h,010h,000h,000h
+    db 000h,000h,074h,00Bh,08Bh,0C8h,0E8h,00Ah,01Dh,0F2h,0FFh,08Bh,0F0h,0EBh,002h,033h
+    db 0F6h,08Bh,04Ch,024h,018h,085h,0C9h,0C7h,044h,024h,010h,0FFh,0FFh,0FFh,0FFh,074h
+    db 00Bh,068h,0C1h,027h,044h,000h,056h,0E8h,074h,0BDh,073h,000h,08Bh,04Ch,024h,008h
+    db 08Bh,0C6h,05Eh,064h,089h,00Dh,000h,000h,000h,000h,083h,0C4h,010h,0C3h
+?friend_newModuleData@HordeSiegeEngineContain@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END

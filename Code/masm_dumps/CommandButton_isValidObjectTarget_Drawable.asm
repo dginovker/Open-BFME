97535be0583c8ebe5cd2cbf512b15f5aa3607cd6
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-18 15:49:55 -0400

    Match Drawable target validation overload

---
+++ Code/masm_dumps/CommandButton_isValidObjectTarget_Drawable.asm
@@ -0,0 +1,16 @@
+.386
+.model flat
+
+; ?isValidObjectTarget@CommandButton@@QBE_NPBVDrawable@@0@Z
+; Exact 67 retail bytes @ RVA 0x0049AE20; Drawable target validation overload
+_TEXT SEGMENT
+public ?isValidObjectTarget@CommandButton@@QBE_NPBVDrawable@@0@Z
+?isValidObjectTarget@CommandButton@@QBE_NPBVDrawable@@0@Z PROC
+    db 08Bh,044h,024h,008h,085h,0C0h,056h,08Bh,0F1h,074h,008h,08Bh,080h,0FCh,000h,000h
+    db 000h,0EBh,002h,033h,0C0h,08Bh,04Ch,024h,008h,085h,0C9h,074h,020h,08Bh,089h,0FCh
+    db 000h,000h,000h,085h,0C9h,074h,016h,085h,0C0h,074h,012h,050h,0E8h,0C8h,0F8h,0BAh
+    db 0FFh,050h,08Bh,0CEh,0E8h,0E6h,075h,0B8h,0FFh,05Eh,0C2h,008h,000h,032h,0C0h,05Eh
+    db 0C2h,008h,000h
+?isValidObjectTarget@CommandButton@@QBE_NPBVDrawable@@0@Z ENDP
+_TEXT ENDS
+END

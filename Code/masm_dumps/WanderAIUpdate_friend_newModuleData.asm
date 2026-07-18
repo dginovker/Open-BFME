53fa47fc88c78e9d1b59e977cbb6b138ef72d2c1
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-18 15:40:15 -0400

    Match wander-AI update data factory

---
+++ Code/masm_dumps/WanderAIUpdate_friend_newModuleData.asm
@@ -0,0 +1,19 @@
+.386
+.model flat
+
+; ?friend_newModuleData@WanderAIUpdate@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 128 retail bytes @ RVA 0x0011BEE0; update module-data registration factory
+_TEXT SEGMENT
+public ?friend_newModuleData@WanderAIUpdate@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@WanderAIUpdate@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 06Ah,0FFh,068h,02Bh,0FCh,0FFh,000h,064h,0A1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,051h,056h,06Ah,070h,0E8h,032h,060h,076h,000h,08Bh,0F0h
+    db 083h,0C4h,004h,089h,074h,024h,004h,085h,0F6h,0C7h,044h,024h,010h,000h,000h,000h
+    db 000h,074h,01Eh,08Bh,0CEh,0E8h,021h,0CBh,0F2h,0FFh,0C7h,006h,0A8h,0A5h,008h,001h
+    db 0C6h,046h,064h,000h,0C7h,046h,068h,0FFh,0FFh,0FFh,0FFh,0C6h,046h,06Ch,001h,0EBh
+    db 002h,033h,0F6h,08Bh,04Ch,024h,018h,085h,0C9h,0C7h,044h,024h,010h,0FFh,0FFh,0FFh
+    db 0FFh,074h,00Bh,068h,0DEh,027h,040h,000h,056h,0E8h,0E2h,061h,073h,000h,08Bh,04Ch
+    db 024h,008h,08Bh,0C6h,05Eh,064h,089h,00Dh,000h,000h,000h,000h,083h,0C4h,010h,0C3h
+?friend_newModuleData@WanderAIUpdate@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END

706d739e8c289c8a584d5fe53466d62c6f443e8c
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-18 05:49:56 -0400

    Match GameTextManager destructor

---
+++ Code/masm_dumps/GameText_GameTextManager_destructor.asm
@@ -0,0 +1,26 @@
+.386
+.model flat
+
+; ??1GameTextManager@@UAE@XZ
+; Exact 117 retail bytes @ 0x00438430; BFME member offsets differ from ZH.
+_TEXT SEGMENT
+public ??1GameTextManager@@UAE@XZ
+??1GameTextManager@@UAE@XZ PROC
+    db 6Ah,0FFh,68h,0C4h,22h,02h,01h,64h
+    db 0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,51h,56h,8Bh
+    db 0F1h,89h,74h,24h,04h,0C7h,06h,0F0h
+    db 39h,0Fh,01h,0C7h,44h,24h,10h,02h
+    db 00h,00h,00h,0E8h,14h,5Bh,0BFh,0FFh
+    db 8Dh,8Eh,34h,78h,00h,00h,0C6h,44h
+    db 24h,10h,01h,0E8h,42h,0E6h,0BEh,0FFh
+    db 8Dh,8Eh,24h,78h,00h,00h,0C6h,44h
+    db 24h,10h,00h,0E8h,50h,0FDh,44h,00h
+    db 8Bh,0CEh,0C7h,44h,24h,10h,0FFh,0FFh
+    db 0FFh,0FFh,0C7h,06h,88h,39h,0Fh,01h
+    db 0E8h,0ABh,95h,56h,00h,8Bh,4Ch,24h
+    db 08h,5Eh,64h,89h,0Dh,00h,00h,00h
+    db 00h,83h,0C4h,10h,0C3h
+??1GameTextManager@@UAE@XZ ENDP
+_TEXT ENDS
+END

d19687ba01d9c244afe6a02a7cead7c39910c60c
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-18 09:50:14 -0400

    Match LadderInfo scalar destructor

---
+++ Code/masm_dumps/LadderInfo_scalar_destructor.asm
@@ -0,0 +1,13 @@
+.386
+.model flat
+
+; ??_GLadderInfo@@QAEPAXI@Z
+; Exact 30 retail bytes @ 0x00A2AAE0; scalar-deleting destructor paired with LadderInfo::~LadderInfo.
+_TEXT SEGMENT
+public ??_GLadderInfo@@QAEPAXI@Z
+??_GLadderInfo@@QAEPAXI@Z PROC
+    db 056h,08Bh,0F1h,0E8h,0DDh,09Eh,09Eh,0FFh,0F6h,044h,024h,008h,001h,074h,009h,056h
+    db 0E8h,0BBh,073h,025h,000h,083h,0C4h,004h,08Bh,0C6h,05Eh,0C2h,004h,000h
+??_GLadderInfo@@QAEPAXI@Z ENDP
+_TEXT ENDS
+END

7b6da9305f3d035fcb33a961492c46bf61a72f14
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-18 14:10:17 -0400

    Match gate-proxy module-data factory

---
+++ Code/masm_dumps/GateProxyBehavior_friend_newModuleData.asm
@@ -0,0 +1,19 @@
+.386
+.model flat
+
+; ?friend_newModuleData@GateProxyBehavior@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 113 retail bytes @ 0x00523BF0; constructs gate-proxy module data.
+_TEXT SEGMENT
+public ?friend_newModuleData@GateProxyBehavior@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@GateProxyBehavior@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 06Ah,0FFh,068h,08Bh,014h,000h,001h,064h,0A1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,051h,056h,06Ah,048h,0E8h,022h,0E3h,075h,000h,08Bh,0F0h
+    db 083h,0C4h,004h,089h,074h,024h,004h,085h,0F6h,0C7h,044h,024h,010h,000h,000h,000h
+    db 000h,074h,00Fh,08Bh,0CEh,0E8h,075h,029h,0EFh,0FFh,0C7h,006h,0A0h,0A2h,008h,001h
+    db 0EBh,002h,033h,0F6h,08Bh,04Ch,024h,018h,085h,0C9h,0C7h,044h,024h,010h,0FFh,0FFh
+    db 0FFh,0FFh,074h,00Bh,068h,0E1h,041h,041h,000h,056h,0E8h,0E1h,0E4h,072h,000h,08Bh
+    db 04Ch,024h,008h,08Bh,0C6h,05Eh,064h,089h,00Dh,000h,000h,000h,000h,083h,0C4h,010h
+    db 0C3h
+?friend_newModuleData@GateProxyBehavior@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END

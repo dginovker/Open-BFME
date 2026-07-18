8d9e5b04a4ef80400a9796b243d1fae65173f79a
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-18 15:17:25 -0400

    Match bone-FX damage data factory

---
+++ Code/masm_dumps/BoneFXDamage_friend_newModuleData.asm
@@ -0,0 +1,15 @@
+.386
+.model flat
+
+; ?friend_newModuleData@BoneFXDamage@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 50 retail bytes @ RVA 0x00122D80; damage module-data registration factory
+_TEXT SEGMENT
+public ?friend_newModuleData@BoneFXDamage@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@BoneFXDamage@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 056h,06Ah,008h,0E8h,0A8h,0F1h,075h,000h,083h,0C4h,004h,085h,0C0h,074h,00Ah,0C7h
+    db 000h,038h,0AEh,008h,001h,08Bh,0F0h,0EBh,002h,033h,0F6h,08Bh,04Ch,024h,008h,085h
+    db 0C9h,074h,00Bh,068h,08Ah,08Dh,042h,000h,056h,0E8h,082h,0F3h,072h,000h,08Bh,0C6h
+    db 05Eh,0C3h
+?friend_newModuleData@BoneFXDamage@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END

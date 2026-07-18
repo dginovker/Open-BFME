b348122b24f7975c8a8a85fd635bbb402a30b194
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-18 10:50:26 -0400

    Match AOD horde-contain module-data factory

---
+++ Code/masm_dumps/AODHordeContain_friend_newModuleData.asm
@@ -0,0 +1,19 @@
+.386
+.model flat
+
+; ?friend_newModuleData@AODHordeContain@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 116 retail bytes @ 0x00522FD0; constructs the AOD-specialized HordeContain module data.
+_TEXT SEGMENT
+public ?friend_newModuleData@AODHordeContain@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@AODHordeContain@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 06Ah,0FFh,068h,00Bh,013h,000h,001h,064h,0A1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,051h,056h,068h,0F4h,002h,000h,000h,0E8h,03Fh,0EFh,075h
+    db 000h,08Bh,0F0h,083h,0C4h,004h,089h,074h,024h,004h,085h,0F6h,0C7h,044h,024h,010h
+    db 000h,000h,000h,000h,074h,00Fh,08Bh,0CEh,0E8h,06Ah,0BDh,0EEh,0FFh,0C7h,006h,060h
+    db 099h,008h,001h,0EBh,002h,033h,0F6h,08Bh,04Ch,024h,018h,085h,0C9h,0C7h,044h,024h
+    db 010h,0FFh,0FFh,0FFh,0FFh,074h,00Bh,068h,0F6h,034h,043h,000h,056h,0E8h,0FEh,0F0h
+    db 072h,000h,08Bh,04Ch,024h,008h,08Bh,0C6h,05Eh,064h,089h,00Dh,000h,000h,000h,000h
+    db 083h,0C4h,010h,0C3h
+?friend_newModuleData@AODHordeContain@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END

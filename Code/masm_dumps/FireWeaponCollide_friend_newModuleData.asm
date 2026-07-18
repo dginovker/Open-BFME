d281c0f7b812a1330559644a722f0091dd916b06
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-18 15:29:29 -0400

    Match fire-weapon collide data factory

---
+++ Code/masm_dumps/FireWeaponCollide_friend_newModuleData.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+
+; ?friend_newModuleData@FireWeaponCollide@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 106 retail bytes @ RVA 0x00127D00; collide module-data registration factory
+_TEXT SEGMENT
+public ?friend_newModuleData@FireWeaponCollide@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@FireWeaponCollide@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 056h,06Ah,028h,0E8h,028h,0A2h,075h,000h,083h,0C4h,004h,085h,0C0h,074h,042h,0C7h
+    db 000h,050h,0EBh,008h,001h,08Dh,048h,00Ch,033h,0D2h,08Bh,0F1h,089h,016h,089h,056h
+    db 004h,089h,056h,008h,08Dh,050h,018h,033h,0F6h,057h,08Bh,0FAh,089h,037h,089h,077h
+    db 004h,089h,077h,008h,089h,070h,008h,089h,031h,089h,071h,004h,089h,071h,008h,033h
+    db 0C9h,089h,00Ah,089h,04Ah,004h,089h,04Ah,008h,088h,048h,024h,08Bh,0F0h,05Fh,0EBh
+    db 002h,033h,0F6h,08Bh,04Ch,024h,008h,085h,0C9h,074h,00Bh,068h,0C0h,0ABh,043h,000h
+    db 056h,0E8h,0CAh,0A3h,072h,000h,08Bh,0C6h,05Eh,0C3h
+?friend_newModuleData@FireWeaponCollide@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END

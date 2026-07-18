b591f3be1f60676811625d34e09b58db26cd4881
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-18 15:47:33 -0400

    Match stealth-upgrade data factory

---
+++ Code/masm_dumps/StealthUpgrade_friend_newModuleData.asm
@@ -0,0 +1,15 @@
+.386
+.model flat
+
+; ?friend_newModuleData@StealthUpgrade@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 58 retail bytes @ RVA 0x0012BF00; upgrade module-data registration factory
+_TEXT SEGMENT
+public ?friend_newModuleData@StealthUpgrade@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@StealthUpgrade@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 056h,06Ah,070h,0E8h,028h,060h,075h,000h,08Bh,0F0h,083h,0C4h,004h,085h,0F6h,074h
+    db 010h,08Dh,04Eh,008h,0C7h,006h,0D8h,0F1h,008h,001h,0E8h,087h,031h,0EEh,0FFh,0EBh
+    db 002h,033h,0F6h,08Bh,04Ch,024h,008h,085h,0C9h,074h,00Bh,068h,098h,007h,042h,000h
+    db 056h,0E8h,0FAh,061h,072h,000h,08Bh,0C6h,05Eh,0C3h
+?friend_newModuleData@StealthUpgrade@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END

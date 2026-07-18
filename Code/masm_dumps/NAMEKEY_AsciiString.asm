1077f7616ce2f6df2f0724f1a6063c8788b32c6a
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-18 15:04:25 -0400

    Match AsciiString name-key helper

---
+++ Code/masm_dumps/NAMEKEY_AsciiString.asm
@@ -0,0 +1,14 @@
+.386
+.model flat
+
+; ?NAMEKEY@@YA?AW4NameKeyType@@ABVAsciiString@@@Z
+; Exact 44 retail bytes @ RVA 0x000A3AB0; BFME AsciiString name-key helper
+_TEXT SEGMENT
+public ?NAMEKEY@@YA?AW4NameKeyType@@ABVAsciiString@@@Z
+?NAMEKEY@@YA?AW4NameKeyType@@ABVAsciiString@@@Z PROC
+    db 08Bh,044h,024h,004h,08Bh,000h,085h,0C0h,074h,010h,08Bh,00Dh,000h,0D6h,02Eh,001h
+    db 083h,0C0h,008h,050h,0E8h,00Eh,073h,0F9h,0FFh,0C3h,08Bh,00Dh,000h,0D6h,02Eh,001h
+    db 0B8h,08Bh,038h,007h,001h,050h,0E8h,0FCh,072h,0F9h,0FFh,0C3h
+?NAMEKEY@@YA?AW4NameKeyType@@ABVAsciiString@@@Z ENDP
+_TEXT ENDS
+END

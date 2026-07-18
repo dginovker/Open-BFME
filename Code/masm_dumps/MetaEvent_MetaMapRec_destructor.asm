19c26e40128fe8e21f6e97ecb1b2d4235c95379a
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-18 05:58:27 -0400

    Match MetaMapRec destructor

---
+++ Code/masm_dumps/MetaEvent_MetaMapRec_destructor.asm
@@ -0,0 +1,26 @@
+.386
+.model flat
+
+; ??1MetaMapRec@@MAE@XZ
+; Exact 114 retail bytes @ 0x006F4C50; queue candidate began inside the prologue.
+_TEXT SEGMENT
+public ??1MetaMapRec@@MAE@XZ
+??1MetaMapRec@@MAE@XZ PROC
+    db 6Ah,0FFh,68h,31h,0BAh,04h,01h,64h
+    db 0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,51h,56h,8Bh
+    db 0F1h,89h,74h,24h,04h,0C7h,06h,0A8h
+    db 0FEh,11h,01h,8Dh,8Eh,0B0h,01h,00h
+    db 00h,0C7h,44h,24h,10h,02h,00h,00h
+    db 00h,0E8h,4Ah,35h,19h,00h,8Dh,8Eh
+    db 0E0h,00h,00h,00h,0C6h,44h,24h,10h
+    db 01h,0E8h,5Ah,0BDh,24h,00h,8Dh,4Eh
+    db 14h,0C6h,44h,24h,10h,00h,0E8h,4Dh
+    db 0BDh,24h,00h,8Bh,0CEh,0C7h,44h,24h
+    db 10h,0FFh,0FFh,0FFh,0FFh,0E8h,71h,8Bh
+    db 91h,0FFh,8Bh,4Ch,24h,08h,5Eh,64h
+    db 89h,0Dh,00h,00h,00h,00h,83h,0C4h
+    db 10h,0C3h
+??1MetaMapRec@@MAE@XZ ENDP
+_TEXT ENDS
+END

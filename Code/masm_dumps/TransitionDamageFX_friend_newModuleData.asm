42eb61f0d782ca076efa8ced0f4329c529123f83
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-18 15:22:30 -0400

    Match transition-damage FX data factory

---
+++ Code/masm_dumps/TransitionDamageFX_friend_newModuleData.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+
+; ?friend_newModuleData@TransitionDamageFX@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 110 retail bytes @ RVA 0x00124770; damage module-data registration factory
+_TEXT SEGMENT
+public ?friend_newModuleData@TransitionDamageFX@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@TransitionDamageFX@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 06Ah,0FFh,068h,0CBh,015h,000h,001h,064h,0A1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,051h,056h,068h,040h,010h,000h,000h,0E8h,09Fh,0D7h,075h
+    db 000h,083h,0C4h,004h,089h,044h,024h,004h,085h,0C0h,0C7h,044h,024h,010h,000h,000h
+    db 000h,000h,074h,00Bh,08Bh,0C8h,0E8h,0EAh,0C2h,0EFh,0FFh,08Bh,0F0h,0EBh,002h,033h
+    db 0F6h,08Bh,04Ch,024h,018h,085h,0C9h,0C7h,044h,024h,010h,0FFh,0FFh,0FFh,0FFh,074h
+    db 00Bh,068h,08Eh,093h,042h,000h,056h,0E8h,064h,0D9h,072h,000h,08Bh,04Ch,024h,008h
+    db 08Bh,0C6h,05Eh,064h,089h,00Dh,000h,000h,000h,000h,083h,0C4h,010h,0C3h
+?friend_newModuleData@TransitionDamageFX@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END

20d8952e8eba9d323284c87a315e614666cfc0da
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-18 19:52:31 -0400

    Match LocomotorSetUpgrade data factory

---
+++ Code/masm_dumps/LocomotorSetUpgrade_friend_newModuleData.asm
@@ -0,0 +1,15 @@
+.386
+.model flat
+
+; ?friend_newModuleData@LocomotorSetUpgrade@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 62 retail bytes @ RVA 0x0012B430; LocomotorSetUpgrade module-data factory
+_TEXT SEGMENT
+public ?friend_newModuleData@LocomotorSetUpgrade@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@LocomotorSetUpgrade@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 056h,06Ah,074h,0E8h,0F8h,06Ah,075h,000h,08Bh,0F0h,083h,0C4h,004h,085h,0F6h,074h
+    db 014h,08Dh,04Eh,008h,0E8h,05Dh,03Ch,0EEh,0FFh,0C7h,006h,040h,0F4h,008h,001h,0C6h
+    db 046h,070h,000h,0EBh,002h,033h,0F6h,08Bh,04Ch,024h,008h,085h,0C9h,074h,00Bh,068h
+    db 00Eh,0A3h,044h,000h,056h,0E8h,0C6h,06Ch,072h,000h,08Bh,0C6h,05Eh,0C3h
+?friend_newModuleData@LocomotorSetUpgrade@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END

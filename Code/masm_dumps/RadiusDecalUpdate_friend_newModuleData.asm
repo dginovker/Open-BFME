043ed98412a633acc7eeb5deeea6d1abca4b6a89
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 08:45:46 -0400

    Match RadiusDecalUpdate data factory

---
+++ Code/masm_dumps/RadiusDecalUpdate_friend_newModuleData.asm
@@ -0,0 +1,15 @@
+.386
+.model flat
+
+; ?friend_newModuleData@RadiusDecalUpdate@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 50 retail bytes @ 0x00123E50; allocates and initializes RadiusDecalUpdate module data.
+_TEXT SEGMENT
+public ?friend_newModuleData@RadiusDecalUpdate@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@RadiusDecalUpdate@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 056h, 06Ah, 008h, 0E8h, 0D8h, 0E0h, 075h, 000h, 083h, 0C4h, 004h, 085h, 0C0h, 074h, 00Ah, 0C7h
+    db 000h, 010h, 0CEh, 008h, 001h, 08Bh, 0F0h, 0EBh, 002h, 033h, 0F6h, 08Bh, 04Ch, 024h, 008h, 085h
+    db 0C9h, 074h, 00Bh, 068h, 0F0h, 020h, 044h, 000h, 056h, 0E8h, 0B2h, 0E2h, 072h, 000h, 08Bh, 0C6h
+    db 05Eh, 0C3h
+?friend_newModuleData@RadiusDecalUpdate@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END

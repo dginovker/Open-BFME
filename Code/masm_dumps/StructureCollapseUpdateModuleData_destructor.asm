4c73b19e378d6d08faf1e5aadba388bb30137e25
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 16:10:24 -0400

    Match StructureCollapse module data destructor

---
+++ Code/masm_dumps/StructureCollapseUpdateModuleData_destructor.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+
+; ??1StructureCollapseUpdateModuleData@@UAE@XZ
+; Exact 103 retail bytes @ 0x00128A90; registration, vtable, and deleting-dtor xrefs prove ownership.
+_TEXT SEGMENT
+public ??1StructureCollapseUpdateModuleData@@UAE@XZ
+??1StructureCollapseUpdateModuleData@@UAE@XZ PROC
+    db 06Ah, 0FFh, 068h, 0CEh, 01Ch, 000h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 056h, 08Bh, 0F1h, 089h, 074h, 024h, 004h, 068h, 077h, 074h
+    db 041h, 000h, 06Ah, 005h, 06Ah, 00Ch, 08Dh, 086h, 08Ch, 000h, 000h, 000h, 050h, 0C7h, 044h, 024h
+    db 020h, 001h, 000h, 000h, 000h, 0E8h, 0ACh, 0E2h, 08Ch, 000h, 068h, 03Bh, 02Dh, 042h, 000h, 06Ah
+    db 005h, 06Ah, 00Ch, 08Dh, 04Eh, 050h, 051h, 0C6h, 044h, 024h, 020h, 000h, 0E8h, 095h, 0E2h, 08Ch
+    db 000h, 08Bh, 04Ch, 024h, 008h, 0C7h, 006h, 044h, 037h, 007h, 001h, 05Eh, 064h, 089h, 00Dh, 000h
+    db 000h, 000h, 000h, 083h, 0C4h, 010h, 0C3h
+??1StructureCollapseUpdateModuleData@@UAE@XZ ENDP
+_TEXT ENDS
+END

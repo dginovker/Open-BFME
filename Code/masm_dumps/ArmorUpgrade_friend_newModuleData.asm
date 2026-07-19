8afc46e8a9f20f042b924c8c8cf8c9924c25a416
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 12:11:49 -0400

    Match ArmorUpgrade data factory

---
+++ Code/masm_dumps/ArmorUpgrade_friend_newModuleData.asm
@@ -0,0 +1,16 @@
+.386
+.model flat
+
+; ?friend_newModuleData@ArmorUpgrade@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 73 retail bytes @ 0x0012AF20; registration ordinal 0x98 proves ownership.
+_TEXT SEGMENT
+public ?friend_newModuleData@ArmorUpgrade@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@ArmorUpgrade@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 056h, 06Ah, 078h, 0E8h, 008h, 070h, 075h, 000h, 08Bh, 0F0h, 083h, 0C4h, 004h, 085h, 0F6h, 074h
+    db 01Fh, 08Dh, 04Eh, 008h, 0E8h, 06Dh, 041h, 0EEh, 0FFh, 0C7h, 006h, 0E0h, 0F2h, 008h, 001h, 0C6h
+    db 046h, 070h, 000h, 0C6h, 046h, 071h, 000h, 0C7h, 046h, 074h, 003h, 000h, 000h, 000h, 0EBh, 002h
+    db 033h, 0F6h, 08Bh, 04Ch, 024h, 008h, 085h, 0C9h, 074h, 00Bh, 068h, 0F5h, 084h, 044h, 000h, 056h
+    db 0E8h, 0CBh, 071h, 072h, 000h, 08Bh, 0C6h, 05Eh, 0C3h
+?friend_newModuleData@ArmorUpgrade@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END

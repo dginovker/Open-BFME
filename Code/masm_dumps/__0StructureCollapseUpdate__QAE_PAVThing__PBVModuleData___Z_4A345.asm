b6ff28414e21db4e0f61a42caf2db9c428d10f38
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 09:24:20 -0400

    Match StructureCollapseUpdate constructor thunk

---
+++ Code/masm_dumps/__0StructureCollapseUpdate__QAE_PAVThing__PBVModuleData___Z_4A345.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ??0StructureCollapseUpdate@@QAE@PAVThing@@PBVModuleData@@@Z
+; Retail constructor thunk @ 0x0044A345, uniquely called by the StructureCollapseUpdate factory.
+_TEXT SEGMENT
+public ??0StructureCollapseUpdate@@QAE@PAVThing@@PBVModuleData@@@Z
+??0StructureCollapseUpdate@@QAE@PAVThing@@PBVModuleData@@@Z PROC
+    db 0E9h, 0B6h, 03Eh, 026h, 000h
+??0StructureCollapseUpdate@@QAE@PAVThing@@PBVModuleData@@@Z ENDP
+_TEXT ENDS
+END

b39511eb4f18bfe56f49de15fee813b1cd8aed26
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 03:39:38 -0400

    Match DeletionUpdate data factory

---
+++ Code/masm_dumps/DeletionUpdate_friend_newModuleData.asm
@@ -0,0 +1,15 @@
+.386
+.model flat
+
+; ?friend_newModuleData@DeletionUpdate@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 58 retail bytes @ 0x001239B0; allocates and initializes DeletionUpdate module data.
+_TEXT SEGMENT
+public ?friend_newModuleData@DeletionUpdate@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@DeletionUpdate@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 056h, 06Ah, 010h, 0E8h, 078h, 0E5h, 075h, 000h, 033h, 0D2h, 083h, 0C4h, 004h, 03Bh, 0C2h, 074h
+    db 010h, 0C7h, 000h, 050h, 0CAh, 008h, 001h, 089h, 050h, 008h, 089h, 050h, 00Ch, 08Bh, 0F0h, 0EBh
+    db 002h, 033h, 0F6h, 08Bh, 04Ch, 024h, 008h, 03Bh, 0CAh, 074h, 00Bh, 068h, 03Ah, 0F8h, 040h, 000h
+    db 056h, 0E8h, 04Ah, 0E7h, 072h, 000h, 08Bh, 0C6h, 05Eh, 0C3h
+?friend_newModuleData@DeletionUpdate@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END

80d8a32d652afd20c05b876ae03eda57fae259bb
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 17:20:53 -0400

    Match W3DDisplayString destructor

---
+++ Code/masm_dumps/W3DDisplayString_dtor.asm
@@ -0,0 +1,19 @@
+.386
+.model flat
+
+; ??1W3DDisplayString@@MAE@XZ
+; Exact 114 retail bytes @ 0x006F4C50; vtable deleting destructor calls thunk 0x00044B57.
+_TEXT SEGMENT
+public ??1W3DDisplayString@@MAE@XZ
+??1W3DDisplayString@@MAE@XZ PROC
+    db 06Ah, 0FFh, 068h, 031h, 0BAh, 004h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 056h, 08Bh, 0F1h, 089h, 074h, 024h, 004h, 0C7h, 006h, 0A8h
+    db 0FEh, 011h, 001h, 08Dh, 08Eh, 0B0h, 001h, 000h, 000h, 0C7h, 044h, 024h, 010h, 002h, 000h, 000h
+    db 000h, 0E8h, 04Ah, 035h, 019h, 000h, 08Dh, 08Eh, 0E0h, 000h, 000h, 000h, 0C6h, 044h, 024h, 010h
+    db 001h, 0E8h, 05Ah, 0BDh, 024h, 000h, 08Dh, 04Eh, 014h, 0C6h, 044h, 024h, 010h, 000h, 0E8h, 04Dh
+    db 0BDh, 024h, 000h, 08Bh, 0CEh, 0C7h, 044h, 024h, 010h, 0FFh, 0FFh, 0FFh, 0FFh, 0E8h, 071h, 08Bh
+    db 091h, 0FFh, 08Bh, 04Ch, 024h, 008h, 05Eh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h, 0C4h
+    db 010h, 0C3h
+??1W3DDisplayString@@MAE@XZ ENDP
+_TEXT ENDS
+END

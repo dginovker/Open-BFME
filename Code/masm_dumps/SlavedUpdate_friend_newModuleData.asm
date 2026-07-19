65e3e8c7af970877ea4dbc47c6fcf76f63f45be5
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 09:34:16 -0400

    Correct SlavedUpdate data owner

---
+++ Code/masm_dumps/SlavedUpdate_friend_newModuleData.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+
+; ?friend_newModuleData@SlavedUpdate@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 106 retail bytes @ 0x00125A60; registration ordinal 0x77 proves ownership.
+_TEXT SEGMENT
+public ?friend_newModuleData@SlavedUpdate@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@SlavedUpdate@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 056h, 06Ah, 054h, 0E8h, 0C8h, 0C4h, 075h, 000h, 033h, 0D2h, 083h, 0C4h, 004h, 03Bh, 0C2h, 074h
+    db 040h, 0C7h, 000h, 030h, 0E7h, 008h, 001h, 089h, 050h, 048h, 089h, 050h, 04Ch, 089h, 050h, 008h
+    db 089h, 050h, 00Ch, 089h, 050h, 010h, 089h, 050h, 014h, 089h, 050h, 018h, 089h, 050h, 01Ch, 089h
+    db 050h, 020h, 089h, 050h, 030h, 089h, 050h, 034h, 089h, 050h, 028h, 089h, 050h, 02Ch, 089h, 050h
+    db 040h, 089h, 050h, 044h, 089h, 050h, 038h, 089h, 050h, 03Ch, 088h, 050h, 050h, 08Bh, 0F0h, 0EBh
+    db 002h, 033h, 0F6h, 08Bh, 04Ch, 024h, 008h, 03Bh, 0CAh, 074h, 00Bh, 068h, 08Eh, 097h, 043h, 000h
+    db 056h, 0E8h, 06Ah, 0C6h, 072h, 000h, 08Bh, 0C6h, 05Eh, 0C3h
+?friend_newModuleData@SlavedUpdate@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END

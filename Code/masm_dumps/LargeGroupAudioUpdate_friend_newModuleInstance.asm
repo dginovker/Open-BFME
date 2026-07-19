a33c45ff6df787128f6d9e4a6834b247392d58a4
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 10:30:58 -0400

    Match LargeGroupAudioUpdate factory

---
+++ Code/masm_dumps/LargeGroupAudioUpdate_friend_newModuleInstance.asm
@@ -0,0 +1,17 @@
+.386
+.model flat
+
+; ?friend_newModuleInstance@LargeGroupAudioUpdate@@SAPAVModule@@PAVThing@@PBVModuleData@@@Z
+; Exact 96 retail bytes @ 0x0011C200; registration ordinal 0x74 proves ownership.
+_TEXT SEGMENT
+public ?friend_newModuleInstance@LargeGroupAudioUpdate@@SAPAVModule@@PAVThing@@PBVModuleData@@@Z
+?friend_newModuleInstance@LargeGroupAudioUpdate@@SAPAVModule@@PAVThing@@PBVModuleData@@@Z PROC
+    db 06Ah, 0FFh, 068h, 0CBh, 0FCh, 0FFh, 000h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 06Ah, 068h, 0E8h, 013h, 05Dh, 076h, 000h, 083h, 0C4h, 004h
+    db 089h, 004h, 024h, 085h, 0C0h, 0C7h, 044h, 024h, 00Ch, 000h, 000h, 000h, 000h, 074h, 020h, 08Bh
+    db 04Ch, 024h, 018h, 08Bh, 054h, 024h, 014h, 051h, 052h, 08Bh, 0C8h, 0E8h, 0A2h, 0C1h, 0F2h, 0FFh
+    db 08Bh, 04Ch, 024h, 004h, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h, 0C4h, 010h, 0C3h, 08Bh
+    db 04Ch, 024h, 004h, 033h, 0C0h, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h, 0C4h, 010h, 0C3h
+?friend_newModuleInstance@LargeGroupAudioUpdate@@SAPAVModule@@PAVThing@@PBVModuleData@@@Z ENDP
+_TEXT ENDS
+END

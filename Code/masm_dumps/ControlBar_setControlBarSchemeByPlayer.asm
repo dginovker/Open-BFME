0cd83290193dcbfd262e7c80eadeda10424d9343
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 16:47:20 -0400

    Match ControlBar player scheme selection

---
+++ Code/masm_dumps/ControlBar_setControlBarSchemeByPlayer.asm
@@ -0,0 +1,61 @@
+.386
+.model flat
+
+; ?setControlBarSchemeByPlayer@ControlBar@@QAEXPAVPlayer@@@Z
+; Exact 790 retail bytes @ 0x0049F8B0; the unique public thunk is at 0x00019B64.
+_TEXT SEGMENT
+public ?setControlBarSchemeByPlayer@ControlBar@@QAEXPAVPlayer@@@Z
+?setControlBarSchemeByPlayer@ControlBar@@QAEXPAVPlayer@@@Z PROC
+    db 06Ah, 0FFh, 068h, 0BAh, 07Fh, 002h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 053h, 08Bh, 05Ch, 024h, 018h, 055h, 056h, 08Bh, 0F1h, 08Bh
+    db 04Eh, 030h, 085h, 0C9h, 057h, 074h, 006h, 053h, 0E8h, 056h, 0F6h, 0B7h, 0FFh, 0A0h, 01Ch, 034h
+    db 02Fh, 001h, 083h, 0CFh, 0FFh, 0A8h, 001h, 075h, 028h, 083h, 00Dh, 01Ch, 034h, 02Fh, 001h, 001h
+    db 08Bh, 00Dh, 000h, 0D6h, 02Eh, 001h, 068h, 054h, 0B9h, 00Fh, 001h, 0C7h, 044h, 024h, 020h, 000h
+    db 000h, 000h, 000h, 0E8h, 0CFh, 0B4h, 0B9h, 0FFh, 0A3h, 018h, 034h, 02Fh, 001h, 089h, 07Ch, 024h
+    db 01Ch, 0F6h, 005h, 01Ch, 034h, 02Fh, 001h, 002h, 075h, 028h, 083h, 00Dh, 01Ch, 034h, 02Fh, 001h
+    db 002h, 08Bh, 00Dh, 000h, 0D6h, 02Eh, 001h, 068h, 0C0h, 056h, 00Fh, 001h, 0C7h, 044h, 024h, 020h
+    db 001h, 000h, 000h, 000h, 0E8h, 09Eh, 0B4h, 0B9h, 0FFh, 0A3h, 014h, 034h, 02Fh, 001h, 089h, 07Ch
+    db 024h, 01Ch, 0F6h, 005h, 01Ch, 034h, 02Fh, 001h, 004h, 075h, 028h, 083h, 00Dh, 01Ch, 034h, 02Fh
+    db 001h, 004h, 08Bh, 00Dh, 000h, 0D6h, 02Eh, 001h, 068h, 044h, 0B8h, 00Fh, 001h, 0C7h, 044h, 024h
+    db 020h, 002h, 000h, 000h, 000h, 0E8h, 06Dh, 0B4h, 0B9h, 0FFh, 0A3h, 010h, 034h, 02Fh, 001h, 089h
+    db 07Ch, 024h, 01Ch, 08Bh, 015h, 018h, 034h, 02Fh, 001h, 08Bh, 00Dh, 040h, 01Bh, 02Fh, 001h, 08Bh
+    db 001h, 052h, 06Ah, 000h, 0FFh, 090h, 0DCh, 000h, 000h, 000h, 08Bh, 015h, 014h, 034h, 02Fh, 001h
+    db 08Bh, 00Dh, 040h, 01Bh, 02Fh, 001h, 052h, 08Bh, 0E8h, 08Bh, 001h, 06Ah, 000h, 0FFh, 090h, 0DCh
+    db 000h, 000h, 000h, 08Bh, 015h, 010h, 034h, 02Fh, 001h, 08Bh, 00Dh, 040h, 01Bh, 02Fh, 001h, 08Bh
+    db 0F8h, 08Bh, 001h, 052h, 06Ah, 000h, 089h, 07Ch, 024h, 018h, 0FFh, 090h, 0DCh, 000h, 000h, 000h
+    db 08Bh, 0CBh, 089h, 044h, 024h, 024h, 0E8h, 0F2h, 07Fh, 0B7h, 0FFh, 084h, 0C0h, 00Fh, 085h, 052h
+    db 001h, 000h, 000h, 0C6h, 086h, 070h, 002h, 000h, 000h, 001h, 08Bh, 00Dh, 07Ch, 04Ch, 02Fh, 001h
+    db 085h, 0C9h, 074h, 005h, 08Bh, 001h, 0FFh, 050h, 010h, 08Bh, 00Dh, 08Ch, 014h, 02Fh, 001h, 08Bh
+    db 011h, 0FFh, 092h, 01Ch, 001h, 000h, 000h, 0A1h, 00Ch, 034h, 02Fh, 001h, 085h, 0C0h, 074h, 012h
+    db 08Bh, 046h, 05Ch, 085h, 0C0h, 074h, 00Bh, 08Bh, 08Eh, 0F0h, 002h, 000h, 000h, 08Bh, 001h, 0FFh
+    db 050h, 010h, 08Bh, 04Eh, 05Ch, 0C7h, 046h, 05Ch, 000h, 000h, 000h, 000h, 0A1h, 088h, 049h, 02Fh
+    db 001h, 085h, 0C0h, 089h, 00Dh, 00Ch, 034h, 02Fh, 001h, 075h, 032h, 0A1h, 038h, 04Ch, 02Fh, 001h
+    db 085h, 0C0h, 075h, 029h, 0A1h, 098h, 008h, 02Fh, 001h, 085h, 0C0h, 074h, 020h, 08Bh, 080h, 00Ch
+    db 001h, 000h, 000h, 083h, 0F8h, 008h, 074h, 015h, 083h, 0F8h, 004h, 074h, 010h, 08Bh, 00Dh, 040h
+    db 01Bh, 02Fh, 001h, 08Bh, 011h, 06Ah, 000h, 0FFh, 092h, 0B0h, 000h, 000h, 000h, 08Bh, 08Eh, 0F0h
+    db 002h, 000h, 000h, 06Ah, 000h, 0E8h, 0C5h, 094h, 0B7h, 0FFh, 08Bh, 046h, 064h, 085h, 0C0h, 074h
+    db 021h, 08Bh, 00Dh, 064h, 014h, 02Fh, 001h, 08Bh, 039h, 050h, 0FFh, 057h, 02Ch, 08Bh, 00Dh, 064h
+    db 014h, 02Fh, 001h, 050h, 0FFh, 057h, 060h, 08Bh, 07Ch, 024h, 010h, 0C7h, 046h, 064h, 000h, 000h
+    db 000h, 000h, 08Bh, 00Dh, 098h, 04Bh, 02Fh, 001h, 085h, 0C9h, 074h, 005h, 0E8h, 0DBh, 03Bh, 0B9h
+    db 0FFh, 08Bh, 04Eh, 03Ch, 06Ah, 001h, 0E8h, 07Fh, 084h, 0B8h, 0FFh, 08Bh, 04Eh, 058h, 06Ah, 001h
+    db 0E8h, 075h, 084h, 0B8h, 0FFh, 08Bh, 04Eh, 040h, 06Ah, 001h, 0E8h, 06Bh, 084h, 0B8h, 0FFh, 08Bh
+    db 04Eh, 044h, 06Ah, 001h, 0E8h, 061h, 084h, 0B8h, 0FFh, 08Bh, 04Eh, 048h, 06Ah, 001h, 0E8h, 057h
+    db 084h, 0B8h, 0FFh, 08Bh, 04Eh, 054h, 06Ah, 001h, 0E8h, 04Dh, 084h, 0B8h, 0FFh, 08Bh, 04Eh, 04Ch
+    db 06Ah, 001h, 0E8h, 043h, 084h, 0B8h, 0FFh, 08Bh, 04Eh, 050h, 06Ah, 000h, 0E8h, 039h, 084h, 0B8h
+    db 0FFh, 08Bh, 0CEh, 0E8h, 0A9h, 070h, 0B9h, 0FFh, 085h, 0EDh, 0C7h, 046h, 060h, 009h, 000h, 000h
+    db 000h, 074h, 009h, 06Ah, 001h, 08Bh, 0CDh, 0E8h, 01Eh, 084h, 0B8h, 0FFh, 085h, 0FFh, 074h, 009h
+    db 06Ah, 001h, 08Bh, 0CFh, 0E8h, 011h, 084h, 0B8h, 0FFh, 08Bh, 04Ch, 024h, 024h, 085h, 0C9h, 074h
+    db 075h, 06Ah, 000h, 0EBh, 06Ch, 06Ah, 000h, 06Ah, 000h, 08Bh, 0CEh, 0E8h, 0A4h, 02Eh, 0B9h, 0FFh
+    db 085h, 0EDh, 0C6h, 086h, 070h, 002h, 000h, 000h, 000h, 074h, 034h, 0A1h, 098h, 008h, 02Fh, 001h
+    db 08Bh, 080h, 00Ch, 001h, 000h, 000h, 083h, 0F8h, 001h, 074h, 005h, 083h, 0F8h, 005h, 075h, 013h
+    db 08Bh, 00Dh, 08Ch, 070h, 02Fh, 001h, 08Bh, 011h, 0FFh, 052h, 02Ch, 084h, 0C0h, 074h, 004h, 032h
+    db 0C0h, 0EBh, 004h, 033h, 0C0h, 0B0h, 001h, 050h, 08Bh, 0CDh, 0E8h, 0BBh, 083h, 0B8h, 0FFh, 085h
+    db 0FFh, 074h, 009h, 06Ah, 000h, 08Bh, 0CFh, 0E8h, 0AEh, 083h, 0B8h, 0FFh, 08Bh, 07Ch, 024h, 024h
+    db 085h, 0FFh, 074h, 012h, 06Ah, 000h, 08Bh, 0CFh, 0E8h, 09Dh, 083h, 0B8h, 0FFh, 06Ah, 001h, 08Bh
+    db 0CFh, 0E8h, 065h, 0A6h, 0BAh, 0FFh, 08Bh, 00Dh, 02Ch, 0D6h, 02Eh, 001h, 085h, 0C9h, 074h, 00Ah
+    db 0E8h, 046h, 043h, 0BAh, 0FFh, 083h, 0F8h, 001h, 074h, 007h, 08Bh, 0CEh, 0E8h, 0C2h, 042h, 0BAh
+    db 0FFh, 08Bh, 04Ch, 024h, 014h, 05Fh, 05Eh, 05Dh, 05Bh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h
+    db 083h, 0C4h, 010h, 0C2h, 004h, 000h
+?setControlBarSchemeByPlayer@ControlBar@@QAEXPAVPlayer@@@Z ENDP
+_TEXT ENDS
+END

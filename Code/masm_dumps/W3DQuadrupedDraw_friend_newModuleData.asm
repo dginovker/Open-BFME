56eaafe8f7ccfa9a250cf0e3247348aa8b664da4
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 16:17:57 -0400

    Match W3DQuadrupedDraw data factory

---
+++ Code/masm_dumps/W3DQuadrupedDraw_friend_newModuleData.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+
+; ?friend_newModuleData@W3DQuadrupedDraw@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 110 retail bytes @ 0x006BFEB0; W3D module registration ordinal 0x05 proves ownership.
+_TEXT SEGMENT
+public ?friend_newModuleData@W3DQuadrupedDraw@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@W3DQuadrupedDraw@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 06Ah, 0FFh, 068h, 05Bh, 09Bh, 004h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 056h, 068h, 06Ch, 001h, 000h, 000h, 0E8h, 05Fh, 020h, 01Ch
+    db 000h, 083h, 0C4h, 004h, 089h, 044h, 024h, 004h, 085h, 0C0h, 0C7h, 044h, 024h, 010h, 000h, 000h
+    db 000h, 000h, 074h, 00Bh, 08Bh, 0C8h, 0E8h, 05Ah, 00Ah, 097h, 0FFh, 08Bh, 0F0h, 0EBh, 002h, 033h
+    db 0F6h, 08Bh, 04Ch, 024h, 018h, 085h, 0C9h, 0C7h, 044h, 024h, 010h, 0FFh, 0FFh, 0FFh, 0FFh, 074h
+    db 00Bh, 068h, 0A9h, 0E2h, 041h, 000h, 056h, 0E8h, 024h, 022h, 019h, 000h, 08Bh, 04Ch, 024h, 008h
+    db 08Bh, 0C6h, 05Eh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h, 0C4h, 010h, 0C3h
+?friend_newModuleData@W3DQuadrupedDraw@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END

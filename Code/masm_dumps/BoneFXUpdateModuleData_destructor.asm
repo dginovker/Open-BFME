b7e20add4c3b7aa7d438a3303e5eefe5e6f8b929
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 19:43:05 -0400

    Match BoneFX module data destructor

---
+++ Code/masm_dumps/BoneFXUpdateModuleData_destructor.asm
@@ -0,0 +1,20 @@
+.386
+.model flat
+
+; ??1BoneFXUpdateModuleData@@UAE@XZ
+; Exact 129 retail bytes @ 0x00288490; factory, ctor vtable, and deleting wrapper prove ownership.
+_TEXT SEGMENT
+public ??1BoneFXUpdateModuleData@@UAE@XZ
+??1BoneFXUpdateModuleData@@UAE@XZ PROC
+    db 06Ah, 0FFh, 068h, 016h, 010h, 001h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 056h, 08Bh, 0F1h, 089h, 074h, 024h, 004h, 068h, 0C9h, 071h
+    db 042h, 000h, 06Ah, 020h, 06Ah, 024h, 08Dh, 086h, 014h, 009h, 000h, 000h, 050h, 0C7h, 044h, 024h
+    db 020h, 002h, 000h, 000h, 000h, 0E8h, 0ACh, 0E8h, 076h, 000h, 068h, 017h, 076h, 040h, 000h, 06Ah
+    db 020h, 06Ah, 024h, 08Dh, 08Eh, 090h, 004h, 000h, 000h, 051h, 0C6h, 044h, 024h, 020h, 001h, 0E8h
+    db 092h, 0E8h, 076h, 000h, 068h, 08Eh, 078h, 044h, 000h, 06Ah, 020h, 06Ah, 024h, 08Dh, 056h, 00Ch
+    db 052h, 0C6h, 044h, 024h, 020h, 000h, 0E8h, 07Bh, 0E8h, 076h, 000h, 08Bh, 04Ch, 024h, 008h, 0C7h
+    db 006h, 044h, 037h, 007h, 001h, 05Eh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h, 0C4h, 010h
+    db 0C3h
+??1BoneFXUpdateModuleData@@UAE@XZ ENDP
+_TEXT ENDS
+END

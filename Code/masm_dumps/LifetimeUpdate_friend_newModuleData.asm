57e4acf246cfcd4a8b06bf981f8a899d158bf97c
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-18 21:05:10 -0400

    Match LifetimeUpdate data factory

---
+++ Code/masm_dumps/LifetimeUpdate_friend_newModuleData.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+
+; ?friend_newModuleData@LifetimeUpdate@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 107 retail bytes @ 0x0051A350; allocates, constructs, and parses LifetimeUpdate module data.
+_TEXT SEGMENT
+public ?friend_newModuleData@LifetimeUpdate@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@LifetimeUpdate@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 06Ah,0FFh,068h,0EBh,0F5h,0FFh,000h,064h,0A1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,051h,056h,06Ah,018h,0E8h,0C2h,07Bh,076h,000h,083h,0C4h
+    db 004h,089h,044h,024h,004h,085h,0C0h,0C7h,044h,024h,010h,000h,000h,000h,000h,074h
+    db 00Bh,08Bh,0C8h,0E8h,049h,043h,0F1h,0FFh,08Bh,0F0h,0EBh,002h,033h,0F6h,08Bh,04Ch
+    db 024h,018h,085h,0C9h,0C7h,044h,024h,010h,0FFh,0FFh,0FFh,0FFh,074h,00Bh,068h,00Dh
+    db 049h,040h,000h,056h,0E8h,087h,07Dh,073h,000h,08Bh,04Ch,024h,008h,08Bh,0C6h,05Eh
+    db 064h,089h,00Dh,000h,000h,000h,000h,083h,0C4h,010h,0C3h
+?friend_newModuleData@LifetimeUpdate@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END

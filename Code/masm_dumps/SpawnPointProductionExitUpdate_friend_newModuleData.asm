6dee096f124f00edb63192b2b758add19e28c389
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 10:28:35 -0400

    Match SpawnPointProductionExitUpdate data factory

---
+++ Code/masm_dumps/SpawnPointProductionExitUpdate_friend_newModuleData.asm
@@ -0,0 +1,15 @@
+.386
+.model flat
+
+; ?friend_newModuleData@SpawnPointProductionExitUpdate@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 57 retail bytes @ 0x00126C70; registration ordinal 0x76 proves ownership.
+_TEXT SEGMENT
+public ?friend_newModuleData@SpawnPointProductionExitUpdate@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@SpawnPointProductionExitUpdate@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 056h, 06Ah, 00Ch, 0E8h, 0B8h, 0B2h, 075h, 000h, 083h, 0C4h, 004h, 085h, 0C0h, 074h, 011h, 0C7h
+    db 000h, 088h, 0E7h, 008h, 001h, 0C7h, 040h, 008h, 000h, 000h, 000h, 000h, 08Bh, 0F0h, 0EBh, 002h
+    db 033h, 0F6h, 08Bh, 04Ch, 024h, 008h, 085h, 0C9h, 074h, 00Bh, 068h, 079h, 029h, 044h, 000h, 056h
+    db 0E8h, 08Bh, 0B4h, 072h, 000h, 08Bh, 0C6h, 05Eh, 0C3h
+?friend_newModuleData@SpawnPointProductionExitUpdate@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END

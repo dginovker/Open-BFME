a72fec208ebbc705a9c40f72f219ade2c3013d78
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 15:53:59 -0400

    Match UpgradeSoundSelectorClientBehavior instance factory

---
+++ Code/masm_dumps/UpgradeSoundSelectorClientBehavior_friend_newModuleInstance.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+
+; ?friend_newModuleInstance@UpgradeSoundSelectorClientBehavior@@SAPAVModule@@PAVThing@@PBVModuleData@@@Z
+; Exact 104 retail bytes @ 0x00124B30; registration ordinal 0xF5 proves ownership.
+_TEXT SEGMENT
+public ?friend_newModuleInstance@UpgradeSoundSelectorClientBehavior@@SAPAVModule@@PAVThing@@PBVModuleData@@@Z
+?friend_newModuleInstance@UpgradeSoundSelectorClientBehavior@@SAPAVModule@@PAVThing@@PBVModuleData@@@Z PROC
+    db 06Ah, 0FFh, 068h, 06Bh, 016h, 000h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 056h, 06Ah, 010h, 0E8h, 0E2h, 0D3h, 075h, 000h, 08Bh, 0F0h
+    db 083h, 0C4h, 004h, 089h, 074h, 024h, 004h, 033h, 0C0h, 03Bh, 0F0h, 089h, 044h, 024h, 010h, 074h
+    db 027h, 08Bh, 044h, 024h, 01Ch, 08Bh, 04Ch, 024h, 018h, 050h, 051h, 08Bh, 0CEh, 0E8h, 002h, 0DDh
+    db 0EDh, 0FFh, 0C7h, 046h, 00Ch, 0F8h, 0ACh, 008h, 001h, 0C7h, 006h, 018h, 0ADh, 008h, 001h, 0C7h
+    db 046h, 00Ch, 008h, 0ADh, 008h, 001h, 08Bh, 0C6h, 08Bh, 04Ch, 024h, 008h, 05Eh, 064h, 089h, 00Dh
+    db 000h, 000h, 000h, 000h, 083h, 0C4h, 010h, 0C3h
+?friend_newModuleInstance@UpgradeSoundSelectorClientBehavior@@SAPAVModule@@PAVThing@@PBVModuleData@@@Z ENDP
+_TEXT ENDS
+END

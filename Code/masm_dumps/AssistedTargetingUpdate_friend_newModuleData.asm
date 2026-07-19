b3423183000005aa9f2db7107da993db1b5e77d8
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 03:20:27 -0400

    Correct AssistedTargetingUpdate data owner

---
+++ Code/masm_dumps/AssistedTargetingUpdate_friend_newModuleData.asm
@@ -0,0 +1,16 @@
+.386
+.model flat
+
+; ?friend_newModuleData@AssistedTargetingUpdate@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 68 retail bytes @ 0x001237B0; allocates and initializes AssistedTargetingUpdate module data.
+_TEXT SEGMENT
+public ?friend_newModuleData@AssistedTargetingUpdate@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@AssistedTargetingUpdate@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 056h, 06Ah, 018h, 0E8h, 078h, 0E7h, 075h, 000h, 033h, 0D2h, 083h, 0C4h, 004h, 03Bh, 0C2h, 074h
+    db 01Ah, 0C7h, 000h, 0E0h, 0B8h, 008h, 001h, 0C7h, 040h, 008h, 001h, 000h, 000h, 000h, 089h, 050h
+    db 00Ch, 089h, 050h, 010h, 089h, 050h, 014h, 08Bh, 0F0h, 0EBh, 002h, 033h, 0F6h, 08Bh, 04Ch, 024h
+    db 008h, 03Bh, 0CAh, 074h, 00Bh, 068h, 0C2h, 04Ch, 041h, 000h, 056h, 0E8h, 040h, 0E9h, 072h, 000h
+    db 08Bh, 0C6h, 05Eh, 0C3h
+?friend_newModuleData@AssistedTargetingUpdate@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END

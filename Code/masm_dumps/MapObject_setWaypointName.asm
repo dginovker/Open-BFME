c9f5d5aec219b1d12b6f7d81fdd2ff5bce777c48
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 17:23:18 -0400

    Match MapObject waypoint name setter

---
+++ Code/masm_dumps/MapObject_setWaypointName.asm
@@ -0,0 +1,13 @@
+.386
+.model flat
+
+; ?setWaypointName@MapObject@@QAEXVAsciiString@@@Z
+; Exact 31 retail bytes @ 0x00087E50; waypoint key and matched getter prove ownership.
+_TEXT SEGMENT
+public ?setWaypointName@MapObject@@QAEXVAsciiString@@@Z
+?setWaypointName@MapObject@@QAEXVAsciiString@@@Z PROC
+    db 08Bh, 044h, 024h, 004h, 056h, 08Bh, 0F1h, 050h, 0B9h, 0A8h, 077h, 02Ah, 001h, 0E8h, 0A2h, 014h
+    db 0F8h, 0FFh, 050h, 08Dh, 04Eh, 024h, 0E8h, 025h, 031h, 0FAh, 0FFh, 05Eh, 0C2h, 004h, 000h
+?setWaypointName@MapObject@@QAEXVAsciiString@@@Z ENDP
+_TEXT ENDS
+END

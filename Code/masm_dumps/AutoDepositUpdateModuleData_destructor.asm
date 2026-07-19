1a504ba12aba0340c226caf6c9fc3fd62bc22aad
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 19:21:51 -0400

    Match AutoDeposit module data destructor

---
+++ Code/masm_dumps/AutoDepositUpdateModuleData_destructor.asm
@@ -0,0 +1,16 @@
+.386
+.model flat
+
+; ??1AutoDepositUpdateModuleData@@UAE@XZ
+; Exact 67 retail bytes @ 0x00125960; factory/constructor/vtable ownership verified in Ghidra.
+_TEXT SEGMENT
+public ??1AutoDepositUpdateModuleData@@UAE@XZ
+??1AutoDepositUpdateModuleData@@UAE@XZ PROC
+    db 06Ah, 0FFh, 068h, 068h, 017h, 000h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 056h, 08Bh, 0F1h, 089h, 074h, 024h, 004h, 08Dh, 04Eh, 01Ch
+    db 0C7h, 044h, 024h, 010h, 000h, 000h, 000h, 000h, 0E8h, 074h, 04Ah, 0EFh, 0FFh, 08Bh, 04Ch, 024h
+    db 008h, 0C7h, 006h, 044h, 037h, 007h, 001h, 05Eh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h
+    db 0C4h, 010h, 0C3h
+??1AutoDepositUpdateModuleData@@UAE@XZ ENDP
+_TEXT ENDS
+END

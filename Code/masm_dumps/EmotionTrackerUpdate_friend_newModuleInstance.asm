95323a3b104c43ed1c73382bf990b1cdd97df570
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 11:44:35 -0400

    Match EmotionTrackerUpdate factory

---
+++ Code/masm_dumps/EmotionTrackerUpdate_friend_newModuleInstance.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+
+; ?friend_newModuleInstance@EmotionTrackerUpdate@@SAPAVModule@@PAVThing@@PBVModuleData@@@Z
+; Exact 99 retail bytes @ 0x0011CE60; registration ordinal 0x96 proves ownership.
+_TEXT SEGMENT
+public ?friend_newModuleInstance@EmotionTrackerUpdate@@SAPAVModule@@PAVThing@@PBVModuleData@@@Z
+?friend_newModuleInstance@EmotionTrackerUpdate@@SAPAVModule@@PAVThing@@PBVModuleData@@@Z PROC
+    db 06Ah, 0FFh, 068h, 08Bh, 0FFh, 0FFh, 000h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 068h, 0B8h, 000h, 000h, 000h, 0E8h, 0B0h, 050h, 076h, 000h
+    db 083h, 0C4h, 004h, 089h, 004h, 024h, 085h, 0C0h, 0C7h, 044h, 024h, 00Ch, 000h, 000h, 000h, 000h
+    db 074h, 020h, 08Bh, 04Ch, 024h, 018h, 08Bh, 054h, 024h, 014h, 051h, 052h, 08Bh, 0C8h, 0E8h, 06Fh
+    db 054h, 0F1h, 0FFh, 08Bh, 04Ch, 024h, 004h, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h, 0C4h
+    db 010h, 0C3h, 08Bh, 04Ch, 024h, 004h, 033h, 0C0h, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h
+    db 0C4h, 010h, 0C3h
+?friend_newModuleInstance@EmotionTrackerUpdate@@SAPAVModule@@PAVThing@@PBVModuleData@@@Z ENDP
+_TEXT ENDS
+END

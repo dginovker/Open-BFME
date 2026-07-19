45ce4b0495d5f8465332f37a8f0ef721cd7f73ed
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 19:20:54 -0400

    Match GameWindowManager window creation

---
+++ Code/masm_dumps/GameWindowManager_winCreate.asm
@@ -0,0 +1,28 @@
+.386
+.model flat
+
+; ?winCreate@GameWindowManager@@UAEPAVGameWindow@@PAV2@IHHHHP6A?AW4WindowMsgHandledType@@0III@ZPAVWinInstanceData@@@Z
+; Exact 269 retail bytes @ 0x006F5A90; caller and Times New Roman fallback verified in Ghidra.
+_TEXT SEGMENT
+public ?winCreate@GameWindowManager@@UAEPAVGameWindow@@PAV2@IHHHHP6A?AW4WindowMsgHandledType@@0III@ZPAVWinInstanceData@@@Z
+?winCreate@GameWindowManager@@UAEPAVGameWindow@@PAV2@IHHHHP6A?AW4WindowMsgHandledType@@0III@ZPAVWinInstanceData@@@Z PROC
+    db 06Ah, 0FFh, 068h, 033h, 0BBh, 004h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 055h, 056h, 057h, 068h, 010h, 002h, 000h, 000h, 08Bh, 0E9h
+    db 0E8h, 07Bh, 0C4h, 018h, 000h, 083h, 0C4h, 004h, 089h, 044h, 024h, 00Ch, 085h, 0C0h, 0C7h, 044h
+    db 024h, 018h, 000h, 000h, 000h, 000h, 074h, 00Bh, 08Bh, 0C8h, 0E8h, 04Eh, 0CEh, 091h, 0FFh, 08Bh
+    db 0F0h, 0EBh, 002h, 033h, 0F6h, 083h, 0CFh, 0FFh, 085h, 0F6h, 089h, 07Ch, 024h, 018h, 075h, 014h
+    db 05Fh, 05Eh, 033h, 0C0h, 05Dh, 08Bh, 04Ch, 024h, 004h, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h
+    db 083h, 0C4h, 010h, 0C3h, 0A1h, 084h, 014h, 02Fh, 001h, 085h, 0C0h, 074h, 040h, 08Bh, 090h, 0ACh
+    db 000h, 000h, 000h, 085h, 0D2h, 08Dh, 088h, 0ACh, 000h, 000h, 000h, 074h, 030h, 066h, 083h, 07Ah
+    db 004h, 000h, 074h, 029h, 0DBh, 080h, 0B0h, 000h, 000h, 000h, 08Bh, 03Eh, 033h, 0D2h, 08Ah, 090h
+    db 0B4h, 000h, 000h, 000h, 052h, 051h, 0D9h, 01Ch, 024h, 051h, 08Bh, 00Dh, 038h, 01Bh, 02Fh, 001h
+    db 0E8h, 08Eh, 050h, 091h, 0FFh, 050h, 08Bh, 0CEh, 0FFh, 057h, 018h, 0EBh, 044h, 053h, 068h, 0D4h
+    db 077h, 00Fh, 001h, 08Dh, 04Ch, 024h, 014h, 0E8h, 074h, 030h, 019h, 000h, 08Bh, 00Dh, 038h, 01Bh
+    db 02Fh, 001h, 08Bh, 01Eh, 06Ah, 000h, 068h, 000h, 000h, 040h, 041h, 08Dh, 044h, 024h, 018h, 050h
+    db 0C7h, 044h, 024h, 028h, 001h, 000h, 000h, 000h, 0E8h, 056h, 050h, 091h, 0FFh, 050h, 08Bh, 0CEh
+    db 0FFh, 053h, 018h, 08Dh, 04Ch, 024h, 010h, 089h, 07Ch, 024h, 01Ch, 0E8h, 0C0h, 01Dh, 019h, 000h
+    db 05Bh, 056h, 08Bh, 0CDh, 0E8h, 0A5h, 0C0h, 092h, 0FFh, 08Bh, 04Ch, 024h, 010h, 05Fh, 08Bh, 0C6h
+    db 05Eh, 05Dh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h, 0C4h, 010h, 0C3h
+?winCreate@GameWindowManager@@UAEPAVGameWindow@@PAV2@IHHHHP6A?AW4WindowMsgHandledType@@0III@ZPAVWinInstanceData@@@Z ENDP
+_TEXT ENDS
+END

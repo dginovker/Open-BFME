855ac11062c7080721e7c1faa8aa5a163464de82
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 01:40:56 -0400

    Match HordeSiegeEngine factory

---
+++ Code/masm_dumps/HordeSiegeEngineContain_friend_newModuleInstance.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+
+; ?friend_newModuleInstance@HordeSiegeEngineContain@@SAPAVModule@@PAVThing@@PBVModuleData@@@Z
+; Exact 99 retail bytes @ 0x005163F0; allocates and constructs a HordeSiegeEngineContain module instance.
+_TEXT SEGMENT
+public ?friend_newModuleInstance@HordeSiegeEngineContain@@SAPAVModule@@PAVThing@@PBVModuleData@@@Z
+?friend_newModuleInstance@HordeSiegeEngineContain@@SAPAVModule@@PAVThing@@PBVModuleData@@@Z PROC
+    db 06Ah,0FFh,068h,0CBh,0E8h,0FFh,000h,064h,0A1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,051h,068h,008h,001h,000h,000h,0E8h,020h,0BBh,076h,000h
+    db 083h,0C4h,004h,089h,004h,024h,085h,0C0h,0C7h,044h,024h,00Ch,000h,000h,000h,000h
+    db 074h,020h,08Bh,04Ch,024h,018h,08Bh,054h,024h,014h,051h,052h,08Bh,0C8h,0E8h,068h
+    db 06Eh,0F0h,0FFh,08Bh,04Ch,024h,004h,064h,089h,00Dh,000h,000h,000h,000h,083h,0C4h
+    db 010h,0C3h,08Bh,04Ch,024h,004h,033h,0C0h,064h,089h,00Dh,000h,000h,000h,000h,083h
+    db 0C4h,010h,0C3h
+?friend_newModuleInstance@HordeSiegeEngineContain@@SAPAVModule@@PAVThing@@PBVModuleData@@@Z ENDP
+_TEXT ENDS
+END

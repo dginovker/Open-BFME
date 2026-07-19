cae44e9f852e1d1c57a90f74e9b1641ae685c453
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 16:57:15 -0400

    Match HotKeyManager constructor

---
+++ Code/masm_dumps/HotKeyManager_ctor.asm
@@ -0,0 +1,21 @@
+.386
+.model flat
+
+; ??0HotKeyManager@@QAE@XZ
+; Exact 146 retail bytes @ 0x005B39E0; GameClient::init calls public thunk 0x0000B389.
+_TEXT SEGMENT
+public ??0HotKeyManager@@QAE@XZ
+??0HotKeyManager@@QAE@XZ PROC
+    db 06Ah, 0FFh, 068h, 073h, 090h, 003h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 053h, 056h, 08Bh, 0F1h, 089h, 074h, 024h, 008h, 0E8h, 02Dh
+    db 0E0h, 03Eh, 000h, 033h, 0DBh, 0C7h, 006h, 008h, 0DEh, 010h, 001h, 06Ah, 01Ch, 089h, 05Ch, 024h
+    db 018h, 089h, 05Eh, 008h, 0E8h, 027h, 0ABh, 027h, 000h, 089h, 046h, 008h, 089h, 05Eh, 00Ch, 088h
+    db 018h, 08Bh, 046h, 008h, 089h, 058h, 004h, 08Bh, 046h, 008h, 089h, 040h, 008h, 08Bh, 046h, 008h
+    db 089h, 040h, 00Ch, 06Ah, 01Ch, 0C6h, 044h, 024h, 01Ch, 001h, 089h, 05Eh, 014h, 0E8h, 0FEh, 0AAh
+    db 027h, 000h, 089h, 046h, 014h, 089h, 05Eh, 018h, 088h, 018h, 08Bh, 04Eh, 014h, 089h, 059h, 004h
+    db 08Bh, 046h, 014h, 08Bh, 04Ch, 024h, 014h, 089h, 040h, 008h, 08Bh, 046h, 014h, 083h, 0C4h, 008h
+    db 089h, 040h, 00Ch, 08Bh, 0C6h, 05Eh, 05Bh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h, 0C4h
+    db 010h, 0C3h
+??0HotKeyManager@@QAE@XZ ENDP
+_TEXT ENDS
+END

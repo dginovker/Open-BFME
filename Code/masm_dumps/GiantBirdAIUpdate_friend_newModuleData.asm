3b7f875946e3bc7bdbdeac0f8d9d55ff21b2ec6c
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 03:59:27 -0400

    Match GiantBirdAIUpdate data factory

---
+++ Code/masm_dumps/GiantBirdAIUpdate_friend_newModuleData.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+
+; ?friend_newModuleData@GiantBirdAIUpdate@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 110 retail bytes @ 0x00118990; allocates and initializes GiantBirdAIUpdate module data.
+_TEXT SEGMENT
+public ?friend_newModuleData@GiantBirdAIUpdate@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@GiantBirdAIUpdate@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 06Ah, 0FFh, 068h, 00Bh, 0F1h, 0FFh, 000h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 056h, 068h, 084h, 000h, 000h, 000h, 0E8h, 07Fh, 095h, 076h
+    db 000h, 083h, 0C4h, 004h, 089h, 044h, 024h, 004h, 085h, 0C0h, 0C7h, 044h, 024h, 010h, 000h, 000h
+    db 000h, 000h, 074h, 00Bh, 08Bh, 0C8h, 0E8h, 08Fh, 099h, 0EFh, 0FFh, 08Bh, 0F0h, 0EBh, 002h, 033h
+    db 0F6h, 08Bh, 04Ch, 024h, 018h, 085h, 0C9h, 0C7h, 044h, 024h, 010h, 0FFh, 0FFh, 0FFh, 0FFh, 074h
+    db 00Bh, 068h, 0EDh, 0A5h, 044h, 000h, 056h, 0E8h, 044h, 097h, 073h, 000h, 08Bh, 04Ch, 024h, 008h
+    db 08Bh, 0C6h, 05Eh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h, 0C4h, 010h, 0C3h
+?friend_newModuleData@GiantBirdAIUpdate@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END

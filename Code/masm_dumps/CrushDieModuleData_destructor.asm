19b6496183423802a209c9a0d6e981b3c4177128
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 19:07:46 -0400

    Match CrushDie module data destructor

---
+++ Code/masm_dumps/CrushDieModuleData_destructor.asm
@@ -0,0 +1,16 @@
+.386
+.model flat
+
+; ??1CrushDieModuleData@@UAE@XZ
+; Exact 77 retail bytes @ 0x001255C0; derived vtable dtor wrapper calls thunk 0x0002C089.
+_TEXT SEGMENT
+public ??1CrushDieModuleData@@UAE@XZ
+??1CrushDieModuleData@@UAE@XZ PROC
+    db 06Ah, 0FFh, 068h, 0E8h, 016h, 000h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 056h, 08Bh, 0F1h, 089h, 074h, 024h, 004h, 068h, 035h, 06Fh
+    db 042h, 000h, 06Ah, 004h, 06Ah, 070h, 08Dh, 046h, 034h, 050h, 0C7h, 044h, 024h, 020h, 000h, 000h
+    db 000h, 000h, 0E8h, 07Fh, 017h, 08Dh, 000h, 08Bh, 04Ch, 024h, 008h, 0C7h, 006h, 044h, 037h, 007h
+    db 001h, 05Eh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h, 0C4h, 010h, 0C3h
+??1CrushDieModuleData@@UAE@XZ ENDP
+_TEXT ENDS
+END

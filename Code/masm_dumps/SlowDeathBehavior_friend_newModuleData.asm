455a49613a8704f7915f4d1dc729b590f420cb5d
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 00:31:34 -0400

    Correct death and spawn factory owners

---
+++ Code/masm_dumps/SlowDeathBehavior_friend_newModuleData.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+
+; ?friend_newModuleData@SlowDeathBehavior@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 110 retail bytes @ 0x005154F0; allocates, constructs, and parses SlowDeathBehavior module data.
+_TEXT SEGMENT
+public ?friend_newModuleData@SlowDeathBehavior@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@SlowDeathBehavior@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 06Ah,0FFh,068h,06Bh,0E5h,0FFh,000h,064h,0A1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,051h,056h,068h,0A8h,001h,000h,000h,0E8h,01Fh,0CAh,076h
+    db 000h,083h,0C4h,004h,089h,044h,024h,004h,085h,0C0h,0C7h,044h,024h,010h,000h,000h
+    db 000h,000h,074h,00Bh,08Bh,0C8h,0E8h,0AFh,021h,0EFh,0FFh,08Bh,0F0h,0EBh,002h,033h
+    db 0F6h,08Bh,04Ch,024h,018h,085h,0C9h,0C7h,044h,024h,010h,0FFh,0FFh,0FFh,0FFh,074h
+    db 00Bh,068h,086h,0BDh,043h,000h,056h,0E8h,0E4h,0CBh,073h,000h,08Bh,04Ch,024h,008h
+    db 08Bh,0C6h,05Eh,064h,089h,00Dh,000h,000h,000h,000h,083h,0C4h,010h,0C3h
+?friend_newModuleData@SlowDeathBehavior@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END
--- Code/masm_dumps/SpawnBehavior_friend_newModuleData.asm
+++ Code/masm_dumps/SpawnBehavior_friend_newModuleData.asm
@@ -2,16 +2,16 @@
 .model flat
 
 ; ?friend_newModuleData@SpawnBehavior@@SAPAVModuleData@@PAVINI@@@Z
-; Exact 110 retail bytes @ 0x005154F0; allocates, constructs, and parses SpawnBehavior module data.
+; Exact 110 retail bytes @ 0x0052AAC0; allocates, constructs, and parses SpawnBehavior module data.
 _TEXT SEGMENT
 public ?friend_newModuleData@SpawnBehavior@@SAPAVModuleData@@PAVINI@@@Z
 ?friend_newModuleData@SpawnBehavior@@SAPAVModuleData@@PAVINI@@@Z PROC
-    db 06Ah,0FFh,068h,06Bh,0E5h,0FFh,000h,064h,0A1h,000h,000h,000h,000h,050h,064h,089h
-    db 025h,000h,000h,000h,000h,051h,056h,068h,0A8h,001h,000h,000h,0E8h,01Fh,0CAh,076h
+    db 06Ah,0FFh,068h,0DBh,022h,000h,001h,064h,0A1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,051h,056h,068h,0C0h,000h,000h,000h,0E8h,04Fh,074h,075h
     db 000h,083h,0C4h,004h,089h,044h,024h,004h,085h,0C0h,0C7h,044h,024h,010h,000h,000h
-    db 000h,000h,074h,00Bh,08Bh,0C8h,0E8h,0AFh,021h,0EFh,0FFh,08Bh,0F0h,0EBh,002h,033h
+    db 000h,000h,074h,00Bh,08Bh,0C8h,0E8h,0ABh,052h,0F0h,0FFh,08Bh,0F0h,0EBh,002h,033h
     db 0F6h,08Bh,04Ch,024h,018h,085h,0C9h,0C7h,044h,024h,010h,0FFh,0FFh,0FFh,0FFh,074h
-    db 00Bh,068h,086h,0BDh,043h,000h,056h,0E8h,0E4h,0CBh,073h,000h,08Bh,04Ch,024h,008h
+    db 00Bh,068h,060h,0AFh,040h,000h,056h,0E8h,014h,076h,072h,000h,08Bh,04Ch,024h,008h
     db 08Bh,0C6h,05Eh,064h,089h,00Dh,000h,000h,000h,000h,083h,0C4h,010h,0C3h
 ?friend_newModuleData@SpawnBehavior@@SAPAVModuleData@@PAVINI@@@Z ENDP
 _TEXT ENDS

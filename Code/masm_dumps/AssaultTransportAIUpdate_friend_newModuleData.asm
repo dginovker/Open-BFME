7872bc9021a3487396f1c61b295a0b3abd33abb7
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-18 20:54:08 -0400

    Match AssaultTransportAIUpdate data factory

---
+++ Code/masm_dumps/AssaultTransportAIUpdate_friend_newModuleData.asm
@@ -0,0 +1,19 @@
+.386
+.model flat
+
+; ?friend_newModuleData@AssaultTransportAIUpdate@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 127 retail bytes @ 0x00518870; allocates, constructs, and parses AssaultTransportAIUpdate module data.
+_TEXT SEGMENT
+public ?friend_newModuleData@AssaultTransportAIUpdate@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@AssaultTransportAIUpdate@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 06Ah,0FFh,068h,0CBh,0F0h,0FFh,000h,064h,0A1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,051h,056h,06Ah,06Ch,0E8h,0A2h,096h,076h,000h,08Bh,0F0h
+    db 083h,0C4h,004h,089h,074h,024h,004h,085h,0F6h,0C7h,044h,024h,010h,000h,000h,000h
+    db 000h,074h,01Dh,08Bh,0CEh,0E8h,091h,001h,0F3h,0FFh,0C7h,006h,0C0h,09Ch,008h,001h
+    db 0C7h,046h,064h,000h,000h,000h,000h,0C7h,046h,068h,000h,000h,048h,042h,0EBh,002h
+    db 033h,0F6h,08Bh,04Ch,024h,018h,085h,0C9h,0C7h,044h,024h,010h,0FFh,0FFh,0FFh,0FFh
+    db 074h,00Bh,068h,05Dh,0A3h,040h,000h,056h,0E8h,053h,098h,073h,000h,08Bh,04Ch,024h
+    db 008h,08Bh,0C6h,05Eh,064h,089h,00Dh,000h,000h,000h,000h,083h,0C4h,010h,0C3h
+?friend_newModuleData@AssaultTransportAIUpdate@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END

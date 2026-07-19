111b9cd5445f07a8d2419a014b1a6bfc95436f01
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 12:09:54 -0400

    Match DelayedUpgrade data factory

---
+++ Code/masm_dumps/DelayedUpgrade_friend_newModuleData.asm
@@ -0,0 +1,16 @@
+.386
+.model flat
+
+; ?friend_newModuleData@DelayedUpgrade@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 65 retail bytes @ 0x0012B200; registration ordinal 0x9B proves ownership.
+_TEXT SEGMENT
+public ?friend_newModuleData@DelayedUpgrade@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@DelayedUpgrade@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 056h, 06Ah, 074h, 0E8h, 028h, 06Dh, 075h, 000h, 08Bh, 0F0h, 083h, 0C4h, 004h, 085h, 0F6h, 074h
+    db 017h, 08Dh, 04Eh, 008h, 0E8h, 08Dh, 03Eh, 0EEh, 0FFh, 0C7h, 006h, 090h, 0F3h, 008h, 001h, 0C7h
+    db 046h, 070h, 000h, 000h, 000h, 000h, 0EBh, 002h, 033h, 0F6h, 08Bh, 04Ch, 024h, 008h, 085h, 0C9h
+    db 074h, 00Bh, 068h, 078h, 072h, 042h, 000h, 056h, 0E8h, 0F3h, 06Eh, 072h, 000h, 08Bh, 0C6h, 05Eh
+    db 0C3h
+?friend_newModuleData@DelayedUpgrade@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END

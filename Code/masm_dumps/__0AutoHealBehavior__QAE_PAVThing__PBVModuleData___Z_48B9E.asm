ac03cf0e28e4ca1ee5bbb5b807dcf5fbfcc4f3a8
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 09:22:26 -0400

    Match AutoHealBehavior constructor thunk

---
+++ Code/masm_dumps/__0AutoHealBehavior__QAE_PAVThing__PBVModuleData___Z_48B9E.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ??0AutoHealBehavior@@QAE@PAVThing@@PBVModuleData@@@Z
+; Retail constructor thunk @ 0x00448B9E, uniquely called by the AutoHealBehavior factory.
+_TEXT SEGMENT
+public ??0AutoHealBehavior@@QAE@PAVThing@@PBVModuleData@@@Z
+??0AutoHealBehavior@@QAE@PAVThing@@PBVModuleData@@@Z PROC
+    db 0E9h, 0ADh, 05Dh, 01Ah, 000h
+??0AutoHealBehavior@@QAE@PAVThing@@PBVModuleData@@@Z ENDP
+_TEXT ENDS
+END

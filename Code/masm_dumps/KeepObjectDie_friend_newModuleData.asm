f99dc62ebce1ab4adc58b9c5073af46e26879fd9
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 03:12:54 -0400

    Match KeepObjectDie data factory

---
+++ Code/masm_dumps/KeepObjectDie_friend_newModuleData.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+
+; ?friend_newModuleData@KeepObjectDie@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 107 retail bytes @ 0x00125340; shared retail body used by KeepObjectDie module data.
+_TEXT SEGMENT
+public ?friend_newModuleData@KeepObjectDie@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@KeepObjectDie@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 06Ah, 0FFh, 068h, 08Bh, 016h, 000h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 056h, 06Ah, 034h, 0E8h, 0D2h, 0CBh, 075h, 000h, 083h, 0C4h
+    db 004h, 089h, 044h, 024h, 004h, 085h, 0C0h, 0C7h, 044h, 024h, 010h, 000h, 000h, 000h, 000h, 074h
+    db 00Bh, 08Bh, 0C8h, 0E8h, 080h, 06Ch, 0F0h, 0FFh, 08Bh, 0F0h, 0EBh, 002h, 033h, 0F6h, 08Bh, 04Ch
+    db 024h, 018h, 085h, 0C9h, 0C7h, 044h, 024h, 010h, 0FFh, 0FFh, 0FFh, 0FFh, 074h, 00Bh, 068h, 0DEh
+    db 053h, 041h, 000h, 056h, 0E8h, 097h, 0CDh, 072h, 000h, 08Bh, 04Ch, 024h, 008h, 08Bh, 0C6h, 05Eh
+    db 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h, 0C4h, 010h, 0C3h
+?friend_newModuleData@KeepObjectDie@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END

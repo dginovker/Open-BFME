2add1f1524d3244d4c7db208c736812d49a5b0d2
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 02:55:39 -0400

    Correct CreateObjectDie data owner

---
+++ Code/masm_dumps/CreateObjectDie_friend_newModuleData.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+
+; ?friend_newModuleData@CreateObjectDie@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 107 retail bytes @ 0x00117860; allocates and initializes CreateObjectDie module data.
+_TEXT SEGMENT
+public ?friend_newModuleData@CreateObjectDie@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@CreateObjectDie@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 06Ah, 0FFh, 068h, 04Bh, 0EDh, 0FFh, 000h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 056h, 06Ah, 03Ch, 0E8h, 0B2h, 0A6h, 076h, 000h, 083h, 0C4h
+    db 004h, 089h, 044h, 024h, 004h, 085h, 0C0h, 0C7h, 044h, 024h, 010h, 000h, 000h, 000h, 000h, 074h
+    db 00Bh, 08Bh, 0C8h, 0E8h, 013h, 03Eh, 0F2h, 0FFh, 08Bh, 0F0h, 0EBh, 002h, 033h, 0F6h, 08Bh, 04Ch
+    db 024h, 018h, 085h, 0C9h, 0C7h, 044h, 024h, 010h, 0FFh, 0FFh, 0FFh, 0FFh, 074h, 00Bh, 068h, 05Eh
+    db 066h, 040h, 000h, 056h, 0E8h, 077h, 0A8h, 073h, 000h, 08Bh, 04Ch, 024h, 008h, 08Bh, 0C6h, 05Eh
+    db 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h, 0C4h, 010h, 0C3h
+?friend_newModuleData@CreateObjectDie@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END

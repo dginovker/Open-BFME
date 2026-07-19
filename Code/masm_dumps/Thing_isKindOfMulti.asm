b1945c7486f351b64abb78e4853013658d3894a2
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 19:06:42 -0400

    Match Thing multi-kind predicate

---
+++ Code/masm_dumps/Thing_isKindOfMulti.asm
@@ -0,0 +1,14 @@
+.386
+.model flat
+
+; ?isKindOfMulti@Thing@@QBE_NABV?$BitFlags@$0HE@@@0@Z
+; Exact 41 retail bytes @ 0x00132B20; callers use thunk 0x0003DCCB.
+_TEXT SEGMENT
+public ?isKindOfMulti@Thing@@QBE_NABV?$BitFlags@$0HE@@@0@Z
+?isKindOfMulti@Thing@@QBE_NABV?$BitFlags@$0HE@@@0@Z PROC
+    db 08Bh, 041h, 004h, 085h, 0C0h, 075h, 00Bh, 08Dh, 088h, 0C8h, 000h, 000h, 000h, 0E9h, 09Bh, 070h
+    db 0EEh, 0FFh, 08Bh, 048h, 004h, 085h, 0C9h, 074h, 005h, 0E8h, 07Dh, 0F7h, 0ECh, 0FFh, 08Dh, 088h
+    db 0C8h, 000h, 000h, 000h, 0E9h, 084h, 070h, 0EEh, 0FFh
+?isKindOfMulti@Thing@@QBE_NABV?$BitFlags@$0HE@@@0@Z ENDP
+_TEXT ENDS
+END

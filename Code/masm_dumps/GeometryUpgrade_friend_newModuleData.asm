2eea26ab45b7c2c32e17205985e92d9ae8567477
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 12:23:12 -0400

    Match GeometryUpgrade data factory

---
+++ Code/masm_dumps/GeometryUpgrade_friend_newModuleData.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+
+; ?friend_newModuleData@GeometryUpgrade@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 110 retail bytes @ 0x0011DFA0; registration ordinal 0xAC proves ownership.
+_TEXT SEGMENT
+public ?friend_newModuleData@GeometryUpgrade@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@GeometryUpgrade@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 06Ah, 0FFh, 068h, 0ABh, 003h, 000h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 056h, 068h, 094h, 000h, 000h, 000h, 0E8h, 06Fh, 03Fh, 076h
+    db 000h, 083h, 0C4h, 004h, 089h, 044h, 024h, 004h, 085h, 0C0h, 0C7h, 044h, 024h, 010h, 000h, 000h
+    db 000h, 000h, 074h, 00Bh, 08Bh, 0C8h, 0E8h, 071h, 0D1h, 0F1h, 0FFh, 08Bh, 0F0h, 0EBh, 002h, 033h
+    db 0F6h, 08Bh, 04Ch, 024h, 018h, 085h, 0C9h, 0C7h, 044h, 024h, 010h, 0FFh, 0FFh, 0FFh, 0FFh, 074h
+    db 00Bh, 068h, 0C3h, 0A4h, 042h, 000h, 056h, 0E8h, 034h, 041h, 073h, 000h, 08Bh, 04Ch, 024h, 008h, 08Bh
+    db 0C6h, 05Eh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h, 0C4h, 010h, 0C3h
+?friend_newModuleData@GeometryUpgrade@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END

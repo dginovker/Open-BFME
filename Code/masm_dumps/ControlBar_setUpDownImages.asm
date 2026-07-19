fb467a49f987b1bae2183f3d68c491faf5761f36
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 19:52:26 -0400

    Match ControlBar up-down images

---
+++ Code/masm_dumps/ControlBar_setUpDownImages.asm
@@ -0,0 +1,28 @@
+.386
+.model flat
+
+; ?setUpDownImages@ControlBar@@IAEXXZ
+; Exact 259 retail bytes @ 0x0049D160; source string and three caller paths prove ownership.
+_TEXT SEGMENT
+public ?setUpDownImages@ControlBar@@IAEXXZ
+?setUpDownImages@ControlBar@@IAEXXZ PROC
+    db 0A1h, 040h, 01Bh, 02Fh, 001h, 056h, 08Bh, 030h, 057h, 08Bh, 0F9h, 08Bh, 00Dh, 000h, 0D6h, 02Eh
+    db 001h, 068h, 068h, 0B8h, 00Fh, 001h, 0E8h, 05Ch, 0DCh, 0B9h, 0FFh, 08Bh, 00Dh, 040h, 01Bh, 02Fh
+    db 001h, 050h, 06Ah, 000h, 0FFh, 096h, 0DCh, 000h, 000h, 000h, 08Bh, 0F0h, 085h, 0F6h, 00Fh, 084h
+    db 0CCh, 000h, 000h, 000h, 083h, 07Fh, 020h, 002h, 075h, 054h, 08Bh, 08Fh, 0A4h, 002h, 000h, 000h
+    db 051h, 06Ah, 000h, 08Bh, 0CEh, 0E8h, 05Fh, 08Ch, 0B9h, 0FFh, 06Ah, 000h, 06Ah, 005h, 08Bh, 0CEh
+    db 0E8h, 054h, 08Ch, 0B9h, 0FFh, 06Ah, 000h, 06Ah, 006h, 08Bh, 0CEh, 0E8h, 049h, 08Ch, 0B9h, 0FFh
+    db 08Bh, 097h, 0A0h, 002h, 000h, 000h, 052h, 06Ah, 000h, 08Bh, 0CEh, 0E8h, 036h, 0E2h, 0B6h, 0FFh
+    db 06Ah, 000h, 06Ah, 005h, 08Bh, 0CEh, 0E8h, 02Eh, 08Ch, 0B9h, 0FFh, 06Ah, 000h, 06Ah, 006h, 08Bh
+    db 0CEh, 0E8h, 023h, 08Ch, 0B9h, 0FFh, 08Bh, 087h, 0A8h, 002h, 000h, 000h, 0EBh, 052h, 08Bh, 08Fh
+    db 0B0h, 002h, 000h, 000h, 051h, 06Ah, 000h, 08Bh, 0CEh, 0E8h, 00Bh, 08Ch, 0B9h, 0FFh, 06Ah, 000h
+    db 06Ah, 005h, 08Bh, 0CEh, 0E8h, 000h, 08Ch, 0B9h, 0FFh, 06Ah, 000h, 06Ah, 006h, 08Bh, 0CEh, 0E8h
+    db 0F5h, 08Bh, 0B9h, 0FFh, 08Bh, 097h, 0ACh, 002h, 000h, 000h, 052h, 06Ah, 000h, 08Bh, 0CEh, 0E8h
+    db 0E2h, 0E1h, 0B6h, 0FFh, 06Ah, 000h, 06Ah, 005h, 08Bh, 0CEh, 0E8h, 0DAh, 08Bh, 0B9h, 0FFh, 06Ah
+    db 000h, 06Ah, 006h, 08Bh, 0CEh, 0E8h, 0CFh, 08Bh, 0B9h, 0FFh, 08Bh, 087h, 0B4h, 002h, 000h, 000h
+    db 050h, 08Bh, 0CEh, 06Ah, 001h, 0E8h, 0BCh, 0E1h, 0B6h, 0FFh, 06Ah, 000h, 08Bh, 0CEh, 06Ah, 002h
+    db 0E8h, 0B4h, 08Bh, 0B9h, 0FFh, 06Ah, 000h, 08Bh, 0CEh, 06Ah, 003h, 0E8h, 0A9h, 08Bh, 0B9h, 0FFh
+    db 05Fh, 05Eh, 0C3h
+?setUpDownImages@ControlBar@@IAEXXZ ENDP
+_TEXT ENDS
+END

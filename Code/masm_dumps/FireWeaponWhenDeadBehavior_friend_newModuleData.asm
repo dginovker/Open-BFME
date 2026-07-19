178a8745646bd9cf452774a41613532304035409
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 02:09:01 -0400

    Correct

---
+++ Code/masm_dumps/FireWeaponWhenDeadBehavior_friend_newModuleData.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+
+; ?friend_newModuleData@FireWeaponWhenDeadBehavior@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 110 retail bytes @ 0x0012A250; allocates and initializes FireWeaponWhenDeadBehavior module data.
+_TEXT SEGMENT
+public ?friend_newModuleData@FireWeaponWhenDeadBehavior@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@FireWeaponWhenDeadBehavior@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 06Ah, 0FFh, 068h, 0DBh, 020h, 000h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 056h, 068h, 0B4h, 000h, 000h, 000h, 0E8h, 0BFh, 07Ch, 075h
+    db 000h, 083h, 0C4h, 004h, 089h, 044h, 024h, 004h, 085h, 0C0h, 0C7h, 044h, 024h, 010h, 000h, 000h
+    db 000h, 000h, 074h, 00Bh, 08Bh, 0C8h, 0E8h, 0D8h, 01Dh, 0EEh, 0FFh, 08Bh, 0F0h, 0EBh, 002h, 033h
+    db 0F6h, 08Bh, 04Ch, 024h, 018h, 085h, 0C9h, 0C7h, 044h, 024h, 010h, 0FFh, 0FFh, 0FFh, 0FFh, 074h
+    db 00Bh, 068h, 0B0h, 0F3h, 042h, 000h, 056h, 0E8h, 084h, 07Eh, 072h, 000h, 08Bh, 04Ch, 024h, 008h
+    db 08Bh, 0C6h, 05Eh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h, 0C4h, 010h, 0C3h
+?friend_newModuleData@FireWeaponWhenDeadBehavior@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END

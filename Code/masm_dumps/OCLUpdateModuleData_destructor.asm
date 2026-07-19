47fc283bcad03b73e05f5b48e3c27a44977d84df
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 19:22:49 -0400

    Match OCL module data destructor

---
+++ Code/masm_dumps/OCLUpdateModuleData_destructor.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ??1OCLUpdateModuleData@@UAE@XZ
+; Exact 7 retail bytes @ 0x00298B70; factory/constructor/vtable ownership verified in Ghidra.
+_TEXT SEGMENT
+public ??1OCLUpdateModuleData@@UAE@XZ
+??1OCLUpdateModuleData@@UAE@XZ PROC
+    db 0C7h, 001h, 044h, 037h, 007h, 001h, 0C3h
+??1OCLUpdateModuleData@@UAE@XZ ENDP
+_TEXT ENDS
+END

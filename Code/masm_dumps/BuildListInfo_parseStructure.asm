4c73b7d712f9c313b2a4a69f7424a091bc7ef720
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 18:42:51 -0400

    Match BuildList structure parser

---
+++ Code/masm_dumps/BuildListInfo_parseStructure.asm
@@ -0,0 +1,24 @@
+.386
+.model flat
+
+; ?parseStructure@BuildListInfo@@SAXPAVINI@@PAX1PBX@Z
+; Exact 204 retail bytes @ 0x001943D0; the "Structure" parser table calls thunk 0x00018525.
+_TEXT SEGMENT
+public ?parseStructure@BuildListInfo@@SAXPAVINI@@PAX1PBX@Z
+?parseStructure@BuildListInfo@@SAXPAVINI@@PAX1PBX@Z PROC
+    db 06Ah, 0FFh, 068h, 07Bh, 06Fh, 000h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 083h, 0ECh, 008h, 056h, 057h, 08Bh, 07Ch, 024h, 020h, 06Ah, 000h
+    db 08Bh, 0CFh, 0E8h, 079h, 0C5h, 06Bh, 000h, 050h, 08Dh, 04Ch, 024h, 010h, 0E8h, 0BFh, 047h, 06Fh
+    db 000h, 068h, 08Ch, 000h, 000h, 000h, 0C7h, 044h, 024h, 01Ch, 000h, 000h, 000h, 000h, 0E8h, 01Dh
+    db 0DBh, 06Eh, 000h, 083h, 0C4h, 004h, 089h, 044h, 024h, 008h, 085h, 0C0h, 0C6h, 044h, 024h, 018h
+    db 001h, 074h, 00Bh, 08Bh, 0C8h, 0E8h, 058h, 085h, 0E7h, 0FFh, 08Bh, 0F0h, 0EBh, 002h, 033h, 0F6h
+    db 08Dh, 044h, 024h, 00Ch, 050h, 08Dh, 04Ch, 024h, 00Ch, 0C6h, 044h, 024h, 01Ch, 000h, 0E8h, 01Dh
+    db 037h, 06Fh, 000h, 08Dh, 054h, 024h, 008h, 08Dh, 04Eh, 008h, 052h, 0C6h, 044h, 024h, 01Ch, 002h
+    db 0E8h, 03Bh, 038h, 06Fh, 000h, 08Dh, 04Ch, 024h, 008h, 0C6h, 044h, 024h, 018h, 000h, 0E8h, 0DDh
+    db 034h, 06Fh, 000h, 068h, 0A0h, 0C0h, 009h, 001h, 056h, 08Bh, 0CFh, 0E8h, 030h, 0DCh, 06Bh, 000h
+    db 08Bh, 04Ch, 024h, 024h, 056h, 0E8h, 076h, 064h, 0EAh, 0FFh, 08Dh, 04Ch, 024h, 00Ch, 0C7h, 044h
+    db 024h, 018h, 0FFh, 0FFh, 0FFh, 0FFh, 0E8h, 0B5h, 034h, 06Fh, 000h, 08Bh, 04Ch, 024h, 010h, 05Fh
+    db 05Eh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h, 0C4h, 014h, 0C3h
+?parseStructure@BuildListInfo@@SAXPAVINI@@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

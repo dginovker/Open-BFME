b93333332c9e5301c2aeb98ad1c5d66041cb87e4
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 15:34:48 -0400

    Match SiegeDeployHordeSpecialPower data factory

---
+++ Code/masm_dumps/SiegeDeployHordeSpecialPower_friend_newModuleData.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+
+; ?friend_newModuleData@SiegeDeployHordeSpecialPower@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 110 retail bytes @ 0x00120DF0; registration ordinal 0xE5 proves ownership.
+_TEXT SEGMENT
+public ?friend_newModuleData@SiegeDeployHordeSpecialPower@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@SiegeDeployHordeSpecialPower@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 06Ah, 0FFh, 068h, 06Bh, 00Eh, 000h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 056h, 068h, 0D4h, 001h, 000h, 000h, 0E8h, 01Fh, 011h, 076h
+    db 000h, 083h, 0C4h, 004h, 089h, 044h, 024h, 004h, 085h, 0C0h, 0C7h, 044h, 024h, 010h, 000h, 000h
+    db 000h, 000h, 074h, 00Bh, 08Bh, 0C8h, 0E8h, 0A5h, 000h, 0F2h, 0FFh, 08Bh, 0F0h, 0EBh, 002h, 033h
+    db 0F6h, 08Bh, 04Ch, 024h, 018h, 085h, 0C9h, 0C7h, 044h, 024h, 010h, 0FFh, 0FFh, 0FFh, 0FFh, 074h
+    db 00Bh, 068h, 08Ch, 0DDh, 040h, 000h, 056h, 0E8h, 0E4h, 012h, 073h, 000h, 08Bh, 04Ch, 024h, 008h
+    db 08Bh, 0C6h, 05Eh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h, 0C4h, 010h, 0C3h
+?friend_newModuleData@SiegeDeployHordeSpecialPower@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END

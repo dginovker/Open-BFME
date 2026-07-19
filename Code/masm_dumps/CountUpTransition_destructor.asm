f3b6e74aa763827ed03cafb84008b6fe02af7321
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 19:19:54 -0400

    Match CountUpTransition destructor

---
+++ Code/masm_dumps/CountUpTransition_destructor.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+
+; ??1CountUpTransition@@UAE@XZ
+; Exact 102 retail bytes @ 0x0059BD60; constructor/vtable ownership verified in Ghidra.
+_TEXT SEGMENT
+public ??1CountUpTransition@@UAE@XZ
+??1CountUpTransition@@UAE@XZ PROC
+    db 06Ah, 0FFh, 068h, 0D3h, 081h, 003h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 056h, 08Bh, 0F1h, 089h, 074h, 024h, 004h, 0C7h, 006h, 088h
+    db 0C6h, 010h, 001h, 08Dh, 04Eh, 030h, 0C7h, 044h, 024h, 010h, 001h, 000h, 000h, 000h, 0C7h, 046h
+    db 00Ch, 000h, 000h, 000h, 000h, 0E8h, 036h, 0C4h, 02Eh, 000h, 08Dh, 04Eh, 02Ch, 0C6h, 044h, 024h
+    db 010h, 000h, 0E8h, 029h, 0C4h, 02Eh, 000h, 08Bh, 0CEh, 0C7h, 044h, 024h, 010h, 0FFh, 0FFh, 0FFh
+    db 0FFh, 0E8h, 0E5h, 0ECh, 0A7h, 0FFh, 08Bh, 04Ch, 024h, 008h, 05Eh, 064h, 089h, 00Dh, 000h, 000h
+    db 000h, 000h, 083h, 0C4h, 010h, 0C3h
+??1CountUpTransition@@UAE@XZ ENDP
+_TEXT ENDS
+END

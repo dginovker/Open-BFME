9383a97bfeee1d1c63d68cfb03bdfdd82999a2d2
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 16:40:26 -0400

    Correct SpawnUnitBehavior data factory owner

--- Code/masm_dumps/SpawnUnitBehavior_friend_newModuleData.asm
+++ Code/masm_dumps/SpawnUnitBehavior_friend_newModuleData.asm
@@ -2,17 +2,17 @@
 .model flat
 
 ; ?friend_newModuleData@SpawnUnitBehavior@@SAPAVModuleData@@PAVINI@@@Z
-; Exact 110 retail bytes @ 0x005159E0; allocates, constructs, and parses SpawnUnitBehavior module data.
+; Exact 107 retail bytes @ 0x00125D70; registration ordinal 0x19 proves ownership.
 _TEXT SEGMENT
 public ?friend_newModuleData@SpawnUnitBehavior@@SAPAVModuleData@@PAVINI@@@Z
 ?friend_newModuleData@SpawnUnitBehavior@@SAPAVModuleData@@PAVINI@@@Z PROC
-    db 06Ah,0FFh,068h,08Bh,0E6h,0FFh,000h,064h,0A1h,000h,000h,000h,000h,050h,064h,089h
-    db 025h,000h,000h,000h,000h,051h,056h,068h,068h,001h,000h,000h,0E8h,02Fh,0C5h,076h
-    db 000h,083h,0C4h,004h,089h,044h,024h,004h,085h,0C0h,0C7h,044h,024h,010h,000h,000h
-    db 000h,000h,074h,00Bh,08Bh,0C8h,0E8h,015h,0E6h,0F2h,0FFh,08Bh,0F0h,0EBh,002h,033h
-    db 0F6h,08Bh,04Ch,024h,018h,085h,0C9h,0C7h,044h,024h,010h,0FFh,0FFh,0FFh,0FFh,074h
-    db 00Bh,068h,072h,097h,041h,000h,056h,0E8h,0F4h,0C6h,073h,000h,08Bh,04Ch,024h,008h
-    db 08Bh,0C6h,05Eh,064h,089h,00Dh,000h,000h,000h,000h,083h,0C4h,010h,0C3h
+    db 06Ah, 0FFh, 068h, 01Bh, 018h, 000h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 056h, 06Ah, 020h, 0E8h, 0A2h, 0C1h, 075h, 000h, 083h, 0C4h
+    db 004h, 089h, 044h, 024h, 004h, 085h, 0C0h, 0C7h, 044h, 024h, 010h, 000h, 000h, 000h, 000h, 074h
+    db 00Bh, 08Bh, 0C8h, 0E8h, 0ADh, 091h, 0F0h, 0FFh, 08Bh, 0F0h, 0EBh, 002h, 033h, 0F6h, 08Bh, 04Ch
+    db 024h, 018h, 085h, 0C9h, 0C7h, 044h, 024h, 010h, 0FFh, 0FFh, 0FFh, 0FFh, 074h, 00Bh, 068h, 027h
+    db 0A7h, 040h, 000h, 056h, 0E8h, 067h, 0C3h, 072h, 000h, 08Bh, 04Ch, 024h, 008h, 08Bh, 0C6h, 05Eh
+    db 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h, 0C4h, 010h, 0C3h
 ?friend_newModuleData@SpawnUnitBehavior@@SAPAVModuleData@@PAVINI@@@Z ENDP
 _TEXT ENDS
 END

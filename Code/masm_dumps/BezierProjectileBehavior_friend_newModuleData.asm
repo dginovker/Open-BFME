237f047cce7b982a8d5462e8f6a89be4540e1f2a
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 00:25:11 -0400

    Correct behavior data-factory owners

--- Code/masm_dumps/BezierProjectileBehavior_friend_newModuleData.asm
+++ Code/masm_dumps/BezierProjectileBehavior_friend_newModuleData.asm
@@ -2,17 +2,17 @@
 .model flat
 
 ; ?friend_newModuleData@BezierProjectileBehavior@@SAPAVModuleData@@PAVINI@@@Z
-; Exact 107 retail bytes @ 0x005151B0; allocates, constructs, and parses BezierProjectileBehavior module data.
+; Exact 110 retail bytes @ 0x005152C0; allocates, constructs, and parses BezierProjectileBehavior module data.
 _TEXT SEGMENT
 public ?friend_newModuleData@BezierProjectileBehavior@@SAPAVModuleData@@PAVINI@@@Z
 ?friend_newModuleData@BezierProjectileBehavior@@SAPAVModuleData@@PAVINI@@@Z PROC
-    db 06Ah,0FFh,068h,0ABh,0E4h,0FFh,000h,064h,0A1h,000h,000h,000h,000h,050h,064h,089h
-    db 025h,000h,000h,000h,000h,051h,056h,06Ah,018h,0E8h,062h,0CDh,076h,000h,083h,0C4h
-    db 004h,089h,044h,024h,004h,085h,0C0h,0C7h,044h,024h,010h,000h,000h,000h,000h,074h
-    db 00Bh,08Bh,0C8h,0E8h,072h,03Dh,0F2h,0FFh,08Bh,0F0h,0EBh,002h,033h,0F6h,08Bh,04Ch
-    db 024h,018h,085h,0C9h,0C7h,044h,024h,010h,0FFh,0FFh,0FFh,0FFh,074h,00Bh,068h,023h
-    db 003h,043h,000h,056h,0E8h,027h,0CFh,073h,000h,08Bh,04Ch,024h,008h,08Bh,0C6h,05Eh
-    db 064h,089h,00Dh,000h,000h,000h,000h,083h,0C4h,010h,0C3h
+    db 06Ah,0FFh,068h,0EBh,0E4h,0FFh,000h,064h,0A1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,051h,056h,068h,0ACh,000h,000h,000h,0E8h,04Fh,0CCh,076h
+    db 000h,083h,0C4h,004h,089h,044h,024h,004h,085h,0C0h,0C7h,044h,024h,010h,000h,000h
+    db 000h,000h,074h,00Bh,08Bh,0C8h,0E8h,003h,0F9h,0F0h,0FFh,08Bh,0F0h,0EBh,002h,033h
+    db 0F6h,08Bh,04Ch,024h,018h,085h,0C9h,0C7h,044h,024h,010h,0FFh,0FFh,0FFh,0FFh,074h
+    db 00Bh,068h,05Fh,027h,042h,000h,056h,0E8h,014h,0CEh,073h,000h,08Bh,04Ch,024h,008h
+    db 08Bh,0C6h,05Eh,064h,089h,00Dh,000h,000h,000h,000h,083h,0C4h,010h,0C3h
 ?friend_newModuleData@BezierProjectileBehavior@@SAPAVModuleData@@PAVINI@@@Z ENDP
 _TEXT ENDS
 END
--- Code/masm_dumps/DualWeaponBehavior_friend_newModuleData.asm
+++ Code/masm_dumps/DualWeaponBehavior_friend_newModuleData.asm
@@ -2,16 +2,16 @@
 .model flat
 
 ; ?friend_newModuleData@DualWeaponBehavior@@SAPAVModuleData@@PAVINI@@@Z
-; Exact 107 retail bytes @ 0x00514F90; allocates, constructs, and parses DualWeaponBehavior module data.
+; Exact 107 retail bytes @ 0x005150A0; allocates, constructs, and parses DualWeaponBehavior module data.
 _TEXT SEGMENT
 public ?friend_newModuleData@DualWeaponBehavior@@SAPAVModuleData@@PAVINI@@@Z
 ?friend_newModuleData@DualWeaponBehavior@@SAPAVModuleData@@PAVINI@@@Z PROC
-    db 06Ah,0FFh,068h,02Bh,0E4h,0FFh,000h,064h,0A1h,000h,000h,000h,000h,050h,064h,089h
-    db 025h,000h,000h,000h,000h,051h,056h,06Ah,00Ch,0E8h,082h,0CFh,076h,000h,083h,0C4h
+    db 06Ah,0FFh,068h,06Bh,0E4h,0FFh,000h,064h,0A1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,051h,056h,06Ah,018h,0E8h,072h,0CEh,076h,000h,083h,0C4h
     db 004h,089h,044h,024h,004h,085h,0C0h,0C7h,044h,024h,010h,000h,000h,000h,000h,074h
-    db 00Bh,08Bh,0C8h,0E8h,0F6h,0AAh,0F0h,0FFh,08Bh,0F0h,0EBh,002h,033h,0F6h,08Bh,04Ch
-    db 024h,018h,085h,0C9h,0C7h,044h,024h,010h,0FFh,0FFh,0FFh,0FFh,074h,00Bh,068h,012h
-    db 049h,040h,000h,056h,0E8h,047h,0D1h,073h,000h,08Bh,04Ch,024h,008h,08Bh,0C6h,05Eh
+    db 00Bh,08Bh,0C8h,0E8h,036h,028h,0F0h,0FFh,08Bh,0F0h,0EBh,002h,033h,0F6h,08Bh,04Ch
+    db 024h,018h,085h,0C9h,0C7h,044h,024h,010h,0FFh,0FFh,0FFh,0FFh,074h,00Bh,068h,04Eh
+    db 05Bh,042h,000h,056h,0E8h,037h,0D0h,073h,000h,08Bh,04Ch,024h,008h,08Bh,0C6h,05Eh
     db 064h,089h,00Dh,000h,000h,000h,000h,083h,0C4h,010h,0C3h
 ?friend_newModuleData@DualWeaponBehavior@@SAPAVModuleData@@PAVINI@@@Z ENDP
 _TEXT ENDS

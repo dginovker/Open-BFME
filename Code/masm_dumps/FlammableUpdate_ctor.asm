160f9173302d4beb6c8b754823b0b8a7fa401f79
Ancalgonn <1760627+Ancalgonn@users.noreply.github.com>
2026-07-19 20:22:39 -0400

    Match Flammable update constructor

---
+++ Code/masm_dumps/FlammableUpdate_ctor.asm
@@ -0,0 +1,23 @@
+.386
+.model flat
+
+; ??0FlammableUpdate@@QAE@PAVThing@@PBVModuleData@@@Z
+; Exact 187 retail bytes @ RVA 0x00293270; constructs FlammableUpdate.
+_TEXT SEGMENT
+public ??0FlammableUpdate@@QAE@PAVThing@@PBVModuleData@@@Z
+??0FlammableUpdate@@QAE@PAVThing@@PBVModuleData@@@Z PROC
+    db 06Ah,0FFh,068h,0F8h,01Ah,001h,001h,064h,0A1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,051h,08Bh,044h,024h,018h,056h,08Bh,0F1h,08Bh,04Ch,024h
+    db 018h,050h,051h,08Bh,0CEh,089h,074h,024h,00Ch,0E8h,046h,03Eh,0D8h,0FFh,0C7h,046h
+    db 00Ch,0D0h,0C9h,009h,001h,0C7h,046h,010h,0A0h,0CBh,009h,001h,083h,0C9h,0FFh,033h
+    db 0C0h,089h,046h,014h,089h,04Eh,018h,089h,04Eh,01Ch,0C7h,046h,020h,0FCh,01Bh,00Ah
+    db 001h,08Bh,056h,004h,0C7h,006h,03Ch,0EFh,00Bh,001h,0C7h,046h,00Ch,078h,0EEh,00Bh
+    db 001h,0C7h,046h,010h,06Ch,0EEh,00Bh,001h,0C7h,046h,020h,05Ch,0EEh,00Bh,001h,089h
+    db 046h,024h,089h,046h,028h,089h,046h,02Ch,089h,046h,030h,089h,046h,034h,08Bh,04Ah
+    db 01Ch,08Bh,056h,008h,068h,0FFh,0FFh,0FFh,03Fh,089h,04Eh,038h,052h,08Bh,0CEh,089h
+    db 044h,024h,018h,089h,046h,03Ch,088h,046h,040h,089h,046h,044h,089h,046h,048h,088h
+    db 046h,04Ch,0E8h,0C3h,024h,0D8h,0FFh,08Bh,04Ch,024h,008h,08Bh,0C6h,05Eh,064h,089h
+    db 00Dh,000h,000h,000h,000h,083h,0C4h,010h,0C2h,008h,000h
+??0FlammableUpdate@@QAE@PAVThing@@PBVModuleData@@@Z ENDP
+_TEXT ENDS
+END

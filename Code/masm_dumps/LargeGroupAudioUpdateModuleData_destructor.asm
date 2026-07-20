97f2aba6260740c3ae96028da8455ca3edb56934
dginovker <danielginovker@gmail.com>
2026-07-19 22:55:57 -0700

    Match LargeGroupAudioUpdate ModuleData destructor
    Exact retail MASM at 0x297390/99; unique factory→ctor→vtable→deleting wrapper proves ownership.

---
+++ Code/masm_dumps/LargeGroupAudioUpdateModuleData_destructor.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+
+; ??1LargeGroupAudioUpdateModuleData@@UAE@XZ
+; Exact 99 retail bytes @ 0x00297390; factory 0x11C280 ctor 0x2971F0 vtable 0x10BFA60 deleting wrapper prove ownership.
+_TEXT SEGMENT
+public ??1LargeGroupAudioUpdateModuleData@@UAE@XZ
+??1LargeGroupAudioUpdateModuleData@@UAE@XZ PROC
+    db 06Ah, 0FFh, 068h, 063h, 01Eh, 001h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 083h, 0ECh, 008h, 056h, 08Bh, 0F1h, 089h, 074h, 024h, 008h, 0C7h
+    db 006h, 060h, 0FAh, 00Bh, 001h, 08Dh, 044h, 024h, 004h, 050h, 0B9h, 0A0h, 0FFh, 02Eh, 001h, 0C7h
+    db 044h, 024h, 018h, 001h, 000h, 000h, 000h, 089h, 074h, 024h, 008h, 0E8h, 064h, 0E8h, 0D8h, 0FFh
+    db 08Dh, 04Eh, 008h, 0C6h, 044h, 024h, 014h, 000h, 0E8h, 0ECh, 00Dh, 0D8h, 0FFh, 08Bh, 04Ch, 024h
+    db 00Ch, 0C7h, 006h, 044h, 037h, 007h, 001h, 05Eh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h
+    db 0C4h, 014h, 0C3h
+??1LargeGroupAudioUpdateModuleData@@UAE@XZ ENDP
+_TEXT ENDS
+END

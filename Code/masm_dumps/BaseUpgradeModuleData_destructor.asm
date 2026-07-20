4d22038885337dfe5d1ecde049e15182934bc25e
dginovker <danielginovker@gmail.com>
2026-07-19 22:53:15 -0700

    Match BaseUpgrade ModuleData destructor
    Exact retail MASM at 0x2D3D20/96; unique factory→ctor→vtable→deleting wrapper proves ownership.

---
+++ Code/masm_dumps/BaseUpgradeModuleData_destructor.asm
@@ -0,0 +1,17 @@
+.386
+.model flat
+
+; ??1BaseUpgradeModuleData@@UAE@XZ
+; Exact 96 retail bytes @ 0x002D3D20; factory 0x11D190 ctor 0x2D3C50 vtable 0x10CC120 deleting wrapper prove ownership.
+_TEXT SEGMENT
+public ??1BaseUpgradeModuleData@@UAE@XZ
+??1BaseUpgradeModuleData@@UAE@XZ PROC
+    db 06Ah, 0FFh, 068h, 00Bh, 047h, 001h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 056h, 08Bh, 0F1h, 089h, 074h, 024h, 004h, 08Dh, 04Eh, 074h
+    db 0C7h, 044h, 024h, 010h, 001h, 000h, 000h, 000h, 0E8h, 0F3h, 03Bh, 05Bh, 000h, 08Dh, 04Eh, 070h
+    db 0C6h, 044h, 024h, 010h, 000h, 0E8h, 0E6h, 03Bh, 05Bh, 000h, 08Dh, 04Eh, 008h, 0C7h, 044h, 024h
+    db 010h, 002h, 000h, 000h, 000h, 0E8h, 013h, 07Ch, 0D4h, 0FFh, 08Bh, 04Ch, 024h, 008h, 0C7h, 006h
+    db 044h, 037h, 007h, 001h, 05Eh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h, 0C4h, 010h, 0C3h
+??1BaseUpgradeModuleData@@UAE@XZ ENDP
+_TEXT ENDS
+END

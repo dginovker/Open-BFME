c2f4efe867d767571a400b3d037446f97b5317f8
dginovker <danielginovker@gmail.com>
2026-07-19 22:51:27 -0700

    Match DynamicShroudClearingRangeUpdate ModuleData destructor
    Exact retail MASM at 0x28ECA0/95; unique factory→ctor→vtable→deleting wrapper proves ownership.

---
+++ Code/masm_dumps/DynamicShroudClearingRangeUpdateModuleData_destructor.asm
@@ -0,0 +1,17 @@
+.386
+.model flat
+
+; ??1DynamicShroudClearingRangeUpdateModuleData@@UAE@XZ
+; Exact 95 retail bytes @ 0x0028ECA0; factory 0x118130 ctor 0x28EBF0 vtable 0x10BE100 deleting wrapper prove ownership.
+_TEXT SEGMENT
+public ??1DynamicShroudClearingRangeUpdateModuleData@@UAE@XZ
+??1DynamicShroudClearingRangeUpdateModuleData@@UAE@XZ PROC
+    db 06Ah, 0FFh, 068h, 010h, 017h, 001h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 083h, 0ECh, 008h, 056h, 08Bh, 0F1h, 057h, 089h, 074h, 024h, 008h
+    db 08Dh, 07Eh, 028h, 0C7h, 044h, 024h, 018h, 000h, 000h, 000h, 000h, 089h, 07Ch, 024h, 00Ch, 08Dh
+    db 04Fh, 004h, 0C6h, 044h, 024h, 018h, 001h, 0E8h, 064h, 08Ch, 05Fh, 000h, 08Bh, 0CFh, 0C6h, 044h
+    db 024h, 018h, 000h, 0E8h, 058h, 08Ch, 05Fh, 000h, 08Bh, 04Ch, 024h, 010h, 0C7h, 006h, 044h, 037h
+    db 007h, 001h, 05Fh, 05Eh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h, 0C4h, 014h, 0C3h
+??1DynamicShroudClearingRangeUpdateModuleData@@UAE@XZ ENDP
+_TEXT ENDS
+END

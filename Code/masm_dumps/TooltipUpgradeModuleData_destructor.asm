66a254180954c89a6ab88042f027200b32bc6213
dginovker <danielginovker@gmail.com>
2026-07-19 22:54:37 -0700

    Match TooltipUpgrade ModuleData destructor
    Exact retail MASM at 0x12BBB0/96; unique factory→ctor→vtable→deleting wrapper proves ownership.

---
+++ Code/masm_dumps/TooltipUpgradeModuleData_destructor.asm
@@ -0,0 +1,17 @@
+.386
+.model flat
+
+; ??1TooltipUpgradeModuleData@@UAE@XZ
+; Exact 96 retail bytes @ 0x0012BBB0; factory 0x12BC30 ctor 0x12BAE0 vtable 0x108F650 deleting wrapper prove ownership.
+_TEXT SEGMENT
+public ??1TooltipUpgradeModuleData@@UAE@XZ
+??1TooltipUpgradeModuleData@@UAE@XZ PROC
+    db 06Ah, 0FFh, 068h, 0CBh, 025h, 000h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 056h, 08Bh, 0F1h, 089h, 074h, 024h, 004h, 08Dh, 04Eh, 074h
+    db 0C7h, 044h, 024h, 010h, 001h, 000h, 000h, 000h, 0E8h, 063h, 0BDh, 075h, 000h, 08Dh, 04Eh, 070h
+    db 0C6h, 044h, 024h, 010h, 000h, 0E8h, 056h, 0BDh, 075h, 000h, 08Dh, 04Eh, 008h, 0C7h, 044h, 024h
+    db 010h, 002h, 000h, 000h, 000h, 0E8h, 083h, 0FDh, 0EEh, 0FFh, 08Bh, 04Ch, 024h, 008h, 0C7h, 006h
+    db 044h, 037h, 007h, 001h, 05Eh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h, 0C4h, 010h, 0C3h
+??1TooltipUpgradeModuleData@@UAE@XZ ENDP
+_TEXT ENDS
+END

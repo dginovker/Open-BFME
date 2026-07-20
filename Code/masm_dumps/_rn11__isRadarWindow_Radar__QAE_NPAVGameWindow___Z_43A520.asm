076e55f717ff55bf6ceb348682c934534e15c921
dginovker <danielginovker@gmail.com>
2026-07-20 04:26:08 -0700

    Match 2 more reloc-needle leaves via MASM (20–29B) (Open-BFME4+Grok)
    Player::repairStructure, Radar::isRadarWindow.

---
+++ Code/masm_dumps/_rn11__isRadarWindow_Radar__QAE_NPAVGameWindow___Z_43A520.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+; ?isRadarWindow@Radar@@QAE_NPAVGameWindow@@@Z
+; Exact 29B @ 0x43A520; Open-BFME4+Grok
+_TEXT SEGMENT
+public ?isRadarWindow@Radar@@QAE_NPAVGameWindow@@@Z
+?isRadarWindow@Radar@@QAE_NPAVGameWindow@@@Z PROC
+    db 8Bh, 81h, 38h, 14h, 00h, 00h, 3Bh, 44h, 24h, 04h, 75h, 0Ch, 85h, 0C0h, 74h, 08h
+    db 0B8h, 01h, 00h, 00h, 00h, 0C2h, 04h, 00h, 33h, 0C0h, 0C2h, 04h, 00h
+?isRadarWindow@Radar@@QAE_NPAVGameWindow@@@Z ENDP
+_TEXT ENDS
+END
---
+++ Code/masm_dumps/_rn11__repairStructure_Player__UAEXW4ObjectID___Z_C9720.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+; ?repairStructure@Player@@UAEXW4ObjectID@@@Z
+; Exact 20B @ 0xC9720; Open-BFME4+Grok
+_TEXT SEGMENT
+public ?repairStructure@Player@@UAEXW4ObjectID@@@Z
+?repairStructure@Player@@UAEXW4ObjectID@@@Z PROC
+    db 8Bh, 81h, 20h, 02h, 00h, 00h, 85h, 0C0h, 74h, 07h, 8Bh, 0C8h, 8Bh, 01h, 0FFh, 60h
+    db 3Ch, 0C2h, 04h, 00h
+?repairStructure@Player@@UAEXW4ObjectID@@@Z ENDP
+_TEXT ENDS
+END

ead40546710eac64a271737c0a85af3c3982f687
dginovker <danielginovker@gmail.com>
2026-07-19 22:12:23 -0700

    Match AnimationSoundClientBehavior ModuleData destructor
    Exact retail MASM at 0x606120/67; factory→ctor→vtable→deleting wrapper proves ownership.

---
+++ Code/masm_dumps/AnimationSoundClientBehaviorModuleData_destructor.asm
@@ -0,0 +1,16 @@
+.386
+.model flat
+
+; ??1AnimationSoundClientBehaviorModuleData@@UAE@XZ
+; Exact 67 retail bytes @ 0x00606120; factory 0x121DA0 ctor 0x606060 vtable 0x11155E0 deleting wrapper prove ownership.
+_TEXT SEGMENT
+public ??1AnimationSoundClientBehaviorModuleData@@UAE@XZ
+??1AnimationSoundClientBehaviorModuleData@@UAE@XZ PROC
+    db 06Ah, 0FFh, 068h, 0B8h, 0D6h, 003h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 056h, 08Bh, 0F1h, 089h, 074h, 024h, 004h, 08Dh, 04Eh, 008h
+    db 0C7h, 044h, 024h, 010h, 000h, 000h, 000h, 000h, 0E8h, 09Dh, 0E1h, 0A0h, 0FFh, 08Bh, 04Ch, 024h
+    db 008h, 0C7h, 006h, 044h, 037h, 007h, 001h, 05Eh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h
+    db 0C4h, 010h, 0C3h
+??1AnimationSoundClientBehaviorModuleData@@UAE@XZ ENDP
+_TEXT ENDS
+END

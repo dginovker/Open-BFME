74062bcd360a642d58ef542a89db8f2654730459
dginovker <danielginovker@gmail.com>
2026-07-19 23:00:42 -0700

    Match W3DTreeDraw ModuleData destructor
    Exact retail MASM at 0x77F460/112; unique factory→ctor→vtable→deleting wrapper proves ownership.

---
+++ Code/masm_dumps/W3DTreeDrawModuleData_destructor.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+
+; ??1W3DTreeDrawModuleData@@UAE@XZ
+; Exact 112 retail bytes @ 0x0077F460; factory 0x6BF710 ctor 0x77F360 vtable 0x1125D08 deleting wrapper prove ownership.
+_TEXT SEGMENT
+public ??1W3DTreeDrawModuleData@@UAE@XZ
+??1W3DTreeDrawModuleData@@UAE@XZ PROC
+    db 06Ah, 0FFh, 068h, 029h, 00Ah, 005h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 056h, 08Bh, 0F1h, 089h, 074h, 024h, 004h, 0C7h, 006h, 008h
+    db 05Dh, 012h, 001h, 08Dh, 04Eh, 048h, 0C7h, 044h, 024h, 010h, 003h, 000h, 000h, 000h, 0E8h, 0ADh
+    db 084h, 010h, 000h, 08Dh, 04Eh, 028h, 0C6h, 044h, 024h, 010h, 002h, 0E8h, 0A0h, 084h, 010h, 000h
+    db 08Dh, 04Eh, 00Ch, 0C6h, 044h, 024h, 010h, 001h, 0E8h, 093h, 084h, 010h, 000h, 08Dh, 04Eh, 008h
+    db 0C6h, 044h, 024h, 010h, 000h, 0E8h, 086h, 084h, 010h, 000h, 08Bh, 04Ch, 024h, 008h, 0C7h, 006h
+    db 044h, 037h, 007h, 001h, 05Eh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h, 0C4h, 010h, 0C3h
+??1W3DTreeDrawModuleData@@UAE@XZ ENDP
+_TEXT ENDS
+END

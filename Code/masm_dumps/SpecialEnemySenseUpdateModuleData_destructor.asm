bbcb28cff7abcf2673c51117fe9c0c3944c4825f
dginovker <danielginovker@gmail.com>
2026-07-19 22:31:19 -0700

    Match SpecialEnemySenseUpdate ModuleData destructor
    Exact retail MASM at 0x125890/67; factory→ctor→vtable→deleting wrapper proves ownership.

---
+++ Code/masm_dumps/SpecialEnemySenseUpdateModuleData_destructor.asm
@@ -0,0 +1,16 @@
+.386
+.model flat
+
+; ??1SpecialEnemySenseUpdateModuleData@@UAE@XZ
+; Exact 67 retail bytes @ 0x00125890; factory 0x123D90 ctor 0x123D00 vtable 0x108CD38 deleting wrapper prove ownership.
+_TEXT SEGMENT
+public ??1SpecialEnemySenseUpdateModuleData@@UAE@XZ
+??1SpecialEnemySenseUpdateModuleData@@UAE@XZ PROC
+    db 06Ah, 0FFh, 068h, 048h, 017h, 000h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 056h, 08Bh, 0F1h, 089h, 074h, 024h, 004h, 08Dh, 04Eh, 008h
+    db 0C7h, 044h, 024h, 010h, 000h, 000h, 000h, 000h, 0E8h, 044h, 04Bh, 0EFh, 0FFh, 08Bh, 04Ch, 024h
+    db 008h, 0C7h, 006h, 044h, 037h, 007h, 001h, 05Eh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h
+    db 0C4h, 010h, 0C3h
+??1SpecialEnemySenseUpdateModuleData@@UAE@XZ ENDP
+_TEXT ENDS
+END

907c5b41a78566907d6dde413b2dc299f77b2b69
dginovker <danielginovker@gmail.com>
2026-07-19 21:55:56 -0700

    Match StealthDetectorUpdate ModuleData destructor
    Exact retail MASM at 0x126B60/99; factory→ctor→vtable→deleting wrapper proves ownership.

---
+++ Code/masm_dumps/StealthDetectorUpdateModuleData_destructor.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+
+; ??1StealthDetectorUpdateModuleData@@UAE@XZ
+; Exact 99 retail bytes @ 0x00126B60; factory 0x126BE0 ctor 0x1269F0 vtable 0x108EA48 deleting wrapper prove ownership.
+_TEXT SEGMENT
+public ??1StealthDetectorUpdateModuleData@@UAE@XZ
+??1StealthDetectorUpdateModuleData@@UAE@XZ PROC
+    db 06Ah, 0FFh, 068h, 011h, 01Ah, 000h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 056h, 08Bh, 0F1h, 089h, 074h, 024h, 004h, 08Dh, 08Eh, 004h
+    db 001h, 000h, 000h, 0C7h, 044h, 024h, 010h, 002h, 000h, 000h, 000h, 0E8h, 0B0h, 00Dh, 076h, 000h
+    db 08Dh, 08Eh, 084h, 000h, 000h, 000h, 0C6h, 044h, 024h, 010h, 001h, 0E8h, 095h, 003h, 0F0h, 0FFh
+    db 08Dh, 04Eh, 014h, 0C6h, 044h, 024h, 010h, 000h, 0E8h, 088h, 003h, 0F0h, 0FFh, 08Bh, 04Ch, 024h
+    db 008h, 0C7h, 006h, 044h, 037h, 007h, 001h, 05Eh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h
+    db 0C4h, 010h, 0C3h
+??1StealthDetectorUpdateModuleData@@UAE@XZ ENDP
+_TEXT ENDS
+END

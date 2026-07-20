ea1afb8735967f46700ad562dafc2a4c630bcf74
dginovker <danielginovker@gmail.com>
2026-07-19 22:35:12 -0700

    Match AutoPickUpUpdate ModuleData destructor
    Exact retail MASM at 0x282A80/80; unique factory→ctor→vtable→deleting wrapper proves ownership.

---
+++ Code/masm_dumps/AutoPickUpUpdateModuleData_destructor.asm
@@ -0,0 +1,16 @@
+.386
+.model flat
+
+; ??1AutoPickUpUpdateModuleData@@UAE@XZ
+; Exact 80 retail bytes @ 0x00282A80; factory 0x119B30 ctor 0x2827E0 vtable 0x10BB588 deleting wrapper prove ownership.
+_TEXT SEGMENT
+public ??1AutoPickUpUpdateModuleData@@UAE@XZ
+??1AutoPickUpUpdateModuleData@@UAE@XZ PROC
+    db 06Ah, 0FFh, 068h, 073h, 00Bh, 001h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 056h, 08Bh, 0F1h, 089h, 074h, 024h, 004h, 08Dh, 04Eh, 030h
+    db 0C7h, 044h, 024h, 010h, 001h, 000h, 000h, 000h, 0E8h, 069h, 025h, 0DCh, 0FFh, 08Dh, 04Eh, 02Ch
+    db 0C6h, 044h, 024h, 010h, 000h, 0E8h, 047h, 079h, 0D9h, 0FFh, 08Bh, 04Ch, 024h, 008h, 0C7h, 006h
+    db 044h, 037h, 007h, 001h, 05Eh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h, 0C4h, 010h, 0C3h
+??1AutoPickUpUpdateModuleData@@UAE@XZ ENDP
+_TEXT ENDS
+END

bf3ad4c32653db3f6faaa9e210f8f0b9eaab8d9f
dginovker <danielginovker@gmail.com>
2026-07-19 22:24:37 -0700

    Match MonsterDockUpdate ModuleData destructor
    Exact retail MASM at 0x2CDD90/67; factory→ctor→vtable→deleting wrapper proves ownership.

---
+++ Code/masm_dumps/MonsterDockUpdateModuleData_destructor.asm
@@ -0,0 +1,16 @@
+.386
+.model flat
+
+; ??1MonsterDockUpdateModuleData@@UAE@XZ
+; Exact 67 retail bytes @ 0x002CDD90; factory 0x11B1E0 ctor 0x2CDCF0 vtable 0x10CAB58 deleting wrapper prove ownership.
+_TEXT SEGMENT
+public ??1MonsterDockUpdateModuleData@@UAE@XZ
+??1MonsterDockUpdateModuleData@@UAE@XZ PROC
+    db 06Ah, 0FFh, 068h, 0D8h, 042h, 001h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 056h, 08Bh, 0F1h, 089h, 074h, 024h, 004h, 08Dh, 04Eh, 010h
+    db 0C7h, 044h, 024h, 010h, 000h, 000h, 000h, 000h, 0E8h, 044h, 0C6h, 0D4h, 0FFh, 08Bh, 04Ch, 024h
+    db 008h, 0C7h, 006h, 044h, 037h, 007h, 001h, 05Eh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h
+    db 0C4h, 010h, 0C3h
+??1MonsterDockUpdateModuleData@@UAE@XZ ENDP
+_TEXT ENDS
+END

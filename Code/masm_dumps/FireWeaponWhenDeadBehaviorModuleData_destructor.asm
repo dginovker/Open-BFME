2bc9f15f1c9491a4f3a4f545a79d284967fd8922
dginovker <danielginovker@gmail.com>
2026-07-19 22:12:50 -0700

    Match FireWeaponWhenDeadBehavior ModuleData destructor
    Exact retail MASM at 0x12AD60/67; factory→ctor→vtable→deleting wrapper proves ownership.

---
+++ Code/masm_dumps/FireWeaponWhenDeadBehaviorModuleData_destructor.asm
@@ -0,0 +1,16 @@
+.386
+.model flat
+
+; ??1FireWeaponWhenDeadBehaviorModuleData@@UAE@XZ
+; Exact 67 retail bytes @ 0x0012AD60; factory 0x12A250 ctor 0x12A1C0 vtable 0x108F0D0 deleting wrapper prove ownership.
+_TEXT SEGMENT
+public ??1FireWeaponWhenDeadBehaviorModuleData@@UAE@XZ
+??1FireWeaponWhenDeadBehaviorModuleData@@UAE@XZ PROC
+    db 06Ah, 0FFh, 068h, 038h, 023h, 000h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 056h, 08Bh, 0F1h, 089h, 074h, 024h, 004h, 08Dh, 04Eh, 008h
+    db 0C7h, 044h, 024h, 010h, 000h, 000h, 000h, 000h, 0E8h, 0F0h, 00Bh, 0EFh, 0FFh, 08Bh, 04Ch, 024h
+    db 008h, 0C7h, 006h, 044h, 037h, 007h, 001h, 05Eh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h
+    db 0C4h, 010h, 0C3h
+??1FireWeaponWhenDeadBehaviorModuleData@@UAE@XZ ENDP
+_TEXT ENDS
+END

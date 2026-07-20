06647817e5231bc30b6e5bb542840a78e4e3ee13
dginovker <danielginovker@gmail.com>
2026-07-19 21:57:02 -0700

    Match AutoHealBehavior ModuleData destructor
    Exact retail MASM at 0x12A910/67; factory→ctor→vtable→deleting wrapper proves ownership.

---
+++ Code/masm_dumps/AutoHealBehaviorModuleData_destructor.asm
@@ -0,0 +1,16 @@
+.386
+.model flat
+
+; ??1AutoHealBehaviorModuleData@@UAE@XZ
+; Exact 67 retail bytes @ 0x0012A910; factory 0x129E40 ctor 0x129D50 vtable 0x108EFC8 deleting wrapper prove ownership.
+_TEXT SEGMENT
+public ??1AutoHealBehaviorModuleData@@UAE@XZ
+??1AutoHealBehaviorModuleData@@UAE@XZ PROC
+    db 06Ah, 0FFh, 068h, 048h, 022h, 000h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 056h, 08Bh, 0F1h, 089h, 074h, 024h, 004h, 08Dh, 04Eh, 008h
+    db 0C7h, 044h, 024h, 010h, 000h, 000h, 000h, 000h, 0E8h, 040h, 010h, 0EFh, 0FFh, 08Bh, 04Ch, 024h
+    db 008h, 0C7h, 006h, 044h, 037h, 007h, 001h, 05Eh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h
+    db 0C4h, 010h, 0C3h
+??1AutoHealBehaviorModuleData@@UAE@XZ ENDP
+_TEXT ENDS
+END

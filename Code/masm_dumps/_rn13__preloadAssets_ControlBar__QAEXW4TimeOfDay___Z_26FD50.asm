09691741155010b3d8735cc1bf707f9968eea43c
dginovker <danielginovker@gmail.com>
2026-07-20 12:42:37 -0700

    Match 2 more reloc-needle leaves via MASM (13–15B) (Open-BFME4+Grok)
    GameWindow::winSetPrev, ControlBar::preloadAssets.

---
+++ Code/masm_dumps/_rn13__preloadAssets_ControlBar__QAEXW4TimeOfDay___Z_26FD50.asm
@@ -0,0 +1,11 @@
+.386
+.model flat
+; ?preloadAssets@ControlBar@@QAEXW4TimeOfDay@@@Z
+; Exact 13B @ 0x26FD50; Open-BFME4+Grok
+_TEXT SEGMENT
+public ?preloadAssets@ControlBar@@QAEXW4TimeOfDay@@@Z
+?preloadAssets@ControlBar@@QAEXW4TimeOfDay@@@Z PROC
+    db 8Bh, 49h, 30h, 85h, 0C9h, 74h, 05h, 0E9h, 0Ah, 78h, 0DBh, 0FFh, 0C3h
+?preloadAssets@ControlBar@@QAEXW4TimeOfDay@@@Z ENDP
+_TEXT ENDS
+END
---
+++ Code/masm_dumps/_rn13__winSetPrev_GameWindow__QAEXPAV1__Z_478EB0.asm
@@ -0,0 +1,11 @@
+.386
+.model flat
+; ?winSetPrev@GameWindow@@QAEXPAV1@@Z
+; Exact 15B @ 0x478EB0; Open-BFME4+Grok
+_TEXT SEGMENT
+public ?winSetPrev@GameWindow@@QAEXPAV1@@Z
+?winSetPrev@GameWindow@@QAEXPAV1@@Z PROC
+    db 8Bh, 44h, 24h, 04h, 89h, 81h, 0ECh, 01h, 00h, 00h, 33h, 0C0h, 0C2h, 04h, 00h
+?winSetPrev@GameWindow@@QAEXPAV1@@Z ENDP
+_TEXT ENDS
+END

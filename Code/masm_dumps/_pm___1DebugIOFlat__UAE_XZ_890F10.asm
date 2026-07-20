b8c3295a8fc769e3dce2d3b2f1d9656f12ebd6dd
dginovker <danielginovker@gmail.com>
2026-07-20 01:13:37 -0700

    Match DebugIONet/DebugIOFlat dtors as ICF of DebugIOCon body (85B)
    Present-unmatched markers; shared 85B SEH dtor RVA with already-matched
    DebugIOCon destructor.
    
    Open-BFME4+Grok

---
+++ Code/masm_dumps/_pm___1DebugIOFlat__UAE_XZ_890F10.asm
@@ -0,0 +1,16 @@
+.386
+.model flat
+; ??1DebugIOFlat@@UAE@XZ
+; Exact 85B @ 0x00890F10; Open-BFME4
+_TEXT SEGMENT
+public ??1DebugIOFlat@@UAE@XZ
+??1DebugIOFlat@@UAE@XZ PROC
+    db 55h, 8Bh, 0ECh, 6Ah, 0FFh, 68h, 98h, 6Bh, 05h, 01h, 64h, 0A1h, 00h, 00h, 00h, 00h
+    db 50h, 64h, 89h, 25h, 00h, 00h, 00h, 00h, 51h, 56h, 8Bh, 0F1h, 89h, 75h, 0F0h, 0C7h
+    db 06h, 9Ch, 5Ch, 13h, 01h, 0C7h, 45h, 0FCh, 00h, 00h, 00h, 00h, 8Ah, 46h, 04h, 84h
+    db 0C0h, 74h, 06h, 0FFh, 15h, 44h, 8Dh, 35h, 01h, 0C7h, 45h, 0FCh, 0FFh, 0FFh, 0FFh, 0FFh
+    db 8Bh, 4Dh, 0F4h, 0C7h, 06h, 44h, 50h, 13h, 01h, 5Eh, 64h, 89h, 0Dh, 00h, 00h, 00h
+    db 00h, 8Bh, 0E5h, 5Dh, 0C3h
+??1DebugIOFlat@@UAE@XZ ENDP
+_TEXT ENDS
+END

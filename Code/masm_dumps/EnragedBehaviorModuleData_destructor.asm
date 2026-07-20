8901092569afe5e8663a865799b5d8ca0053e7af
dginovker <danielginovker@gmail.com>
2026-07-19 22:13:17 -0700

    Match EnragedBehavior ModuleData destructor
    Exact retail MASM at 0x203F30/67; factory→ctor→vtable→deleting wrapper proves ownership.

---
+++ Code/masm_dumps/EnragedBehaviorModuleData_destructor.asm
@@ -0,0 +1,16 @@
+.386
+.model flat
+
+; ??1EnragedBehaviorModuleData@@UAE@XZ
+; Exact 67 retail bytes @ 0x00203F30; factory 0x114A40 ctor 0x203E60 vtable 0x10A5A58 deleting wrapper prove ownership.
+_TEXT SEGMENT
+public ??1EnragedBehaviorModuleData@@UAE@XZ
+??1EnragedBehaviorModuleData@@UAE@XZ PROC
+    db 06Ah, 0FFh, 068h, 068h, 0BBh, 000h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 056h, 08Bh, 0F1h, 089h, 074h, 024h, 004h, 08Dh, 04Eh, 008h
+    db 0C7h, 044h, 024h, 010h, 000h, 000h, 000h, 000h, 0E8h, 055h, 02Bh, 0E2h, 0FFh, 08Bh, 04Ch, 024h
+    db 008h, 0C7h, 006h, 044h, 037h, 007h, 001h, 05Eh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 083h
+    db 0C4h, 010h, 0C3h
+??1EnragedBehaviorModuleData@@UAE@XZ ENDP
+_TEXT ENDS
+END

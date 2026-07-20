6a10c0058bd7230139d85e4b5ef9d629b5cc1618
dginovker <danielginovker@gmail.com>
2026-07-19 22:59:20 -0700

    Match AttributeModifierAuraUpdate ModuleData destructor
    Exact retail MASM at 0x280C40/106; unique factory→ctor→vtable→deleting wrapper proves ownership.

---
+++ Code/masm_dumps/AttributeModifierAuraUpdateModuleData_destructor.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+
+; ??1AttributeModifierAuraUpdateModuleData@@UAE@XZ
+; Exact 106 retail bytes @ 0x00280C40; factory 0x11A950 ctor 0x280B20 vtable 0x10BB0A8 deleting wrapper prove ownership.
+_TEXT SEGMENT
+public ??1AttributeModifierAuraUpdateModuleData@@UAE@XZ
+??1AttributeModifierAuraUpdateModuleData@@UAE@XZ PROC
+    db 06Ah, 0FFh, 068h, 089h, 009h, 001h, 001h, 064h, 0A1h, 000h, 000h, 000h, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 051h, 056h, 08Bh, 0F1h, 089h, 074h, 024h, 004h, 08Dh, 04Eh, 028h
+    db 0C7h, 044h, 024h, 010h, 003h, 000h, 000h, 000h, 0E8h, 010h, 0ADh, 0D9h, 0FFh, 08Dh, 04Eh, 024h
+    db 0C6h, 044h, 024h, 010h, 002h, 0E8h, 087h, 097h, 0D9h, 0FFh, 08Dh, 04Eh, 00Ch, 0C6h, 044h, 024h
+    db 010h, 001h, 0E8h, 02Bh, 05Eh, 0DAh, 0FFh, 08Dh, 04Eh, 008h, 0C6h, 044h, 024h, 010h, 000h, 0E8h
+    db 0ACh, 06Ch, 060h, 000h, 08Bh, 04Ch, 024h, 008h, 0C7h, 006h, 044h, 037h, 007h, 001h, 05Eh, 064h
+    db 089h, 00Dh, 000h, 000h, 000h, 000h, 083h, 0C4h, 010h, 0C3h
+??1AttributeModifierAuraUpdateModuleData@@UAE@XZ ENDP
+_TEXT ENDS
+END

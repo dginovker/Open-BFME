1f347aa6f206ae0b4d17012fe5970aedadd1df87
dginovker <danielginovker@gmail.com>
2026-07-20 00:57:49 -0700

    Match TemporarilyDefectUpdate ctor as ICF alias of PoisonedBehavior (146B)
    Last remaining unique friend_newModuleInstance factory without a constructor.
    
    Open-BFME4+Grok

---
+++ Code/masm_dumps/TemporarilyDefectUpdate_constructor.asm
@@ -0,0 +1,20 @@
+.386
+.model flat
+; ??0TemporarilyDefectUpdate@@QAE@PAVThing@@PBVModuleData@@@Z
+; Exact 146B ICF alias
+_TEXT SEGMENT
+public ??0TemporarilyDefectUpdate@@QAE@PAVThing@@PBVModuleData@@@Z
+??0TemporarilyDefectUpdate@@QAE@PAVThing@@PBVModuleData@@@Z PROC
+    db 6Ah, 0FFh, 68h, 58h, 2Fh, 01h, 01h, 64h, 0A1h, 00h, 00h, 00h, 00h, 50h, 64h, 89h
+    db 25h, 00h, 00h, 00h, 00h, 51h, 8Bh, 44h, 24h, 18h, 56h, 8Bh, 0F1h, 8Bh, 4Ch, 24h
+    db 18h, 50h, 51h, 8Bh, 0CEh, 89h, 74h, 24h, 0Ch, 0E8h, 0E6h, 65h, 0D6h, 0FFh, 0C7h, 46h
+    db 0Ch, 0D0h, 0C9h, 09h, 01h, 0C7h, 46h, 10h, 0A0h, 0CBh, 09h, 01h, 83h, 0C9h, 0FFh, 33h
+    db 0C0h, 89h, 46h, 14h, 89h, 4Eh, 18h, 89h, 4Eh, 1Ch, 8Bh, 56h, 08h, 68h, 0FFh, 0FFh
+    db 0FFh, 3Fh, 52h, 8Bh, 0CEh, 89h, 44h, 24h, 18h, 0C7h, 06h, 0CCh, 4Dh, 0Ch, 01h, 0C7h
+    db 46h, 0Ch, 08h, 4Dh, 0Ch, 01h, 0C7h, 46h, 10h, 0F8h, 4Ch, 0Ch, 01h, 89h, 46h, 20h
+    db 89h, 46h, 24h, 89h, 46h, 28h, 88h, 46h, 2Ch, 0E8h, 8Ch, 4Ch, 0D6h, 0FFh, 8Bh, 4Ch
+    db 24h, 08h, 8Bh, 0C6h, 5Eh, 64h, 89h, 0Dh, 00h, 00h, 00h, 00h, 83h, 0C4h, 10h, 0C2h
+    db 08h, 00h
+??0TemporarilyDefectUpdate@@QAE@PAVThing@@PBVModuleData@@@Z ENDP
+_TEXT ENDS
+END

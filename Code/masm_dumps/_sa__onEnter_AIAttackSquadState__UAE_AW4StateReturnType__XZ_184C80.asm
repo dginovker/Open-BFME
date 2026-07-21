844a57ce9cdc7f4a3d58bc513e4ab175e90acac7
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-21 23:24:58 +0000

    match AIAttackSquadState::onEnter @ 0x184C80 (162B MASM)
    Queue RVA 0x1842A3 was INSIDE an unrelated factory switch (ret 4).
    True body from AIAttackSquadState vtable 0xC97E30 slot 4 (ctor-anchored).

---
+++ Code/masm_dumps/_sa__onEnter_AIAttackSquadState__UAE_AW4StateReturnType__XZ_184C80.asm
@@ -0,0 +1,23 @@
+.386
+.model flat
+; ?onEnter@AIAttackSquadState@@UAE?AW4StateReturnType@@XZ
+; Exact 162B @ 0x00184C80; vtable-anchored AIAttackSquadState vt@0xC97E30 slot4
+; (ctor stores vt after State(machine,"AIAttackSquadState"); same slot as Area onEnter)
+; queue RVA 0x1842A3 was INSIDE unrelated factory switch (ret 4); true body unclaimed
+_TEXT SEGMENT
+public ?onEnter@AIAttackSquadState@@UAE?AW4StateReturnType@@XZ
+?onEnter@AIAttackSquadState@@UAE?AW4StateReturnType@@XZ PROC
+    db 56h, 8Bh, 0F1h, 8Bh, 4Eh, 1Ch, 57h, 8Bh, 79h, 10h, 85h, 0FFh, 75h, 08h, 5Fh, 0B8h
+    db 0FEh, 0FFh, 0FFh, 0FFh, 5Eh, 0C3h, 8Bh, 01h, 53h, 55h, 0FFh, 50h, 24h, 89h, 46h, 24h
+    db 8Bh, 0AFh, 04h, 02h, 00h, 00h, 8Bh, 0CEh, 0E8h, 0D0h, 5Eh, 0E8h, 0FFh, 6Ah, 00h, 8Bh
+    db 0CFh, 8Bh, 0D8h, 0E8h, 0C7h, 0CDh, 0EAh, 0FFh, 85h, 0C0h, 74h, 30h, 85h, 0DBh, 74h, 2Ch
+    db 6Ah, 00h, 53h, 57h, 8Bh, 0C8h, 0E8h, 91h, 9Bh, 0EAh, 0FFh, 84h, 0C0h, 75h, 1Dh, 0E8h
+    db 3Ch, 63h, 0FEh, 0FFh, 84h, 0C0h, 74h, 0Ah, 8Ah, 85h, 3Ah, 03h, 00h, 00h, 84h, 0C0h
+    db 74h, 0Ah, 5Dh, 5Bh, 5Fh, 0B8h, 0FEh, 0FFh, 0FFh, 0FFh, 5Eh, 0C3h, 8Bh, 4Eh, 24h, 8Bh
+    db 11h, 53h, 0FFh, 52h, 38h, 85h, 0EDh, 74h, 19h, 53h, 8Bh, 0CDh, 0E8h, 4Bh, 5Eh, 0ECh
+    db 0FFh, 8Bh, 4Eh, 1Ch, 8Bh, 01h, 53h, 0FFh, 50h, 38h, 53h, 8Bh, 0CDh, 0E8h, 0D7h, 0D8h
+    db 0E8h, 0FFh, 8Bh, 4Eh, 24h, 5Dh, 5Bh, 0C6h, 46h, 28h, 00h, 8Bh, 11h, 5Fh, 5Eh, 0FFh
+    db 62h, 1Ch
+?onEnter@AIAttackSquadState@@UAE?AW4StateReturnType@@XZ ENDP
+_TEXT ENDS
+END

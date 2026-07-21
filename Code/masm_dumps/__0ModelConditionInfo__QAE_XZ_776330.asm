8f6e436f2ee9415b13829c26943f8d8675b3600e
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-21 09:29:45 +0000

    Match ModelConditionInfo default ctor via MASM retail dump.
    Queue RVA 0x776349 was mid-prologue; true body starts at 0x776330
    (SEH frame) size 339. Exact-byte dump of the out-of-line ctor.

---
+++ Code/masm_dumps/__0ModelConditionInfo__QAE_XZ_776330.asm
@@ -0,0 +1,34 @@
+.386
+.model flat
+
+; ??0ModelConditionInfo@@QAE@XZ
+; Retail @ 0x00776330 size 339
+; SEH default ctor: member default-init then clear()
+_TEXT SEGMENT
+public ??0ModelConditionInfo@@QAE@XZ
+??0ModelConditionInfo@@QAE@XZ PROC
+    db 6Ah,0FFh,68h,14h,0FEh,04h,01h,64h,0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,51h,33h,0C0h,56h,8Bh,0F1h,89h,01h,89h,41h,04h
+    db 89h,41h,08h,89h,41h,0Ch,89h,41h,10h,89h,41h,14h,89h,41h,18h,89h
+    db 41h,1Ch,89h,41h,20h,57h,33h,0FFh,89h,41h,24h,89h,7Eh,28h,89h,7Eh
+    db 2Ch,89h,74h,24h,08h,89h,7Eh,30h,89h,7Ch,24h,14h,89h,7Eh,34h,89h
+    db 7Eh,3Ch,89h,7Eh,40h,89h,7Eh,44h,89h,7Eh,48h,68h,28h,0D8h,40h,00h
+    db 68h,0D9h,7Bh,41h,00h,6Ah,04h,6Ah,04h,8Dh,56h,4Ch,52h,0C6h,44h,24h
+    db 28h,03h,0E8h,3Dh,0Bh,28h,00h,68h,28h,0D8h,40h,00h,68h,0D9h,7Bh,41h
+    db 00h,6Ah,04h,6Ah,04h,8Dh,46h,5Ch,50h,0C6h,44h,24h,28h,04h,0E8h,21h
+    db 0Bh,28h,00h,68h,28h,0D8h,40h,00h,68h,0D9h,7Bh,41h,00h,6Ah,04h,6Ah
+    db 04h,8Dh,4Eh,6Ch,51h,0C6h,44h,24h,28h,05h,0E8h,05h,0Bh,28h,00h,68h
+    db 28h,0D8h,40h,00h,68h,0D9h,7Bh,41h,00h,6Ah,04h,6Ah,04h,8Dh,56h,7Ch
+    db 52h,0C6h,44h,24h,28h,06h,0E8h,0E9h,0Ah,28h,00h,68h,28h,0D8h,40h,00h
+    db 68h,0D9h,7Bh,41h,00h,6Ah,04h,6Ah,04h,8Dh,86h,8Ch,00h,00h,00h,50h
+    db 0C6h,44h,24h,28h,07h,0E8h,0CAh,0Ah,28h,00h,6Ah,2Ch,0C6h,44h,24h,18h
+    db 08h,89h,0BEh,9Ch,00h,00h,00h,0E8h,14h,81h,0Bh,00h,83h,0C4h,04h,89h
+    db 00h,89h,40h,04h,89h,86h,9Ch,00h,00h,00h,89h,0BEh,0A0h,00h,00h,00h
+    db 89h,0BEh,0A4h,00h,00h,00h,89h,0BEh,0A8h,00h,00h,00h,89h,0BEh,0ACh,00h
+    db 00h,00h,89h,0BEh,0B0h,00h,00h,00h,89h,0BEh,0B4h,00h,00h,00h,89h,0BEh
+    db 0BCh,00h,00h,00h,8Bh,0CEh,0C6h,44h,24h,14h,0Ch,0E8h,0A1h,46h,8Ch,0FFh
+    db 8Bh,4Ch,24h,0Ch,5Fh,8Bh,0C6h,5Eh,64h,89h,0Dh,00h,00h,00h,00h,83h
+    db 0C4h,10h,0C3h
+??0ModelConditionInfo@@QAE@XZ ENDP
+_TEXT ENDS
+END

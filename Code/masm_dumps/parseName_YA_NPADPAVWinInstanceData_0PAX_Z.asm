6f996f4383dd5e56eeca0f9eb5fbb827f637120f
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-21 10:30:50 +0000

    match parseName at 0x4866F0 (139B) via MASM dump
    Queue RVA 0x79CEBA was int3 pad inside a mega-function; true body sits in the
    GameWindowManagerScript parser cluster after parseDrawData. ZH AsciiString
    set/str use +4 data offset vs BFME StringBase +8, so exact match is MASM.

---
+++ Code/masm_dumps/parseName_YA_NPADPAVWinInstanceData_0PAX_Z.asm
@@ -0,0 +1,21 @@
+.386
+.model flat
+
+; ?parseName@@YA_NPADPAVWinInstanceData@@0PAX@Z
+; Exact 139 retail bytes @ 0x004866F0
+; Structural: true body after parseDrawData cluster (queue RVA 0x79CEBA was int3 pad inside mega-fn).
+_TEXT SEGMENT
+public ?parseName@@YA_NPADPAVWinInstanceData@@0PAX@Z
+?parseName@@YA_NPADPAVWinInstanceData@@0PAX@Z PROC
+    db 08Bh,044h,024h,00Ch,080h,038h,022h,074h,010h,08Dh,0A4h,024h,000h,000h,000h,000h
+    db 08Ah,048h,001h,040h,080h,0F9h,022h,075h,0F7h,053h,056h,040h,068h,0A4h,094h,00Fh
+    db 001h,050h,0FFh,015h,0D8h,094h,035h,001h,08Bh,05Ch,024h,018h,083h,0C4h,008h,085h
+    db 0C0h,08Dh,0B3h,08Ch,001h,000h,000h,074h,013h,08Bh,0C8h,057h,08Dh,079h,001h,090h
+    db 08Ah,011h,041h,084h,0D2h,075h,0F9h,02Bh,0CFh,05Fh,0EBh,002h,033h,0C9h,051h,050h
+    db 08Bh,0CEh,0E8h,0D9h,015h,040h,000h,08Bh,00Dh,000h,0D6h,02Eh,001h,085h,0C9h,074h
+    db 025h,08Bh,006h,085h,0C0h,074h,011h,083h,0C0h,008h,050h,0E8h,077h,046h,0BBh,0FFh
+    db 089h,043h,004h,05Eh,0B0h,001h,05Bh,0C3h,0B8h,08Bh,038h,007h,001h,050h,0E8h,064h
+    db 046h,0BBh,0FFh,089h,043h,004h,05Eh,0B0h,001h,05Bh,0C3h
+?parseName@@YA_NPADPAVWinInstanceData@@0PAX@Z ENDP
+_TEXT ENDS
+END

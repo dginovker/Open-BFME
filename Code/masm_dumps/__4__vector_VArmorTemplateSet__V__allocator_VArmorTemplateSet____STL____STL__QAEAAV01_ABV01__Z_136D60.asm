895f149e1d1fdd0daf7802829eeded00084ddd49
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-21 20:42:17 +0000

    match vector<ArmorTemplateSet>::operator= at true body 0x136D60
    Queue RVA 0x3B62CE was int3 padding before the already-matched
    vector<PrereqUnitRec>::operator= (0x3B62F0). True 280B body found via
    __copy@PAVArmorTemplateSet call sites; trivial-dtor STLport shape does not
    match our 324B C++ emit, so land exact retail bytes as MASM.

---
+++ Code/masm_dumps/__4__vector_VArmorTemplateSet__V__allocator_VArmorTemplateSet____STL____STL__QAEAAV01_ABV01__Z_136D60.asm
@@ -0,0 +1,29 @@
+.386
+.model flat
+
+; ??4?$vector@VArmorTemplateSet@@V?$allocator@VArmorTemplateSet@@@_STL@@@_STL@@QAEAAV01@ABV01@@Z
+; Exact 280B @ 0x00136D60; queue 0x3B62CE was int3 before PrereqUnitRec op=; proven via __copy@PAVArmorTemplateSet
+_TEXT SEGMENT
+public ??4?$vector@VArmorTemplateSet@@V?$allocator@VArmorTemplateSet@@@_STL@@@_STL@@QAEAAV01@ABV01@@Z
+??4?$vector@VArmorTemplateSet@@V?$allocator@VArmorTemplateSet@@@_STL@@@_STL@@QAEAAV01@ABV01@@Z PROC
+    db 55h, 8Bh, 6Ch, 24h, 08h, 56h, 8Bh, 0F1h, 3Bh, 0EEh, 0Fh, 84h, 01h, 01h, 00h, 00h
+    db 8Bh, 55h, 04h, 8Bh, 4Dh, 00h, 89h, 54h, 24h, 0Ch, 2Bh, 0D1h, 0B8h, 0ABh, 0AAh, 0AAh
+    db 2Ah, 0F7h, 0EAh, 0D1h, 0FAh, 53h, 8Bh, 1Eh, 57h, 8Bh, 0FAh, 0C1h, 0EFh, 1Fh, 03h, 0FAh
+    db 8Bh, 56h, 08h, 2Bh, 0D3h, 0B8h, 0ABh, 0AAh, 0AAh, 2Ah, 0F7h, 0EAh, 0D1h, 0FAh, 8Bh, 0C2h
+    db 0C1h, 0E8h, 1Fh, 03h, 0C2h, 3Bh, 0F8h, 76h, 36h, 8Bh, 54h, 24h, 14h, 52h, 51h, 57h
+    db 8Bh, 0CEh, 0E8h, 0A4h, 0B6h, 0F0h, 0FFh, 8Bh, 0CEh, 8Bh, 0D8h, 0E8h, 0ECh, 0EFh, 0EDh, 0FFh
+    db 8Dh, 04h, 7Fh, 8Dh, 0Ch, 83h, 89h, 4Eh, 08h, 8Dh, 04h, 7Fh, 8Bh, 0CBh, 5Fh, 89h
+    db 1Eh, 8Dh, 14h, 81h, 5Bh, 89h, 56h, 04h, 8Bh, 0C6h, 5Eh, 5Dh, 0C2h, 04h, 00h, 8Bh
+    db 56h, 04h, 2Bh, 0D3h, 0B8h, 0ABh, 0AAh, 0AAh, 2Ah, 0F7h, 0EAh, 0D1h, 0FAh, 8Bh, 0C2h, 0C1h
+    db 0E8h, 1Fh, 03h, 0C2h, 3Bh, 0C7h, 6Ah, 00h, 8Dh, 54h, 24h, 18h, 52h, 53h, 72h, 22h
+    db 8Bh, 44h, 24h, 20h, 50h, 51h, 0E8h, 0BDh, 3Fh, 0F0h, 0FFh, 8Bh, 0Eh, 83h, 0C4h, 14h
+    db 8Dh, 04h, 7Fh, 5Fh, 8Dh, 14h, 81h, 5Bh, 89h, 56h, 04h, 8Bh, 0C6h, 5Eh, 5Dh, 0C2h
+    db 04h, 00h, 8Dh, 04h, 40h, 8Dh, 14h, 81h, 52h, 51h, 0E8h, 99h, 3Fh, 0F0h, 0FFh, 8Bh
+    db 46h, 04h, 8Bh, 55h, 04h, 8Dh, 4Ch, 24h, 28h, 51h, 8Bh, 0Eh, 50h, 2Bh, 0C1h, 8Bh
+    db 0C8h, 52h, 0B8h, 0ABh, 0AAh, 0AAh, 2Ah, 0F7h, 0E9h, 8Bh, 4Dh, 00h, 0D1h, 0FAh, 8Bh, 0C2h
+    db 0C1h, 0E8h, 1Fh, 03h, 0C2h, 8Dh, 04h, 40h, 8Dh, 14h, 81h, 52h, 0E8h, 0EDh, 0AFh, 0EFh
+    db 0FFh, 8Bh, 0Eh, 83h, 0C4h, 24h, 8Dh, 04h, 7Fh, 8Dh, 14h, 81h, 5Fh, 89h, 56h, 04h
+    db 5Bh, 8Bh, 0C6h, 5Eh, 5Dh, 0C2h, 04h, 00h
+??4?$vector@VArmorTemplateSet@@V?$allocator@VArmorTemplateSet@@@_STL@@@_STL@@QAEAAV01@ABV01@@Z ENDP
+_TEXT ENDS
+END

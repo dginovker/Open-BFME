ce8925560c5096f020b89b062e285c17535b97cc
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-21 20:07:25 +0000

    Match setActiveBoundary@TerrainLogic via exact MASM dump.
    Queue RVA 0xC70E5F was int3 padding; true body is 183B at 0x1A8080
    (proven by matched doBorderSwitch call chain). BFME uses a simplified
    rewrite vs ZH's 428B shape, so land retail bytes rather than force C++.

---
+++ Code/masm_dumps/_setActiveBoundary_TerrainLogic__QAEXH_Z_1A8080.asm
@@ -0,0 +1,23 @@
+.386
+.model flat
+
+; ?setActiveBoundary@TerrainLogic@@QAEXH@Z
+; Exact 183B @ 0x001A8080; doBorderSwitch@0x2EFC50 call->thunk 0x1E29A->body; BFME simplified vs ZH
+_TEXT SEGMENT
+public ?setActiveBoundary@TerrainLogic@@QAEXH@Z
+?setActiveBoundary@TerrainLogic@@QAEXH@Z PROC
+    db 08Bh,044h,024h,004h,083h,0ECh,018h,085h,0C0h,00Fh,08Ch,0A2h,000h,000h,000h,08Bh
+    db 051h,028h,056h,08Bh,071h,024h,02Bh,0D6h,0C1h,0FAh,003h,03Bh,0C2h,00Fh,083h,08Dh
+    db 000h,000h,000h,03Bh,041h,030h,00Fh,084h,084h,000h,000h,000h,08Bh,0D6h,08Bh,034h
+    db 0C2h,085h,0F6h,08Dh,014h,0C2h,074h,078h,08Bh,072h,004h,085h,0F6h,074h,071h,08Dh
+    db 054h,024h,004h,089h,041h,030h,08Bh,001h,052h,0FFh,050h,020h,08Bh,00Dh,0E4h,0F0h
+    db 02Eh,001h,08Bh,015h,0CCh,0F4h,02Eh,001h,08Bh,001h,052h,0FFh,050h,018h,08Bh,00Dh
+    db 0B8h,0D5h,02Eh,001h,08Dh,044h,024h,004h,050h,0E8h,052h,0A5h,084h,000h,06Ah,000h
+    db 08Dh,04Ch,024h,008h,051h,08Bh,00Dh,0BCh,0D5h,02Eh,001h,0E8h,090h,0F2h,074h,000h
+    db 08Bh,00Dh,098h,008h,02Fh,001h,0E8h,0B3h,0D7h,0E8h,0FFh,08Bh,0F0h,085h,0F6h,074h
+    db 011h,08Bh,0CEh,0E8h,08Ah,039h,0E9h,0FFh,08Bh,0B6h,088h,000h,000h,000h,085h,0F6h
+    db 075h,0EFh,08Bh,00Dh,000h,016h,02Fh,001h,08Bh,011h,0FFh,092h,0B4h,001h,000h,000h
+    db 05Eh,083h,0C4h,018h,0C2h,004h,000h
+?setActiveBoundary@TerrainLogic@@QAEXH@Z ENDP
+_TEXT ENDS
+END

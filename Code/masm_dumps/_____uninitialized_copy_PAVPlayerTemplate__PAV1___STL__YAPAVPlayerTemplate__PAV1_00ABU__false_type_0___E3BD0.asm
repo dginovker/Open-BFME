1e882e7b0e772b8ffbe79f02491cd33eec043ceb
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-21 21:55:41 +0000

    Match STLport __uninitialized_copy<PlayerTemplate*> via exact MASM dump.
    Queue RVA 0x853EDF was mid-function of an unrelated ctor; true body is
    0xE3BD0 (51B) beside matched _Construct/_Destroy, a no-SEH stride-0x124
    loop through the _Construct thunk. C++ emits a 132B SEH frame under /EHs.

---
+++ Code/masm_dumps/_____uninitialized_copy_PAVPlayerTemplate__PAV1___STL__YAPAVPlayerTemplate__PAV1_00ABU__false_type_0___E3BD0.asm
@@ -0,0 +1,15 @@
+.386
+.model flat
+
+; ??$__uninitialized_copy@PAVPlayerTemplate@@PAV1@@_STL@@YAPAVPlayerTemplate@@PAV1@00ABU__false_type@0@@Z
+; Exact 51 retail bytes @ 0x000E3BD0 (true body; queue 0x853EDF was mid-fn of unrelated ctor)
+_TEXT SEGMENT
+public ??$__uninitialized_copy@PAVPlayerTemplate@@PAV1@@_STL@@YAPAVPlayerTemplate@@PAV1@00ABU__false_type@0@@Z
+??$__uninitialized_copy@PAVPlayerTemplate@@PAV1@@_STL@@YAPAVPlayerTemplate@@PAV1@00ABU__false_type@0@@Z PROC
+    db 053h,08Bh,05Ch,024h,00Ch,056h,08Bh,074h,024h,00Ch,03Bh,0F3h,057h,08Bh,07Ch,024h
+    db 018h,074h,01Ah,056h,057h,0E8h,036h,007h,0F4h,0FFh,081h,0C6h,024h,001h,000h,000h
+    db 083h,0C4h,008h,081h,0C7h,024h,001h,000h,000h,03Bh,0F3h,075h,0E6h,08Bh,0C7h,05Fh
+    db 05Eh,05Bh,0C3h
+??$__uninitialized_copy@PAVPlayerTemplate@@PAV1@@_STL@@YAPAVPlayerTemplate@@PAV1@00ABU__false_type@0@@Z ENDP
+_TEXT ENDS
+END

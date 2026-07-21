c6829cc4b00846834e308a64237df24bc5ae2fe9
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-21 18:14:53 +0000

    match: MapObject deque _M_create_nodes @ 0x8F9E30 (40B MASM)
    Queue RVA 0x853EEA was mid-epilogue of an unrelated function. True body is
    the no-SEH 40B push-0x78 node alloc loop at 0x8F9E30 in the MapObject deque
    cluster (paired with destroy_nodes@0x8F9E60; initialize_map inlines the same
    loop). C++ emits 121B with SEH; land exact retail bytes via MASM.

---
+++ Code/masm_dumps/_M_create_nodes_Deque_base_PAVMapObject_8F9E30.asm
@@ -0,0 +1,14 @@
+.386
+.model flat
+
+; ?_M_create_nodes@?$_Deque_base@PAVMapObject@@V?$allocator@PAVMapObject@@@_STL@@@_STL@@IAEXPAPAPAVMapObject@@0@Z
+; Exact 40B @ 0x8F9E30; true body (queue 0x853EEA was mid-epilogue); no-SEH loop push 0x78
+_TEXT SEGMENT
+public ?_M_create_nodes@?$_Deque_base@PAVMapObject@@V?$allocator@PAVMapObject@@@_STL@@@_STL@@IAEXPAPAPAVMapObject@@0@Z
+?_M_create_nodes@?$_Deque_base@PAVMapObject@@V?$allocator@PAVMapObject@@@_STL@@@_STL@@IAEXPAPAPAVMapObject@@0@Z PROC
+    db 056h,08Bh,074h,024h,008h,057h,08Bh,07Ch,024h,010h,03Bh,0F7h,073h,015h,08Bh,0FFh
+    db 06Ah,078h,0E8h,0F9h,046h,0F3h,0FFh,089h,006h,083h,0C6h,004h,083h,0C4h,004h,03Bh
+    db 0F7h,072h,0EDh,05Fh,05Eh,0C2h,008h,000h
+?_M_create_nodes@?$_Deque_base@PAVMapObject@@V?$allocator@PAVMapObject@@@_STL@@@_STL@@IAEXPAPAPAVMapObject@@0@Z ENDP
+_TEXT ENDS
+END

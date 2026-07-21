581b59987bad73257e9f66a1a426255ab4de3432
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-21 09:02:37 +0000

    Match INI::parseDynamicGameLODDefinition via MASM exact dump.
    Queue RVA 0x14C080 was int3 pad; true body is 191B at 0x7D470
    (INI type-table DynamicGameLOD -> ILT 0x1A230 -> body).

---
+++ Code/masm_dumps/INI_parseDynamicGameLODDefinition_7d470.asm
@@ -0,0 +1,23 @@
+.386
+.model flat
+
+; ?parseDynamicGameLODDefinition@INI@@SAXPAV1@@Z
+; Retail @ 0x0007D470 size 191
+_TEXT SEGMENT
+public ?parseDynamicGameLODDefinition@INI@@SAXPAV1@@Z
+?parseDynamicGameLODDefinition@INI@@SAXPAV1@@Z PROC
+    db 6Ah,0FFh,68h,28h,44h,0FFh,00h,64h,0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,83h,0ECh,08h,56h,57h,0C7h,44h,24h,08h,00h,00h
+    db 00h,00h,8Bh,7Ch,24h,20h,6Ah,00h,8Bh,0CFh,0C7h,44h,24h,1Ch,00h,00h
+    db 00h,00h,0E8h,0C9h,34h,7Dh,00h,85h,0C0h,74h,10h,8Bh,0C8h,8Dh,71h,01h
+    db 8Ah,11h,41h,84h,0D2h,75h,0F9h,2Bh,0CEh,0EBh,02h,33h,0C9h,51h,50h,8Dh
+    db 4Ch,24h,10h,0E8h,58h,0A8h,80h,00h,0A1h,0ACh,0D5h,2Eh,01h,85h,0C0h,74h
+    db 3Ch,51h,8Dh,44h,24h,0Ch,89h,64h,24h,10h,8Bh,0CCh,50h,0E8h,7Eh,0A6h
+    db 80h,00h,8Bh,0Dh,0ACh,0D5h,2Eh,01h,0E8h,52h,9Bh,0F9h,0FFh,83h,0F8h,0FFh
+    db 74h,1Bh,8Bh,35h,0ACh,0D5h,2Eh,01h,83h,0C0h,12h,0C1h,0E0h,04h,03h,0C6h
+    db 68h,90h,68h,07h,01h,50h,8Bh,0CFh,0E8h,93h,4Bh,7Dh,00h,8Dh,4Ch,24h
+    db 08h,0C7h,44h,24h,18h,0FFh,0FFh,0FFh,0FFh,0E8h,22h,0A4h,80h,00h,8Bh,4Ch
+    db 24h,10h,5Fh,64h,89h,0Dh,00h,00h,00h,00h,5Eh,83h,0C4h,14h,0C3h
+?parseDynamicGameLODDefinition@INI@@SAXPAV1@@Z ENDP
+_TEXT ENDS
+END

e1b91d5b46f7cb8b36f2cb959b62b67a7de11f0d
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-21 13:45:27 +0000

    Match NickMap map<AsciiString,list>::operator[] at true 0x82050
    Queue RVA 0x64E690 is a different string-key map; sole caller of the
    matched insert_unique list body is 244B at 0x82050. Land exact retail
    bytes via MASM with object-symbol alias for the long mangled name.

---
+++ Code/masm_dumps/_map_op_index_AsciiString_list_95bb81ee1e54_82050.asm
@@ -0,0 +1,30 @@
+.386
+.model flat
+
+; ??A?$map@VAsciiString@@V?$list@VAsciiString@@V?$allocator@VAsciiString@@@_STL@@@_STL@@U?$less@VAscii
+; String@@@3@V?$allocator@U?$pair@$$CBVAsciiString@@V?$list@VAsciiString@@V?$allocator@VAsciiString@@@
+; _STL@@@_STL@@@_STL@@@3@@_STL@@QAEAAV?$list@VAsciiString@@V?$allocator@VAsciiString@@@_STL@@@1@ABVAsc
+; iiString@@@Z
+; Exact 244B NickMap map operator[] @ 0x82050; queue 0x64E690 was string-key map misplacement
+_TEXT SEGMENT
+public _bfme_export_95bb81ee
+_bfme_export_95bb81ee PROC
+    db 06Ah, 0FFh, 064h, 0A1h, 000h, 000h, 000h, 000h, 068h, 038h, 049h, 0FFh, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 083h, 0ECh, 00Ch, 053h, 08Bh, 05Ch, 024h, 020h, 055h, 056h, 057h
+    db 053h, 08Bh, 0F9h, 0E8h, 080h, 0D2h, 0F9h, 0FFh, 08Bh, 0E8h, 03Bh, 02Fh, 074h, 013h, 08Dh, 045h
+    db 010h, 050h, 08Bh, 0CBh, 0E8h, 03Ch, 000h, 0FAh, 0FFh, 085h, 0C0h, 00Fh, 08Dh, 09Bh, 000h, 000h
+    db 000h, 06Ah, 00Ch, 0E8h, 0A8h, 0C4h, 07Ah, 000h, 08Bh, 0F0h, 083h, 0C4h, 004h, 089h, 036h, 089h
+    db 076h, 004h, 089h, 074h, 024h, 02Ch, 053h, 08Dh, 04Ch, 024h, 018h, 0C7h, 044h, 024h, 028h, 000h
+    db 000h, 000h, 000h, 0E8h, 0A8h, 05Ah, 080h, 000h, 08Dh, 04Ch, 024h, 02Ch, 051h, 08Dh, 04Ch, 024h
+    db 01Ch, 0C6h, 044h, 024h, 028h, 001h, 0E8h, 010h, 0FAh, 0F8h, 0FFh, 08Dh, 054h, 024h, 014h, 052h
+    db 051h, 08Bh, 0C4h, 089h, 028h, 08Dh, 044h, 024h, 018h, 050h, 08Bh, 0CFh, 0C6h, 044h, 024h, 030h
+    db 002h, 0E8h, 06Bh, 02Dh, 0FAh, 0FFh, 08Bh, 07Ch, 024h, 010h, 08Dh, 04Ch, 024h, 014h, 0C6h, 044h
+    db 024h, 024h, 000h, 0E8h, 09Eh, 023h, 0FBh, 0FFh, 08Dh, 04Ch, 024h, 02Ch, 0C7h, 044h, 024h, 024h
+    db 0FFh, 0FFh, 0FFh, 0FFh, 0E8h, 002h, 06Fh, 0F9h, 0FFh, 06Ah, 00Ch, 056h, 0E8h, 0DFh, 0C4h, 07Ah
+    db 000h, 083h, 0C4h, 008h, 08Dh, 047h, 014h, 08Bh, 04Ch, 024h, 01Ch, 064h, 089h, 00Dh, 000h, 000h
+    db 000h, 000h, 05Fh, 05Eh, 05Dh, 05Bh, 083h, 0C4h, 018h, 0C2h, 004h, 000h, 08Bh, 04Ch, 024h, 01Ch
+    db 05Fh, 05Eh, 08Dh, 045h, 014h, 05Dh, 064h, 089h, 00Dh, 000h, 000h, 000h, 000h, 05Bh, 083h, 0C4h
+    db 018h, 0C2h, 004h, 000h
+_bfme_export_95bb81ee ENDP
+_TEXT ENDS
+END

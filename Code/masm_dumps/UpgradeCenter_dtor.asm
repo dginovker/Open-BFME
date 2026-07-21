47f962c118259524b421c1bfc40f651e060fa6a6
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-21 17:45:07 +0000

    Match ??1UpgradeCenter@@UAE@XZ at true body 0x10A8C0 (111B)
    Queue RVA 0x9F27EB was int3 pad before the ICF-folded TeamFactory/
    PartitionManager dtor group. True body recovered from UpgradeCenter
    ctor-anchored vtable 0x1088D24 via scalar deleting dtor chain.
    C++ blocked by UpgradeTemplate m_next (+0x108 retail vs +0xF8 ZH) and
    deleteInstance vs virtual scalar delete; land exact retail via MASM.

---
+++ Code/masm_dumps/UpgradeCenter_dtor.asm
@@ -0,0 +1,19 @@
+.386
+.model flat
+
+; ??1UpgradeCenter@@UAE@XZ
+; Exact 111 retail bytes @ 0x0010A8C0; ctor-anchored vtable 0x1088D24 slot0 scalar dtor
+; (ILT 0x40976->0x10AEC0) calls this body; next link +0x108, virtual delete, base dtor 0x9A1A40.
+_TEXT SEGMENT
+public ??1UpgradeCenter@@UAE@XZ
+??1UpgradeCenter@@UAE@XZ PROC
+    db 06Ah,0FFh,068h,0B8h,0D2h,0FFh,000h,064h,0A1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,051h,056h,08Bh,0F1h,089h,074h,024h,004h,0C7h,006h,024h
+    db 08Dh,008h,001h,08Bh,046h,008h,085h,0C0h,0C7h,044h,024h,010h,000h,000h,000h,000h
+    db 074h,01Eh,057h,08Bh,04Eh,008h,085h,0C9h,08Bh,0B9h,008h,001h,000h,000h,074h,006h
+    db 08Bh,001h,06Ah,001h,0FFh,010h,08Bh,0C7h,085h,0C0h,089h,07Eh,008h,075h,0E4h,05Fh
+    db 08Bh,0CEh,0C7h,044h,024h,010h,0FFh,0FFh,0FFh,0FFh,0E8h,021h,071h,089h,000h,08Bh
+    db 04Ch,024h,008h,05Eh,064h,089h,00Dh,000h,000h,000h,000h,083h,0C4h,010h,0C3h
+??1UpgradeCenter@@UAE@XZ ENDP
+_TEXT ENDS
+END

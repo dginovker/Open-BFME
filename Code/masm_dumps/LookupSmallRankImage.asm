110470707040391ee0c287ff74d052e35e380603
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-21 12:19:51 +0000

    Match LookupSmallRankImage at true retail 0x4F9CE0 (302B).
    Drift candidate 0xC6C3EF was int3 padding plus CRT/static-init stubs.
    True body is the lobby %s-%s rank-image lookup immediately before
    OfficersClubsmall usage; land exact bytes via MASM (C++ blocked by
    AsciiString layout and SEH prologue order).

---
+++ Code/masm_dumps/LookupSmallRankImage.asm
@@ -0,0 +1,30 @@
+.386
+.model flat
+
+; ?LookupSmallRankImage@@YAPBVImage@@HH@Z
+; Exact 302 retail bytes @ 0x004F9CE0; %s-%s rank image format + OfficersClubsmall neighbor proves lobby ownership.
+_TEXT SEGMENT
+public ?LookupSmallRankImage@@YAPBVImage@@HH@Z
+?LookupSmallRankImage@@YAPBVImage@@HH@Z PROC
+    db 64h, 0A1h, 00h, 00h, 00h, 00h, 8Bh, 4Ch, 24h, 08h, 6Ah, 0FFh, 68h, 0F0h, 0CEh, 02h
+    db 01h, 50h, 64h, 89h, 25h, 00h, 00h, 00h, 00h, 83h, 0ECh, 08h, 85h, 0C9h, 56h, 0Fh
+    db 84h, 0F7h, 00h, 00h, 00h, 0A1h, 1Ch, 40h, 2Fh, 01h, 8Bh, 50h, 04h, 83h, 0C0h, 04h
+    db 33h, 0F6h, 3Bh, 0CAh, 7Ch, 1Ch, 8Bh, 50h, 04h, 83h, 0C0h, 04h, 46h, 3Bh, 0CAh, 7Dh
+    db 0F5h, 85h, 0F6h, 0Fh, 8Ch, 0D3h, 00h, 00h, 00h, 83h, 0FEh, 0Ah, 0Fh, 8Dh, 0CAh, 00h
+    db 00h, 00h, 68h, 0CCh, 2Fh, 10h, 01h, 8Dh, 4Ch, 24h, 24h, 0E8h, 80h, 0EEh, 38h, 00h
+    db 8Bh, 44h, 24h, 1Ch, 83h, 0E8h, 02h, 0C7h, 44h, 24h, 14h, 00h, 00h, 00h, 00h, 74h
+    db 14h, 48h, 74h, 0Ah, 48h, 75h, 1Ch, 68h, 40h, 0Ch, 10h, 01h, 0EBh, 0Ch, 68h, 0C8h
+    db 2Fh, 10h, 01h, 0EBh, 05h, 68h, 6Ch, 1Dh, 10h, 01h, 8Dh, 4Ch, 24h, 24h, 0E8h, 46h
+    db 0EEh, 0B2h, 0FFh, 0C7h, 44h, 24h, 04h, 00h, 00h, 00h, 00h, 8Bh, 44h, 24h, 20h, 85h
+    db 0C0h, 0C6h, 44h, 24h, 14h, 01h, 74h, 05h, 83h, 0C0h, 08h, 0EBh, 05h, 0B8h, 8Bh, 38h
+    db 07h, 01h, 50h, 8Bh, 04h, 0B5h, 0FCh, 6Fh, 2Bh, 01h, 50h, 51h, 89h, 64h, 24h, 14h
+    db 8Bh, 0CCh, 68h, 0C0h, 2Fh, 10h, 01h, 0E8h, 14h, 0EEh, 38h, 00h, 8Dh, 4Ch, 24h, 10h
+    db 51h, 0E8h, 3Ah, 0F2h, 38h, 00h, 8Bh, 0Dh, 24h, 69h, 2Fh, 01h, 83h, 0C4h, 10h, 8Dh
+    db 54h, 24h, 04h, 52h, 0E8h, 3Dh, 38h, 0B2h, 0FFh, 8Dh, 4Ch, 24h, 04h, 8Bh, 0F0h, 0C6h
+    db 44h, 24h, 14h, 00h, 0E8h, 67h, 0DBh, 38h, 00h, 8Dh, 4Ch, 24h, 20h, 0C7h, 44h, 24h
+    db 14h, 0FFh, 0FFh, 0FFh, 0FFh, 0E8h, 56h, 0DBh, 38h, 00h, 8Bh, 0C6h, 8Bh, 4Ch, 24h, 0Ch
+    db 64h, 89h, 0Dh, 00h, 00h, 00h, 00h, 5Eh, 83h, 0C4h, 14h, 0C3h, 8Bh, 4Ch, 24h, 0Ch
+    db 33h, 0C0h, 64h, 89h, 0Dh, 00h, 00h, 00h, 00h, 5Eh, 83h, 0C4h, 14h, 0C3h
+?LookupSmallRankImage@@YAPBVImage@@HH@Z ENDP
+_TEXT ENDS
+END

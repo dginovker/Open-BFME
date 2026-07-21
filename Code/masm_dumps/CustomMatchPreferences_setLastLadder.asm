4660f1f8e48277e2394dc766cc3029e4e98c2bbf
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-21 21:43:42 +0000

    Match CustomMatchPreferences::setLastLadder via exact MASM dump
    Ghidra ENTRY at 0xAD7A0 is 223B (queue 384 oversize into next fn).
    String anchors LastLadderAddr/LastLadderPort/%d prove identity; C++ blocked
    by thin AsciiString(const char*) path. Exact retail bytes under masm_dumps.

---
+++ Code/masm_dumps/CustomMatchPreferences_setLastLadder.asm
@@ -0,0 +1,25 @@
+.386
+.model flat
+
+; ?setLastLadder@CustomMatchPreferences@@QAEXABVAsciiString@@G@Z
+; Exact 223 retail bytes @ 0x000AD7A0; LastLadderAddr/Port format+map assign
+_TEXT SEGMENT
+public ?setLastLadder@CustomMatchPreferences@@QAEXABVAsciiString@@G@Z
+?setLastLadder@CustomMatchPreferences@@QAEXABVAsciiString@@G@Z PROC
+    db 06Ah,0FFh,068h,028h,07Ah,0FFh,000h,064h,0A1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,051h,053h,033h,0DBh,056h,08Bh,0F1h,089h,05Ch,024h,008h
+    db 00Fh,0B7h,044h,024h,020h,050h,051h,089h,064h,024h,028h,08Bh,0CCh,068h,0B4h,0C7h
+    db 007h,001h,089h,05Ch,024h,020h,0E8h,0E5h,0B3h,07Dh,000h,08Dh,04Ch,024h,010h,051h
+    db 0E8h,00Bh,0B8h,07Dh,000h,083h,0C4h,00Ch,068h,05Ch,011h,008h,001h,08Dh,04Ch,024h
+    db 024h,0E8h,0CAh,0B3h,07Dh,000h,08Dh,054h,024h,020h,083h,0C6h,004h,052h,08Bh,0CEh
+    db 0C6h,044h,024h,018h,001h,0E8h,0D0h,00Eh,0F9h,0FFh,08Bh,04Ch,024h,01Ch,051h,08Bh
+    db 0C8h,0E8h,07Ah,0A4h,07Dh,000h,08Dh,04Ch,024h,020h,088h,05Ch,024h,014h,0E8h,01Dh
+    db 0A1h,07Dh,000h,068h,070h,011h,008h,001h,08Dh,04Ch,024h,024h,0E8h,08Fh,0B3h,07Dh
+    db 000h,08Dh,054h,024h,020h,052h,08Bh,0CEh,0C6h,044h,024h,018h,002h,0E8h,098h,00Eh
+    db 0F9h,0FFh,08Dh,04Ch,024h,008h,051h,08Bh,0C8h,0E8h,042h,0A4h,07Dh,000h,08Dh,04Ch
+    db 024h,020h,088h,05Ch,024h,014h,0E8h,0E5h,0A0h,07Dh,000h,08Dh,04Ch,024h,008h,0C7h
+    db 044h,024h,014h,0FFh,0FFh,0FFh,0FFh,0E8h,0D4h,0A0h,07Dh,000h,08Bh,04Ch,024h,00Ch
+    db 05Eh,064h,089h,00Dh,000h,000h,000h,000h,05Bh,083h,0C4h,010h,0C2h,008h,000h
+?setLastLadder@CustomMatchPreferences@@QAEXABVAsciiString@@G@Z ENDP
+_TEXT ENDS
+END

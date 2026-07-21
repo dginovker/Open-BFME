dcf13dbff1ea3dd92e0c73adfa28fc90c7abf0a8
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-21 11:39:35 +0000

    Match doRotateCameraTowardObject@ScriptActions via MASM (118B @ 0x2EE060).
    Drift-report RVA 0x7A8655 was misplaced; true body is getUnitNamed +
    rotateCameraTowardObject with BFME vtable slots and an extra stack arg.

---
+++ Code/masm_dumps/ScriptActions_doRotateCameraTowardObject.asm
@@ -0,0 +1,19 @@
+.386
+.model flat
+
+; ?doRotateCameraTowardObject@ScriptActions@@IAEXABVAsciiString@@MMMM@Z
+; Exact 118 retail bytes @ 0x002EE060; drift 0x007A8655 was misplaced.
+_TEXT SEGMENT
+public ?doRotateCameraTowardObject@ScriptActions@@IAEXABVAsciiString@@MMMM@Z
+?doRotateCameraTowardObject@ScriptActions@@IAEXABVAsciiString@@MMMM@Z PROC
+    db 08Bh,00Dh,06Ch,007h,02Fh,001h,08Bh,054h,024h,004h,08Bh,001h,056h,052h,0FFh,050h
+    db 068h,08Bh,0F0h,085h,0F6h,074h,05Bh,08Bh,044h,024h,01Ch,0D9h,044h,024h,018h,0D8h
+    db 00Dh,068h,05Ch,007h,001h,053h,057h,08Bh,03Dh,000h,016h,02Fh,001h,08Bh,01Fh,050h
+    db 083h,0ECh,008h,0D9h,05Ch,024h,004h,0D9h,044h,024h,028h,0D8h,00Dh,068h,05Ch,007h
+    db 001h,0D9h,01Ch,024h,0D9h,044h,024h,024h,0D8h,00Dh,068h,05Ch,007h,001h,0E8h,085h
+    db 08Dh,070h,000h,0D9h,044h,024h,020h,0D8h,00Dh,068h,05Ch,007h,001h,050h,0E8h,075h
+    db 08Dh,070h,000h,08Bh,04Eh,074h,050h,051h,08Bh,0CFh,0FFh,093h,0CCh,000h,000h,000h
+    db 05Fh,05Bh,05Eh,0C2h,018h,000h
+?doRotateCameraTowardObject@ScriptActions@@IAEXABVAsciiString@@MMMM@Z ENDP
+_TEXT ENDS
+END

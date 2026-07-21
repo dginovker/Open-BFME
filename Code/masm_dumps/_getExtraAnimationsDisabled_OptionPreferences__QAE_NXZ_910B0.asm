845f4e8b13c6a68ebfd70af1c79215d75946d186
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-21 14:25:04 +0000

    Match getExtraAnimationsDisabled via MASM at 0x910B0.
    Queue RVA 0x560A55 was OptionsMenuInit interior; true 101B body
    is string-anchored on ExtraAnimations (sibling of getDynamicLODEnabled).

---
+++ Code/masm_dumps/_getExtraAnimationsDisabled_OptionPreferences__QAE_NXZ_910B0.asm
@@ -0,0 +1,17 @@
+.386
+.model flat
+; ?getExtraAnimationsDisabled@OptionPreferences@@QAE_NXZ
+; Exact 101B @ 0x910B0; string-anchor ExtraAnimations true body (queue 0x560A55 was OptionsMenuInit interior)
+_TEXT SEGMENT
+public ?getExtraAnimationsDisabled@OptionPreferences@@QAE_NXZ
+?getExtraAnimationsDisabled@OptionPreferences@@QAE_NXZ PROC
+    db 51h, 56h, 57h, 8Bh, 0F1h, 68h, 0ECh, 6Ch, 07h, 01h, 8Dh, 4Ch, 24h, 0Ch, 0E8h, 0FDh
+    db 7Ah, 7Fh, 00h, 8Dh, 44h, 24h, 08h, 83h, 0C6h, 04h, 50h, 8Bh, 0CEh, 0E8h, 0DAh, 9Dh
+    db 0F7h, 0FFh, 8Dh, 4Ch, 24h, 08h, 8Bh, 0F8h, 0E8h, 63h, 68h, 7Fh, 00h, 3Bh, 3Eh, 75h
+    db 0Dh, 8Bh, 0Dh, 0C8h, 0D5h, 2Eh, 01h, 8Ah, 41h, 1Ch, 5Fh, 5Eh, 59h, 0C3h, 8Bh, 7Fh
+    db 14h, 85h, 0FFh, 8Dh, 47h, 08h, 75h, 05h, 0B8h, 8Bh, 38h, 07h, 01h, 68h, 6Ch, 0C7h
+    db 07h, 01h, 50h, 0FFh, 15h, 3Ch, 93h, 35h, 01h, 83h, 0C4h, 08h, 85h, 0C0h, 5Fh, 0Fh
+    db 95h, 0C0h, 5Eh, 59h, 0C3h
+?getExtraAnimationsDisabled@OptionPreferences@@QAE_NXZ ENDP
+_TEXT ENDS
+END

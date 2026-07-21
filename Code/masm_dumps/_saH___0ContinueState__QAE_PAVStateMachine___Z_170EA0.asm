1518e19f6f8af27141baba86f255d7b8f1be89c0
dginovker <danielginovker@gmail.com>
2026-07-20 17:20:11 -0700

    Match ContinueState ctor 46B via string-anchor MASM (Open-BFME4+Grok)

---
+++ Code/masm_dumps/_saH___0ContinueState__QAE_PAVStateMachine___Z_170EA0.asm
@@ -0,0 +1,13 @@
+.386
+.model flat
+; ??0ContinueState@@QAE@PAVStateMachine@@@Z
+; Exact 46B @ 0x170EA0; Open-BFME4+Grok
+_TEXT SEGMENT
+public ??0ContinueState@@QAE@PAVStateMachine@@@Z
+??0ContinueState@@QAE@PAVStateMachine@@@Z PROC
+    db 51h, 56h, 51h, 8Bh, 0F1h, 89h, 64h, 24h, 08h, 8Bh, 0CCh, 68h, 0Ch, 7Ah, 09h, 01h
+    db 0E8h, 0Bh, 7Dh, 71h, 00h, 8Bh, 44h, 24h, 10h, 50h, 8Bh, 0CEh, 0E8h, 0F1h, 26h, 0E9h
+    db 0FFh, 0C7h, 06h, 0B8h, 79h, 09h, 01h, 8Bh, 0C6h, 5Eh, 59h, 0C2h, 04h, 00h
+??0ContinueState@@QAE@PAVStateMachine@@@Z ENDP
+_TEXT ENDS
+END

ca4a8feb04c4c129762fcc61594a2e6eb741bb82
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-21 10:35:32 +0000

    Match doAudioFeedback at true RVA 0x4B85F0
    Queue RVA 0xC6C420 was CRT/static-init stubs; true body is 153B at
    0x4B85F0 (GUIComboBoxClick string xref). Land exact retail bytes via MASM.

---
+++ Code/masm_dumps/doAudioFeedback_YAXPAVGameWindow_Z_4B85F0.asm
@@ -0,0 +1,25 @@
+.386
+.model flat
+
+; ?doAudioFeedback@@YAXPAVGameWindow@@@Z
+; Retail @ 0x004B85F0 size 153
+; True body: queue RVA 0x00C6C420 was CRT/static-init stubs (misplaced).
+_TEXT SEGMENT
+public ?doAudioFeedback@@YAXPAVGameWindow@@@Z
+?doAudioFeedback@@YAXPAVGameWindow@@@Z PROC
+    db 64h,0A1h,00h,00h,00h,00h,6Ah,0FFh,68h,0B0h,8Ch,02h
+    db 01h,50h,64h,89h,25h,00h,00h,00h,00h,83h,0ECh,74h
+    db 85h,0C9h,74h,6Bh,0E8h,27h,0DFh,0B8h,0FFh,85h,0C0h,74h
+    db 62h,8Ah,48h,0Eh,84h,0C9h,74h,5Bh,68h,90h,0D8h,0Fh
+    db 01h,8Dh,4Ch,24h,04h,0E8h,96h,05h,3Dh,00h,6Ah,02h
+    db 8Dh,44h,24h,04h,50h,8Dh,4Ch,24h,0Ch,0C7h,84h,24h
+    db 84h,00h,00h,00h,00h,00h,00h,00h,0E8h,0C1h,0CCh,0B6h
+    db 0FFh,8Dh,0Ch,24h,0C6h,44h,24h,7Ch,02h,0E8h,0EEh,0F2h
+    db 3Ch,00h,8Bh,0Dh,68h,0D6h,2Eh,01h,85h,0C9h,74h,0Ah
+    db 8Bh,11h,8Dh,44h,24h,04h,50h,0FFh,52h,44h,8Dh,4Ch
+    db 24h,04h,0C7h,44h,24h,7Ch,0FFh,0FFh,0FFh,0FFh,0E8h,0BEh
+    db 0E8h,0B6h,0FFh,8Bh,4Ch,24h,74h,64h,89h,0Dh,00h,00h
+    db 00h,00h,81h,0C4h,80h,00h,00h,00h,0C3h
+?doAudioFeedback@@YAXPAVGameWindow@@@Z ENDP
+_TEXT ENDS
+END

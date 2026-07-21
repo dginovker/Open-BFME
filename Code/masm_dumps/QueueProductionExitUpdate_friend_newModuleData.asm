b02a68d66fef1772649ec77a5609eabf145c91d4
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-21 12:46:03 +0000

    Match QueueProductionExitUpdate::friend_newModuleData at 0x1241B0
    Drift candidate 0xB026DA was a WWMath epilogue misplacement. True 88B body
    proven via ModuleFactory registration ordinal 0x69 (class string + ILT thunk).

---
+++ Code/masm_dumps/QueueProductionExitUpdate_friend_newModuleData.asm
@@ -0,0 +1,19 @@
+.386
+.model flat
+
+; ?friend_newModuleData@QueueProductionExitUpdate@@SAPAVModuleData@@PAVINI@@@Z
+; Exact 88 retail bytes @ 0x001241B0; ModuleFactory registration ordinal 0x69
+; (push string "QueueProductionExitUpdate" + ILT 0x41964b -> 0x1241b0) proves ownership.
+; Drift candidate 0x00B026DA was WWMath epilogue misplacement (pop esi; ret 4).
+_TEXT SEGMENT
+public ?friend_newModuleData@QueueProductionExitUpdate@@SAPAVModuleData@@PAVINI@@@Z
+?friend_newModuleData@QueueProductionExitUpdate@@SAPAVModuleData@@PAVINI@@@Z PROC
+    db 056h, 06Ah, 034h, 0E8h, 078h, 0DDh, 075h, 000h, 033h, 0D2h, 083h, 0C4h, 004h, 03Bh, 0C2h, 074h
+    db 02Eh, 0C7h, 000h, 0C0h, 0CFh, 008h, 001h, 089h, 050h, 008h, 089h, 050h, 00Ch, 089h, 050h, 010h
+    db 089h, 050h, 014h, 089h, 050h, 018h, 089h, 050h, 01Ch, 089h, 050h, 020h, 088h, 050h, 024h, 089h
+    db 050h, 028h, 089h, 050h, 02Ch, 088h, 050h, 030h, 088h, 050h, 031h, 08Bh, 0F0h, 0EBh, 002h, 033h
+    db 0F6h, 08Bh, 04Ch, 024h, 008h, 03Bh, 0CAh, 074h, 00Bh, 068h, 051h, 004h, 041h, 000h, 056h, 0E8h
+    db 02Ch, 0DFh, 072h, 000h, 08Bh, 0C6h, 05Eh, 0C3h
+?friend_newModuleData@QueueProductionExitUpdate@@SAPAVModuleData@@PAVINI@@@Z ENDP
+_TEXT ENDS
+END

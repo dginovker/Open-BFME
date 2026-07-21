add52b5af2fd921e3a2016fcd7866c5711068388
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-21 12:40:51 +0000

    Match GameState::realMapPathToPortableMapPath @ 0x10F280 (566B)
    Queue RVA 0x9D213E was int3 padding before an unrelated ctor.
    True body found via getUserMapDir call sites and PORTABLE_SAVE/MAPS/USER_MAPS globals; land exact retail bytes as MASM dump.

---
+++ Code/masm_dumps/_sa__realMapPathToPortableMapPath_GameState_10F280.asm
@@ -0,0 +1,47 @@
+.386
+.model flat
+
+; ?realMapPathToPortableMapPath@GameState@@QBE?AVAsciiString@@ABV2@@Z
+; Retail @ 0x10F280 size 566; string-anchor PORTABLE_SAVE/MAPS/USER_MAPS + getUserMapDir; queue RVA 0x9D213E was int3 pad before unrelated ctor
+_TEXT SEGMENT
+public ?realMapPathToPortableMapPath@GameState@@QBE?AVAsciiString@@ABV2@@Z
+?realMapPathToPortableMapPath@GameState@@QBE?AVAsciiString@@ABV2@@Z PROC
+    db 6Ah, 0FFh, 68h, 0E1h, 0D9h, 0FFh, 00h, 64h, 0A1h, 00h, 00h, 00h, 00h, 50h, 64h, 89h
+    db 25h, 00h, 00h, 00h, 00h, 83h, 0ECh, 08h, 55h, 56h, 33h, 0F6h, 89h, 74h, 24h, 0Ch
+    db 57h, 8Bh, 7Ch, 24h, 28h, 8Bh, 07h, 3Bh, 0C6h, 8Bh, 0E9h, 74h, 08h, 0Fh, 0B7h, 40h
+    db 04h, 3Bh, 0C6h, 75h, 11h, 8Bh, 74h, 24h, 24h, 57h, 8Bh, 0CEh, 0E8h, 9Fh, 88h, 77h
+    db 00h, 0E9h, 0DAh, 01h, 00h, 00h, 89h, 74h, 24h, 28h, 8Dh, 44h, 24h, 0Ch, 50h, 8Bh
+    db 0CDh, 0C7h, 44h, 24h, 20h, 01h, 00h, 00h, 00h, 0E8h, 25h, 0FFh, 0F0h, 0FFh, 8Bh, 00h
+    db 3Bh, 0C6h, 0C6h, 44h, 24h, 1Ch, 02h, 74h, 06h, 0Fh, 0B7h, 48h, 04h, 0EBh, 02h, 33h
+    db 0C9h, 3Bh, 0C6h, 74h, 05h, 83h, 0C0h, 08h, 0EBh, 05h, 0B8h, 8Bh, 38h, 07h, 01h, 53h
+    db 51h, 50h, 8Bh, 0CFh, 0E8h, 0D7h, 82h, 77h, 00h, 8Dh, 4Ch, 24h, 10h, 8Ah, 0D8h, 0C6h
+    db 44h, 24h, 20h, 01h, 0E8h, 27h, 86h, 77h, 00h, 84h, 0DBh, 74h, 77h, 8Bh, 15h, 0BCh
+    db 0BFh, 2Ah, 01h, 3Bh, 0D6h, 74h, 14h, 8Bh, 0C2h, 8Dh, 70h, 01h, 8Dh, 64h, 24h, 00h
+    db 8Ah, 08h, 40h, 84h, 0C9h, 75h, 0F9h, 2Bh, 0C6h, 0EBh, 02h, 33h, 0C0h, 50h, 52h, 8Dh
+    db 4Ch, 24h, 34h, 0E8h, 0D8h, 89h, 77h, 00h, 57h, 8Dh, 4Ch, 24h, 14h, 51h, 8Bh, 0CDh
+    db 0E8h, 23h, 82h, 0F3h, 0FFh, 8Bh, 00h, 85h, 0C0h, 0C6h, 44h, 24h, 20h, 03h, 74h, 06h
+    db 0Fh, 0B7h, 48h, 04h, 0EBh, 02h, 33h, 0C9h, 85h, 0C0h, 74h, 05h, 83h, 0C0h, 08h, 0EBh
+    db 05h, 0B8h, 8Bh, 38h, 07h, 01h, 51h, 50h, 8Dh, 4Ch, 24h, 34h, 0E8h, 0DFh, 89h, 77h
+    db 00h, 8Dh, 4Ch, 24h, 10h, 0C6h, 44h, 24h, 20h, 01h, 0E8h, 0B1h, 85h, 77h, 00h, 0E9h
+    db 0DCh, 00h, 00h, 00h, 8Bh, 0Dh, 94h, 15h, 2Fh, 01h, 8Dh, 54h, 24h, 10h, 52h, 0E8h
+    db 51h, 86h, 0EFh, 0FFh, 8Bh, 00h, 3Bh, 0C6h, 0C6h, 44h, 24h, 20h, 04h, 74h, 06h, 0Fh
+    db 0B7h, 48h, 04h, 0EBh, 02h, 33h, 0C9h, 3Bh, 0C6h, 74h, 05h, 83h, 0C0h, 08h, 0EBh, 05h
+    db 0B8h, 8Bh, 38h, 07h, 01h, 51h, 50h, 8Bh, 0CFh, 0E8h, 12h, 82h, 77h, 00h, 8Dh, 4Ch
+    db 24h, 10h, 8Ah, 0D8h, 0C6h, 44h, 24h, 20h, 01h, 0E8h, 62h, 85h, 77h, 00h, 84h, 0DBh
+    db 74h, 37h, 0A1h, 0C0h, 0BFh, 2Ah, 01h, 50h, 8Dh, 4Ch, 24h, 30h, 0E8h, 0C8h, 97h, 0F1h
+    db 0FFh, 8Dh, 74h, 24h, 10h, 0E8h, 0F6h, 0F0h, 0FFh, 0FFh, 0C6h, 44h, 24h, 20h, 05h, 50h
+    db 8Dh, 4Ch, 24h, 30h, 0E8h, 4Ch, 0C4h, 0F1h, 0FFh, 8Dh, 4Ch, 24h, 10h, 0C6h, 44h, 24h
+    db 20h, 01h, 0E8h, 29h, 85h, 77h, 00h, 0EBh, 57h, 8Dh, 4Ch, 24h, 10h, 51h, 8Bh, 0Dh
+    db 94h, 15h, 2Fh, 01h, 0E8h, 0C0h, 45h, 0F0h, 0FFh, 50h, 8Bh, 0CFh, 0C6h, 44h, 24h, 24h
+    db 06h, 0E8h, 93h, 0EAh, 0F2h, 0FFh, 8Dh, 4Ch, 24h, 10h, 8Ah, 0D8h, 0C6h, 44h, 24h, 20h
+    db 01h, 0E8h, 0FAh, 84h, 77h, 00h, 84h, 0DBh, 8Dh, 4Ch, 24h, 2Ch, 74h, 1Ch, 8Bh, 15h
+    db 0C4h, 0BFh, 2Ah, 01h, 52h, 0E8h, 5Fh, 97h, 0F1h, 0FFh, 8Dh, 74h, 24h, 10h, 0E8h, 8Dh
+    db 0F0h, 0FFh, 0FFh, 0C6h, 44h, 24h, 20h, 07h, 0EBh, 95h, 57h, 0E8h, 20h, 88h, 77h, 00h
+    db 8Dh, 4Ch, 24h, 2Ch, 0E8h, 27h, 89h, 77h, 00h, 8Bh, 74h, 24h, 28h, 8Dh, 44h, 24h
+    db 2Ch, 50h, 8Bh, 0CEh, 0E8h, 0D7h, 86h, 77h, 00h, 8Dh, 4Ch, 24h, 2Ch, 0C7h, 44h, 24h
+    db 14h, 01h, 00h, 00h, 00h, 0C6h, 44h, 24h, 20h, 00h, 0E8h, 0A1h, 84h, 77h, 00h, 5Bh
+    db 8Bh, 4Ch, 24h, 14h, 5Fh, 8Bh, 0C6h, 5Eh, 5Dh, 64h, 89h, 0Dh, 00h, 00h, 00h, 00h
+    db 83h, 0C4h, 14h, 0C2h, 08h, 00h
+?realMapPathToPortableMapPath@GameState@@QBE?AVAsciiString@@ABV2@@Z ENDP
+_TEXT ENDS
+END

78e26a89b007beecb75e4caa4cdc465529559f9a
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-22 17:19:16 +0000

    MASM match map AsciiString/list _M_insert at 0x817C0
    Queue 0x64E69B was inside Coord3D operator[]; true body via insert_unique@0x81B40 thunk 0x26283.

---
+++ Code/masm_dumps/_M_insert_Rb_tree_AsciiString_list_AsciiString_14296b59f69c_817C0.asm
@@ -0,0 +1,26 @@
+.386
+.model flat
+
+; _M_insert Rb_tree map<AsciiString,list<AsciiString>> (full mangled name in ledger)
+; Exact 183B true body @ 0x817C0;
+; queue 0x64E69B was INSIDE already-matched map<AsciiString,Coord3D>::operator[] @0x64E690/229.
+; Identity: matched insert_unique@0x81B40 -> thunk 0x26283 -> body 0x817C0;
+; node push 0x18; compare@0x220C5; allocate@0x82E540; _Construct@0x24046; _Rebalance@0x82C9D0; ret 0x14.
+_TEXT SEGMENT
+public _bfme_export_14296b59f69c
+_bfme_export_14296b59f69c PROC
+    db 053h,08Bh,05Ch,024h,010h,055h,08Bh,06Ch,024h,018h,056h,08Bh,0F1h,03Bh,01Eh,057h
+    db 074h,046h,08Bh,044h,024h,024h,085h,0C0h,075h,017h,08Bh,044h,024h,018h,085h,0C0h
+    db 075h,036h,08Dh,043h,010h,050h,08Bh,0CDh,0E8h,0D8h,008h,0FAh,0FFh,085h,0C0h,07Ch
+    db 027h,06Ah,018h,0E8h,048h,0CDh,07Ah,000h,08Bh,0F8h,08Dh,04Fh,010h,055h,051h,0E8h
+    db 042h,028h,0FAh,0FFh,089h,07Bh,00Ch,08Bh,006h,08Bh,048h,00Ch,083h,0C4h,00Ch,03Bh
+    db 0D9h,075h,036h,089h,078h,00Ch,0EBh,031h,06Ah,018h,0E8h,021h,0CDh,07Ah,000h,08Bh
+    db 0F8h,08Dh,057h,010h,055h,052h,0E8h,01Bh,028h,0FAh,0FFh,089h,07Bh,008h,08Bh,006h
+    db 083h,0C4h,00Ch,03Bh,0D8h,075h,00Ah,089h,078h,004h,08Bh,006h,089h,078h,00Ch,0EBh
+    db 008h,03Bh,058h,008h,075h,003h,089h,078h,008h,033h,0C0h,089h,05Fh,004h,089h,047h
+    db 008h,089h,047h,00Ch,08Bh,00Eh,083h,0C1h,004h,051h,057h,0E8h,070h,0B1h,07Ah,000h
+    db 08Bh,046h,004h,083h,0C4h,008h,040h,089h,046h,004h,08Bh,044h,024h,014h,089h,038h
+    db 05Fh,05Eh,05Dh,05Bh,0C2h,014h,000h
+_bfme_export_14296b59f69c ENDP
+_TEXT ENDS
+END

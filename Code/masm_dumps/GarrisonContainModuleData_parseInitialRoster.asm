8ed77596a0ee5d92bd13cc061f4855aa342edb21
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-22 02:53:57 +0000

    match parseInitialRoster@GarrisonContainModuleData (104B @ 0x1230B0)
    Queue RVA 0x29FCD6 was misplaced inside ProductionUpdate territory.
    True body via FieldParse "InitialRoster" thunk 0x26B61 -> 0x1230B0;
    m_initialRoster at +0x174/+0x178. Exact retail MASM dump.

---
+++ Code/masm_dumps/GarrisonContainModuleData_parseInitialRoster.asm
@@ -0,0 +1,17 @@
+.386
+.model flat
+; ?parseInitialRoster@GarrisonContainModuleData@@SAXPAVINI@@PAX1PBX@Z
+; Exact 104B @ 0x001230B0; FieldParse InitialRoster thunk 0x26B61->body; m_initialRoster @+0x174/+0x178
+_TEXT SEGMENT
+public ?parseInitialRoster@GarrisonContainModuleData@@SAXPAVINI@@PAX1PBX@Z
+?parseInitialRoster@GarrisonContainModuleData@@SAXPAVINI@@PAX1PBX@Z PROC
+    db 53h,56h,8Bh,74h,24h,0Ch,57h,6Ah,00h,8Bh,0CEh,0E8h,0B0h,0D8h,72h,00h
+    db 6Ah,00h,8Bh,0CEh,8Bh,0F8h,0E8h,0F5h,0D8h,72h,00h,85h,0C0h,74h,0Dh,50h
+    db 0E8h,4Bh,0F5h,72h,00h,83h,0C4h,04h,8Bh,0D8h,0EBh,05h,0BBh,01h,00h,00h
+    db 00h,85h,0FFh,74h,16h,8Bh,0C7h,8Dh,50h,01h,8Dh,9Bh,00h,00h,00h,00h
+    db 8Ah,08h,40h,84h,0C9h,75h,0F9h,2Bh,0C2h,0EBh,02h,33h,0C0h,8Bh,74h,24h
+    db 14h,50h,57h,8Dh,8Eh,74h,01h,00h,00h,0E8h,12h,4Ch,76h,00h,5Fh,89h
+    db 9Eh,78h,01h,00h,00h,5Eh,5Bh,0C3h
+?parseInitialRoster@GarrisonContainModuleData@@SAXPAVINI@@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

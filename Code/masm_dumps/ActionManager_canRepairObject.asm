f97c642ce559bbe341eb4d99618f101580dad8e9
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-22 18:25:29 +0000

    match: ActionManager::canRepairObject as MASM at true body 0xC5230
    Queue RVA 0x5AB05D was foreign mid-function SEH; retail body is 316B at
    0xC5230 (ILT 0x338D4, Dozer/Worker callers, ret 0xC).

---
+++ Code/masm_dumps/ActionManager_canRepairObject.asm
@@ -0,0 +1,33 @@
+.386
+.model flat
+
+; ?canRepairObject@ActionManager@@QAE_NPBVObject@@0W4CommandSourceType@@@Z
+; Exact 316B @ RVA 0x000C5230; true body (queue 0x5AB05D was foreign mid-fn/pad).
+; Identity: ILT thunk 0x338D4; Dozer/WorkerAI + canDoSpecialPowerAtObject callers;
+; TheActionManager thiscall; ret 0xC; Object-layout blocks C++.
+_TEXT SEGMENT
+public ?canRepairObject@ActionManager@@QAE_NPBVObject@@0W4CommandSourceType@@@Z
+?canRepairObject@ActionManager@@QAE_NPBVObject@@0W4CommandSourceType@@@Z PROC
+    db 056h,057h,08Bh,07Ch,024h,00Ch,085h,0FFh,00Fh,084h,027h,001h,000h,000h,08Bh,074h
+    db 024h,010h,085h,0F6h,00Fh,084h,01Bh,001h,000h,000h,056h,08Bh,0CFh,0E8h,0C7h,054h
+    db 0F8h,0FFh,083h,0F8h,002h,00Fh,085h,00Ah,001h,000h,000h,0F6h,086h,044h,003h,000h
+    db 000h,001h,074h,00Fh,08Bh,0CEh,0E8h,018h,0E0h,0F3h,0FFh,084h,0C0h,00Fh,084h,0F2h
+    db 000h,000h,000h,06Ah,016h,08Bh,0CEh,0E8h,0A3h,0D2h,0F6h,0FFh,084h,0C0h,00Fh,085h
+    db 0E1h,000h,000h,000h,06Ah,018h,08Bh,0CEh,0E8h,092h,0D2h,0F6h,0FFh,084h,0C0h,00Fh
+    db 085h,0D0h,000h,000h,000h,06Ah,002h,08Bh,0CFh,0E8h,006h,0C4h,0F3h,0FFh,084h,0C0h
+    db 00Fh,085h,0BFh,000h,000h,000h,06Ah,002h,08Bh,0CEh,0E8h,0F5h,0C3h,0F3h,0FFh,084h
+    db 0C0h,00Fh,085h,0AEh,000h,000h,000h,06Ah,026h,08Bh,0CEh,0E8h,05Fh,0D2h,0F6h,0FFh
+    db 03Ch,001h,00Fh,084h,09Dh,000h,000h,000h,06Ah,00Eh,08Bh,0CFh,0E8h,04Eh,0D2h,0F6h
+    db 0FFh,084h,0C0h,00Fh,084h,08Ch,000h,000h,000h,06Ah,007h,08Bh,0CEh,0E8h,03Dh,0D2h
+    db 0F6h,0FFh,084h,0C0h,074h,07Fh,053h,08Bh,09Eh,000h,002h,000h,000h,08Bh,003h,08Bh
+    db 0CBh,0FFh,050h,010h,0D9h,05Ch,024h,010h,08Bh,013h,08Bh,0CBh,0FFh,052h,018h,0D9h
+    db 044h,024h,010h,0DAh,0E9h,05Bh,0DFh,0E0h,0F6h,0C4h,044h,07Bh,058h,068h,095h,000h
+    db 000h,000h,08Bh,0CEh,0E8h,006h,0D2h,0F6h,0FFh,084h,0C0h,075h,048h,08Bh,0CEh,0E8h
+    db 0B5h,0FEh,0F6h,0FFh,085h,0C0h,074h,00Bh,08Bh,010h,08Bh,0C8h,0FFh,052h,020h,084h
+    db 0C0h,075h,032h,08Bh,044h,024h,014h,050h,0E8h,043h,0ECh,0FFh,0FFh,083h,0C4h,004h
+    db 084h,0C0h,075h,021h,08Bh,087h,014h,002h,000h,000h,085h,0C0h,075h,017h,08Bh,0CEh
+    db 0E8h,082h,0D6h,0F3h,0FFh,085h,0C0h,074h,005h,03Bh,047h,074h,075h,007h,05Fh,0B0h
+    db 001h,05Eh,0C2h,00Ch,000h,05Fh,032h,0C0h,05Eh,0C2h,00Ch,000h
+?canRepairObject@ActionManager@@QAE_NPBVObject@@0W4CommandSourceType@@@Z ENDP
+_TEXT ENDS
+END

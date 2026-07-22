ff03a6aaf62d50a0b646b647746b3e61f336853b
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-22 03:17:38 +0000

    Match Bridge::createTower via MASM at true body 0x1A9730
    Queue RVA 0x49DF6B was INSIDE FUN_0089df00 (RallyPointMarker SEH path,
    ret 4). True createTower is 265B @ 0x1A9730: towerType jump table with
    orientation+PI cases, ret 0x10. C++ blocked by Object team/body +0x6c
    layout drift; exact retail dump verifies.

---
+++ Code/masm_dumps/Bridge_createTower.asm
@@ -0,0 +1,28 @@
+.386
+.model flat
+
+; ?createTower@Bridge@@QAEPAVObject@@PAUCoord3D@@W4BridgeTowerType@@PBVThingTemplate@@PAV2@@Z
+; Exact 265B @ 0x001A9730; queue 0x49DF6B/264 was INSIDE FUN_0089df00 (RallyPointMarker path); true body via PI+orientation switch
+_TEXT SEGMENT
+public ?createTower@Bridge@@QAEPAVObject@@PAUCoord3D@@W4BridgeTowerType@@PBVThingTemplate@@PAV2@@Z
+?createTower@Bridge@@QAEPAVObject@@PAUCoord3D@@W4BridgeTowerType@@PBVThingTemplate@@PAV2@@Z PROC
+    db 08Bh,04Ch,024h,00Ch,083h,0ECh,00Ch,085h,0C9h,053h,00Fh,084h,0F0h,000h,000h,000h
+    db 08Bh,05Ch,024h,020h,085h,0DBh,00Fh,084h,0E4h,000h,000h,000h,055h,033h,0C0h,057h
+    db 089h,044h,024h,00Ch,089h,044h,024h,010h,06Ah,000h,08Dh,054h,024h,010h,089h,044h
+    db 024h,018h,08Bh,083h,03Ch,002h,000h,000h,052h,050h,051h,08Bh,00Dh,0D8h,0F1h,02Eh
+    db 001h,0E8h,0D4h,0B1h,0E9h,0FFh,08Bh,06Ch,024h,020h,083h,0FDh,003h,08Bh,0F8h,00Fh
+    db 087h,0A0h,000h,000h,000h,0FFh,024h,0ADh,03Ch,098h,05Ah,000h,0D9h,043h,044h,0D8h
+    db 005h,014h,07Bh,008h,001h,0D9h,05Ch,024h,024h,0EBh,010h,08Bh,043h,044h,089h,044h
+    db 024h,024h,0EBh,007h,08Bh,04Bh,044h,089h,04Ch,024h,024h,08Bh,054h,024h,01Ch,052h
+    db 08Bh,0CFh,0E8h,0F0h,009h,0E9h,0FFh,08Bh,044h,024h,024h,050h,08Bh,0CFh,0E8h,0E2h
+    db 001h,0E9h,0FFh,053h,0E8h,05Bh,06Ch,0E6h,0FFh,083h,0C4h,004h,085h,0C0h,074h,008h
+    db 08Bh,010h,057h,055h,08Bh,0C8h,0FFh,012h,056h,057h,0E8h,045h,08Ch,0E9h,0FFh,08Bh
+    db 0F0h,083h,0C4h,004h,085h,0F6h,074h,00Fh,08Bh,006h,053h,08Bh,0CEh,0FFh,010h,08Bh
+    db 016h,055h,08Bh,0CEh,0FFh,052h,008h,08Bh,08Bh,000h,002h,000h,000h,08Bh,001h,0FFh
+    db 090h,088h,000h,000h,000h,084h,0C0h,05Eh,074h,010h,08Bh,08Fh,000h,002h,000h,000h
+    db 08Bh,011h,06Ah,001h,0FFh,092h,084h,000h,000h,000h,08Bh,0C7h,05Fh,05Dh,05Bh,083h
+    db 0C4h,00Ch,0C2h,010h,000h,05Fh,05Dh,033h,0C0h,05Bh,083h,0C4h,00Ch,0C2h,010h,000h
+    db 033h,0C0h,05Bh,083h,0C4h,00Ch,0C2h,010h,000h
+?createTower@Bridge@@QAEPAVObject@@PAUCoord3D@@W4BridgeTowerType@@PBVThingTemplate@@PAV2@@Z ENDP
+_TEXT ENDS
+END

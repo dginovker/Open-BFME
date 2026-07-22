f8830f80e2065570a02c87084507d9d92eec914f
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-22 01:33:59 +0000

    match NameKeyType/float _Rb_tree::_M_copy true body at 0x5671E0
    Queue RVA 0x853EA7 was mid-function epilogue into GarrisonContain::xfer.
    True 171B body via symbols.csv pin 0x2BD3C (e9 thunk). C++ emits SEH +
    _M_create_node (191B); land exact retail with MASM object-symbol alias.

---
+++ Code/masm_dumps/_M_copy_NameKeyType_float_0f152c6bcb06_5671E0.asm
@@ -0,0 +1,24 @@
+.386
+.model flat
+
+; ?_M_copy@?$_Rb_tree@W4NameKeyType@@U?$pair@$$CBW4NameKeyType@@M@_STL@@U?$_Select1st@U?$pair@$$CBW4NameKeyType@@M@_STL@@@3@U?$less@W4NameKeyType@@@3@V?$allocator@U?$pair@$$CBW4NameKeyType@@M@_STL@@@3@@_STL@@AAEPAU?$_Rb_tree_node@U?$pair@$$CBW4NameKeyType@@M@_STL@@@2@PAU32@0@Z
+; Retail true body @ 0x005671E0 size 171
+; Identity: symbols.csv pin 0x2BD3C is e9->this; queue 0x853EA7 was mid-fn epilogue
+; object-symbol=_bfme_export_0f152c6bcb06 (MASM A2041 on 275-char mangled name)
+_TEXT SEGMENT
+public _bfme_export_0f152c6bcb06
+_bfme_export_0f152c6bcb06 PROC
+    db 051h,053h,055h,056h,057h,08Bh,0D9h,06Ah,018h,089h,05Ch,024h,014h,0E8h,04Eh,073h
+    db 02Ch,000h,08Bh,07Ch,024h,01Ch,08Bh,0E8h,08Dh,047h,010h,050h,08Dh,04Dh,010h,051h
+    db 0E8h,075h,0D6h,0ABh,0FFh,08Ah,017h,08Bh,044h,024h,028h,033h,0F6h,088h,055h,000h
+    db 089h,075h,008h,089h,075h,00Ch,089h,045h,004h,08Bh,047h,00Ch,083h,0C4h,00Ch,03Bh
+    db 0C6h,074h,00Ch,055h,050h,08Bh,0CBh,0E8h,010h,04Bh,0ACh,0FFh,089h,045h,00Ch,08Bh
+    db 07Fh,008h,03Bh,0FEh,08Bh,0DDh,074h,049h,06Ah,018h,0E8h,001h,073h,02Ch,000h,08Bh
+    db 0F0h,08Dh,04Fh,010h,051h,08Dh,056h,010h,052h,0E8h,02Ch,0D6h,0ABh,0FFh,08Ah,007h
+    db 033h,0C9h,088h,006h,089h,04Eh,008h,089h,04Eh,00Ch,089h,073h,008h,089h,05Eh,004h
+    db 08Bh,047h,00Ch,083h,0C4h,00Ch,03Bh,0C1h,074h,00Eh,08Bh,04Ch,024h,010h,056h,050h
+    db 0E8h,0C7h,04Ah,0ACh,0FFh,089h,046h,00Ch,08Bh,07Fh,008h,085h,0FFh,08Bh,0DEh,075h
+    db 0B7h,05Fh,05Eh,08Bh,0C5h,05Dh,05Bh,059h,0C2h,008h,000h
+_bfme_export_0f152c6bcb06 ENDP
+_TEXT ENDS
+END

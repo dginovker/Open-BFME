34f2a09eb5f890174eafd9881cff5ed9113b3d4b
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-22 01:50:21 +0000

    Match SpawnPointProductionExitUpdate::exitObjectViaDoor via exact MASM dump.
    Retail body at 0x2D2010 is 252B (Ghidra FUN_006d2010 to ret); C++ blocked by Module Snapshot + Object::getLayer layout drift.

---
+++ Code/masm_dumps/SpawnPointProductionExitUpdate_exitObjectViaDoor_2D2010.asm
@@ -0,0 +1,27 @@
+.386
+.model flat
+
+; ?exitObjectViaDoor@SpawnPointProductionExitUpdate@@UAEXPAVObject@@W4ExitDoorType@@@Z
+; Exact 252 retail bytes @ 0x002D2010 (Ghidra FUN_006d2010; size to ret; not queue 247)
+_TEXT SEGMENT
+public ?exitObjectViaDoor@SpawnPointProductionExitUpdate@@UAEXPAVObject@@W4ExitDoorType@@@Z
+?exitObjectViaDoor@SpawnPointProductionExitUpdate@@UAEXPAVObject@@W4ExitDoorType@@@Z PROC
+    db 083h,0ECh,010h,053h,08Bh,0D9h,08Ah,043h,004h,084h,0C0h,055h,075h,008h,08Dh,04Bh
+    db 0E0h,0E8h,040h,095h,0D6h,0FFh,08Bh,06Bh,0E8h,085h,0EDh,00Fh,084h,0D3h,000h,000h
+    db 000h,08Bh,043h,008h,056h,033h,0F6h,085h,0C0h,07Eh,013h,08Dh,08Bh,0ACh,000h,000h
+    db 000h,083h,039h,000h,074h,008h,046h,083h,0C1h,004h,03Bh,0F0h,07Ch,0F3h,03Bh,0F0h
+    db 00Fh,084h,0ADh,000h,000h,000h,08Dh,044h,076h,003h,08Dh,00Ch,083h,08Bh,011h,08Bh
+    db 041h,004h,08Bh,049h,008h,089h,044h,024h,014h,0A1h,0CCh,0F4h,02Eh,001h,085h,0C0h
+    db 057h,089h,054h,024h,014h,089h,04Ch,024h,01Ch,074h,027h,08Bh,038h,06Ah,001h,06Ah
+    db 000h,08Bh,0CDh,0E8h,009h,083h,0D6h,0FFh,08Bh,054h,024h,020h,08Bh,00Dh,0CCh,0F4h
+    db 02Eh,001h,050h,08Bh,044h,024h,020h,052h,050h,0FFh,057h,01Ch,0D9h,05Ch,024h,01Ch
+    db 0EBh,008h,0C7h,044h,024h,01Ch,000h,000h,000h,000h,08Bh,07Ch,024h,024h,0D9h,084h
+    db 0B3h,084h,000h,000h,000h,08Bh,04Fh,074h,0D9h,05Ch,024h,010h,08Dh,054h,024h,014h
+    db 089h,08Ch,0B3h,0ACh,000h,000h,000h,052h,08Bh,0CFh,0E8h,0D8h,080h,0D6h,0FFh,08Bh
+    db 044h,024h,010h,050h,08Bh,0CFh,0E8h,0CAh,078h,0D6h,0FFh,08Bh,0CDh,0E8h,0AFh,082h
+    db 0D6h,0FFh,050h,08Bh,0CFh,0E8h,024h,03Dh,0D6h,0FFh,08Bh,00Dh,014h,0F2h,02Eh,001h
+    db 08Bh,049h,00Ch,057h,0E8h,022h,097h,0D3h,0FFh,06Ah,003h,08Bh,0CFh,0E8h,000h,0C3h
+    db 0D4h,0FFh,05Fh,05Eh,05Dh,05Bh,083h,0C4h,010h,0C2h,008h,000h
+?exitObjectViaDoor@SpawnPointProductionExitUpdate@@UAEXPAVObject@@W4ExitDoorType@@@Z ENDP
+_TEXT ENDS
+END

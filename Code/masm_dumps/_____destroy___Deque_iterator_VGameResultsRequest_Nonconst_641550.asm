e7d8e08c1a37ab233a2b67a0db1b1759aa8f86ec
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-22 16:15:18 +0000

    match: STLport __destroy deque iterator GameResultsRequest (MASM)
    Queue RVA 0x64191C was mid-instruction inside matched deque dtor;
    true freestanding body is 78B at 0x641550 calling __destroy_aux via ILT.

---
+++ Code/masm_dumps/_____destroy___Deque_iterator_VGameResultsRequest_Nonconst_641550.asm
@@ -0,0 +1,17 @@
+.386
+.model flat
+
+; ??$__destroy@U?$_Deque_iterator@VGameResultsRequest@@U?$_Nonconst_traits@VGameResultsRequest@@@_STL@@@_STL@@VGameResultsRequest@@@_STL@@YAXU?$_Deque_iterator@VGameResultsRequest@@U?$_Nonconst_traits@VGameResultsRequest@@@_STL@@@0@0PAVGameResultsRequest@@@Z
+; Exact 78B @ 0x641550; freestanding __destroy (queue 0x64191C was mid-dtor of deque dtor)
+; object-symbol=_bfme_export_2ea8ff67
+_TEXT SEGMENT
+public _bfme_export_2ea8ff67
+_bfme_export_2ea8ff67 PROC
+    db 8Bh, 4Ch, 24h, 14h, 8Bh, 54h, 24h, 18h, 8Dh, 44h, 24h, 14h, 50h, 83h, 0ECh, 10h
+    db 8Bh, 0C4h, 89h, 08h, 8Bh, 4Ch, 24h, 30h, 89h, 50h, 04h, 8Bh, 54h, 24h, 34h, 89h
+    db 48h, 08h, 8Bh, 4Ch, 24h, 18h, 89h, 50h, 0Ch, 8Bh, 54h, 24h, 1Ch, 83h, 0ECh, 10h
+    db 8Bh, 0C4h, 89h, 08h, 8Bh, 4Ch, 24h, 30h, 89h, 50h, 04h, 8Bh, 54h, 24h, 34h, 89h
+    db 48h, 08h, 89h, 50h, 0Ch, 0E8h, 32h, 52h, 0A0h, 0FFh, 83h, 0C4h, 24h, 0C3h
+_bfme_export_2ea8ff67 ENDP
+_TEXT ENDS
+END

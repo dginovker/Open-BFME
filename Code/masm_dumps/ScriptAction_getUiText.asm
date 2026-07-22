03a0aad4a9426d9731d1e115cd54d158c6dd8536
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-22 02:41:38 +0000

    Match ScriptAction::getUiText via exact MASM at true body 0x356340.
    Queue RVA 0x601FFC was mid-constructor; identity proven by Script::getUiText
    THEN/ELSE callers, unique [???] string, and ScriptEngine vtbl slot 0x28.

---
+++ Code/masm_dumps/ScriptAction_getUiText.asm
@@ -0,0 +1,35 @@
+.386
+.model flat
+; ?getUiText@ScriptAction@@QAE?AVAsciiString@@XZ
+; Exact 395B @ 0x00356340; Script::getUiText THEN/ELSE calls thunk->here; [???] + engine slot 0x28
+_TEXT SEGMENT
+public ?getUiText@ScriptAction@@QAE?AVAsciiString@@XZ
+?getUiText@ScriptAction@@QAE?AVAsciiString@@XZ PROC
+    db 06Ah, 0FFh, 068h, 0DCh, 094h, 01h, 01h, 064h, 0A1h, 00h, 00h, 00h, 00h, 050h, 064h, 089h
+    db 025h, 00h, 00h, 00h, 00h, 083h, 0ECh, 040h, 053h, 055h, 056h, 057h, 033h, 0FFh, 08Bh, 0E9h
+    db 089h, 07Ch, 024h, 01Ch, 089h, 06Ch, 024h, 014h, 089h, 07Ch, 024h, 010h, 068h, 028h, 0D8h, 040h
+    db 00h, 068h, 0D9h, 07Bh, 041h, 00h, 06Ah, 00Ch, 06Ah, 04h, 08Dh, 044h, 024h, 030h, 050h, 0C7h
+    db 044h, 024h, 06Ch, 01h, 00h, 00h, 00h, 0E8h, 058h, 00Bh, 06Ah, 00h, 08Bh, 00Dh, 06Ch, 07h
+    db 02Fh, 01h, 08Bh, 045h, 04h, 08Bh, 011h, 050h, 0C6h, 044h, 024h, 05Ch, 02h, 0FFh, 052h, 028h
+    db 08Bh, 0F0h, 039h, 07Eh, 010h, 07Eh, 023h, 08Dh, 06Ch, 024h, 020h, 08Dh, 05Eh, 014h, 08Bh, 0FFh
+    db 053h, 08Bh, 0CDh, 0E8h, 0D8h, 018h, 053h, 00h, 08Bh, 046h, 010h, 047h, 083h, 0C5h, 04h, 083h
+    db 0C3h, 04h, 03Bh, 0F8h, 07Ch, 0EAh, 08Bh, 06Ch, 024h, 014h, 08Ah, 045h, 040h, 084h, 0C0h, 08Bh
+    db 04Eh, 010h, 089h, 04Ch, 024h, 014h, 074h, 010h, 06Ah, 05h, 068h, 0E4h, 08Ah, 00Eh, 01h, 08Dh
+    db 04Ch, 024h, 018h, 0E8h, 038h, 019h, 053h, 00h, 033h, 0FFh, 08Dh, 075h, 00Ch, 0B3h, 03h, 090h
+    db 03Bh, 07Ch, 024h, 014h, 07Dh, 029h, 08Bh, 044h, 0BCh, 020h, 085h, 0C0h, 074h, 06h, 00Fh, 0B7h
+    db 048h, 04h, 0EBh, 02h, 033h, 0C9h, 085h, 0C0h, 074h, 05h, 083h, 0C0h, 08h, 0EBh, 05h, 0B8h
+    db 08Bh, 038h, 07h, 01h, 051h, 050h, 08Dh, 04Ch, 024h, 018h, 0E8h, 041h, 019h, 053h, 00h, 03Bh
+    db 07Dh, 08h, 07Dh, 045h, 08Bh, 00Eh, 08Dh, 054h, 024h, 018h, 052h, 0E8h, 0BAh, 068h, 0CEh, 0FFh
+    db 08Bh, 00h, 085h, 0C0h, 088h, 05Ch, 024h, 058h, 074h, 06h, 00Fh, 0B7h, 048h, 04h, 0EBh, 02h
+    db 033h, 0C9h, 085h, 0C0h, 074h, 05h, 083h, 0C0h, 08h, 0EBh, 05h, 0B8h, 08Bh, 038h, 07h, 01h
+    db 051h, 050h, 08Dh, 04Ch, 024h, 018h, 0E8h, 05h, 019h, 053h, 00h, 08Dh, 04Ch, 024h, 018h, 0C6h
+    db 044h, 024h, 058h, 02h, 0E8h, 0D7h, 014h, 053h, 00h, 047h, 083h, 0C6h, 04h, 083h, 0FFh, 00Ch
+    db 00Fh, 08Ch, 07Ah, 0FFh, 0FFh, 0FFh, 08Bh, 074h, 024h, 060h, 08Dh, 044h, 024h, 010h, 050h, 08Bh
+    db 0CEh, 0E8h, 0DAh, 016h, 053h, 00h, 068h, 028h, 0D8h, 040h, 00h, 06Ah, 00Ch, 06Ah, 04h, 08Dh
+    db 04Ch, 024h, 02Ch, 051h, 0C7h, 044h, 024h, 02Ch, 01h, 00h, 00h, 00h, 0C6h, 044h, 024h, 068h
+    db 01h, 0E8h, 0D0h, 08h, 06Ah, 00h, 08Dh, 04Ch, 024h, 010h, 0C6h, 044h, 024h, 058h, 00h, 0E8h
+    db 08Ch, 014h, 053h, 00h, 08Bh, 04Ch, 024h, 050h, 05Fh, 08Bh, 0C6h, 05Eh, 05Dh, 05Bh, 064h, 089h
+    db 00Dh, 00h, 00h, 00h, 00h, 083h, 0C4h, 04Ch, 0C2h, 04h, 00h
+?getUiText@ScriptAction@@QAE?AVAsciiString@@XZ ENDP
+_TEXT ENDS
+END

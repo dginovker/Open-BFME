3da0ef465eb3e533d55f6812a0abaf252b64c4aa
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-22 03:29:08 +0000

    Match W3DShadowGeometryManager::Load_Geom via exact MASM dump.
    True body is 342B at 0x007BD140 (queue 0x007BD150 was mid-SEH prologue).
    Retail uses StringBase name at +0x10; ZH char m_namebuf[] cannot emit the shape.

---
+++ Code/masm_dumps/W3DShadowGeometryManager_Load_Geom.asm
@@ -0,0 +1,36 @@
+.386
+.model flat
+
+; ?Load_Geom@W3DShadowGeometryManager@@QAEHPAVRenderObjClass@@PBD@Z
+; Exact 342 retail bytes @ 0x007BD140 (queue 0x007BD150 was mid SEH prologue).
+; Identity: NEW 0x209C -> ctor thunk 0x31624 -> StringBase::set name@+0x10;
+; Class_ID switch MESH/HLOD; HashTable Find/Add; SEH frame. C++ blocked by
+; BFME StringBase name layout vs ZH char m_namebuf[].
+_TEXT SEGMENT
+public ?Load_Geom@W3DShadowGeometryManager@@QAEHPAVRenderObjClass@@PBD@Z
+?Load_Geom@W3DShadowGeometryManager@@QAEHPAVRenderObjClass@@PBD@Z PROC
+    db 6Ah, 0FFh, 68h, 0ABh, 2Ch, 05h, 01h, 64h, 0A1h, 00h, 00h, 00h, 00h, 50h, 64h, 89h
+    db 25h, 00h, 00h, 00h, 00h, 51h, 53h, 55h, 56h, 57h, 68h, 9Ch, 20h, 00h, 00h, 8Bh
+    db 0E9h, 0E8h, 0CAh, 4Dh, 0Ch, 00h, 83h, 0C4h, 04h, 89h, 44h, 24h, 10h, 85h, 0C0h, 0C7h
+    db 44h, 24h, 1Ch, 00h, 00h, 00h, 00h, 74h, 0Bh, 8Bh, 0C8h, 0E8h, 0A4h, 44h, 87h, 0FFh
+    db 8Bh, 0F0h, 0EBh, 02h, 33h, 0F6h, 85h, 0F6h, 0C7h, 44h, 24h, 1Ch, 0FFh, 0FFh, 0FFh, 0FFh
+    db 0Fh, 84h, 0B1h, 00h, 00h, 00h, 8Bh, 54h, 24h, 28h, 85h, 0D2h, 8Dh, 5Eh, 10h, 74h
+    db 10h, 8Bh, 0C2h, 8Dh, 78h, 01h, 8Ah, 08h, 40h, 84h, 0C9h, 75h, 0F9h, 2Bh, 0C7h, 0EBh
+    db 02h, 33h, 0C0h, 50h, 52h, 8Bh, 0CBh, 0E8h, 64h, 0ABh, 0Ch, 00h, 8Bh, 7Ch, 24h, 24h
+    db 8Bh, 07h, 8Bh, 0CFh, 0FFh, 50h, 0Ch, 85h, 0C0h, 74h, 0Fh, 83h, 0F8h, 19h, 75h, 25h
+    db 57h, 8Bh, 0CEh, 0E8h, 36h, 5Fh, 87h, 0FFh, 0EBh, 12h, 8Bh, 17h, 56h, 6Ah, 0FFh, 8Bh
+    db 0CFh, 0FFh, 52h, 14h, 50h, 8Bh, 0CEh, 0E8h, 09h, 0D1h, 88h, 0FFh, 85h, 0C0h, 0Fh, 95h
+    db 0C0h, 3Ch, 01h, 74h, 25h, 0FFh, 4Eh, 04h, 75h, 4Dh, 8Bh, 06h, 8Bh, 0CEh, 0FFh, 10h
+    db 5Fh, 5Eh, 5Dh, 0B8h, 01h, 00h, 00h, 00h, 5Bh, 8Bh, 4Ch, 24h, 04h, 64h, 89h, 0Dh
+    db 00h, 00h, 00h, 00h, 83h, 0C4h, 10h, 0C2h, 08h, 00h, 8Bh, 03h, 85h, 0C0h, 74h, 05h
+    db 83h, 0C0h, 08h, 0EBh, 05h, 0B8h, 8Bh, 38h, 07h, 01h, 8Bh, 4Dh, 00h, 50h, 0E8h, 1Dh
+    db 2Fh, 22h, 00h, 85h, 0C0h, 74h, 2Ah, 83h, 0C0h, 0F8h, 74h, 25h, 0FFh, 4Eh, 04h, 75h
+    db 06h, 8Bh, 16h, 8Bh, 0CEh, 0FFh, 12h, 5Fh, 5Eh, 5Dh, 0B8h, 01h, 00h, 00h, 00h, 5Bh
+    db 8Bh, 4Ch, 24h, 04h, 64h, 89h, 0Dh, 00h, 00h, 00h, 00h, 83h, 0C4h, 10h, 0C2h, 08h
+    db 00h, 8Bh, 56h, 04h, 42h, 8Dh, 46h, 08h, 89h, 56h, 04h, 8Bh, 4Dh, 00h, 50h, 0E8h
+    db 2Ch, 2Eh, 22h, 00h, 0FFh, 4Eh, 04h, 75h, 06h, 8Bh, 16h, 8Bh, 0CEh, 0FFh, 12h, 8Bh
+    db 4Ch, 24h, 14h, 5Fh, 5Eh, 5Dh, 33h, 0C0h, 5Bh, 64h, 89h, 0Dh, 00h, 00h, 00h, 00h
+    db 83h, 0C4h, 10h, 0C2h, 08h, 00h
+?Load_Geom@W3DShadowGeometryManager@@QAEHPAVRenderObjClass@@PBD@Z ENDP
+_TEXT ENDS
+END

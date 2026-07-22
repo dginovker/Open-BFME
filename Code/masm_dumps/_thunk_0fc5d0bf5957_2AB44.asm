552b2ad38bdd62ef73ebf9aacc013ddc8487ac6a
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-22 18:11:09 +0000

    match: vector<pair<ObjectID,unsigned>>::_M_insert_overflow ILT thunk
    Queue RVA 0x853E20 is mid INI-parse (misplaced). True entry is ILT
    0x2AB44 -> body 0x960A0/306, proven via push_back@0x96350 (8-byte stride)
    in the partition_solver pair cluster. MASM exact 5-byte jmp dump.

---
+++ Code/masm_dumps/_thunk_0fc5d0bf5957_2AB44.asm
@@ -0,0 +1,15 @@
+.386
+.model flat
+
+; ?_M_insert_overflow@?$vector@U?$pair@W4ObjectID@@I@_STL@@V?$allocator@U?$pair@W4ObjectID@@I@_STL@@@2@@_STL@@IAEXPAU?$pair@W4ObjectID@@I@2@ABU32@ABU__false_type@2@I_N@Z
+; Retail @ 0x02ab44 size 5
+; Identity: push_back@0x96350 (elem stride 8) calls construct ILT
+; 0x267F6->0x952A0 (2-dword pair) + this thunk->body 0x960A0/306.
+; Queue 0x853E20/304 is mid INI-parse (misplaced); not this function.
+_TEXT SEGMENT
+public ?_M_insert_overflow@?$vector@U?$pair@W4ObjectID@@I@_STL@@V?$allocator@U?$pair@W4ObjectID@@I@_STL@@@2@@_STL@@IAEXPAU?$pair@W4ObjectID@@I@2@ABU32@ABU__false_type@2@I_N@Z
+?_M_insert_overflow@?$vector@U?$pair@W4ObjectID@@I@_STL@@V?$allocator@U?$pair@W4ObjectID@@I@_STL@@@2@@_STL@@IAEXPAU?$pair@W4ObjectID@@I@2@ABU32@ABU__false_type@2@I_N@Z PROC
+    db 0E9h, 057h, 0B5h, 006h, 000h
+?_M_insert_overflow@?$vector@U?$pair@W4ObjectID@@I@_STL@@V?$allocator@U?$pair@W4ObjectID@@I@_STL@@@2@@_STL@@IAEXPAU?$pair@W4ObjectID@@I@2@ABU32@ABU__false_type@2@I_N@Z ENDP
+_TEXT ENDS
+END

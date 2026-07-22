35a334152f159709469f70d0781fc0454f0ef8ba
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-22 17:14:25 +0000

    match: DozerActionPickActionPosState::update MASM 372B @ 0x2B8E10
    Queue RVA 0x2CE79D was int3 pad; true body is vtable slot 6 (thunk 0x2CE8F)
    immediately before DozerActionDoActionState::update.

---
+++ Code/masm_dumps/_str__update_DozerActionPickActionPosState_UAE_AW4StateReturnType_2B8E10.asm
@@ -0,0 +1,35 @@
+.386
+.model flat
+
+; ?update@DozerActionPickActionPosState@@UAE?AW4StateReturnType@@XZ
+; Exact 372B @ 0x002B8E10; vtable slot6 thunk 0x2CE8F; sibling DoAction update @ 0x2B8FF0; true body (queue 0x2CE79D was pad)
+_TEXT SEGMENT
+public ?update@DozerActionPickActionPosState@@UAE?AW4StateReturnType@@XZ
+?update@DozerActionPickActionPosState@@UAE?AW4StateReturnType@@XZ PROC
+    db 83h,0ECh,38h,53h,8Bh,0D9h,8Bh,43h,1Ch,89h,44h,24h,0Ch,8Bh,40h,10h
+    db 55h,8Bh,0A8h,04h,02h,00h,00h,85h,0EDh,89h,44h,24h,08h,75h,0Bh,5Dh
+    db 0B8h,0FEh,0FFh,0FFh,0FFh,5Bh,83h,0C4h,38h,0C3h,8Bh,45h,00h,56h,8Bh,0CDh
+    db 0FFh,90h,3Ch,01h,00h,00h,8Bh,0F0h,85h,0F6h,75h,0Ch,5Eh,5Dh,0B8h,0FEh
+    db 0FFh,0FFh,0FFh,5Bh,83h,0C4h,38h,0C3h,8Bh,43h,24h,8Bh,16h,57h,50h,8Bh
+    db 0CEh,0FFh,52h,1Ch,8Bh,0Dh,98h,08h,2Fh,01h,50h,0E8h,0E3h,63h,0D6h,0FFh
+    db 8Bh,0F8h,85h,0FFh,6Ah,00h,75h,20h,8Bh,4Bh,1Ch,8Bh,11h,0FFh,52h,38h
+    db 8Bh,4Bh,24h,8Bh,06h,51h,8Bh,0CEh,0FFh,50h,34h,5Fh,5Eh,5Dh,0B8h,0FEh
+    db 0FFh,0FFh,0FFh,5Bh,83h,0C4h,38h,0C3h,8Bh,43h,24h,8Bh,16h,50h,8Bh,0CEh
+    db 0FFh,52h,44h,33h,0C9h,3Bh,0C1h,74h,19h,8Bh,08h,89h,4Ch,24h,1Ch,8Bh
+    db 50h,04h,89h,54h,24h,20h,8Bh,40h,08h,89h,44h,24h,24h,0E9h,88h,00h
+    db 00h,00h,8Bh,44h,24h,10h,0D9h,40h,38h,8Dh,77h,38h,0D8h,26h,89h,4Ch
+    db 24h,28h,89h,4Ch,24h,3Ch,89h,4Ch,24h,40h,0D9h,5Ch,24h,10h,89h,4Ch
+    db 24h,44h,0D9h,40h,3Ch,8Dh,4Ch,24h,10h,0D8h,67h,3Ch,0C7h,44h,24h,34h
+    db 0F3h,4Fh,0C3h,0C7h,0C7h,44h,24h,38h,0F9h,02h,15h,50h,0D9h,5Ch,24h,14h
+    db 0D9h,87h,0C0h,00h,00h,00h,0D9h,54h,24h,2Ch,0D9h,5Ch,24h,30h,0E8h,3Fh
+    db 0DFh,0D8h,0FFh,8Dh,4Ch,24h,1Ch,0D9h,5Ch,24h,34h,51h,8Dh,54h,24h,2Ch
+    db 52h,56h,0E8h,25h,0DDh,0D6h,0FFh,83h,0C4h,0Ch,84h,0C0h,75h,14h,8Bh,06h
+    db 89h,44h,24h,1Ch,8Bh,4Eh,04h,89h,4Ch,24h,20h,8Bh,56h,08h,89h,54h
+    db 24h,24h,57h,8Bh,0CDh,0E8h,12h,0A2h,0D4h,0FFh,8Bh,74h,24h,18h,8Bh,06h
+    db 57h,8Bh,0CEh,0FFh,50h,38h,8Dh,4Ch,24h,1Ch,51h,8Bh,0CEh,0E8h,0EBh,0A1h
+    db 0D4h,0FFh,57h,8Bh,0CDh,0E8h,0F2h,0A1h,0D4h,0FFh,6Ah,02h,8Dh,54h,24h,20h
+    db 52h,8Dh,4Dh,20h,0E8h,0F1h,0B3h,0D7h,0FFh,5Fh,5Eh,5Dh,83h,0C8h,0FFh,5Bh
+    db 83h,0C4h,38h,0C3h
+?update@DozerActionPickActionPosState@@UAE?AW4StateReturnType@@XZ ENDP
+_TEXT ENDS
+END

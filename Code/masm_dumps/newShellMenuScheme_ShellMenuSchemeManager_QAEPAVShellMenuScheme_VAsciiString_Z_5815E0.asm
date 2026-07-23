e46af29259e9cedcc72c4398af26ae52f4f81fed
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-23 15:01:40 +0000

    Recover ShellMenuSchemeManager newShellMenuScheme

---
+++ Code/masm_dumps/newShellMenuScheme_ShellMenuSchemeManager_QAEPAVShellMenuScheme_VAsciiString_Z_5815E0.asm
@@ -0,0 +1,33 @@
+.386
+.model flat
+
+; ?newShellMenuScheme@ShellMenuSchemeManager@@QAEPAVShellMenuScheme@@VAsciiString@@@Z
+; Retail @ 0x005815E0 size 341
+_TEXT SEGMENT
+public ?newShellMenuScheme@ShellMenuSchemeManager@@QAEPAVShellMenuScheme@@VAsciiString@@@Z
+?newShellMenuScheme@ShellMenuSchemeManager@@QAEPAVShellMenuScheme@@VAsciiString@@@Z PROC
+    db 6Ah,0FFh,68h,0F3h,69h,03h,01h,64h,0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,83h,0ECh,0Ch,53h,55h,56h,8Bh,0F1h,57h,89h,74h
+    db 24h,10h,8Bh,06h,8Bh,28h,8Dh,4Ch,24h,2Ch,0C7h,44h,24h,24h,00h,00h
+    db 00h,00h,0E8h,89h,67h,30h,00h,8Bh,06h,3Bh,0E8h,89h,44h,24h,14h,0Fh
+    db 84h,91h,00h,00h,00h,8Bh,7Ch,24h,2Ch,85h,0FFh,8Bh,45h,08h,89h,44h
+    db 24h,18h,74h,09h,0Fh,0B7h,5Fh,04h,83h,0C7h,08h,0EBh,07h,33h,0DBh,0BFh
+    db 8Bh,38h,07h,01h,8Bh,00h,85h,0C0h,74h,06h,0Fh,0B7h,50h,04h,0EBh,02h
+    db 33h,0D2h,85h,0C0h,8Dh,70h,08h,75h,05h,0BEh,8Bh,38h,07h,01h,3Bh,0D3h
+    db 8Bh,0CAh,7Ch,02h,8Bh,0CBh,33h,0C0h,0F3h,0A6h,74h,05h,1Bh,0C0h,83h,0D8h
+    db 0FFh,85h,0C0h,75h,08h,2Bh,0D3h,8Bh,0C2h,85h,0C0h,74h,0Bh,8Bh,6Dh,00h
+    db 3Bh,6Ch,24h,14h,75h,9Fh,0EBh,2Ah,8Bh,45h,00h,8Bh,4Dh,04h,6Ah,0Ch
+    db 89h,01h,55h,89h,48h,04h,0E8h,55h,0CFh,2Ah,00h,8Bh,74h,24h,20h,83h
+    db 0C4h,08h,8Bh,0CEh,0E8h,46h,0FFh,0A8h,0FFh,56h,0E8h,01h,08h,30h,00h,83h
+    db 0C4h,04h,8Bh,74h,24h,10h,6Ah,0Ch,0E8h,73h,08h,30h,00h,83h,0C4h,04h
+    db 89h,44h,24h,18h,85h,0C0h,0C6h,44h,24h,24h,01h,74h,0Bh,8Bh,0C8h,0E8h
+    db 61h,40h,0A9h,0FFh,8Bh,0F8h,0EBh,02h,33h,0FFh,8Dh,4Ch,24h,2Ch,51h,8Bh
+    db 0CFh,0C6h,44h,24h,28h,00h,0E8h,0A5h,65h,30h,00h,8Bh,36h,6Ah,0Ch,0E8h
+    db 4Ch,0CEh,2Ah,00h,8Dh,48h,08h,83h,0C4h,04h,85h,0C9h,74h,02h,89h,39h
+    db 8Bh,4Eh,04h,89h,48h,04h,89h,30h,89h,01h,8Dh,4Ch,24h,2Ch,89h,46h
+    db 04h,0C7h,44h,24h,24h,0FFh,0FFh,0FFh,0FFh,0E8h,22h,62h,30h,00h,8Bh,4Ch
+    db 24h,1Ch,8Bh,0C7h,5Fh,5Eh,5Dh,64h,89h,0Dh,00h,00h,00h,00h,5Bh,83h
+    db 0C4h,18h,0C2h,04h,00h
+?newShellMenuScheme@ShellMenuSchemeManager@@QAEPAVShellMenuScheme@@VAsciiString@@@Z ENDP
+_TEXT ENDS
+END

c5b2afe334aee31dbe7e4aa7d8b121fbdab9f213
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-23 23:14:17 +0000

    Recover InGameUI addMessageText retail body

---
+++ Code/masm_dumps/InGameUI_addMessageText_43DF00.asm
@@ -0,0 +1,36 @@
+.386
+.model flat
+
+; ?addMessageText@InGameUI@@IAEXABVUnicodeString@@PBURGBColor@@@Z
+; Certified retail body @ 0x0043DF00, size 388.
+_TEXT SEGMENT
+public ?addMessageText@InGameUI@@IAEXABVUnicodeString@@PBURGBColor@@@Z
+?addMessageText@InGameUI@@IAEXABVUnicodeString@@PBURGBColor@@@Z PROC
+    db 51h, 53h, 8Bh, 5Ch, 24h, 10h, 85h, 0DBh, 55h, 56h, 8Bh, 0F1h, 8Bh, 86h, 3Ch, 08h
+    db 00h, 00h, 8Bh, 0AEh, 40h, 08h, 00h, 00h, 57h, 89h, 44h, 24h, 10h, 74h, 41h, 0D9h
+    db 03h, 0DCh, 0Dh, 48h, 0F9h, 07h, 01h, 0E8h, 0Ch, 8Fh, 5Bh, 00h, 0D9h, 43h, 04h, 0DCh
+    db 0Dh, 48h, 0F9h, 07h, 01h, 8Bh, 0F8h, 81h, 0CFh, 00h, 0FFh, 0FFh, 0FFh, 0C1h, 0E7h, 08h
+    db 0E8h, 0F3h, 8Eh, 5Bh, 00h, 0D9h, 43h, 08h, 0DCh, 0Dh, 48h, 0F9h, 07h, 01h, 0Bh, 0F8h
+    db 0C1h, 0E7h, 08h, 0E8h, 0E0h, 8Eh, 5Bh, 00h, 0Bh, 0F8h, 89h, 7Ch, 24h, 10h, 8Bh, 0EFh
+    db 8Dh, 8Eh, 0BCh, 05h, 00h, 00h, 0E8h, 65h, 0A2h, 44h, 00h, 8Bh, 86h, 0C0h, 05h, 00h
+    db 00h, 85h, 0C0h, 8Dh, 0BEh, 0C0h, 05h, 00h, 00h, 74h, 0Ch, 8Bh, 0Dh, 0CCh, 12h, 2Fh
+    db 01h, 8Bh, 11h, 50h, 0FFh, 52h, 28h, 0C7h, 07h, 00h, 00h, 00h, 00h, 0C7h, 86h, 0C4h
+    db 05h, 00h, 00h, 00h, 00h, 00h, 00h, 0BBh, 05h, 00h, 00h, 00h, 8Dh, 64h, 24h, 00h
+    db 8Dh, 47h, 0ECh, 50h, 8Dh, 4Fh, 0FCh, 0E8h, 84h, 0A5h, 44h, 00h, 8Bh, 4Fh, 0F0h, 89h
+    db 0Fh, 8Bh, 57h, 0F4h, 89h, 57h, 04h, 8Bh, 47h, 0F8h, 89h, 47h, 08h, 83h, 0C7h, 0F0h
+    db 4Bh, 75h, 0DDh, 8Bh, 4Ch, 24h, 18h, 8Dh, 0BEh, 6Ch, 05h, 00h, 00h, 51h, 8Bh, 0CFh
+    db 0E8h, 5Bh, 0A5h, 44h, 00h, 8Bh, 15h, 98h, 08h, 2Fh, 01h, 8Bh, 42h, 3Ch, 89h, 86h
+    db 74h, 05h, 00h, 00h, 8Bh, 0Dh, 0CCh, 12h, 2Fh, 01h, 8Bh, 11h, 0FFh, 52h, 24h, 8Bh
+    db 8Eh, 50h, 08h, 00h, 00h, 89h, 86h, 70h, 05h, 00h, 00h, 8Bh, 18h, 33h, 0C0h, 8Ah
+    db 86h, 54h, 08h, 00h, 00h, 50h, 51h, 8Bh, 0Dh, 84h, 14h, 2Fh, 01h, 0E8h, 55h, 6Eh
+    db 0BCh, 0FFh, 89h, 44h, 24h, 20h, 0DBh, 44h, 24h, 20h, 51h, 8Bh, 0Dh, 38h, 1Bh, 2Fh
+    db 01h, 8Dh, 96h, 4Ch, 08h, 00h, 00h, 0D9h, 1Ch, 24h, 52h, 0E8h, 93h, 0CBh, 0BCh, 0FFh
+    db 8Bh, 8Eh, 70h, 05h, 00h, 00h, 50h, 0FFh, 53h, 18h, 51h, 89h, 64h, 24h, 20h, 8Bh
+    db 0CCh, 57h, 0E8h, 0B9h, 0A3h, 44h, 00h, 8Bh, 8Eh, 70h, 05h, 00h, 00h, 8Bh, 01h, 0FFh
+    db 50h, 04h, 8Bh, 86h, 80h, 05h, 00h, 00h, 85h, 0C0h, 74h, 16h, 39h, 0AEh, 88h, 05h
+    db 00h, 00h, 74h, 0Eh, 89h, 0AEh, 78h, 05h, 00h, 00h, 5Fh, 5Eh, 5Dh, 5Bh, 59h, 0C2h
+    db 08h, 00h, 8Bh, 4Ch, 24h, 10h, 5Fh, 89h, 8Eh, 78h, 05h, 00h, 00h, 5Eh, 5Dh, 5Bh
+    db 59h, 0C2h, 08h, 00h
+?addMessageText@InGameUI@@IAEXABVUnicodeString@@PBURGBColor@@@Z ENDP
+_TEXT ENDS
+END

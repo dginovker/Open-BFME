da71dcf158dde19968075f4d3df36630825017b6
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-23 16:06:02 +0000

    Recover TerrainLogic xfer retail body

---
+++ Code/masm_dumps/TerrainLogic_xfer_6BE430.asm
@@ -0,0 +1,14 @@
+.386
+.model flat
+
+; ?xfer@TerrainLogic@@MAEXPAVXfer@@@Z
+; Exact 45 retail bytes @ 0x006BE430
+_TEXT SEGMENT
+public ?xfer@TerrainLogic@@MAEXPAVXfer@@@Z
+?xfer@TerrainLogic@@MAEXPAVXfer@@@Z PROC
+    db 051h,056h,08Bh,074h,024h,00Ch,057h,08Bh,0F9h,0B0h,001h,08Dh,04Ch,024h,008h,088h
+    db 044h,024h,008h,088h,044h,024h,009h,08Bh,006h,051h,08Bh,0CEh,0FFh,050h,028h,056h
+    db 08Bh,0CFh,0E8h,080h,05Ah,098h,0FFh,05Fh,05Eh,059h,0C2h,004h,000h
+?xfer@TerrainLogic@@MAEXPAVXfer@@@Z ENDP
+_TEXT ENDS
+END

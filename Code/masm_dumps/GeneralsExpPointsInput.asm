950960caf419af481f3048a2bf93b386365512f0
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-23 17:35:45 +0000

    Recover GeneralsExpPointsInput retail body

---
+++ Code/masm_dumps/GeneralsExpPointsInput.asm
@@ -0,0 +1,16 @@
+.386
+.model flat
+
+; ?GeneralsExpPointsInput@@YA?AW4WindowMsgHandledType@@PAVGameWindow@@III@Z
+; Exact 67 retail bytes @ 0x004C48C0; parser-table thunk target.
+_TEXT SEGMENT
+public ?GeneralsExpPointsInput@@YA?AW4WindowMsgHandledType@@PAVGameWindow@@III@Z
+?GeneralsExpPointsInput@@YA?AW4WindowMsgHandledType@@PAVGameWindow@@III@Z PROC
+    db 08Bh,044h,024h,008h,083h,0F8h,011h,074h,01Eh,083h,0F8h,015h,075h,02Fh,00Fh,0B6h
+    db 044h,024h,00Ch,048h,075h,027h,08Bh,00Dh,0F8h,033h,02Fh,001h,0E8h,066h,0BAh,0B5h
+    db 0FFh,0B8h,001h,000h,000h,000h,0C3h,08Bh,00Dh,08Ch,014h,02Fh,001h,085h,0C9h,074h
+    db 00Ch,08Bh,001h,06Ah,000h,06Ah,000h,0FFh,090h,0C0h,000h,000h,000h,0B8h,001h,000h
+    db 000h,000h,0C3h
+?GeneralsExpPointsInput@@YA?AW4WindowMsgHandledType@@PAVGameWindow@@III@Z ENDP
+_TEXT ENDS
+END

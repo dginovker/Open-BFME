75d2a2018da129d9586e1c6f205b80ac717298de
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-23 23:35:53 +0000

    Recover PartitionFilterPolygonTrigger allow retail body

---
+++ Code/masm_dumps/PartitionFilterPolygonTrigger_allow_1DD490.asm
@@ -0,0 +1,14 @@
+.386
+.model flat
+; ?allow@PartitionFilterPolygonTrigger@@MAE_NPAVObject@@@Z
+; Exact 64B retail body @ RVA 0x001DD490; SHA-256 89e04e3372950bff993e7ca31cb0e541c676877f77818a4ff12136a8c8520520
+_TEXT SEGMENT
+public ?allow@PartitionFilterPolygonTrigger@@MAE_NPAVObject@@@Z
+?allow@PartitionFilterPolygonTrigger@@MAE_NPAVObject@@@Z PROC
+    db 83h, 0ECh, 0Ch, 56h, 57h, 8Bh, 7Ch, 24h, 18h, 0D9h, 47h, 38h, 8Bh, 0F1h, 0E8h, 95h
+    db 99h, 81h, 00h, 0D9h, 47h, 3Ch, 89h, 44h, 24h, 08h, 0E8h, 89h, 99h, 81h, 00h, 8Bh
+    db 4Eh, 08h, 89h, 44h, 24h, 0Ch, 8Dh, 44h, 24h, 08h, 50h, 0C7h, 44h, 24h, 14h, 00h
+    db 00h, 00h, 00h, 0E8h, 0A7h, 0D6h, 0E6h, 0FFh, 5Fh, 5Eh, 83h, 0C4h, 0Ch, 0C2h, 04h, 00h
+?allow@PartitionFilterPolygonTrigger@@MAE_NPAVObject@@@Z ENDP
+_TEXT ENDS
+END

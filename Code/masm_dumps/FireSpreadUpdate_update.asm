579a796362eec339408d3fda8d67be0e4b29512d
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-23 19:37:48 +0000

    Recover FireSpreadUpdate update retail body

---
+++ Code/masm_dumps/FireSpreadUpdate_update.asm
@@ -0,0 +1,35 @@
+.386
+.model flat
+; ?update@FireSpreadUpdate@@UAE?AW4UpdateSleepTime@@XZ
+; Certified exact 386B retail body @ RVA 0x00292900
+_TEXT SEGMENT
+public ?update@FireSpreadUpdate@@UAE?AW4UpdateSleepTime@@XZ
+?update@FireSpreadUpdate@@UAE?AW4UpdateSleepTime@@XZ PROC
+    db 64h, 0A1h, 00h, 00h, 00h, 00h, 6Ah, 0FFh, 68h, 76h, 1Ah, 01h, 01h, 50h, 64h, 89h
+    db 25h, 00h, 00h, 00h, 00h, 83h, 0ECh, 08h, 53h, 8Bh, 0D9h, 55h, 8Bh, 6Bh, 0F8h, 8Bh
+    db 85h, 90h, 00h, 00h, 00h, 0F6h, 0C4h, 04h, 57h, 8Bh, 7Bh, 0F4h, 75h, 17h, 5Fh, 5Dh
+    db 0B8h, 0FFh, 0FFh, 0FFh, 3Fh, 5Bh, 8Bh, 4Ch, 24h, 08h, 64h, 89h, 0Dh, 00h, 00h, 00h
+    db 00h, 83h, 0C4h, 14h, 0C3h, 8Bh, 4Fh, 08h, 56h, 33h, 0F6h, 3Bh, 0CEh, 74h, 08h, 56h
+    db 56h, 55h, 0E8h, 7Ah, 37h, 0D8h, 0FFh, 0D9h, 05h, 50h, 53h, 07h, 01h, 0D9h, 47h, 14h
+    db 0DAh, 0E9h, 0DFh, 0E0h, 0F6h, 0C4h, 44h, 0Fh, 8Bh, 0DBh, 00h, 00h, 00h, 89h, 74h, 24h
+    db 14h, 0C7h, 44h, 24h, 10h, 0ECh, 0E9h, 0Bh, 01h, 8Bh, 43h, 0F8h, 8Bh, 57h, 14h, 8Dh
+    db 4Ch, 24h, 10h, 51h, 8Bh, 0Dh, 0B8h, 0D5h, 2Eh, 01h, 6Ah, 02h, 52h, 83h, 0C0h, 38h
+    db 50h, 89h, 74h, 24h, 30h, 0E8h, 06h, 0FDh, 75h, 00h, 8Bh, 0F0h, 85h, 0F6h, 0C7h, 44h
+    db 24h, 20h, 0FFh, 0FFh, 0FFh, 0FFh, 0C7h, 44h, 24h, 10h, 5Ch, 3Bh, 08h, 01h, 75h, 42h
+    db 0A1h, 14h, 0F2h, 2Eh, 01h, 8Bh, 40h, 14h, 8Ah, 88h, 0B8h, 00h, 00h, 00h, 84h, 0C9h
+    db 0Fh, 84h, 82h, 00h, 00h, 00h, 8Bh, 4Fh, 14h, 56h, 6Ah, 01h, 51h, 8Bh, 0Dh, 0CCh
+    db 0F4h, 2Eh, 01h, 83h, 0C5h, 38h, 55h, 0E8h, 0CFh, 0FCh, 0D8h, 0FFh, 85h, 0C0h, 74h, 68h
+    db 8Bh, 0Dh, 0CCh, 0F4h, 2Eh, 01h, 50h, 0E8h, 0B6h, 22h, 0DAh, 0FFh, 8Bh, 0F0h, 85h, 0F6h
+    db 74h, 56h, 0F6h, 05h, 54h, 0FFh, 2Eh, 01h, 01h, 75h, 34h, 8Bh, 0Dh, 54h, 0FFh, 2Eh
+    db 01h, 0B8h, 01h, 00h, 00h, 00h, 0Bh, 0C8h, 89h, 0Dh, 54h, 0FFh, 2Eh, 01h, 8Bh, 0Dh
+    db 00h, 0D6h, 2Eh, 01h, 68h, 0D0h, 05h, 09h, 01h, 89h, 44h, 24h, 24h, 0E8h, 0B5h, 83h
+    db 0DAh, 0FFh, 0A3h, 50h, 0FFh, 2Eh, 01h, 0C7h, 44h, 24h, 20h, 0FFh, 0FFh, 0FFh, 0FFh, 8Bh
+    db 15h, 50h, 0FFh, 2Eh, 01h, 52h, 8Bh, 0CEh, 0E8h, 0E6h, 83h, 0D9h, 0FFh, 85h, 0C0h, 74h
+    db 07h, 8Bh, 0C8h, 0E8h, 8Eh, 0F8h, 0D9h, 0FFh, 8Bh, 43h, 0F4h, 8Bh, 48h, 10h, 8Bh, 50h
+    db 0Ch, 68h, 98h, 00h, 00h, 00h, 68h, 08h, 0ECh, 0Bh, 01h, 51h, 52h, 0E8h, 4Ch, 0F1h
+    db 0D6h, 0FFh, 83h, 0C4h, 10h, 83h, 0F8h, 01h, 5Eh, 73h, 05h, 0B8h, 01h, 00h, 00h, 00h
+    db 8Bh, 4Ch, 24h, 14h, 5Fh, 5Dh, 5Bh, 64h, 89h, 0Dh, 00h, 00h, 00h, 00h, 83h, 0C4h
+    db 14h, 0C3h
+?update@FireSpreadUpdate@@UAE?AW4UpdateSleepTime@@XZ ENDP
+_TEXT ENDS
+END

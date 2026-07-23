59e0515c78744f70d94fc51c8ca0e68420af470c
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-23 17:58:04 +0000

    Recover MapObject destructor retail body

---
+++ Code/masm_dumps/MapObject_destructor_MAE_00088350.asm
@@ -0,0 +1,27 @@
+.386
+.model flat
+
+; ??1MapObject@@MAE@XZ
+; Exact 236 retail bytes @ 0x00088350; SHA-256 2153036e51fadef47b0cd6327b6b5302b715c084b8abea3a210a02519f0bc8f5.
+; MapObject vtable slot 0 -> deleting destructor -> thunk -> this body.
+_TEXT SEGMENT
+public ??1MapObject@@MAE@XZ
+??1MapObject@@MAE@XZ PROC
+    db 6Ah,0FFh,68h,0F9h,55h,0FFh,00h,64h,0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,51h,53h,56h,57h,8Bh,0F9h,89h,7Ch,24h,0Ch,0C7h
+    db 07h,0E0h,0C7h,07h,01h,8Bh,4Fh,2Ch,33h,0DBh,3Bh,0CBh,0C7h,44h,24h,18h
+    db 03h,00h,00h,00h,74h,09h,0FFh,49h,04h,75h,04h,8Bh,01h,0FFh,10h,8Bh
+    db 4Fh,04h,3Bh,0CBh,89h,5Fh,2Ch,89h,5Fh,30h,74h,16h,8Dh,64h,24h,00h
+    db 8Bh,71h,04h,89h,59h,04h,8Bh,11h,6Ah,01h,0FFh,12h,3Bh,0F3h,8Bh,0CEh
+    db 75h,0EEh,55h,33h,0F6h,8Dh,6Fh,34h,3Bh,0F3h,7Ch,18h,83h,0FEh,04h,7Dh
+    db 13h,8Bh,4Dh,00h,3Bh,0CBh,74h,09h,0FFh,49h,04h,75h,04h,8Bh,01h,0FFh
+    db 10h,89h,5Dh,00h,46h,83h,0C5h,04h,83h,0FEh,04h,7Ch,0DBh,8Bh,0CFh,0E8h
+    db 91h,0BCh,0F7h,0FFh,8Bh,77h,5Ch,3Bh,0F3h,0C6h,44h,24h,1Ch,02h,5Dh,74h
+    db 1Ah,8Dh,4Eh,04h,51h,0FFh,15h,54h,8Eh,35h,01h,3Bh,0C3h,7Fh,0Ch,3Bh
+    db 0F3h,74h,08h,8Bh,16h,6Ah,01h,8Bh,0CEh,0FFh,12h,8Dh,4Fh,24h,0C6h,44h
+    db 24h,18h,01h,0E8h,5Dh,0C0h,0F8h,0FFh,8Dh,4Fh,14h,88h,5Ch,24h,18h,0E8h
+    db 1Ch,0F5h,7Fh,00h,8Bh,4Ch,24h,10h,0C7h,07h,0DCh,0C7h,07h,01h,5Fh,5Eh
+    db 5Bh,64h,89h,0Dh,00h,00h,00h,00h,83h,0C4h,10h,0C3h
+??1MapObject@@MAE@XZ ENDP
+_TEXT ENDS
+END

e687f18cbe296b38a1be49e6d41070ad70186ca6
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-23 07:26:25 +0000

    Recover AIAttackApproachTargetState onExit

---
+++ Code/masm_dumps/onExit_AIAttackApproachTargetState_UAEXW4StateExitType_Z_001751B0.asm
@@ -0,0 +1,22 @@
+.386
+.model flat
+; ?onExit@AIAttackApproachTargetState@@UAEXW4StateExitType@@@Z
+; Exact 181B @ 0x001751B0; certified recovery packet 4
+_TEXT SEGMENT
+public ?onExit@AIAttackApproachTargetState@@UAEXW4StateExitType@@@Z
+?onExit@AIAttackApproachTargetState@@UAEXW4StateExitType@@@Z PROC
+    db 08Bh,044h,024h,004h,055h,056h,057h,050h,08Bh,0F1h,0E8h,052h,041h,0EBh,0FFh,08Bh
+    db 04Eh,01Ch,08Bh,069h,010h,08Bh,0BDh,004h,002h,000h,000h,085h,0FFh,00Fh,084h,088h
+    db 000h,000h,000h,06Ah,000h,08Bh,0CFh,0E8h,080h,0DFh,0E8h,0FFh,08Bh,056h,01Ch,08Bh
+    db 042h,010h,083h,0C0h,004h,08Bh,000h,085h,0C0h,074h,00Ch,08Bh,048h,004h,085h,0C9h
+    db 074h,005h,0E8h,0C4h,0D0h,0E8h,0FFh,0F7h,080h,0C8h,000h,000h,000h,000h,000h,000h
+    db 002h,074h,00Eh,08Bh,087h,0CCh,001h,000h,000h,085h,0C0h,074h,004h,083h,060h,040h
+    db 0F7h,08Bh,007h,08Bh,0CFh,0FFh,090h,0ECh,001h,000h,000h,084h,0C0h,074h,032h,0D9h
+    db 046h,024h,08Dh,04Eh,024h,0D8h,065h,038h,0D9h,046h,028h,0D8h,065h,03Ch,0D9h,0C0h
+    db 0D8h,0C9h,0D9h,0C2h,0D8h,0CBh,0DEh,0C1h,0D8h,01Dh,078h,093h,009h,001h,0DFh,0E0h
+    db 0DDh,0D8h,0F6h,0C4h,005h,0DDh,0D8h,07Ah,008h,051h,08Bh,0CDh,0E8h,056h,04Fh,0ECh
+    db 0FFh,08Bh,017h,08Bh,0CFh,0FFh,092h,0E8h,001h,000h,000h,05Fh,0C6h,046h,073h,000h
+    db 05Eh,05Dh,0C2h,004h,000h
+?onExit@AIAttackApproachTargetState@@UAEXW4StateExitType@@@Z ENDP
+_TEXT ENDS
+END

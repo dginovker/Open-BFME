a6988a056cf87eaf27eeb307baa4404c88a57490
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-23 23:40:57 +0000

    Recover BuddyMessage copy constructor

---
+++ Code/masm_dumps/BuddyMessage_copy_constructor.asm
@@ -0,0 +1,19 @@
+.386
+.model flat
+
+; ??0BuddyMessage@@QAE@ABV0@@Z
+; Exact 120 retail bytes @ 0x004EA520; certified packet 91.
+_TEXT SEGMENT
+public ??0BuddyMessage@@QAE@ABV0@@Z
+??0BuddyMessage@@QAE@ABV0@@Z PROC
+    db 6Ah,0FFh,68h,66h,0BBh,02h,01h,64h,0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,51h,56h,57h,8Bh,7Ch,24h,1Ch,8Bh,07h,8Bh,0F1h
+    db 89h,06h,8Bh,4Fh,04h,8Dh,57h,08h,89h,4Eh,04h,8Dh,4Eh,08h,52h,89h,74h
+    db 24h,0Ch,0E8h,08h,0D6h,39h,00h,8Bh,47h,0Ch,8Dh,57h,10h,8Dh,4Eh,10h,52h
+    db 0C7h,44h,24h,18h,00h,00h,00h,00h,89h,46h,0Ch,0E8h,0EEh,0D5h,39h,00h
+    db 83h,0C7h,14h,57h,8Dh,4Eh,14h,0C6h,44h,24h,18h,01h,0E8h,7Dh,0DEh,39h
+    db 00h,8Bh,4Ch,24h,0Ch,5Fh,8Bh,0C6h,5Eh,64h,89h,0Dh,00h,00h,00h,00h,83h
+    db 0C4h,10h,0C2h,04h,00h
+??0BuddyMessage@@QAE@ABV0@@Z ENDP
+_TEXT ENDS
+END

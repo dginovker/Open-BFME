367be4dde69214633cec7287aba68839bb3f811e
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-23 18:26:34 +0000

    Recover W3DBridgeBuffer addBridge retail body

---
+++ Code/masm_dumps/W3DBridgeBuffer_addBridge_006DA0B0.asm
@@ -0,0 +1,33 @@
+.386
+.model flat
+; ?addBridge@W3DBridgeBuffer@@IAEXVVector3@@0VAsciiString@@PAVW3DTerrainLogic@@PAVDict@@@Z
+; Exact 364B @ 0x006DA0B0; certified packet 47 retail SHA-256 33b8e76ca10052635be7a1570f5fdec1c2e3c0e4c218a4e3fd394ab81620aaa2
+_TEXT SEGMENT
+public ?addBridge@W3DBridgeBuffer@@IAEXVVector3@@0VAsciiString@@PAVW3DTerrainLogic@@PAVDict@@@Z
+?addBridge@W3DBridgeBuffer@@IAEXVVector3@@0VAsciiString@@PAVW3DTerrainLogic@@PAVDict@@@Z PROC
+    db 6Ah, 0FFh, 68h, 0C8h, 0ABh, 04h, 01h, 64h, 0A1h, 00h, 00h, 00h, 00h, 50h, 64h, 89h
+    db 25h, 00h, 00h, 00h, 00h, 83h, 0ECh, 70h, 53h, 56h, 57h, 8Bh, 0F1h, 81h, 0BEh, 0B8h
+    db 0D7h, 00h, 00h, 0C8h, 00h, 00h, 00h, 0C7h, 84h, 24h, 84h, 00h, 00h, 00h, 00h, 00h
+    db 00h, 00h, 0Fh, 8Dh, 09h, 01h, 00h, 00h, 8Ah, 86h, 0BCh, 0D7h, 00h, 00h, 84h, 0C0h
+    db 0Fh, 84h, 0FBh, 00h, 00h, 00h, 51h, 8Dh, 84h, 24h, 0A8h, 00h, 00h, 00h, 89h, 64h
+    db 24h, 10h, 8Bh, 0CCh, 50h, 0E8h, 56h, 0DAh, 1Ah, 00h, 8Bh, 8Ch, 24h, 9Ch, 00h, 00h
+    db 00h, 8Bh, 94h, 24h, 0A0h, 00h, 00h, 00h, 83h, 0ECh, 0Ch, 8Bh, 0C4h, 89h, 08h, 8Bh
+    db 8Ch, 24h, 0B0h, 00h, 00h, 00h, 89h, 50h, 04h, 8Bh, 94h, 24h, 9Ch, 00h, 00h, 00h
+    db 89h, 48h, 08h, 8Bh, 8Ch, 24h, 0A0h, 00h, 00h, 00h, 89h, 64h, 24h, 1Ch, 83h, 0ECh
+    db 0Ch, 8Bh, 0C4h, 89h, 10h, 8Bh, 94h, 24h, 0B0h, 00h, 00h, 00h, 89h, 48h, 04h, 89h
+    db 50h, 08h, 8Bh, 86h, 0B8h, 0D7h, 00h, 00h, 69h, 0C0h, 14h, 01h, 00h, 00h, 8Dh, 4Ch
+    db 30h, 18h, 89h, 64h, 24h, 28h, 0E8h, 4Eh, 0EEh, 95h, 0FFh, 8Bh, 8Eh, 0B8h, 0D7h, 00h
+    db 00h, 69h, 0C9h, 14h, 01h, 00h, 00h, 6Ah, 00h, 8Dh, 4Ch, 31h, 18h, 0E8h, 7Ch, 90h
+    db 95h, 0FFh, 84h, 0C0h, 74h, 6Bh, 8Bh, 96h, 0B8h, 0D7h, 00h, 00h, 8Bh, 0BCh, 24h, 0A8h
+    db 00h, 00h, 00h, 69h, 0D2h, 14h, 01h, 00h, 00h, 85h, 0FFh, 8Dh, 5Ch, 32h, 18h, 74h
+    db 4Ah, 8Dh, 4Ch, 24h, 10h, 0E8h, 0F3h, 0FCh, 93h, 0FFh, 8Dh, 44h, 24h, 10h, 50h, 8Bh
+    db 0CBh, 0E8h, 03h, 0D4h, 96h, 0FFh, 8Bh, 8Eh, 0B8h, 0D7h, 00h, 00h, 51h, 89h, 4Ch, 24h
+    db 60h, 8Dh, 94h, 24h, 0A8h, 00h, 00h, 00h, 89h, 64h, 24h, 10h, 8Bh, 0CCh, 52h, 0E8h
+    db 8Ch, 0D9h, 1Ah, 00h, 8Bh, 8Ch, 24h, 0B0h, 00h, 00h, 00h, 8Bh, 07h, 51h, 8Dh, 54h
+    db 24h, 18h, 52h, 8Bh, 0CFh, 0FFh, 90h, 0ACh, 00h, 00h, 00h, 0FFh, 86h, 0B8h, 0D7h, 00h
+    db 00h, 8Dh, 8Ch, 24h, 0A4h, 00h, 00h, 00h, 0C7h, 84h, 24h, 84h, 00h, 00h, 00h, 0FFh
+    db 0FFh, 0FFh, 0FFh, 0E8h, 38h, 0D7h, 1Ah, 00h, 8Bh, 4Ch, 24h, 7Ch, 5Fh, 5Eh, 64h, 89h
+    db 0Dh, 00h, 00h, 00h, 00h, 5Bh, 83h, 0C4h, 7Ch, 0C2h, 24h, 00h
+?addBridge@W3DBridgeBuffer@@IAEXVVector3@@0VAsciiString@@PAVW3DTerrainLogic@@PAVDict@@@Z ENDP
+_TEXT ENDS
+END

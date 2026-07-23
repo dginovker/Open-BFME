9cf169201c1dee347f4b097a93fe9d6e04d4cb8d
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-23 17:54:18 +0000

    Recover parseDefaultColor retail body

---
+++ Code/masm_dumps/parseDefaultColor_YA_NPAHPAVFile_PAD_Z_00485970.asm
@@ -0,0 +1,19 @@
+.386
+.model flat
+; ?parseDefaultColor@@YA_NPAHPAVFile@@PAD@Z
+; Exact 143B @ 0x00485970; reviewed Ghidra entry, caller, string, and call-graph evidence
+_TEXT SEGMENT
+public ?parseDefaultColor@@YA_NPAHPAVFile@@PAD@Z
+?parseDefaultColor@@YA_NPAHPAVFile@@PAD@Z PROC
+    db 06Ah,0FFh,068h,088h,069h,002h,001h,064h,0A1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,051h,055h,08Bh,06Ch,024h,018h,056h,033h,0C0h,057h,08Bh
+    db 0F1h,089h,044h,024h,00Ch,08Dh,04Ch,024h,00Ch,089h,044h,024h,018h,08Bh,006h,051h
+    db 08Bh,0CEh,0FFh,050h,024h,068h,000h,008h,000h,000h,053h,056h,0E8h,02Fh,0EAh,0FFh
+    db 0FFh,083h,0C4h,00Ch,0BFh,048h,094h,00Fh,001h,08Bh,0F3h,0B9h,00Ch,000h,000h,000h
+    db 033h,0D2h,0F3h,0A6h,075h,009h,0C7h,045h,000h,0FFh,0FFh,0FFh,000h,0EBh,00Bh,055h
+    db 08Bh,0C3h,0E8h,039h,0ECh,0FFh,0FFh,083h,0C4h,004h,08Dh,04Ch,024h,00Ch,0C7h,044h
+    db 024h,018h,0FFh,0FFh,0FFh,0FFh,0E8h,055h,01Fh,040h,000h,08Bh,04Ch,024h,010h,05Fh
+    db 05Eh,0B0h,001h,05Dh,064h,089h,00Dh,000h,000h,000h,000h,083h,0C4h,010h,0C3h
+?parseDefaultColor@@YA_NPAHPAVFile@@PAD@Z ENDP
+_TEXT ENDS
+END

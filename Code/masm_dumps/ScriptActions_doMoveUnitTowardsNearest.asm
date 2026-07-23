b0e334af778e4332175281f4676b966a9ffe8307
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-23 15:08:11 +0000

    Recover ScriptActions doMoveUnitTowardsNearest

---
+++ Code/masm_dumps/ScriptActions_doMoveUnitTowardsNearest.asm
@@ -0,0 +1,32 @@
+.386
+.model flat
+
+; ?doMoveUnitTowardsNearest@ScriptActions@@IAEXABVAsciiString@@0V2@@Z
+; Exact 314 retail bytes @ 0x00302D70. Identity sealed by the reviewed packet:
+; executeAction unit-move switch thunk 0x9179, source order, and unique call graph.
+_TEXT SEGMENT
+public ?doMoveUnitTowardsNearest@ScriptActions@@IAEXABVAsciiString@@0V2@@Z
+?doMoveUnitTowardsNearest@ScriptActions@@IAEXABVAsciiString@@0V2@@Z PROC
+    db 6Ah,0FFh,68h,0D0h,6Ch,01h,01h,64h,0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,83h,0ECh,28h,53h,55h,56h,57h,8Bh,0Dh,6Ch,07h
+    db 2Fh,01h,8Bh,54h,24h,48h,8Bh,01h,33h,0DBh,52h,89h,5Ch,24h,44h,0FFh
+    db 50h,68h,8Bh,0F0h,3Bh,0F3h,0Fh,84h,0D8h,00h,00h,00h,8Bh,0AEh,04h,02h
+    db 00h,00h,3Bh,0EBh,0Fh,84h,0CAh,00h,00h,00h,8Bh,44h,24h,4Ch,8Bh,0Dh
+    db 0D8h,0F1h,2Eh,01h,50h,0E8h,96h,57h,0D2h,0FFh,8Bh,0F8h,3Bh,0FBh,0Fh,84h
+    db 0B0h,00h,00h,00h,51h,8Dh,54h,24h,54h,89h,64h,24h,4Ch,8Bh,0CCh,52h
+    db 0E8h,7Bh,4Dh,58h,00h,8Bh,0Dh,6Ch,07h,2Fh,01h,8Bh,01h,0FFh,50h,58h
+    db 3Bh,0C3h,0Fh,84h,8Ch,00h,00h,00h,89h,5Ch,24h,20h,0C7h,44h,24h,1Ch
+    db 0D0h,5Dh,08h,01h,89h,74h,24h,24h,89h,5Ch,24h,14h,0C7h,44h,24h,10h
+    db 14h,57h,09h,01h,89h,44h,24h,18h,89h,5Ch,24h,2Ch,0C7h,44h,24h,28h
+    db 0D0h,0FFh,0Ch,01h,89h,7Ch,24h,30h,0C6h,44h,24h,34h,01h,8Dh,4Ch,24h
+    db 1Ch,51h,8Dh,4Ch,24h,14h,0C6h,44h,24h,44h,03h,0E8h,0A0h,0FCh,6Eh,00h
+    db 50h,8Dh,4Ch,24h,2Ch,0E8h,96h,0FCh,6Eh,00h,8Bh,0Dh,0B8h,0D5h,2Eh,01h
+    db 50h,53h,68h,00h,24h,74h,49h,83h,0C6h,38h,56h,0E8h,40h,0F8h,6Eh,00h
+    db 3Bh,0C3h,0B9h,5Ch,3Bh,08h,01h,89h,4Ch,24h,28h,89h,4Ch,24h,10h,88h
+    db 5Ch,24h,40h,89h,4Ch,24h,1Ch,74h,0Bh,6Ah,01h,50h,8Dh,4Dh,20h,0E8h
+    db 0F0h,0E1h,0D3h,0FFh,8Dh,4Ch,24h,50h,0C7h,44h,24h,40h,0FFh,0FFh,0FFh,0FFh
+    db 0E8h,0ABh,4Ah,58h,00h,8Bh,4Ch,24h,38h,5Fh,5Eh,5Dh,64h,89h,0Dh,00h
+    db 00h,00h,00h,5Bh,83h,0C4h,34h,0C2h,0Ch,00h
+?doMoveUnitTowardsNearest@ScriptActions@@IAEXABVAsciiString@@0V2@@Z ENDP
+_TEXT ENDS
+END

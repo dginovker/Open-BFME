e64224175f5bb2ad5da6e54be116d98b43f38c90
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-23 23:32:48 +0000

    Recover AIPlayer guardSupplyCenter retail body

---
+++ Code/masm_dumps/AIPlayer_guardSupplyCenter_1668C0.asm
@@ -0,0 +1,27 @@
+.386
+.model flat
+; ?guardSupplyCenter@AIPlayer@@QAEXPAVTeam@@H@Z
+; Exact 269B retail body @ RVA 0x001668C0; SHA-256 919501bef5c2729b4a79f736c76b01996e53c4bb91c621b3ea4ecb64477b03d8
+_TEXT SEGMENT
+public ?guardSupplyCenter@AIPlayer@@QAEXPAVTeam@@H@Z
+?guardSupplyCenter@AIPlayer@@QAEXPAVTeam@@H@Z PROC
+    db 83h, 0ECh, 28h, 56h, 57h, 8Bh, 0F9h, 0C7h, 47h, 6Ch, 00h, 00h, 00h, 00h, 0E8h, 6Fh
+    db 5Ch, 0EDh, 0FFh, 84h, 0C0h, 74h, 15h, 8Bh, 47h, 70h, 8Bh, 0Dh, 98h, 08h, 2Fh, 01h
+    db 50h, 0E8h, 6Dh, 89h, 0EBh, 0FFh, 8Bh, 0F0h, 85h, 0F6h, 75h, 16h, 8Bh, 4Ch, 24h, 38h
+    db 51h, 8Bh, 0CFh, 0E8h, 03h, 78h, 0EBh, 0FFh, 8Bh, 0F0h, 85h, 0F6h, 0Fh, 84h, 0C3h, 00h
+    db 00h, 00h, 8Bh, 0Dh, 14h, 0F2h, 2Eh, 01h, 0E8h, 63h, 4Ch, 0EDh, 0FFh, 8Bh, 0F8h, 85h
+    db 0FFh, 0Fh, 84h, 0AEh, 00h, 00h, 00h, 8Bh, 4Ch, 24h, 34h, 57h, 0E8h, 0A0h, 0C5h, 0EBh
+    db 0FFh, 8Bh, 4Eh, 40h, 8Bh, 56h, 38h, 8Bh, 46h, 3Ch, 89h, 4Ch, 24h, 10h, 8Bh, 0Dh
+    db 6Ch, 07h, 2Fh, 01h, 89h, 54h, 24h, 08h, 89h, 44h, 24h, 0Ch, 8Bh, 11h, 0FFh, 52h
+    db 48h, 8Bh, 40h, 24h, 50h, 8Dh, 4Ch, 24h, 24h, 51h, 0E8h, 5Bh, 0F9h, 0ECh, 0FFh, 0D9h
+    db 44h, 24h, 30h, 0D8h, 44h, 24h, 28h, 83h, 0C4h, 08h, 8Dh, 4Ch, 24h, 14h, 0C7h, 44h
+    db 24h, 1Ch, 00h, 00h, 00h, 00h, 0D8h, 0Dh, 3Ch, 53h, 07h, 01h, 0D8h, 6Ch, 24h, 08h
+    db 0D9h, 5Ch, 24h, 14h, 0D9h, 44h, 24h, 2Ch, 0D8h, 44h, 24h, 24h, 0D8h, 0Dh, 3Ch, 53h
+    db 07h, 01h, 0D8h, 6Ch, 24h, 0Ch, 0D9h, 5Ch, 24h, 18h, 0E8h, 0F3h, 53h, 0ECh, 0FFh, 0D9h
+    db 86h, 0BCh, 00h, 00h, 00h, 6Ah, 01h, 0D8h, 0Dh, 0C0h, 6Eh, 09h, 01h, 6Ah, 00h, 0D9h
+    db 44h, 24h, 1Ch, 8Dh, 54h, 24h, 10h, 52h, 0D8h, 0C9h, 8Bh, 0CFh, 0D8h, 6Ch, 24h, 14h
+    db 0D9h, 5Ch, 24h, 14h, 0D8h, 4Ch, 24h, 24h, 0D8h, 6Ch, 24h, 18h, 0D9h, 5Ch, 24h, 18h
+    db 0E8h, 9Ah, 0A7h, 0EDh, 0FFh, 5Fh, 5Eh, 83h, 0C4h, 28h, 0C2h, 08h, 00h
+?guardSupplyCenter@AIPlayer@@QAEXPAVTeam@@H@Z ENDP
+_TEXT ENDS
+END

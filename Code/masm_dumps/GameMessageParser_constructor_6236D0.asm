875c4aeeb19cc78c5e101ab5e0f34b801c0096af
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-23 23:10:40 +0000

    Recover GameMessageParser constructor

---
+++ Code/masm_dumps/GameMessageParser_constructor_6236D0.asm
@@ -0,0 +1,20 @@
+.386
+.model flat
+
+; ??0GameMessageParser@@QAE@PAVGameMessage@@@Z
+; Exact 142 retail bytes @ 0x006236D0
+_TEXT SEGMENT
+public ??0GameMessageParser@@QAE@PAVGameMessage@@@Z
+??0GameMessageParser@@QAE@PAVGameMessage@@@Z PROC
+    db 08Bh,044h,024h,004h,083h,0ECh,008h,055h,056h,08Bh,0F1h,057h,033h,0FFh,0C7h,006h
+    db 0ECh,076h,011h,001h,089h,07Eh,004h,089h,07Eh,008h,089h,07Eh,00Ch,08Ah,040h,018h
+    db 084h,0C0h,0BDh,00Ch,000h,000h,000h,076h,05Ah,00Fh,0B6h,0C8h,089h,07Ch,024h,00Ch
+    db 089h,04Ch,024h,010h,053h,08Bh,054h,024h,010h,08Bh,04Ch,024h,01Ch,052h,0E8h,0C1h
+    db 06Ch,09Fh,0FFh,08Bh,0D8h,03Bh,0DDh,074h,014h,085h,0FFh,07Eh,00Ch,057h,055h,08Bh
+    db 0CEh,0E8h,043h,07Eh,09Eh,0FFh,0FFh,046h,00Ch,08Bh,0EBh,033h,0FFh,08Bh,04Ch,024h
+    db 010h,08Bh,044h,024h,014h,047h,041h,048h,089h,04Ch,024h,010h,089h,044h,024h,014h
+    db 075h,0C3h,085h,0FFh,05Bh,07Eh,00Ch,057h,055h,08Bh,0CEh,0E8h,019h,07Eh,09Eh,0FFh
+    db 0FFh,046h,00Ch,05Fh,08Bh,0C6h,05Eh,05Dh,083h,0C4h,008h,0C2h,004h,000h
+??0GameMessageParser@@QAE@PAVGameMessage@@@Z ENDP
+_TEXT ENDS
+END

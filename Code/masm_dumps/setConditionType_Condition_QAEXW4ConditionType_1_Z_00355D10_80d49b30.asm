3ba32c33345df4e64b886b8e4f071984369f2d0e
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-23 19:28:14 +0000

    Recover Condition::setConditionType retail body

---
+++ Code/masm_dumps/setConditionType_Condition_QAEXW4ConditionType_1_Z_00355D10_80d49b30.asm
@@ -0,0 +1,25 @@
+.386
+.model flat
+
+; ?setConditionType@Condition@@QAEXW4ConditionType@1@@Z
+; Exact 215 retail bytes @ 0x00355D10
+_TEXT SEGMENT
+public ?setConditionType@Condition@@QAEXW4ConditionType@1@@Z
+?setConditionType@Condition@@QAEXW4ConditionType@1@@Z PROC
+    db 051h,053h,055h,056h,08Bh,0F1h,08Bh,046h,008h,033h,0DBh,033h,0EDh,03Bh,0C3h,057h
+    db 07Eh,02Fh,08Dh,07Eh,00Ch,08Bh,007h,03Bh,0C3h,089h,044h,024h,010h,074h,015h,08Dh
+    db 048h,010h,0E8h,009h,01Ch,053h,000h,08Bh,044h,024h,010h,050h,0E8h,06Fh,0C1h,052h
+    db 000h,083h,0C4h,004h,089h,01Fh,08Bh,046h,008h,045h,083h,0C7h,004h,03Bh,0E8h,07Ch
+    db 0D4h,08Bh,044h,024h,018h,089h,046h,004h,08Bh,00Dh,06Ch,007h,02Fh,001h,08Bh,011h
+    db 050h,0FFh,052h,02Ch,08Bh,0C8h,08Bh,041h,044h,033h,0FFh,03Bh,0C3h,089h,04Ch,024h
+    db 010h,089h,046h,008h,07Eh,069h,083h,0C1h,048h,08Dh,06Eh,00Ch,089h,04Ch,024h,018h
+    db 06Ah,028h,0E8h,0A9h,0C1h,052h,000h,083h,0C4h,004h,03Bh,0C3h,074h,036h,03Bh,0FBh
+    db 07Ch,011h,08Bh,04Ch,024h,010h,03Bh,079h,044h,07Dh,008h,08Bh,054h,024h,018h,08Bh
+    db 00Ah,0EBh,002h,033h,0C9h,089h,008h,088h,058h,004h,089h,058h,008h,089h,058h,00Ch
+    db 089h,058h,010h,089h,058h,020h,089h,058h,024h,089h,058h,014h,089h,058h,018h,089h
+    db 058h,01Ch,0EBh,002h,033h,0C0h,08Bh,054h,024h,018h,089h,045h,000h,08Bh,046h,008h
+    db 047h,083h,0C2h,004h,083h,0C5h,004h,03Bh,0F8h,089h,054h,024h,018h,07Ch,0A1h,05Fh
+    db 05Eh,05Dh,05Bh,059h,0C2h,004h,000h
+?setConditionType@Condition@@QAEXW4ConditionType@1@@Z ENDP
+_TEXT ENDS
+END

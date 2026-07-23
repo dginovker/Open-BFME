143e86d46934933993351a7385dbd7f163426e8a
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-23 17:38:41 +0000

    Recover SupplyWarehouseDockUpdate setDockCrippled

---
+++ Code/masm_dumps/setDockCrippled_SupplyWarehouseDockUpdate_002CFB70.asm
@@ -0,0 +1,20 @@
+.386
+.model flat
+
+; ?setDockCrippled@SupplyWarehouseDockUpdate@@UAEX_N@Z
+; Retail @ 0x002CFB70 size 142
+_TEXT SEGMENT
+public ?setDockCrippled@SupplyWarehouseDockUpdate@@UAEX_N@Z
+?setDockCrippled@SupplyWarehouseDockUpdate@@UAEX_N@Z PROC
+    db 053h,08Bh,05Ch,024h,008h,084h,0DBh,055h,08Bh,0E9h,074h,075h,08Bh,045h,060h,085h
+    db 0C0h,074h,06Eh,08Bh,00Dh,098h,008h,02Fh,001h,056h,050h,0E8h,0C3h,0F6h,0D4h,0FFh
+    db 08Bh,0F0h,085h,0F6h,074h,05Ah,08Ah,045h,064h,084h,0C0h,074h,024h,08Bh,0CEh,0E8h
+    db 05Dh,0A4h,0D3h,0FFh,084h,0C0h,075h,048h,06Ah,000h,06Ah,008h,08Bh,0CEh,0E8h,053h
+    db 049h,0D4h,0FFh,05Eh,053h,08Bh,0CDh,0E8h,056h,0BDh,0D6h,0FFh,05Dh,05Bh,0C2h,004h
+    db 000h,08Bh,08Eh,004h,002h,000h,000h,08Bh,001h,057h,0FFh,090h,044h,001h,000h,000h
+    db 08Bh,0F8h,085h,0FFh,074h,019h,08Bh,08Eh,004h,002h,000h,000h,06Ah,002h,083h,0C1h
+    db 020h,0E8h,08Ah,051h,0D5h,0FFh,08Bh,017h,06Ah,001h,08Bh,0CFh,0FFh,052h,02Ch,05Fh
+    db 05Eh,053h,08Bh,0CDh,0E8h,019h,0BDh,0D6h,0FFh,05Dh,05Bh,0C2h,004h,000h
+?setDockCrippled@SupplyWarehouseDockUpdate@@UAEX_N@Z ENDP
+_TEXT ENDS
+END

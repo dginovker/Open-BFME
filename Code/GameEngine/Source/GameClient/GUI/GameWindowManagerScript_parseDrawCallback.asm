107d5979daddf1eb993a0a5a289d662c53955697
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-23 04:59:25 +0000

    Match parseDrawCallback with retail MASM

---
+++ Code/GameEngine/Source/GameClient/GUI/GameWindowManagerScript_parseDrawCallback.asm
@@ -0,0 +1,28 @@
+.386
+.model flat
+
+; ?parseDrawCallback@@YA_NPADPAVWinInstanceData@@0PAX@Z
+; Exact 130 retail bytes @ 0x00486A60; parser-table anchored; exact via MASM
+_TEXT SEGMENT
+public ?parseDrawCallback@@YA_NPADPAVWinInstanceData@@0PAX@Z
+?parseDrawCallback@@YA_NPADPAVWinInstanceData@@0PAX@Z PROC
+    db 8Bh,44h,24h,0Ch,80h,38h,22h,74h
+    db 10h,8Dh,0A4h,24h,00h,00h,00h,00h
+    db 8Ah,48h,01h,40h,80h,0F9h,22h,75h
+    db 0F7h,40h,68h,0A4h,94h,0Fh,01h,50h
+    db 0FFh,15h,0D8h,94h,35h,01h,83h,0C4h
+    db 08h,85h,0C0h,74h,12h,8Bh,0C8h,56h
+    db 8Dh,71h,01h,8Ah,11h,41h,84h,0D2h
+    db 75h,0F9h,2Bh,0CEh,5Eh,0EBh,02h,33h
+    db 0C9h,51h,50h,0B9h,7Ch,25h,2Fh,01h
+    db 0E8h,73h,12h,40h,00h,0A1h,7Ch,25h
+    db 2Fh,01h,85h,0C0h,74h,05h,83h,0C0h
+    db 08h,0EBh,05h,0B8h,8Bh,38h,07h,01h
+    db 8Bh,0Dh,00h,0D6h,2Eh,01h,50h,0E8h
+    db 0Bh,43h,0BBh,0FFh,8Bh,0Dh,8Ch,0D8h
+    db 2Eh,01h,6Ah,0FFh,50h,0E8h,0A4h,25h
+    db 0B9h,0FFh,0A3h,60h,25h,2Fh,01h,0B0h
+    db 01h,0C3h
+?parseDrawCallback@@YA_NPADPAVWinInstanceData@@0PAX@Z ENDP
+_TEXT ENDS
+END

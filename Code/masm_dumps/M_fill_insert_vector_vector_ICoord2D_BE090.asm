4bb3057a99c37d9d22842d531e1e3e45544a94ec
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-23 15:18:26 +0000

    Recover nested ICoord2D vector fill insert

---
+++ Code/masm_dumps/M_fill_insert_vector_vector_ICoord2D_BE090.asm
@@ -0,0 +1,38 @@
+.386
+.model flat
+
+; ?_M_fill_insert@?$vector@V?$vector@UICoord2D@@V?$allocator@UICoord2D@@@_STL@@@_STL@@V?$allocator@V?$vector@UICoord2D@@V?$allocator@UICoord2D@@@_STL@@@_STL@@@2@@_STL@@QAEXPAV?$vector@UICoord2D@@V?$allocator@UICoord2D@@@_STL@@@2@IABV32@@Z
+; Exact 421 retail bytes @ 0x000BE090
+_TEXT SEGMENT
+public ?_M_fill_insert@?$vector@V?$vector@UICoord2D@@V?$allocator@UICoord2D@@@_STL@@@_STL@@V?$allocator@V?$vector@UICoord2D@@V?$allocator@UICoord2D@@@_STL@@@_STL@@@2@@_STL@@QAEXPAV?$vector@UICoord2D@@V?$allocator@UICoord2D@@@_STL@@@2@IABV32@@Z
+?_M_fill_insert@?$vector@V?$vector@UICoord2D@@V?$allocator@UICoord2D@@@_STL@@@_STL@@V?$allocator@V?$vector@UICoord2D@@V?$allocator@UICoord2D@@@_STL@@@_STL@@@2@@_STL@@QAEXPAV?$vector@UICoord2D@@V?$allocator@UICoord2D@@@_STL@@@2@IABV32@@Z PROC
+    db 064h,0A1h,000h,000h,000h,000h,06Ah,0FFh,068h,098h,08Dh,0FFh,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,083h,0ECh,00Ch,056h,057h,08Bh,07Ch,024h,028h,085h,0FFh
+    db 08Bh,0F1h,00Fh,084h,06Ah,001h,000h,000h,08Bh,056h,004h,08Bh,04Eh,008h,02Bh,0CAh
+    db 0B8h,0ABh,0AAh,0AAh,02Ah,0F7h,0E9h,0D1h,0FAh,08Bh,0C2h,0C1h,0E8h,01Fh,003h,0C2h
+    db 03Bh,0C7h,00Fh,082h,031h,001h,000h,000h,08Bh,04Ch,024h,02Ch,053h,055h,051h,08Dh
+    db 04Ch,024h,014h,0E8h,0D9h,031h,0F4h,0FFh,08Bh,04Eh,004h,08Bh,06Ch,024h,02Ch,08Bh
+    db 05Eh,004h,02Bh,0CDh,0B8h,0ABh,0AAh,0AAh,02Ah,0F7h,0E9h,08Bh,044h,024h,030h,0D1h
+    db 0FAh,08Bh,0FAh,0C1h,0EFh,01Fh,003h,0FAh,03Bh,0F8h,0C7h,044h,024h,024h,000h,000h
+    db 000h,000h,076h,049h,08Dh,03Ch,040h,08Dh,054h,024h,030h,052h,053h,0C1h,0E7h,002h
+    db 08Bh,0C3h,02Bh,0C7h,053h,050h,089h,044h,024h,040h,0E8h,0B1h,07Fh,0F5h,0FFh,08Bh
+    db 046h,004h,08Bh,04Ch,024h,040h,003h,0C7h,06Ah,000h,089h,046h,004h,08Dh,044h,024h
+    db 044h,050h,053h,051h,055h,0E8h,0FFh,00Ah,0F7h,0FFh,08Dh,054h,024h,034h,052h,003h
+    db 0FDh,057h,055h,0E8h,0C2h,055h,0F4h,0FFh,083h,0C4h,030h,0EBh,04Eh,08Dh,04Ch,024h
+    db 010h,02Bh,0C7h,051h,050h,053h,089h,044h,024h,03Ch,0E8h,053h,077h,0F8h,0FFh,08Bh
+    db 044h,024h,03Ch,08Dh,014h,040h,08Bh,046h,004h,0C1h,0E2h,002h,08Dh,04Ch,024h,03Ch
+    db 051h,003h,0C2h,050h,053h,055h,089h,046h,004h,0E8h,052h,07Fh,0F5h,0FFh,08Dh,014h
+    db 07Fh,08Bh,07Eh,004h,08Dh,044h,024h,02Ch,050h,0C1h,0E2h,002h,003h,0FAh,053h,055h
+    db 089h,07Eh,004h,0E8h,072h,055h,0F4h,0FFh,083h,0C4h,028h,08Bh,04Ch,024h,010h,085h
+    db 0C9h,05Dh,0C7h,044h,024h,020h,0FFh,0FFh,0FFh,0FFh,05Bh,074h,065h,08Bh,044h,024h
+    db 010h,02Bh,0C1h,0C1h,0F8h,002h,0C1h,0E0h,002h,03Dh,080h,000h,000h,000h,076h,01Ch
+    db 051h,0E8h,0DAh,03Ch,07Ch,000h,083h,0C4h,004h,05Fh,05Eh,08Bh,04Ch,024h,00Ch,064h
+    db 089h,00Dh,000h,000h,000h,000h,083h,0C4h,018h,0C2h,00Ch,000h,050h,051h,0E8h,0FDh
+    db 003h,077h,000h,083h,0C4h,008h,05Fh,05Eh,08Bh,04Ch,024h,00Ch,064h,089h,00Dh,000h
+    db 000h,000h,000h,083h,0C4h,018h,0C2h,00Ch,000h,08Bh,054h,024h,02Ch,08Bh,044h,024h
+    db 024h,06Ah,000h,057h,08Dh,04Ch,024h,030h,051h,052h,050h,08Bh,0CEh,0E8h,0B0h,049h
+    db 0F7h,0FFh,08Bh,04Ch,024h,014h,05Fh,05Eh,064h,089h,00Dh,000h,000h,000h,000h,083h
+    db 0C4h,018h,0C2h,00Ch,000h
+?_M_fill_insert@?$vector@V?$vector@UICoord2D@@V?$allocator@UICoord2D@@@_STL@@@_STL@@V?$allocator@V?$vector@UICoord2D@@V?$allocator@UICoord2D@@@_STL@@@_STL@@@2@@_STL@@QAEXPAV?$vector@UICoord2D@@V?$allocator@UICoord2D@@@_STL@@@2@IABV32@@Z ENDP
+_TEXT ENDS
+END

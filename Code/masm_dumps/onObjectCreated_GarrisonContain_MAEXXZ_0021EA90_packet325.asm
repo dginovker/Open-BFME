7386364844caa383db2b52f0e855c6e50fdd1afa
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 11:45:06 +0000

    Recover GarrisonContain onObjectCreated packet 325

---
+++ Code/masm_dumps/onObjectCreated_GarrisonContain_MAEXXZ_0021EA90_packet325.asm
@@ -0,0 +1,22 @@
+.386
+.model flat
+
+; ?onObjectCreated@GarrisonContain@@MAEXXZ
+; Exact 150 retail bytes @ 0x0021EA90; certified packet 325
+; SHA-256 ecd2dc0a48266c9fc68f22440807c510bd473716fb041c60665b006def3b7b3c
+_TEXT SEGMENT
+public ?onObjectCreated@GarrisonContain@@MAEXXZ
+?onObjectCreated@GarrisonContain@@MAEXXZ PROC
+    db 083h,0ECh,00Ch,056h,08Bh,0F1h,08Bh,046h,004h,057h,08Bh,0B8h,078h,001h,000h,000h
+    db 085h,0FFh,07Eh,07Ch,08Bh,00Dh,0D8h,0F1h,02Eh,001h,053h,005h,074h,001h,000h,000h
+    db 050h,0E8h,0AAh,09Ah,0E0h,0FFh,085h,0FFh,08Bh,076h,008h,08Bh,0D8h,07Eh,060h,055h
+    db 08Bh,0EFh,033h,0C0h,089h,044h,024h,010h,089h,044h,024h,014h,08Bh,0CEh,089h,044h
+    db 024h,018h,0E8h,04Dh,01Dh,0E0h,0FFh,08Bh,080h,030h,002h,000h,000h,06Ah,000h,08Dh
+    db 04Ch,024h,014h,051h,08Bh,00Dh,0D8h,0F1h,02Eh,001h,050h,053h,0E8h,059h,05Eh,0E2h
+    db 0FFh,08Bh,08Eh,0FCh,001h,000h,000h,085h,0C9h,08Bh,0F8h,074h,01Eh,08Bh,011h,06Ah
+    db 001h,057h,0FFh,092h,084h,000h,000h,000h,084h,0C0h,074h,00Fh,08Bh,08Eh,0FCh,001h
+    db 000h,000h,08Bh,001h,057h,0FFh,090h,088h,000h,000h,000h,04Dh,075h,0A4h,05Dh,05Bh
+    db 05Fh,05Eh,083h,0C4h,00Ch,0C3h
+?onObjectCreated@GarrisonContain@@MAEXXZ ENDP
+_TEXT ENDS
+END

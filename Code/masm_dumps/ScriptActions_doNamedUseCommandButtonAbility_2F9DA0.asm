9bec01b1571b55203bd03e9341ce9bd6dc0ed9f6
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 08:13:14 +0000

    Recover ScriptActions named ability command

---
+++ Code/masm_dumps/ScriptActions_doNamedUseCommandButtonAbility_2F9DA0.asm
@@ -0,0 +1,19 @@
+.386
+.model flat
+
+; ?doNamedUseCommandButtonAbility@ScriptActions@@IAEXABVAsciiString@@0@Z
+; Certified retail body @ RVA 0x002F9DA0, size 122, ending in ret 8 before INT3 padding.
+_TEXT SEGMENT
+public ?doNamedUseCommandButtonAbility@ScriptActions@@IAEXABVAsciiString@@0@Z
+?doNamedUseCommandButtonAbility@ScriptActions@@IAEXABVAsciiString@@0@Z PROC
+    db 8Bh, 0Dh, 6Ch, 07h, 2Fh, 01h, 8Bh, 54h, 24h, 04h, 8Bh, 01h, 53h, 52h, 0FFh, 50h
+    db 68h, 8Bh, 0D8h, 85h, 0DBh, 74h, 5Fh, 55h, 8Bh, 0CBh, 0E8h, 01h, 00h, 0D3h, 0FFh, 8Bh
+    db 0Dh, 0F8h, 33h, 2Fh, 01h, 50h, 0E8h, 0FFh, 0EEh, 0D4h, 0FFh, 8Bh, 0E8h, 85h, 0EDh, 74h
+    db 44h, 56h, 57h, 33h, 0FFh, 57h, 8Bh, 0CDh, 0E8h, 0A3h, 0A1h, 0D0h, 0FFh, 8Bh, 0F0h, 85h
+    db 0F6h, 74h, 2Ah, 8Bh, 46h, 0Ch, 85h, 0C0h, 8Dh, 4Eh, 0Ch, 74h, 20h, 66h, 83h, 78h
+    db 04h, 00h, 74h, 19h, 8Bh, 44h, 24h, 18h, 50h, 0E8h, 0C7h, 82h, 0D2h, 0FFh, 85h, 0C0h
+    db 75h, 0Bh, 50h, 6Ah, 01h, 56h, 8Bh, 0CBh, 0E8h, 0C2h, 0C5h, 0D0h, 0FFh, 47h, 83h, 0FFh
+    db 14h, 7Ch, 0C2h, 5Fh, 5Eh, 5Dh, 5Bh, 0C2h, 08h, 00h
+?doNamedUseCommandButtonAbility@ScriptActions@@IAEXABVAsciiString@@0@Z ENDP
+_TEXT ENDS
+END

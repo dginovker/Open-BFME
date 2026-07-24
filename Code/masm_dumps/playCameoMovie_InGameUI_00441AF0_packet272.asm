aedbee633e852cdefdf44117d50c1cc89cd6b37c
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 09:12:38 +0000

    Recover InGameUI playCameoMovie retail body

---
+++ Code/masm_dumps/playCameoMovie_InGameUI_00441AF0_packet272.asm
@@ -0,0 +1,29 @@
+.386
+.model flat
+
+; ?playCameoMovie@InGameUI@@UAEXABVAsciiString@@@Z
+; Exact 273 retail bytes @ 0x00441AF0; certified packet 272
+_TEXT SEGMENT
+public ?playCameoMovie@InGameUI@@UAEXABVAsciiString@@@Z
+?playCameoMovie@InGameUI@@UAEXABVAsciiString@@@Z PROC
+    db 6Ah,0FFh,68h,28h,2Ch,02h,01h,64h,0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,51h,56h,8Bh,0F1h,8Bh,06h,57h,0FFh,90h,44h,01h
+    db 00h,00h,8Bh,54h,24h,1Ch,6Ah,00h,51h,89h,64h,24h,10h,8Bh,0CCh,52h
+    db 0E8h,3Bh,60h,44h,00h,8Bh,0Dh,90h,0B1h,30h,01h,8Bh,01h,0FFh,50h,30h
+    db 85h,0C0h,89h,86h,68h,05h,00h,00h,0Fh,84h,0B0h,00h,00h,00h,8Bh,0Dh
+    db 70h,12h,2Fh,01h,8Bh,11h,8Bh,38h,6Ah,00h,0FFh,92h,84h,00h,00h,00h
+    db 8Bh,8Eh,68h,05h,00h,00h,50h,0FFh,57h,38h,84h,0C0h,75h,1Dh,8Bh,06h
+    db 8Bh,0CEh,0FFh,90h,44h,01h,00h,00h,8Bh,4Ch,24h,0Ch,64h,89h,0Dh,00h
+    db 00h,00h,00h,5Fh,5Eh,83h,0C4h,10h,0C2h,04h,00h,68h,40h,59h,0Fh,01h
+    db 8Dh,4Ch,24h,20h,0E8h,37h,70h,44h,00h,8Bh,44h,24h,1Ch,85h,0C0h,0C7h
+    db 44h,24h,14h,00h,00h,00h,00h,74h,05h,83h,0C0h,08h,0EBh,05h,0B8h,8Bh
+    db 38h,07h,01h,8Bh,0Dh,40h,1Bh,2Fh,01h,8Bh,39h,8Bh,0Dh,00h,0D6h,2Eh
+    db 01h,50h,0E8h,20h,92h,0BFh,0FFh,8Bh,0Dh,40h,1Bh,2Fh,01h,50h,6Ah,00h
+    db 0FFh,97h,0DCh,00h,00h,00h,8Dh,4Ch,24h,1Ch,8Bh,0F8h,0C7h,44h,24h,14h
+    db 0FFh,0FFh,0FFh,0FFh,0E8h,67h,5Dh,44h,00h,8Bh,0CFh,0E8h,0B1h,2Dh,0BFh,0FFh
+    db 8Bh,96h,64h,05h,00h,00h,52h,8Bh,0C8h,0E8h,85h,0C4h,0BEh,0FFh,8Bh,4Ch
+    db 24h,0Ch,5Fh,64h,89h,0Dh,00h,00h,00h,00h,5Eh,83h,0C4h,10h,0C2h,04h
+    db 00h
+?playCameoMovie@InGameUI@@UAEXABVAsciiString@@@Z ENDP
+_TEXT ENDS
+END

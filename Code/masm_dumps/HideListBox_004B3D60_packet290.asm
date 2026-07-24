459fadd29124947044740520996dbacda4c9a015
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 09:58:20 +0000

    Recover HideListBox retail function

---
+++ Code/masm_dumps/HideListBox_004B3D60_packet290.asm
@@ -0,0 +1,23 @@
+.386
+.model flat
+
+; ?HideListBox@@YAXPAVGameWindow@@@Z
+; Exact 162 retail bytes @ 0x004B3D60; certified packet 290
+; SHA-256 2f3ff304806bb5fa9ead3ac061d668be9f7f29992386eec6d2d6a74cb567bf95
+_TEXT SEGMENT
+public ?HideListBox@@YAXPAVGameWindow@@@Z
+?HideListBox@@YAXPAVGameWindow@@@Z PROC
+    db 83h,0ECh,10h,57h,8Bh,7Ch,24h,18h,8Bh,0CFh,0E8h,0C9h,27h,0B9h,0FFh,85h
+    db 0C0h,0Fh,84h,86h,00h,00h,00h,56h,8Bh,70h,2Ch,85h,0F6h,74h,7Dh,8Bh
+    db 0CEh,0E8h,31h,68h,0B8h,0FFh,84h,0C0h,75h,72h,6Ah,01h,8Bh,0CEh,0E8h,97h
+    db 41h,0B7h,0FFh,8Bh,0CFh,0E8h,9Eh,27h,0B9h,0FFh,85h,0C0h,74h,07h,8Bh,40h
+    db 28h,85h,0C0h,75h,02h,33h,0C0h,8Dh,4Ch,24h,0Ch,51h,8Dh,54h,24h,0Ch
+    db 52h,8Bh,0C8h,0E8h,04h,31h,0B8h,0FFh,8Dh,44h,24h,14h,50h,8Dh,4Ch,24h
+    db 14h,51h,8Bh,0CFh,0E8h,0F3h,30h,0B8h,0FFh,8Bh,54h,24h,0Ch,8Bh,44h,24h
+    db 10h,52h,50h,8Bh,0CFh,0E8h,0D2h,44h,0B9h,0FFh,8Bh,0Dh,40h,1Bh,2Fh,01h
+    db 8Bh,11h,0FFh,92h,0C0h,00h,00h,00h,3Bh,0C7h,75h,10h,8Bh,0Dh,40h,1Bh
+    db 2Fh,01h,8Bh,01h,6Ah,00h,0FFh,90h,0BCh,00h,00h,00h,5Eh,5Fh,83h,0C4h
+    db 10h,0C3h
+?HideListBox@@YAXPAVGameWindow@@@Z ENDP
+_TEXT ENDS
+END

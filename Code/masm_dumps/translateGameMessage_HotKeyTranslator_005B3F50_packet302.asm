e4683a204fd517ef09b75b35c9bcf3b266f59da3
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 10:30:44 +0000

    Recover HotKeyTranslator translateGameMessage

---
+++ Code/masm_dumps/translateGameMessage_HotKeyTranslator_005B3F50_packet302.asm
@@ -0,0 +1,30 @@
+.386
+.model flat
+
+; ?translateGameMessage@HotKeyTranslator@@UAE?AW4GameMessageDisposition@@PBVGameMessage@@@Z
+; Exact 304 retail bytes @ 0x005B3F50
+_TEXT SEGMENT
+public ?translateGameMessage@HotKeyTranslator@@UAE?AW4GameMessageDisposition@@PBVGameMessage@@@Z
+?translateGameMessage@HotKeyTranslator@@UAE?AW4GameMessageDisposition@@PBVGameMessage@@@Z PROC
+    db 064h,0A1h,000h,000h,000h,000h,06Ah,0FFh,068h,010h,091h,003h,001h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,083h,0ECh,00Ch,053h,056h,08Bh,074h,024h,024h,08Bh,046h
+    db 010h,057h,033h,0DBh,033h,0FFh,083h,0F8h,016h,00Fh,085h,0EBh,000h,000h,000h,06Ah
+    db 001h,08Bh,0CEh,0E8h,05Bh,01Bh,0A9h,0FFh,08Bh,010h,032h,0C9h,033h,0C0h,0F7h,0C2h
+    db 030h,004h,000h,000h,088h,04Ch,024h,028h,074h,00Bh,0B1h,001h,0B8h,010h,000h,000h
+    db 000h,088h,04Ch,024h,028h,0F6h,0C2h,00Ch,074h,009h,083h,0C8h,004h,032h,0C9h,088h
+    db 04Ch,024h,028h,0F6h,0C2h,0C0h,074h,009h,083h,0C8h,040h,032h,0C9h,088h,04Ch,024h
+    db 028h,03Bh,0C3h,074h,01Ah,03Ah,0CBh,075h,016h,05Fh,05Eh,033h,0C0h,05Bh,08Bh,04Ch
+    db 024h,00Ch,064h,089h,00Dh,000h,000h,000h,000h,083h,0C4h,018h,0C2h,004h,000h,053h
+    db 08Bh,0CEh,0E8h,0FCh,01Ah,0A9h,0FFh,00Fh,0B6h,000h,08Bh,00Dh,050h,04Ch,02Fh,001h
+    db 053h,050h,0E8h,007h,0FEh,0A5h,0FFh,089h,05Ch,024h,010h,06Ah,001h,08Dh,04Ch,024h
+    db 018h,051h,08Dh,04Ch,024h,018h,089h,05Ch,024h,028h,089h,044h,024h,01Ch,0E8h,0ADh
+    db 045h,02Dh,000h,089h,05Ch,024h,00Ch,08Dh,054h,024h,010h,052h,08Dh,04Ch,024h,010h
+    db 0C6h,044h,024h,024h,001h,0E8h,026h,050h,02Dh,000h,08Bh,00Dh,07Ch,04Ch,02Fh,001h
+    db 03Bh,0CBh,074h,018h,08Bh,044h,024h,028h,050h,08Dh,054h,024h,010h,052h,0E8h,00Eh
+    db 043h,0A9h,0FFh,084h,0C0h,074h,005h,0BFh,001h,000h,000h,000h,08Dh,04Ch,024h,00Ch
+    db 088h,05Ch,024h,020h,0E8h,0E7h,038h,02Dh,000h,08Dh,04Ch,024h,010h,0C7h,044h,024h
+    db 020h,0FFh,0FFh,0FFh,0FFh,0E8h,066h,041h,02Dh,000h,08Bh,04Ch,024h,018h,08Bh,0C7h
+    db 05Fh,05Eh,05Bh,064h,089h,00Dh,000h,000h,000h,000h,083h,0C4h,018h,0C2h,004h,000h
+?translateGameMessage@HotKeyTranslator@@UAE?AW4GameMessageDisposition@@PBVGameMessage@@@Z ENDP
+_TEXT ENDS
+END

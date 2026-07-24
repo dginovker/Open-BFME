bf2a9ad270521e748b89cf338ede6c2b34ace709
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 09:42:18 +0000

    Recover SupplyWarehouseDockUpdate setCashValue

---
+++ Code/masm_dumps/setCashValue_SupplyWarehouseDockUpdate_QAEXH_Z_002CFA50_packet286.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+
+; ?setCashValue@SupplyWarehouseDockUpdate@@QAEXH@Z
+; Exact 81 retail bytes @ 0x002CFA50; certified packet 286
+; SHA-256 9122611fc240a152fcdc168129631bfe032570eef2ad5d69c25c2155283f975b
+_TEXT SEGMENT
+public ?setCashValue@SupplyWarehouseDockUpdate@@QAEXH@Z
+?setCashValue@SupplyWarehouseDockUpdate@@QAEXH@Z PROC
+    db 0DBh,044h,024h,004h,0A1h,0C8h,0D5h,02Eh,001h,056h,083h,0ECh,008h,0DAh,0B0h,024h
+    db 00Bh,000h,000h,08Bh,0F1h,0DDh,01Ch,024h,0FFh,015h,094h,093h,035h,001h,083h,0C4h
+    db 008h,0E8h,0C2h,073h,072h,000h,08Bh,04Eh,008h,089h,086h,088h,000h,000h,000h,08Bh
+    db 011h,0FFh,052h,028h,085h,0C0h,074h,015h,08Bh,08Eh,088h,000h,000h,000h,08Bh,056h
+    db 004h,051h,08Bh,04Ah,010h,051h,08Bh,0C8h,0E8h,078h,01Bh,0D6h,0FFh,05Eh,0C2h,004h
+    db 000h
+?setCashValue@SupplyWarehouseDockUpdate@@QAEXH@Z ENDP
+_TEXT ENDS
+END

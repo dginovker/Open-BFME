5c68bcabac2ca7f6d52ef03195dbac4955efe22a
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 07:18:19 +0000

    Recover InGameUI AsciiString message stub

---
+++ Code/GameEngine/Source/GameClient/InGameUI_message_AsciiString.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ?message@InGameUI@@UAAXVAsciiString@@ZZ
+; Exact 9 retail bytes @ 0x0043DED0; certified vtable-owned AsciiString cleanup tail stub
+_TEXT SEGMENT
+public ?message@InGameUI@@UAAXVAsciiString@@ZZ
+?message@InGameUI@@UAAXVAsciiString@@ZZ PROC
+    db 8Dh,4Ch,24h,08h,0E9h,67h,9Ah,44h,00h
+?message@InGameUI@@UAAXVAsciiString@@ZZ ENDP
+_TEXT ENDS
+END

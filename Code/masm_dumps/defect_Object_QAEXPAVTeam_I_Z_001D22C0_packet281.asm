88c1458fc54590235c6232a4c3bdb958339d6b1a
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 09:35:00 +0000

    Recover Object defect retail body

---
+++ Code/masm_dumps/defect_Object_QAEXPAVTeam_I_Z_001D22C0_packet281.asm
@@ -0,0 +1,46 @@
+.386
+.model flat
+
+; ?defect@Object@@QAEXPAVTeam@@I@Z
+; Exact 556 retail bytes @ 0x001D22C0; certified packet 281
+_TEXT SEGMENT
+public ?defect@Object@@QAEXPAVTeam@@I@Z
+?defect@Object@@QAEXPAVTeam@@I@Z PROC
+    db 64h,0A1h,00h,00h,00h,00h,6Ah,0FFh,68h,43h,95h,00h,01h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,83h,0ECh,7Ch,56h,8Bh,0F1h,8Bh,86h,14h,02h,00h
+    db 00h,85h,0C0h,0Fh,85h,0EBh,01h,00h,00h,8Bh,8Eh,3Ch,02h,00h,00h,85h
+    db 0C9h,0Fh,84h,0DDh,01h,00h,00h,0E8h,9Fh,13h,0E5h,0FFh,85h,0C0h,0Fh,84h
+    db 0D0h,01h,00h,00h,55h,8Bh,0ACh,24h,94h,00h,00h,00h,57h,8Bh,0B8h,30h
+    db 02h,00h,00h,3Bh,0FDh,0Fh,84h,0B7h,01h,00h,00h,8Bh,86h,90h,00h,00h
+    db 00h,0A8h,04h,0Fh,85h,0A9h,01h,00h,00h,0A9h,00h,00h,08h,00h,0Fh,85h
+    db 9Eh,01h,00h,00h,8Bh,0CEh,0E8h,17h,18h,0E3h,0FFh,85h,0C0h,74h,07h,8Bh
+    db 10h,8Bh,0C8h,0FFh,52h,34h,8Bh,86h,0Ch,02h,00h,00h,85h,0C0h,74h,30h
+    db 8Bh,0CDh,0E8h,44h,13h,0E5h,0FFh,8Bh,0C8h,0E8h,0A5h,0A1h,0E4h,0FFh,84h,0C0h
+    db 74h,1Eh,8Bh,0CFh,0E8h,32h,13h,0E5h,0FFh,8Bh,0C8h,0E8h,93h,0A1h,0E4h,0FFh
+    db 84h,0C0h,74h,0Ch,8Bh,0Dh,0E4h,0F0h,2Eh,01h,56h,0E8h,85h,0B4h,0E3h,0FFh
+    db 8Bh,94h,24h,9Ch,00h,00h,00h,85h,0D2h,0Fh,97h,0C0h,8Bh,0CEh,50h,0E8h
+    db 0D0h,0A4h,0E4h,0FFh,8Bh,8Eh,0E4h,01h,00h,00h,85h,0C9h,74h,08h,6Ah,01h
+    db 52h,0E8h,0D3h,2Ah,0E5h,0FFh,8Bh,16h,55h,8Bh,0CEh,0FFh,52h,50h,8Bh,8Eh
+    db 0B0h,03h,00h,00h,85h,0C9h,8Bh,0BEh,04h,02h,00h,00h,74h,05h,0E8h,6Dh
+    db 57h,72h,00h,85h,0FFh,74h,0Ah,6Ah,02h,8Dh,4Fh,20h,0E8h,9Fh,29h,0E5h
+    db 0FFh,8Bh,06h,8Bh,0CEh,0FFh,50h,28h,85h,0C0h,89h,44h,24h,14h,0Fh,84h
+    db 0CAh,00h,00h,00h,6Ah,00h,8Bh,0C8h,0E8h,91h,0E2h,0E6h,0FFh,8Bh,0Dh,68h
+    db 0D6h,2Eh,01h,8Bh,11h,0FFh,92h,24h,01h,00h,00h,05h,30h,02h,00h,00h
+    db 50h,8Dh,4Ch,24h,1Ch,0E8h,1Dh,57h,0E7h,0FFh,8Bh,46h,74h,50h,8Dh,4Ch
+    db 24h,1Ch,0C7h,84h,24h,94h,00h,00h,00h,00h,00h,00h,00h,0E8h,48h,76h
+    db 0E4h,0FFh,8Bh,0CDh,0E8h,72h,12h,0E5h,0FFh,85h,0C0h,74h,14h,8Bh,0CDh,0E8h
+    db 67h,12h,0E5h,0FFh,8Bh,40h,24h,50h,8Dh,4Ch,24h,1Ch,0E8h,47h,88h,0E6h
+    db 0FFh,8Bh,0Dh,68h,0D6h,2Eh,01h,8Bh,01h,8Dh,54h,24h,18h,52h,0FFh,50h
+    db 44h,8Dh,44h,24h,0Fh,50h,8Dh,4Ch,24h,14h,0E8h,73h,3Fh,0E5h,0FFh,8Dh
+    db 4Ch,24h,14h,51h,8Dh,4Ch,24h,14h,0C6h,84h,24h,94h,00h,00h,00h,01h
+    db 0E8h,0DCh,7Ch,0E7h,0FFh,6Ah,00h,8Dh,54h,24h,14h,68h,0DFh,07h,00h,00h
+    db 52h,0E8h,42h,72h,0E4h,0FFh,83h,0C4h,0Ch,8Dh,4Ch,24h,10h,0C6h,84h,24h
+    db 90h,00h,00h,00h,00h,0E8h,0C9h,0E3h,0E6h,0FFh,8Dh,4Ch,24h,18h,0C7h,84h
+    db 24h,90h,00h,00h,00h,0FFh,0FFh,0FFh,0FFh,0E8h,87h,4Ah,0E5h,0FFh,8Bh,0B6h
+    db 0FCh,01h,00h,00h,85h,0F6h,74h,1Ah,8Bh,06h,8Bh,0CEh,0FFh,90h,0BCh,00h
+    db 00h,00h,84h,0C0h,74h,0Ch,8Bh,16h,6Ah,01h,8Bh,0CEh,0FFh,92h,94h,00h
+    db 00h,00h,5Fh,5Dh,8Bh,8Ch,24h,80h,00h,00h,00h,5Eh,64h,89h,0Dh,00h
+    db 00h,00h,00h,81h,0C4h,88h,00h,00h,00h,0C2h,08h,00h
+?defect@Object@@QAEXPAVTeam@@I@Z ENDP
+_TEXT ENDS
+END

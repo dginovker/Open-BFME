a7799e14a0a16511e99ecac9a2623bf013add353
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 04:59:26 +0000

    Recover Drawable setCaptionText retail body

---
+++ Code/masm_dumps/Drawable_setCaptionText_418880.asm
@@ -0,0 +1,35 @@
+.386
+.model flat
+
+; ?setCaptionText@Drawable@@QAEXABVUnicodeString@@@Z
+; Retail @ 0x00418880 size 381
+_TEXT SEGMENT
+public ?setCaptionText@Drawable@@QAEXABVUnicodeString@@@Z
+?setCaptionText@Drawable@@QAEXABVUnicodeString@@@Z PROC
+    db 64h, 0A1h, 00h, 00h, 00h, 00h, 6Ah, 0FFh, 68h, 20h, 09h, 02h, 01h, 50h, 64h, 89h
+    db 25h, 00h, 00h, 00h, 00h, 83h, 0ECh, 08h, 53h, 56h, 8Bh, 0F1h, 8Bh, 4Ch, 24h, 20h
+    db 8Bh, 01h, 57h, 33h, 0FFh, 3Bh, 0C7h, 74h, 06h, 66h, 39h, 78h, 04h, 75h, 30h, 8Bh
+    db 86h, 0D0h, 02h, 00h, 00h, 3Bh, 0C7h, 74h, 0Ch, 8Bh, 0Dh, 0CCh, 12h, 2Fh, 01h, 8Bh
+    db 11h, 50h, 0FFh, 52h, 28h, 89h, 0BEh, 0D0h, 02h, 00h, 00h, 8Bh, 4Ch, 24h, 14h, 64h
+    db 89h, 0Dh, 00h, 00h, 00h, 00h, 5Fh, 5Eh, 5Bh, 83h, 0C4h, 14h, 0C2h, 04h, 00h, 51h
+    db 8Dh, 4Ch, 24h, 28h, 0E8h, 17h, 0FBh, 46h, 00h, 8Bh, 0Dh, 70h, 15h, 2Fh, 01h, 8Dh
+    db 44h, 24h, 24h, 50h, 89h, 7Ch, 24h, 20h, 0E8h, 0F3h, 0C5h, 0C1h, 0FFh, 8Bh, 8Eh, 0D0h
+    db 02h, 00h, 00h, 3Bh, 0CFh, 0Fh, 85h, 89h, 00h, 00h, 00h, 8Bh, 0Dh, 0CCh, 12h, 2Fh
+    db 01h, 8Bh, 11h, 0FFh, 52h, 24h, 89h, 86h, 0D0h, 02h, 00h, 00h, 8Bh, 0Dh, 8Ch, 14h
+    db 2Fh, 01h, 8Dh, 44h, 24h, 10h, 50h, 0E8h, 33h, 2Eh, 0C2h, 0FFh, 8Bh, 0F8h, 0A1h, 8Ch
+    db 14h, 2Fh, 01h, 8Ah, 88h, 0CCh, 07h, 00h, 00h, 8Bh, 80h, 0C8h, 07h, 00h, 00h, 88h
+    db 4Ch, 24h, 0Ch, 8Bh, 54h, 24h, 0Ch, 8Bh, 0Dh, 84h, 14h, 2Fh, 01h, 52h, 50h, 0C6h
+    db 44h, 24h, 24h, 01h, 0E8h, 0Eh, 0C5h, 0BEh, 0FFh, 89h, 44h, 24h, 10h, 0DBh, 44h, 24h
+    db 10h, 51h, 8Bh, 0Dh, 38h, 1Bh, 2Fh, 01h, 0D9h, 1Ch, 24h, 57h, 0E8h, 52h, 22h, 0BFh
+    db 0FFh, 8Dh, 4Ch, 24h, 10h, 8Bh, 0F8h, 0C6h, 44h, 24h, 1Ch, 00h, 0E8h, 0BFh, 0EFh, 46h
+    db 00h, 8Bh, 8Eh, 0D0h, 02h, 00h, 00h, 8Bh, 01h, 57h, 0FFh, 50h, 18h, 51h, 89h, 64h
+    db 24h, 14h, 0EBh, 2Dh, 8Bh, 11h, 8Dh, 44h, 24h, 10h, 50h, 0FFh, 52h, 08h, 8Dh, 4Ch
+    db 24h, 24h, 51h, 8Bh, 0C8h, 0E8h, 42h, 9Dh, 0C0h, 0FFh, 85h, 0C0h, 8Dh, 4Ch, 24h, 10h
+    db 0Fh, 95h, 0C3h, 0E8h, 18h, 0F8h, 46h, 00h, 84h, 0DBh, 74h, 1Ch, 51h, 89h, 64h, 24h
+    db 10h, 8Dh, 54h, 24h, 28h, 8Bh, 0CCh, 52h, 0E8h, 33h, 0FAh, 46h, 00h, 8Bh, 8Eh, 0D0h
+    db 02h, 00h, 00h, 8Bh, 01h, 0FFh, 50h, 04h, 8Dh, 4Ch, 24h, 24h, 0C7h, 44h, 24h, 1Ch
+    db 0FFh, 0FFh, 0FFh, 0FFh, 0E8h, 0E7h, 0F7h, 46h, 00h, 8Bh, 4Ch, 24h, 14h, 5Fh, 5Eh, 64h
+    db 89h, 0Dh, 00h, 00h, 00h, 00h, 5Bh, 83h, 0C4h, 14h, 0C2h, 04h, 00h
+?setCaptionText@Drawable@@QAEXABVUnicodeString@@@Z ENDP
+_TEXT ENDS
+END

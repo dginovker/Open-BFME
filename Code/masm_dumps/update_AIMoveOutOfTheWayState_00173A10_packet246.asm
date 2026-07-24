1312e4f94ad29b99b1e74dbf2e61fe474a5356ea
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 08:39:09 +0000

    Recover AIMoveOutOfTheWayState update

---
+++ Code/masm_dumps/update_AIMoveOutOfTheWayState_00173A10_packet246.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ?update@AIMoveOutOfTheWayState@@UAE?AW4StateReturnType@@XZ
+; Exact 5 retail bytes @ 0x00173A10; certified packet 246
+_TEXT SEGMENT
+public ?update@AIMoveOutOfTheWayState@@UAE?AW4StateReturnType@@XZ
+?update@AIMoveOutOfTheWayState@@UAE?AW4StateReturnType@@XZ PROC
+    db 0E9h,0E1h,04Eh,0EDh,0FFh
+?update@AIMoveOutOfTheWayState@@UAE?AW4StateReturnType@@XZ ENDP
+_TEXT ENDS
+END

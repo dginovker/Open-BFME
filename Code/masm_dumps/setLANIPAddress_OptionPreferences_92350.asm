a9a93ae3816407f7e2b63f607621d78618dcf5d8
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 10:36:57 +0000

    Recover OptionPreferences LAN IP setter

---
+++ Code/masm_dumps/setLANIPAddress_OptionPreferences_92350.asm
@@ -0,0 +1,19 @@
+.386
+.model flat
+
+; ?setLANIPAddress@OptionPreferences@@QAEXVAsciiString@@@Z
+; Exact 126 retail bytes @ 0x00092350
+_TEXT SEGMENT
+public ?setLANIPAddress@OptionPreferences@@QAEXVAsciiString@@@Z
+?setLANIPAddress@OptionPreferences@@QAEXVAsciiString@@@Z PROC
+    db 06Ah,0FFh,068h,0F0h,05Ah,0FFh,000h,064h,0A1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,051h,056h,08Bh,0F1h,068h,0D8h,0FBh,007h,001h,08Dh,04Ch
+    db 024h,008h,0C7h,044h,024h,014h,000h,000h,000h,000h,0E8h,041h,068h,07Fh,000h,08Dh
+    db 044h,024h,004h,050h,08Dh,04Eh,004h,0C6h,044h,024h,014h,001h,0E8h,049h,0C3h,0FAh
+    db 0FFh,08Dh,04Ch,024h,018h,051h,08Bh,0C8h,0E8h,0F3h,058h,07Fh,000h,08Dh,04Ch,024h
+    db 004h,0C6h,044h,024h,010h,000h,0E8h,095h,055h,07Fh,000h,08Dh,04Ch,024h,018h,0C7h
+    db 044h,024h,010h,0FFh,0FFh,0FFh,0FFh,0E8h,084h,055h,07Fh,000h,08Bh,04Ch,024h,008h
+    db 05Eh,064h,089h,00Dh,000h,000h,000h,000h,083h,0C4h,010h,0C2h,004h,000h
+?setLANIPAddress@OptionPreferences@@QAEXVAsciiString@@@Z ENDP
+_TEXT ENDS
+END

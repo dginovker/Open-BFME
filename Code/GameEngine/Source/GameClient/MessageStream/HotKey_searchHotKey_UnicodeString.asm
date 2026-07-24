98660fb4b992fdc06087d2eb783278bc941f68df
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 10:48:42 +0000

    Recover HotKey Unicode search function

---
+++ Code/GameEngine/Source/GameClient/MessageStream/HotKey_searchHotKey_UnicodeString.asm
@@ -0,0 +1,27 @@
+.386
+.model flat
+
+; ?searchHotKey@HotKeyManager@@QAE?AVAsciiString@@ABVUnicodeString@@@Z
+; Exact 245 retail bytes @ 0x005B27E0
+_TEXT SEGMENT
+public ?searchHotKey@HotKeyManager@@QAE?AVAsciiString@@ABVUnicodeString@@@Z
+?searchHotKey@HotKeyManager@@QAE?AVAsciiString@@ABVUnicodeString@@@Z PROC
+    db 06Ah,0FFh,068h,030h,08Fh,003h,001h,064h,0A1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,083h,0ECh,008h,053h,033h,0DBh,089h,05Ch,024h,008h,08Bh
+    db 044h,024h,020h,08Bh,000h,03Bh,0C3h,056h,074h,020h,066h,039h,058h,004h,074h,01Ah
+    db 08Dh,070h,008h,03Bh,0F3h,074h,013h,066h,08Bh,006h,066h,03Bh,0C3h,074h,00Bh,066h
+    db 03Dh,026h,000h,074h,02Ah,083h,0C6h,002h,075h,0EDh,08Bh,074h,024h,020h,08Bh,0CEh
+    db 068h,050h,06Eh,033h,001h,0E8h,026h,053h,02Dh,000h,08Bh,0C6h,05Eh,05Bh,08Bh,04Ch
+    db 024h,008h,064h,089h,00Dh,000h,000h,000h,000h,083h,0C4h,014h,0C2h,008h,000h,068h
+    db 054h,06Eh,033h,001h,08Dh,04Ch,024h,00Ch,0E8h,0A3h,05Bh,02Dh,000h,033h,0C9h,066h
+    db 08Bh,04Eh,002h,06Ah,001h,08Dh,054h,024h,010h,052h,089h,05Ch,024h,020h,089h,04Ch
+    db 024h,014h,08Dh,04Ch,024h,010h,0E8h,085h,05Dh,02Dh,000h,089h,05Ch,024h,024h,08Dh
+    db 044h,024h,008h,050h,08Dh,04Ch,024h,028h,0C6h,044h,024h,01Ch,001h,0E8h,0BEh,067h
+    db 02Dh,000h,08Bh,074h,024h,020h,08Dh,04Ch,024h,024h,051h,08Bh,0CEh,0E8h,0BEh,052h
+    db 02Dh,000h,08Dh,04Ch,024h,024h,088h,05Ch,024h,018h,0E8h,091h,050h,02Dh,000h,08Dh
+    db 04Ch,024h,008h,0C7h,044h,024h,018h,0FFh,0FFh,0FFh,0FFh,0E8h,010h,059h,02Dh,000h
+    db 08Bh,04Ch,024h,010h,08Bh,0C6h,05Eh,05Bh,064h,089h,00Dh,000h,000h,000h,000h,083h
+    db 0C4h,014h,0C2h,008h,000h
+?searchHotKey@HotKeyManager@@QAE?AVAsciiString@@ABVUnicodeString@@@Z ENDP
+_TEXT ENDS
+END

c35be201fd82a9c839e22ce7d42cdd0ad3a3652f
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 10:26:10 +0000

    Recover PoisonedBehavior update retail body

---
+++ Code/masm_dumps/update_PoisonedBehavior_00202BD0_packet301.asm
@@ -0,0 +1,14 @@
+.386
+.model flat
+
+; ?update@PoisonedBehavior@@UAE?AW4UpdateSleepTime@@XZ
+; Exact 48 retail bytes @ 0x00202BD0; certified packet 301
+_TEXT SEGMENT
+public ?update@PoisonedBehavior@@UAE?AW4UpdateSleepTime@@XZ
+?update@PoisonedBehavior@@UAE?AW4UpdateSleepTime@@XZ PROC
+    db 8Bh,41h,18h,83h,0C1h,0F0h,85h,0C0h,74h,20h,8Bh,15h,98h,08h,2Fh,01h
+    db 3Bh,42h,3Ch,74h,15h,68h,0FFh,0FFh,0FFh,3Fh,68h,0FFh,0FFh,0FFh,3Fh,50h
+    db 8Bh,41h,24h,50h,0E8h,0AEh,0D6h,0E1h,0FFh,0C3h,0B8h,0FFh,0FFh,0FFh,3Fh,0C3h
+?update@PoisonedBehavior@@UAE?AW4UpdateSleepTime@@XZ ENDP
+_TEXT ENDS
+END

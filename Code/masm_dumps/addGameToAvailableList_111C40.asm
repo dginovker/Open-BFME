903f41707acc3d05077479588d73cf9953972818
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 00:01:01 +0000

    Recover addGameToAvailableList

---
+++ Code/masm_dumps/addGameToAvailableList_111C40.asm
@@ -0,0 +1,28 @@
+.386
+.model flat
+; ?addGameToAvailableList@@YAXVAsciiString@@PAX@Z
+; Exact 280B @ 0x00111C40; certified packet 97
+_TEXT SEGMENT
+public ?addGameToAvailableList@@YAXVAsciiString@@PAX@Z
+?addGameToAvailableList@@YAXVAsciiString@@PAX@Z PROC
+    db 055h, 08Bh, 0ECh, 06Ah, 0FFh, 068h, 000h, 0DEh, 0FFh, 000h, 064h, 0A1h, 000h, 000h, 000h, 000h
+    db 050h, 064h, 089h, 025h, 000h, 000h, 000h, 000h, 083h, 0ECh, 038h, 053h, 056h, 057h, 089h, 065h
+    db 0F0h, 033h, 0DBh, 08Dh, 04Dh, 0BCh, 089h, 05Dh, 0FCh, 0E8h, 0AAh, 0D5h, 0EFh, 0FFh, 08Dh, 045h
+    db 0BCh, 050h, 051h, 08Dh, 055h, 008h, 089h, 065h, 0ECh, 08Bh, 0CCh, 052h, 0C6h, 045h, 0FCh, 002h
+    db 0E8h, 0DBh, 05Eh, 077h, 000h, 08Bh, 00Dh, 090h, 0F1h, 02Eh, 001h, 0E8h, 047h, 067h, 0EFh, 0FFh
+    db 084h, 0C0h, 00Fh, 084h, 094h, 000h, 000h, 000h, 06Ah, 03Ch, 0E8h, 091h, 002h, 077h, 000h, 08Bh
+    db 0F8h, 083h, 0C4h, 004h, 03Bh, 0FBh, 074h, 00Ch, 08Dh, 04Fh, 004h, 089h, 01Fh, 0E8h, 066h, 0D5h
+    db 0EFh, 0FFh, 0EBh, 002h, 033h, 0FFh, 08Dh, 045h, 0BCh, 050h, 08Dh, 04Fh, 004h, 089h, 05Fh, 038h
+    db 089h, 05Fh, 034h, 0E8h, 08Ch, 074h, 0F1h, 0FFh, 08Dh, 04Dh, 008h, 051h, 08Bh, 0CFh, 0E8h, 0BDh
+    db 05Fh, 077h, 000h, 08Bh, 045h, 00Ch, 08Bh, 030h, 03Bh, 0F3h, 075h, 004h, 089h, 038h, 0EBh, 04Ch
+    db 085h, 0F6h, 074h, 042h, 08Dh, 056h, 010h, 052h, 08Dh, 04Fh, 010h, 08Bh, 0DEh, 0E8h, 074h, 070h
+    db 0F2h, 0FFh, 084h, 0C0h, 074h, 02Bh, 08Bh, 046h, 038h, 085h, 0C0h, 074h, 011h, 089h, 078h, 034h
+    db 08Bh, 04Eh, 038h, 089h, 04Fh, 038h, 089h, 07Eh, 038h, 089h, 077h, 034h, 0EBh, 01Eh, 08Bh, 045h
+    db 00Ch, 089h, 038h, 08Bh, 04Eh, 038h, 089h, 04Fh, 038h, 089h, 07Eh, 038h, 089h, 077h, 034h, 0EBh
+    db 00Bh, 08Bh, 076h, 034h, 0EBh, 0BAh, 089h, 07Bh, 034h, 089h, 05Fh, 038h, 08Dh, 04Dh, 0BCh, 0C6h
+    db 045h, 0FCh, 001h, 0E8h, 040h, 08Eh, 0EFh, 0FFh, 08Dh, 04Dh, 008h, 0C7h, 045h, 0FCh, 0FFh, 0FFh
+    db 0FFh, 0FFh, 0E8h, 0F9h, 05Bh, 077h, 000h, 08Bh, 04Dh, 0F4h, 05Fh, 05Eh, 064h, 089h, 00Dh, 000h
+    db 000h, 000h, 000h, 05Bh, 08Bh, 0E5h, 05Dh, 0C3h
+?addGameToAvailableList@@YAXVAsciiString@@PAX@Z ENDP
+_TEXT ENDS
+END

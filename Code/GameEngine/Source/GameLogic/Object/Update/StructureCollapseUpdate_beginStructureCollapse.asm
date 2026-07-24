3bdef196cc9fdce9d26d9d290e2364d2c1d61af3
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 05:45:45 +0000

    Recover StructureCollapseUpdate beginStructureCollapse

---
+++ Code/GameEngine/Source/GameLogic/Object/Update/StructureCollapseUpdate_beginStructureCollapse.asm
@@ -0,0 +1,25 @@
+.386
+.model flat
+
+; ?beginStructureCollapse@StructureCollapseUpdate@@IAEXPBVDamageInfo@@@Z
+; Exact 214 retail bytes @ 0x002AE7D0
+_TEXT SEGMENT
+public ?beginStructureCollapse@StructureCollapseUpdate@@IAEXPBVDamageInfo@@@Z
+?beginStructureCollapse@StructureCollapseUpdate@@IAEXPBVDamageInfo@@@Z PROC
+    db 83h,0ECh,10h,53h,55h,56h,8Bh,0F1h,8Bh,46h,04h,57h,8Bh,7Eh,08h,8Dh
+    db 4Fh,38h,8Bh,29h,8Dh,5Eh,38h,8Bh,0D3h,89h,2Ah,8Bh,69h,04h,89h,6Ah
+    db 04h,8Bh,49h,08h,89h,4Ah,08h,8Bh,15h,98h,08h,2Fh,01h,8Bh,48h,38h
+    db 8Bh,6Ah,3Ch,8Bh,50h,34h,68h,83h,00h,00h,00h,68h,0E0h,45h,0Ch,01h
+    db 51h,52h,0E8h,97h,33h,0D5h,0FFh,03h,0C5h,83h,0C4h,10h,89h,46h,24h,8Dh
+    db 47h,38h,50h,6Ah,00h,8Bh,0CEh,0E8h,46h,40h,0D7h,0FFh,0C7h,46h,2Ch,01h
+    db 00h,00h,00h,0C7h,46h,34h,00h,00h,00h,00h,8Bh,87h,34h,01h,00h,00h
+    db 0F6h,0C4h,20h,74h,4Ah,8Bh,87h,20h,02h,00h,00h,8Bh,0CEh,89h,44h,24h
+    db 10h,0E8h,8Ch,0F2h,0D7h,0FFh,0D9h,44h,24h,10h,0D8h,0Dh,24h,6Ch,07h,01h
+    db 6Ah,00h,8Dh,44h,24h,18h,50h,0D8h,2Dh,34h,53h,07h,01h,0DEh,0C9h,0D9h
+    db 0E0h,0D9h,56h,34h,8Bh,0Bh,0D8h,43h,08h,8Bh,53h,04h,89h,4Ch,24h,1Ch
+    db 8Bh,0CFh,0D9h,5Ch,24h,24h,89h,54h,24h,20h,0E8h,8Ch,79h,0D6h,0FFh,8Bh
+    db 4Eh,08h,6Ah,01h,51h,8Bh,0CEh,0E8h,3Eh,6Fh,0D6h,0FFh,5Fh,5Eh,5Dh,5Bh
+    db 83h,0C4h,10h,0C2h,04h,00h
+?beginStructureCollapse@StructureCollapseUpdate@@IAEXPBVDamageInfo@@@Z ENDP
+_TEXT ENDS
+END

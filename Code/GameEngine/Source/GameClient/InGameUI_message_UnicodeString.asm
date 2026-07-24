ada52807b7543a3c269ea3cb6c608456958b363e
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 00:52:05 +0000

    Recover InGameUI Unicode message stub

---
+++ Code/GameEngine/Source/GameClient/InGameUI_message_UnicodeString.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ?message@InGameUI@@UAAXVUnicodeString@@ZZ
+; Exact 9 retail bytes @ 0x0043DEE0; reviewed vtable-owned UnicodeString cleanup tail stub
+_TEXT SEGMENT
+public ?message@InGameUI@@UAAXVUnicodeString@@ZZ
+?message@InGameUI@@UAAXVUnicodeString@@ZZ PROC
+    db 8Dh,4Ch,24h,08h,0E9h,0E7h,0A2h,44h,00h
+?message@InGameUI@@UAAXVUnicodeString@@ZZ ENDP
+_TEXT ENDS
+END

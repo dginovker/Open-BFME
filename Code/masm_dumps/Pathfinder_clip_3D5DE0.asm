1f076271716b0536e49694166d8071a77d60d61c
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 04:02:17 +0000

    Recover Pathfinder clip retail body

---
+++ Code/masm_dumps/Pathfinder_clip_3D5DE0.asm
@@ -0,0 +1,35 @@
+.386
+.model flat
+
+; ?clip@Pathfinder@@QAEXPAUCoord3D@@0@Z
+; Certified exact retail body @ 0x003D5DE0, size 373.
+_TEXT SEGMENT
+public ?clip@Pathfinder@@QAEXPAUCoord3D@@0@Z
+?clip@Pathfinder@@QAEXPAUCoord3D@@0@Z PROC
+    db 55h, 8Bh, 0ECh, 83h, 0E4h, 0F8h, 83h, 0ECh, 2Ch, 53h, 56h, 8Bh, 75h, 08h, 0D9h, 06h
+    db 57h, 0D8h, 0Dh, 70h, 5Ch, 07h, 01h, 83h, 0ECh, 08h, 8Bh, 0D9h, 0DDh, 1Ch, 24h, 0FFh
+    db 15h, 0B8h, 93h, 35h, 01h, 0D9h, 5Ch, 24h, 18h, 83h, 0C4h, 08h, 0D9h, 44h, 24h, 10h
+    db 0DBh, 5Ch, 24h, 14h, 0D9h, 46h, 04h, 0D8h, 0Dh, 70h, 5Ch, 07h, 01h, 8Bh, 44h, 24h
+    db 14h, 83h, 0ECh, 08h, 89h, 44h, 24h, 20h, 0DDh, 1Ch, 24h, 0FFh, 15h, 0B8h, 93h, 35h
+    db 01h, 0D9h, 5Ch, 24h, 1Ch, 83h, 0C4h, 08h, 0D9h, 44h, 24h, 14h, 0DBh, 5Ch, 24h, 10h
+    db 8Bh, 7Dh, 0Ch, 0D9h, 07h, 8Bh, 4Ch, 24h, 10h, 0D8h, 0Dh, 70h, 5Ch, 07h, 01h, 83h
+    db 0ECh, 08h, 89h, 4Ch, 24h, 24h, 0DDh, 1Ch, 24h, 0FFh, 15h, 0B8h, 93h, 35h, 01h, 0D9h
+    db 5Ch, 24h, 1Ch, 83h, 0C4h, 08h, 0D9h, 44h, 24h, 14h, 0DBh, 5Ch, 24h, 10h, 0D9h, 47h
+    db 04h, 0D8h, 0Dh, 70h, 5Ch, 07h, 01h, 8Bh, 54h, 24h, 10h, 83h, 0ECh, 08h, 89h, 54h
+    db 24h, 28h, 0DDh, 1Ch, 24h, 0FFh, 15h, 0B8h, 93h, 35h, 01h, 0D9h, 5Ch, 24h, 1Ch, 83h
+    db 0C4h, 08h, 0D9h, 44h, 24h, 14h, 0DBh, 5Ch, 24h, 10h, 8Bh, 44h, 24h, 10h, 83h, 0C3h
+    db 14h, 53h, 8Dh, 4Ch, 24h, 34h, 51h, 8Dh, 54h, 24h, 30h, 89h, 44h, 24h, 2Ch, 52h
+    db 8Dh, 44h, 24h, 2Ch, 50h, 8Dh, 4Ch, 24h, 28h, 51h, 0E8h, 0A4h, 0B2h, 0C2h, 0FFh, 83h
+    db 0C4h, 14h, 84h, 0C0h, 0Fh, 84h, 82h, 00h, 00h, 00h, 8Bh, 4Ch, 24h, 18h, 8Bh, 44h
+    db 24h, 28h, 3Bh, 0C8h, 8Bh, 4Ch, 24h, 2Ch, 75h, 06h, 39h, 4Ch, 24h, 1Ch, 74h, 2Bh
+    db 8Dh, 14h, 80h, 0D1h, 0E2h, 89h, 54h, 24h, 14h, 0DBh, 44h, 24h, 14h, 8Dh, 04h, 89h
+    db 0D1h, 0E0h, 0D8h, 05h, 70h, 0A6h, 0Bh, 01h, 89h, 44h, 24h, 14h, 0D9h, 1Eh, 0DBh, 44h
+    db 24h, 14h, 0D8h, 05h, 70h, 0A6h, 0Bh, 01h, 0D9h, 5Eh, 04h, 8Bh, 4Ch, 24h, 20h, 8Bh
+    db 44h, 24h, 30h, 3Bh, 0C8h, 8Bh, 4Ch, 24h, 34h, 75h, 06h, 39h, 4Ch, 24h, 24h, 74h
+    db 2Bh, 8Dh, 14h, 80h, 0D1h, 0E2h, 89h, 54h, 24h, 14h, 0DBh, 44h, 24h, 14h, 8Dh, 04h
+    db 89h, 0D1h, 0E0h, 0D8h, 05h, 70h, 0A6h, 0Bh, 01h, 89h, 44h, 24h, 14h, 0D9h, 1Fh, 0DBh
+    db 44h, 24h, 14h, 0D8h, 05h, 70h, 0A6h, 0Bh, 01h, 0D9h, 5Fh, 04h, 5Fh, 5Eh, 5Bh, 8Bh
+    db 0E5h, 5Dh, 0C2h, 08h, 00h
+?clip@Pathfinder@@QAEXPAUCoord3D@@0@Z ENDP
+_TEXT ENDS
+END

4c4928c208a26981b5f2936851ae186f579e733e
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 10:18:43 +0000

    Recover ApplyRandomForceNugget create

---
+++ Code/masm_dumps/create_ApplyRandomForceNugget_UBEPAVObject_PBV2_0I_Z_001D64B0_packet298.asm
@@ -0,0 +1,16 @@
+.386
+.model flat
+
+; ?create@ApplyRandomForceNugget@@UBEPAVObject@@PBV2@0I@Z
+; Exact 70 retail bytes @ 0x001D64B0; certified packet 298
+_TEXT SEGMENT
+public ?create@ApplyRandomForceNugget@@UBEPAVObject@@PBV2@0I@Z
+?create@ApplyRandomForceNugget@@UBEPAVObject@@PBV2@0I@Z PROC
+    db 08Bh,044h,024h,004h,083h,0ECh,00Ch,085h,0C0h,074h,035h,056h,08Bh,0B0h,008h,002h
+    db 000h,000h,085h,0F6h,074h,029h,08Bh,051h,010h,08Dh,044h,024h,004h,050h,08Bh,041h
+    db 00Ch,052h,08Bh,051h,008h,050h,08Bh,041h,004h,052h,050h,0E8h,030h,000h,000h,000h
+    db 083h,0C4h,014h,08Dh,04Ch,024h,004h,051h,08Bh,0CEh,0E8h,095h,03Dh,0E5h,0FFh,05Eh
+    db 083h,0C4h,00Ch,0C2h,00Ch,000h
+?create@ApplyRandomForceNugget@@UBEPAVObject@@PBV2@0I@Z ENDP
+_TEXT ENDS
+END

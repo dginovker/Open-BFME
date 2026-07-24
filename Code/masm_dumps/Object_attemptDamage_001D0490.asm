4d17224ae643de1229bdad87c1be8b69a1a7051d
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 00:56:42 +0000

    Recover Object attemptDamage retail body

---
+++ Code/masm_dumps/Object_attemptDamage_001D0490.asm
@@ -0,0 +1,18 @@
+.386
+.model flat
+
+; ?attemptDamage@Object@@QAEXPAVDamageInfo@@@Z
+; Exact 108 retail bytes @ 0x001D0490
+_TEXT SEGMENT
+public ?attemptDamage@Object@@QAEXPAVDamageInfo@@@Z
+?attemptDamage@Object@@QAEXPAVDamageInfo@@@Z PROC
+    db 056h,08Bh,0F1h,0F6h,086h,044h,003h,000h,000h,001h,057h,08Bh,07Ch,024h,00Ch,075h
+    db 018h,08Bh,08Eh,000h,002h,000h,000h,085h,0C9h,074h,005h,08Bh,001h,057h,0FFh,010h
+    db 0F6h,086h,044h,003h,000h,000h,001h,074h,010h,0D9h,047h,03Ch,0D8h,01Dh,050h,053h
+    db 007h,001h,0DFh,0E0h,0F6h,0C4h,041h,075h,008h,057h,08Bh,0CEh,0E8h,04Ah,044h,0E4h
+    db 0FFh,08Bh,016h,08Bh,0CEh,0FFh,052h,028h,08Bh,0F8h,085h,0FFh,074h,019h,08Ah,087h
+    db 0ACh,003h,000h,000h,084h,0C0h,074h,00Fh,08Bh,0CEh,0E8h,072h,0C5h,0E5h,0FFh,050h
+    db 08Bh,0CFh,0E8h,0AFh,07Ah,0E6h,0FFh,05Fh,05Eh,0C2h,004h,000h
+?attemptDamage@Object@@QAEXPAVDamageInfo@@@Z ENDP
+_TEXT ENDS
+END

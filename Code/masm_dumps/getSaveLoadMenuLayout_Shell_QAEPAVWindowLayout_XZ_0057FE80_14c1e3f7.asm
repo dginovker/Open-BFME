0a7823d2b1ffe3db16ff7ba8afa4984551c9f242
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 01:59:31 +0000

    Recover Shell save load menu layout

---
+++ Code/masm_dumps/getSaveLoadMenuLayout_Shell_QAEPAVWindowLayout_XZ_0057FE80_14c1e3f7.asm
@@ -0,0 +1,14 @@
+.386
+.model flat
+
+; ?getSaveLoadMenuLayout@Shell@@QAEPAVWindowLayout@@XZ
+; Exact 48 retail bytes @ 0x0057FE80
+_TEXT SEGMENT
+public ?getSaveLoadMenuLayout@Shell@@QAEPAVWindowLayout@@XZ
+?getSaveLoadMenuLayout@Shell@@QAEPAVWindowLayout@@XZ PROC
+    db 051h,056h,08Bh,0F1h,08Bh,046h,068h,085h,0C0h,075h,01Fh,051h,089h,064h,024h,008h
+    db 08Bh,0CCh,068h,09Ch,0B5h,010h,001h,0E8h,024h,08Dh,030h,000h,08Bh,00Dh,040h,01Bh
+    db 02Fh,001h,08Bh,001h,0FFh,050h,06Ch,089h,046h,068h,08Bh,046h,068h,05Eh,059h,0C3h
+?getSaveLoadMenuLayout@Shell@@QAEPAVWindowLayout@@XZ ENDP
+_TEXT ENDS
+END

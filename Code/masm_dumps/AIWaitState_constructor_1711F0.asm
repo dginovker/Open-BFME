f990ac0d3178c08175a0eaa05c431275ce901d65
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 00:08:00 +0000

    Recover AIWaitState constructor

---
+++ Code/masm_dumps/AIWaitState_constructor_1711F0.asm
@@ -0,0 +1,13 @@
+.386
+.model flat
+; ??0AIWaitState@@QAE@PAVStateMachine@@@Z
+; Exact 46B @ 0x001711F0; certified packet 100
+_TEXT SEGMENT
+public ??0AIWaitState@@QAE@PAVStateMachine@@@Z
+??0AIWaitState@@QAE@PAVStateMachine@@@Z PROC
+    db 51h, 56h, 51h, 8Bh, 0F1h, 89h, 64h, 24h, 08h, 8Bh, 0CCh, 68h, 2Ch, 7Dh, 09h, 01h
+    db 0E8h, 0BBh, 79h, 71h, 00h, 8Bh, 44h, 24h, 10h, 50h, 8Bh, 0CEh, 0E8h, 0A1h, 23h, 0E9h
+    db 0FFh, 0C7h, 06h, 0D8h, 7Ch, 09h, 01h, 8Bh, 0C6h, 5Eh, 59h, 0C2h, 04h, 00h
+??0AIWaitState@@QAE@PAVStateMachine@@@Z ENDP
+_TEXT ENDS
+END

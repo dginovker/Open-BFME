8f029a226d17ac8f7c320f4e282c198c595396fd
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 09:27:53 +0000

    Recover ScriptConditions supplies perimeter evaluation

---
+++ Code/masm_dumps/evaluateSkirmishSuppliesWithinDistancePerimeter_ScriptConditions_32D300.asm
@@ -0,0 +1,63 @@
+.386
+.model flat
+; ?evaluateSkirmishSuppliesWithinDistancePerimeter@ScriptConditions@@IAE_NPAVParameter@@000@Z
+; Exact 835B @ 0x0032D300; certified retail recovery
+_TEXT SEGMENT
+public ?evaluateSkirmishSuppliesWithinDistancePerimeter@ScriptConditions@@IAE_NPAVParameter@@000@Z
+?evaluateSkirmishSuppliesWithinDistancePerimeter@ScriptConditions@@IAE_NPAVParameter@@000@Z PROC
+    db 6Ah, 0FFh, 68h, 0A1h, 74h, 01h, 01h, 64h, 0A1h, 00h, 00h, 00h, 00h, 50h, 64h, 89h
+    db 25h, 00h, 00h, 00h, 00h, 83h, 0ECh, 78h, 8Bh, 84h, 24h, 88h, 00h, 00h, 00h, 8Bh
+    db 0Dh, 6Ch, 07h, 2Fh, 01h, 53h, 55h, 56h, 57h, 50h, 0E8h, 86h, 5Dh, 0CFh, 0FFh, 33h
+    db 0EDh, 66h, 3Bh, 0C5h, 89h, 44h, 24h, 18h, 0Fh, 84h, 9Dh, 02h, 00h, 00h, 8Bh, 0FFh
+    db 8Dh, 4Ch, 24h, 18h, 51h, 8Bh, 0Dh, 48h, 0D7h, 2Eh, 01h, 0E8h, 10h, 1Bh, 0D0h, 0FFh
+    db 8Bh, 0D8h, 3Bh, 0DDh, 0Fh, 84h, 76h, 02h, 00h, 00h, 8Bh, 94h, 24h, 0A0h, 00h, 00h
+    db 00h, 51h, 83h, 0C2h, 10h, 89h, 64h, 24h, 24h, 8Bh, 0CCh, 52h, 0E8h, 0EFh, 0A7h, 55h
+    db 00h, 8Bh, 0Dh, 6Ch, 07h, 2Fh, 01h, 8Bh, 01h, 0FFh, 50h, 58h, 8Bh, 0F0h, 3Bh, 0F5h
+    db 0Fh, 84h, 4Ah, 02h, 00h, 00h, 8Dh, 4Ch, 24h, 30h, 51h, 8Bh, 0CEh, 0E8h, 44h, 0A7h
+    db 0CDh, 0FFh, 8Bh, 94h, 24h, 9Ch, 00h, 00h, 00h, 8Bh, 42h, 0Ch, 8Bh, 0CEh, 89h, 44h
+    db 24h, 1Ch, 0E8h, 5Bh, 86h, 0D1h, 0FFh, 0D8h, 44h, 24h, 1Ch, 8Bh, 8Ch, 24h, 0A4h, 00h
+    db 00h, 00h, 8Bh, 51h, 0Ch, 0D9h, 5Ch, 24h, 20h, 89h, 54h, 24h, 24h, 0C7h, 44h, 24h
+    db 14h, 00h, 00h, 00h, 00h, 89h, 6Ch, 24h, 2Ch, 0C7h, 44h, 24h, 28h, 7Ch, 68h, 09h
+    db 01h, 89h, 0ACh, 24h, 90h, 00h, 00h, 00h, 89h, 6Ch, 24h, 40h, 0C7h, 44h, 24h, 3Ch
+    db 9Ch, 68h, 09h, 01h, 89h, 5Ch, 24h, 44h, 0C6h, 44h, 24h, 48h, 01h, 0C7h, 44h, 24h
+    db 4Ch, 08h, 00h, 00h, 00h, 33h, 0C0h, 8Bh, 0C8h, 8Bh, 0D0h, 81h, 0C9h, 80h, 00h, 00h
+    db 00h, 8Bh, 0F0h, 8Bh, 0F8h, 89h, 4Ch, 24h, 58h, 8Bh, 0Dh, 0BCh, 0D8h, 2Eh, 01h, 89h
+    db 44h, 24h, 5Ch, 89h, 54h, 24h, 60h, 8Bh, 15h, 0C0h, 0D8h, 2Eh, 01h, 89h, 44h, 24h
+    db 6Ch, 0A1h, 0B8h, 0D8h, 2Eh, 01h, 89h, 44h, 24h, 70h, 0A1h, 0C4h, 0D8h, 2Eh, 01h, 89h
+    db 4Ch, 24h, 74h, 8Bh, 0Dh, 0C8h, 0D8h, 2Eh, 01h, 89h, 54h, 24h, 78h, 8Bh, 15h, 0CCh
+    db 0D8h, 2Eh, 01h, 89h, 6Ch, 24h, 54h, 0C7h, 44h, 24h, 50h, 70h, 3Bh, 08h, 01h, 89h
+    db 74h, 24h, 64h, 89h, 7Ch, 24h, 68h, 89h, 44h, 24h, 7Ch, 89h, 8Ch, 24h, 80h, 00h
+    db 00h, 00h, 89h, 94h, 24h, 84h, 00h, 00h, 00h, 55h, 8Dh, 44h, 24h, 2Ch, 50h, 8Dh
+    db 4Ch, 24h, 44h, 0C6h, 84h, 24h, 98h, 00h, 00h, 00h, 02h, 0E8h, 60h, 56h, 6Ch, 00h
+    db 50h, 8Dh, 4Ch, 24h, 58h, 0E8h, 56h, 56h, 6Ch, 00h, 8Bh, 4Ch, 24h, 24h, 50h, 55h
+    db 51h, 8Bh, 0Dh, 0B8h, 0D5h, 2Eh, 01h, 8Dh, 54h, 24h, 40h, 52h, 8Dh, 44h, 24h, 24h
+    db 50h, 0E8h, 0BAh, 54h, 6Ch, 00h, 0B8h, 5Ch, 3Bh, 08h, 01h, 89h, 44h, 24h, 50h, 89h
+    db 44h, 24h, 3Ch, 0C6h, 84h, 24h, 90h, 00h, 00h, 00h, 04h, 89h, 44h, 24h, 28h, 0EBh
+    db 02h, 0DDh, 0D8h, 8Bh, 4Ch, 24h, 10h, 8Bh, 41h, 04h, 8Bh, 51h, 0Ch, 3Bh, 0D0h, 0Fh
+    db 84h, 97h, 00h, 00h, 00h, 8Bh, 0C2h, 8Bh, 30h, 83h, 0C0h, 08h, 3Bh, 0F5h, 89h, 41h
+    db 0Ch, 0Fh, 84h, 81h, 00h, 00h, 00h, 0F6h, 05h, 48h, 07h, 2Fh, 01h, 01h, 75h, 2Ch
+    db 83h, 0Dh, 48h, 07h, 2Fh, 01h, 01h, 8Bh, 0Dh, 00h, 0D6h, 2Eh, 01h, 68h, 0DCh, 3Bh
+    db 08h, 01h, 0C6h, 84h, 24h, 94h, 00h, 00h, 00h, 07h, 0E8h, 0C8h, 0D8h, 0D0h, 0FFh, 0A3h
+    db 44h, 07h, 2Fh, 01h, 0C6h, 84h, 24h, 90h, 00h, 00h, 00h, 04h, 8Bh, 0Dh, 44h, 07h
+    db 2Fh, 01h, 51h, 8Bh, 0CEh, 0E8h, 0F9h, 0D8h, 0CFh, 0FFh, 3Bh, 0C5h, 74h, 95h, 8Bh, 0B0h
+    db 88h, 00h, 00h, 00h, 8Bh, 0CBh, 0E8h, 00h, 18h, 0CEh, 0FFh, 0Fh, 0AFh, 0C6h, 85h, 0C0h
+    db 89h, 44h, 24h, 20h, 0DBh, 44h, 24h, 20h, 7Dh, 06h, 0D8h, 05h, 58h, 53h, 07h, 01h
+    db 0D8h, 54h, 24h, 14h, 0DFh, 0E0h, 0F6h, 0C4h, 41h, 0Fh, 85h, 62h, 0FFh, 0FFh, 0FFh, 0D9h
+    db 5Ch, 24h, 14h, 0E9h, 5Bh, 0FFh, 0FFh, 0FFh, 8Bh, 4Ch, 24h, 10h, 0D9h, 44h, 24h, 14h
+    db 8Bh, 51h, 10h, 0D8h, 5Ch, 24h, 24h, 4Ah, 89h, 51h, 10h, 0C7h, 84h, 24h, 90h, 00h
+    db 00h, 00h, 0FFh, 0FFh, 0FFh, 0FFh, 0DFh, 0E0h, 0F6h, 0C4h, 41h, 8Bh, 44h, 24h, 10h, 8Bh
+    db 48h, 10h, 74h, 64h, 3Bh, 0CDh, 75h, 38h, 8Bh, 08h, 3Bh, 0CDh, 8Bh, 0F0h, 74h, 27h
+    db 8Bh, 40h, 08h, 2Bh, 0C1h, 0C1h, 0F8h, 03h, 0C1h, 0E0h, 03h, 3Dh, 80h, 00h, 00h, 00h
+    db 76h, 0Bh, 51h, 0E8h, 0F8h, 48h, 55h, 00h, 83h, 0C4h, 04h, 0EBh, 0Ah, 50h, 51h, 0E8h
+    db 2Ch, 10h, 50h, 00h, 83h, 0C4h, 08h, 56h, 0E8h, 0E3h, 48h, 55h, 00h, 83h, 0C4h, 04h
+    db 66h, 39h, 6Ch, 24h, 18h, 0Fh, 85h, 65h, 0FDh, 0FFh, 0FFh, 32h, 0C0h, 8Bh, 8Ch, 24h
+    db 88h, 00h, 00h, 00h, 5Fh, 5Eh, 5Dh, 64h, 89h, 0Dh, 00h, 00h, 00h, 00h, 5Bh, 81h
+    db 0C4h, 84h, 00h, 00h, 00h, 0C2h, 10h, 00h, 3Bh, 0CDh, 75h, 43h, 8Bh, 08h, 3Bh, 0CDh
+    db 8Bh, 0F0h, 74h, 32h, 8Bh, 40h, 08h, 2Bh, 0C1h, 0C1h, 0F8h, 03h, 0C1h, 0E0h, 03h, 3Dh
+    db 80h, 00h, 00h, 00h, 76h, 16h, 51h, 0E8h, 94h, 48h, 55h, 00h, 83h, 0C4h, 04h, 56h
+    db 0E8h, 8Bh, 48h, 55h, 00h, 83h, 0C4h, 04h, 0B0h, 01h, 0EBh, 0B1h, 50h, 51h, 0E8h, 0BDh
+    db 0Fh, 50h, 00h, 83h, 0C4h, 08h, 56h, 0E8h, 74h, 48h, 55h, 00h, 83h, 0C4h, 04h, 0B0h
+    db 01h, 0EBh, 9Ah
+?evaluateSkirmishSuppliesWithinDistancePerimeter@ScriptConditions@@IAE_NPAVParameter@@000@Z ENDP
+_TEXT ENDS
+END

204679177098dff7689e92e658ffc5d7cb3d42ea
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 08:06:46 +0000

    Recover Drawable loadPostProcess retail body

---
+++ Code/masm_dumps/Drawable_loadPostProcess_4120A0.asm
@@ -0,0 +1,14 @@
+.386
+.model flat
+
+; ?loadPostProcess@Drawable@@MAEXXZ
+; Exact 45 retail bytes @ 0x004120A0
+_TEXT SEGMENT
+public ?loadPostProcess@Drawable@@MAEXXZ
+?loadPostProcess@Drawable@@MAEXXZ PROC
+    db 56h,08Bh,0F1h,08Bh,086h,09Ch,00h,00h,00h,085h,0C0h,074h,0Ch,083h,0C0h,08h
+    db 50h,08Dh,04Eh,0A0h,0E8h,15h,41h,0C2h,0FFh,08Bh,086h,05Ch,03h,00h,00h,085h
+    db 0C0h,074h,08h,08Bh,0C8h,08Bh,01h,05Eh,0FFh,60h,04h,05Eh,0C3h
+?loadPostProcess@Drawable@@MAEXXZ ENDP
+_TEXT ENDS
+END

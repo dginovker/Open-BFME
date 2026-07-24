a2a09b9c68c8fdbd07e4e24901b67d2b9dd71e4d
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 04:22:05 +0000

    Recover SpecialPowerModule getReadyFrame

---
+++ Code/masm_dumps/getReadyFrame_SpecialPowerModule_0026ABC0.asm
@@ -0,0 +1,19 @@
+.386
+.model flat
+
+; ?getReadyFrame@SpecialPowerModule@@UBEIXZ
+; Exact 126 retail bytes @ 0x0026ABC0
+_TEXT SEGMENT
+public ?getReadyFrame@SpecialPowerModule@@UBEIXZ
+?getReadyFrame@SpecialPowerModule@@UBEIXZ PROC
+    db 056h,08Bh,0F1h,08Bh,006h,057h,0FFh,050h,018h,08Bh,050h,004h,085h,0D2h,074h,010h
+    db 08Bh,04Ah,004h,085h,0C9h,074h,007h,0E8h,085h,0E0h,0DDh,0FFh,0EBh,002h,08Bh,0C2h
+    db 08Ah,088h,015h,001h,000h,000h,084h,0C9h,074h,024h,08Bh,04Eh,0E4h,085h,0C9h,074h
+    db 01Dh,0E8h,02Eh,05Ch,0DBh,0FFh,08Bh,0F8h,085h,0FFh,074h,012h,08Bh,016h,08Bh,0CEh
+    db 0FFh,052h,018h,050h,08Bh,0CFh,0E8h,024h,084h,0DAh,0FFh,05Fh,05Eh,0C3h,08Bh,046h
+    db 008h,085h,0C0h,07Fh,013h,08Bh,046h,0E4h,08Bh,088h,0A4h,001h,000h,000h,085h,0C9h
+    db 075h,006h,08Bh,046h,004h,05Fh,05Eh,0C3h,08Bh,00Dh,098h,008h,02Fh,001h,08Bh,041h
+    db 03Ch,08Bh,056h,00Ch,08Bh,04Eh,004h,02Bh,0C2h,05Fh,003h,0C1h,05Eh,0C3h
+?getReadyFrame@SpecialPowerModule@@UBEIXZ ENDP
+_TEXT ENDS
+END

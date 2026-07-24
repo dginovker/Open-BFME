924a5d318751531879c42106d705422886dbc390
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 04:25:33 +0000

    Recover ControlBar setControlCommand

---
+++ Code/masm_dumps/ControlBar_setControlCommand.asm
@@ -0,0 +1,51 @@
+.386
+.model flat
+
+; ?setControlCommand@ControlBar@@QAEXPAVGameWindow@@PBVCommandButton@@@Z
+; Exact 603 retail bytes @ 0x0049EDE0
+; Certified identity: Ghidra entry/ret boundary, matched ControlBar::init callers,
+; setCommandBarBorder callee, and GUIControlButtonClick string reference.
+_TEXT SEGMENT
+public ?setControlCommand@ControlBar@@QAEXPAVGameWindow@@PBVCommandButton@@@Z
+?setControlCommand@ControlBar@@QAEXPAVGameWindow@@PBVCommandButton@@@Z PROC
+    db 6Ah,0FFh,68h,40h,7Fh,02h,01h,64h,0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,83h,0ECh,08h,55h,56h,8Bh,74h,24h,20h,8Bh,06h
+    db 89h,4Ch,24h,08h,57h,8Bh,0CEh,0FFh,50h,20h,84h,0C0h,0Fh,84h,15h,02h
+    db 00h,00h,8Bh,7Ch,24h,28h,85h,0FFh,74h,0Eh,8Bh,47h,18h,0F6h,0C4h,04h
+    db 74h,06h,6Ah,00h,6Ah,01h,0EBh,04h,6Ah,00h,6Ah,00h,56h,0E8h,0Dh,9Eh
+    db 0B9h,0FFh,83h,0C4h,0Ch,85h,0FFh,74h,38h,8Bh,0CFh,0E8h,52h,6Eh,0B9h,0FFh
+    db 85h,0C0h,74h,2Dh,51h,89h,64h,24h,28h,8Bh,0CCh,68h,54h,6Eh,33h,01h
+    db 0E8h,0ABh,95h,3Eh,00h,56h,0E8h,96h,36h,0BAh,0FFh,83h,0C4h,08h,8Bh,0CFh
+    db 0E8h,2Dh,6Eh,0B9h,0FFh,50h,56h,0E8h,0B7h,9Bh,0B9h,0FFh,0E9h,0ACh,00h,00h
+    db 00h,6Ah,00h,6Ah,00h,8Bh,0CEh,0E8h,8Dh,6Fh,0B9h,0FFh,6Ah,00h,6Ah,05h
+    db 8Bh,0CEh,0E8h,82h,6Fh,0B9h,0FFh,6Ah,00h,6Ah,06h,8Bh,0CEh,0E8h,77h,6Fh
+    db 0B9h,0FFh,85h,0FFh,74h,70h,8Bh,47h,0Ch,85h,0C0h,8Dh,6Fh,0Ch,74h,05h
+    db 83h,0C0h,08h,0EBh,05h,0B8h,8Bh,38h,07h,01h,6Ah,0Bh,68h,0F8h,0B8h,0Fh
+    db 01h,50h,0FFh,15h,0BCh,94h,35h,01h,83h,0C4h,0Ch,85h,0C0h,74h,47h,0C7h
+    db 44h,24h,24h,00h,00h,00h,00h,55h,8Dh,4Ch,24h,28h,0C7h,44h,24h,20h
+    db 00h,00h,00h,00h,0E8h,17h,0A3h,3Eh,00h,51h,8Dh,54h,24h,28h,89h,64h
+    db 24h,14h,8Bh,0CCh,52h,0E8h,16h,95h,3Eh,00h,56h,0E8h,01h,36h,0BAh,0FFh
+    db 83h,0C4h,08h,8Dh,4Ch,24h,24h,0C7h,44h,24h,1Ch,0FFh,0FFh,0FFh,0FFh,0E8h
+    db 0CCh,92h,3Eh,00h,0EBh,1Ah,51h,89h,64h,24h,28h,8Bh,0CCh,68h,54h,6Eh
+    db 33h,01h,0E8h,0E9h,94h,3Eh,00h,56h,0E8h,0D4h,35h,0BAh,0FFh,83h,0C4h,08h
+    db 57h,56h,0E8h,44h,94h,0B9h,0FFh,83h,0C4h,08h,85h,0FFh,0Fh,84h,0F5h,00h
+    db 00h,00h,68h,90h,0CAh,89h,00h,8Bh,0CEh,0E8h,0E9h,0E1h,0B9h,0FFh,8Bh,0CFh
+    db 0E8h,04h,6Ah,0BAh,0FFh,8Bh,00h,85h,0C0h,74h,07h,66h,83h,78h,04h,00h
+    db 75h,28h,8Bh,87h,84h,00h,00h,00h,3Bh,87h,88h,00h,00h,00h,75h,1Ah
+    db 51h,89h,64h,24h,28h,8Bh,0CCh,68h,0F4h,8Ah,08h,01h,0E8h,6Fh,9Eh,3Eh
+    db 00h,56h,0E8h,7Ah,35h,0BAh,0FFh,83h,0C4h,08h,8Bh,0CFh,89h,0B7h,0A4h,00h
+    db 00h,00h,0E8h,05h,73h,0B9h,0FFh,8Bh,4Ch,24h,0Ch,50h,56h,0E8h,73h,96h
+    db 0B8h,0FFh,8Ah,87h,50h,01h,00h,00h,84h,0C0h,74h,09h,56h,0E8h,51h,0A7h
+    db 0B9h,0FFh,83h,0C4h,04h,0A1h,7Ch,4Ch,2Fh,01h,85h,0C0h,74h,53h,8Bh,0CFh
+    db 0E8h,94h,69h,0BAh,0FFh,50h,8Dh,4Ch,24h,2Ch,51h,8Bh,0Dh,7Ch,4Ch,2Fh
+    db 01h,0E8h,99h,68h,0B9h,0FFh,8Bh,44h,24h,28h,85h,0C0h,0C7h,44h,24h,1Ch
+    db 01h,00h,00h,00h,74h,1Ah,66h,83h,78h,04h,00h,74h,13h,8Bh,0Dh,7Ch
+    db 4Ch,2Fh,01h,6Ah,00h,8Dh,54h,24h,2Ch,52h,56h,0E8h,12h,63h,0B7h,0FFh
+    db 8Dh,4Ch,24h,28h,0C7h,44h,24h,1Ch,0FFh,0FFh,0FFh,0FFh,0E8h,3Fh,89h,3Eh
+    db 00h,51h,89h,64h,24h,28h,8Bh,0CCh,68h,0DCh,0B8h,0Fh,01h,0E8h,0AEh,9Bh
+    db 3Eh,00h,56h,0E8h,0DBh,0B3h,0B6h,0FFh,83h,0C4h,08h,68h,42h,0D9h,43h,00h
+    db 8Bh,0CEh,0E8h,0DDh,5Ch,0B6h,0FFh,8Bh,4Ch,24h,14h,5Fh,5Eh,64h,89h,0Dh
+    db 00h,00h,00h,00h,5Dh,83h,0C4h,14h,0C2h,08h,00h
+?setControlCommand@ControlBar@@QAEXPAVGameWindow@@PBVCommandButton@@@Z ENDP
+_TEXT ENDS
+END

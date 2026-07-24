9ff5ef2b039f38fcbad8169c51d23df6e3dd50f0
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 00:41:34 +0000

    Recover RankInfoStore destructor

---
+++ Code/masm_dumps/RankInfoStore_destructor.asm
@@ -0,0 +1,24 @@
+.386
+.model flat
+; ??1RankInfoStore@@UAE@XZ
+; Exact 212B @ 0x003A0BA0; certified retail MASM recovery
+_TEXT SEGMENT
+public ??1RankInfoStore@@UAE@XZ
+??1RankInfoStore@@UAE@XZ PROC
+    db 6Ah, 0FFh, 68h, 33h, 0CEh, 01h, 01h, 64h, 0A1h, 00h, 00h, 00h, 00h, 50h, 64h, 89h
+    db 25h, 00h, 00h, 00h, 00h, 51h, 56h, 8Bh, 0F1h, 57h, 89h, 74h, 24h, 08h, 0C7h, 06h
+    db 40h, 5Fh, 07h, 01h, 8Bh, 46h, 0Ch, 2Bh, 46h, 08h, 33h, 0FFh, 0A9h, 0FCh, 0FFh, 0FFh
+    db 0FFh, 0C7h, 44h, 24h, 14h, 01h, 00h, 00h, 00h, 7Eh, 23h, 0EBh, 03h, 8Dh, 49h, 00h
+    db 8Bh, 46h, 08h, 8Bh, 0Ch, 0B8h, 85h, 0C9h, 74h, 06h, 8Bh, 11h, 6Ah, 01h, 0FFh, 12h
+    db 8Bh, 46h, 0Ch, 2Bh, 46h, 08h, 47h, 0C1h, 0F8h, 02h, 3Bh, 0F8h, 7Ch, 0E2h, 8Bh, 46h
+    db 0Ch, 3Bh, 0C0h, 8Bh, 4Eh, 08h, 75h, 04h, 8Bh, 0C1h, 0EBh, 12h, 8Bh, 0F8h, 2Bh, 0F8h
+    db 57h, 50h, 51h, 0FFh, 15h, 5Ch, 94h, 35h, 01h, 83h, 0C4h, 0Ch, 03h, 0C7h, 89h, 46h
+    db 0Ch, 8Bh, 4Eh, 08h, 85h, 0C9h, 0C6h, 44h, 24h, 14h, 00h, 74h, 27h, 8Bh, 46h, 10h
+    db 2Bh, 0C1h, 0C1h, 0F8h, 02h, 0C1h, 0E0h, 02h, 3Dh, 80h, 00h, 00h, 00h, 76h, 0Bh, 51h
+    db 0E8h, 6Bh, 12h, 4Eh, 00h, 83h, 0C4h, 04h, 0EBh, 0Ah, 50h, 51h, 0E8h, 9Fh, 0D9h, 48h
+    db 00h, 83h, 0C4h, 08h, 8Bh, 0CEh, 0C7h, 44h, 24h, 14h, 0FFh, 0FFh, 0FFh, 0FFh, 0E8h, 0DDh
+    db 0Dh, 60h, 00h, 8Bh, 4Ch, 24h, 0Ch, 5Fh, 5Eh, 64h, 89h, 0Dh, 00h, 00h, 00h, 00h
+    db 83h, 0C4h, 10h, 0C3h
+??1RankInfoStore@@UAE@XZ ENDP
+_TEXT ENDS
+END

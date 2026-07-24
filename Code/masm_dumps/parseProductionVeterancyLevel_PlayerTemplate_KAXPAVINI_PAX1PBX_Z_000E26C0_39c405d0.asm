44b895222d8fcc8170f4c90082f9c50e9ac24327
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 01:39:46 +0000

    Recover PlayerTemplate production veterancy parser

---
+++ Code/masm_dumps/parseProductionVeterancyLevel_PlayerTemplate_KAXPAVINI_PAX1PBX_Z_000E26C0_39c405d0.asm
@@ -0,0 +1,22 @@
+.386
+.model flat
+
+; ?parseProductionVeterancyLevel@PlayerTemplate@@KAXPAVINI@@PAX1PBX@Z
+; Exact 170 retail bytes @ 0x000E26C0; certified packet 126
+_TEXT SEGMENT
+public ?parseProductionVeterancyLevel@PlayerTemplate@@KAXPAVINI@@PAX1PBX@Z
+?parseProductionVeterancyLevel@PlayerTemplate@@KAXPAVINI@@PAX1PBX@Z PROC
+    db 6Ah,0FFh,68h,78h,0A8h,0FFh,00h,64h,0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,83h,0ECh,08h,56h,8Bh,74h,24h,1Ch,6Ah,00h,8Bh
+    db 0CEh,0E8h,8Ah,0E2h,76h,00h,50h,8Dh,4Ch,24h,08h,0E8h,0D0h,64h,7Ah,00h
+    db 8Bh,44h,24h,04h,85h,0C0h,0C7h,44h,24h,14h,00h,00h,00h,00h,74h,05h
+    db 83h,0C0h,08h,0EBh,05h,0B8h,8Bh,38h,07h,01h,8Bh,0Dh,00h,0D6h,2Eh,01h
+    db 50h,0E8h,0C1h,86h,0F5h,0FFh,68h,0B4h,9Fh,2Ah,01h,6Ah,00h,8Bh,0CEh,89h
+    db 44h,24h,10h,0E8h,48h,0E2h,76h,00h,50h,0E8h,0B2h,0E2h,76h,00h,8Bh,4Ch
+    db 24h,28h,8Bh,0F0h,83h,0C4h,08h,8Dh,44h,24h,08h,50h,81h,0C1h,80h,00h
+    db 00h,00h,0E8h,0ECh,77h,0F3h,0FFh,8Dh,4Ch,24h,04h,89h,30h,0C7h,44h,24h
+    db 14h,0FFh,0FFh,0FFh,0FFh,0E8h,0E6h,51h,7Ah,00h,8Bh,4Ch,24h,0Ch,5Eh,64h
+    db 89h,0Dh,00h,00h,00h,00h,83h,0C4h,14h,0C3h
+?parseProductionVeterancyLevel@PlayerTemplate@@KAXPAVINI@@PAX1PBX@Z ENDP
+_TEXT ENDS
+END

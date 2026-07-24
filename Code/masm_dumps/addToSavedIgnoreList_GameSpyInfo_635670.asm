8a73a16e79e9b525fa07ed3823300d76e6c0b71b
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 06:09:08 +0000

    Recover GameSpyInfo saved-ignore add

---
+++ Code/masm_dumps/addToSavedIgnoreList_GameSpyInfo_635670.asm
@@ -0,0 +1,21 @@
+.386
+.model flat
+
+; ?addToSavedIgnoreList@GameSpyInfo@@UAEXHVAsciiString@@@Z
+; Exact 149 retail bytes @ RVA 0x00635670
+_TEXT SEGMENT
+public ?addToSavedIgnoreList@GameSpyInfo@@UAEXHVAsciiString@@@Z
+?addToSavedIgnoreList@GameSpyInfo@@UAEXHVAsciiString@@@Z PROC
+    db 06Ah,0FFh,068h,080h,00Ch,004h,001h,064h,0A1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,083h,0ECh,014h,08Dh,044h,024h,024h,050h,081h,0C1h,0D4h
+    db 006h,000h,000h,0C7h,044h,024h,020h,000h,000h,000h,000h,0E8h,010h,032h,0A1h,0FFh
+    db 08Dh,04Ch,024h,028h,051h,08Bh,0C8h,0E8h,0E4h,025h,025h,000h,08Dh,00Ch,024h,0E8h
+    db 0D5h,08Ch,09Fh,0FFh,08Bh,054h,024h,024h,06Ah,001h,052h,08Dh,044h,024h,030h,050h
+    db 08Dh,04Ch,024h,00Ch,0C6h,044h,024h,028h,001h,0E8h,0BBh,010h,09Eh,0FFh,08Dh,00Ch
+    db 024h,0E8h,0BFh,0ADh,09Fh,0FFh,08Dh,00Ch,024h,0C6h,044h,024h,01Ch,000h,0E8h,040h
+    db 0E3h,09Fh,0FFh,08Dh,04Ch,024h,028h,0C7h,044h,024h,01Ch,0FFh,0FFh,0FFh,0FFh,0E8h
+    db 04Ch,022h,025h,000h,08Bh,04Ch,024h,014h,064h,089h,00Dh,000h,000h,000h,000h,083h
+    db 0C4h,020h,0C2h,008h,000h
+?addToSavedIgnoreList@GameSpyInfo@@UAEXHVAsciiString@@@Z ENDP
+_TEXT ENDS
+END

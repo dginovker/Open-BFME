07fb152b40ca019bb97b61b774486857a5e2019c
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 03:13:30 +0000

    Recover AIHuntState update retail body

---
+++ Code/masm_dumps/AIHuntState_update_17E480.asm
@@ -0,0 +1,60 @@
+.386
+.model flat
+; ?update@AIHuntState@@UAE?AW4StateReturnType@@XZ
+; Exact 800B @ 0x0017E480; certified retail MASM recovery
+_TEXT SEGMENT
+public ?update@AIHuntState@@UAE?AW4StateReturnType@@XZ
+?update@AIHuntState@@UAE?AW4StateReturnType@@XZ PROC
+    db 083h, 0ech, 00ch, 053h, 055h, 056h, 057h, 08bh, 0f9h, 08bh, 04fh, 01ch, 08bh, 041h, 020h, 085h
+    db 0c0h, 074h, 04dh, 08bh, 057h, 024h, 039h, 042h, 020h, 074h, 045h, 0e8h, 0d0h, 000h, 0e9h, 0ffh
+    db 085h, 0c0h, 075h, 00ch, 08bh, 04fh, 01ch, 08bh, 001h, 06ah, 000h, 0ffh, 050h, 038h, 0ebh, 030h
+    db 08bh, 04fh, 024h, 08bh, 011h, 050h, 0ffh, 052h, 038h, 08bh, 04fh, 024h, 08bh, 041h, 01ch, 085h
+    db 0c0h, 074h, 00eh, 08bh, 050h, 004h, 085h, 0d2h, 075h, 007h, 08bh, 001h, 06ah, 00ah, 0ffh, 050h
+    db 020h, 08bh, 00dh, 098h, 008h, 02fh, 001h, 08bh, 051h, 03ch, 083h, 0c2h, 00fh, 089h, 057h, 028h
+    db 08bh, 057h, 01ch, 0a1h, 098h, 008h, 02fh, 001h, 08bh, 06ah, 010h, 08bh, 048h, 03ch, 08bh, 09dh
+    db 090h, 000h, 000h, 000h, 089h, 04ch, 024h, 014h, 08bh, 04fh, 024h, 0c1h, 0ebh, 01ch, 080h, 0e3h
+    db 001h, 0e8h, 06ah, 000h, 0e9h, 0ffh, 089h, 044h, 024h, 010h, 08bh, 045h, 004h, 085h, 0c0h, 074h
+    db 00ch, 08bh, 048h, 004h, 085h, 0c9h, 074h, 005h, 0e8h, 09eh, 03dh, 0e8h, 0ffh, 08bh, 088h, 0d4h
+    db 000h, 000h, 000h, 0f6h, 0c5h, 010h, 074h, 03fh, 08bh, 08dh, 0fch, 001h, 000h, 000h, 085h, 0c9h
+    db 074h, 035h, 08bh, 001h, 0ffh, 050h, 068h, 08bh, 0f0h, 085h, 0f6h, 074h, 02ah, 08bh, 04ch, 024h
+    db 010h, 085h, 0c9h, 074h, 022h, 06ah, 007h, 0e8h, 0d3h, 03fh, 0ebh, 0ffh, 084h, 0c0h, 075h, 017h
+    db 08bh, 044h, 024h, 010h, 08bh, 016h, 050h, 08bh, 0ceh, 0ffh, 092h, 030h, 001h, 000h, 000h, 084h
+    db 0c0h, 00fh, 085h, 011h, 002h, 000h, 000h, 084h, 0dbh, 00fh, 085h, 009h, 002h, 000h, 000h, 08bh
+    db 04ch, 024h, 014h, 03bh, 04fh, 028h, 00fh, 082h, 0fch, 001h, 000h, 000h, 08bh, 0cdh, 0e8h, 044h
+    db 04ch, 0ebh, 0ffh, 084h, 0c0h, 074h, 01ah, 06ah, 019h, 08bh, 0cdh, 0e8h, 08fh, 03fh, 0ebh, 0ffh
+    db 084h, 0c0h, 075h, 00dh, 05fh, 05eh, 05dh, 0b8h, 0feh, 0ffh, 0ffh, 0ffh, 05bh, 083h, 0c4h, 00ch
+    db 0c3h, 08bh, 0b5h, 004h, 002h, 000h, 000h, 08bh, 0ceh, 089h, 074h, 024h, 018h, 0e8h, 0f6h, 07fh
+    db 0eah, 0ffh, 085h, 0c0h, 074h, 01dh, 08bh, 04fh, 024h, 08bh, 011h, 050h, 0ffh, 052h, 038h, 08bh
+    db 04fh, 024h, 08bh, 001h, 06ah, 027h, 0ffh, 050h, 020h, 05fh, 05eh, 05dh, 033h, 0c0h, 05bh, 083h
+    db 0c4h, 00ch, 0c3h, 08bh, 016h, 08bh, 0ceh, 0b3h, 001h, 0ffh, 092h, 030h, 001h, 000h, 000h, 084h
+    db 0c0h, 074h, 021h, 08bh, 047h, 024h, 08bh, 048h, 01ch, 085h, 0c9h, 074h, 017h, 08bh, 001h, 0ffh
+    db 050h, 034h, 084h, 0c0h, 075h, 00eh, 08bh, 04fh, 024h, 0e8h, 00dh, 05fh, 0e9h, 0ffh, 084h, 0c0h
+    db 075h, 002h, 032h, 0dbh, 08bh, 04ch, 024h, 014h, 083h, 0c1h, 00fh, 084h, 0dbh, 089h, 04fh, 028h
+    db 00fh, 084h, 062h, 001h, 000h, 000h, 08bh, 08dh, 03ch, 002h, 000h, 000h, 08bh, 051h, 004h, 08ah
+    db 082h, 0c2h, 001h, 000h, 000h, 084h, 0c0h, 08bh, 05eh, 070h, 0c7h, 044h, 024h, 010h, 000h, 000h
+    db 000h, 000h, 074h, 018h, 0e8h, 070h, 0b0h, 0eah, 0ffh, 085h, 0c0h, 089h, 044h, 024h, 010h, 074h
+    db 00bh, 085h, 0dbh, 075h, 007h, 08bh, 0f0h, 0e9h, 0b2h, 000h, 000h, 000h, 08bh, 00dh, 014h, 0f2h
+    db 02eh, 001h, 06ah, 000h, 053h, 06ah, 04ah, 068h, 09ah, 03fh, 01ch, 046h, 055h, 0e8h, 02eh, 013h
+    db 0ech, 0ffh, 08bh, 0f0h, 085h, 0f6h, 075h, 033h, 08bh, 0cdh, 0e8h, 0b5h, 021h, 0eah, 0ffh, 085h
+    db 0c0h, 074h, 028h, 08bh, 0cdh, 0e8h, 0aah, 021h, 0eah, 0ffh, 08ah, 088h, 09dh, 002h, 000h, 000h
+    db 084h, 0c9h, 074h, 017h, 08bh, 00dh, 014h, 0f2h, 02eh, 001h, 056h, 056h, 06ah, 04ah, 068h, 09ah
+    db 03fh, 01ch, 046h, 055h, 0e8h, 0f7h, 012h, 0ech, 0ffh, 08bh, 0f0h, 08bh, 085h, 03ch, 002h, 000h
+    db 000h, 08bh, 048h, 004h, 08ah, 081h, 0c2h, 001h, 000h, 000h, 084h, 0c0h, 074h, 050h, 08bh, 04ch
+    db 024h, 010h, 085h, 0c9h, 074h, 03ch, 085h, 0dbh, 074h, 038h, 085h, 0f6h, 075h, 002h, 08bh, 0f1h
+    db 0e8h, 0f3h, 09dh, 0e8h, 0ffh, 050h, 08bh, 0cbh, 0e8h, 051h, 07dh, 0e9h, 0ffh, 085h, 0f6h, 089h
+    db 044h, 024h, 014h, 074h, 011h, 08bh, 0ceh, 0e8h, 0dch, 09dh, 0e8h, 0ffh, 050h, 08bh, 0cbh, 0e8h
+    db 03ah, 07dh, 0e9h, 0ffh, 0ebh, 002h, 033h, 0c0h, 039h, 044h, 024h, 014h, 07ch, 004h, 08bh, 074h
+    db 024h, 010h, 08bh, 08dh, 03ch, 002h, 000h, 000h, 056h, 0e8h, 08fh, 0c1h, 0eah, 0ffh, 08bh, 04fh
+    db 024h, 0e8h, 06ah, 0feh, 0e8h, 0ffh, 08bh, 04fh, 024h, 08bh, 0d8h, 03bh, 0f3h, 00fh, 095h, 0c0h
+    db 084h, 0c0h, 074h, 016h, 08bh, 011h, 056h, 0ffh, 052h, 038h, 085h, 0dbh, 075h, 019h, 08bh, 04ch
+    db 024h, 018h, 056h, 0e8h, 0c8h, 047h, 0e8h, 0ffh, 0ebh, 00dh, 0e8h, 0dch, 05dh, 0e9h, 0ffh, 084h
+    db 0c0h, 074h, 00eh, 085h, 0f6h, 074h, 00ah, 08bh, 04fh, 024h, 08bh, 001h, 06ah, 00ah, 0ffh, 050h
+    db 020h, 08bh, 0cdh, 0e8h, 0dch, 020h, 0eah, 0ffh, 085h, 0c0h, 074h, 02ch, 08bh, 0cdh, 0e8h, 0d1h
+    db 020h, 0eah, 0ffh, 08ah, 088h, 09dh, 002h, 000h, 000h, 084h, 0c9h, 075h, 01bh, 08bh, 04fh, 024h
+    db 0e8h, 0c6h, 0f5h, 0eah, 0ffh, 085h, 0c0h, 075h, 00fh, 085h, 0f6h, 075h, 00bh, 05fh, 05eh, 05dh
+    db 083h, 0c8h, 0ffh, 05bh, 083h, 0c4h, 00ch, 0c3h, 08bh, 04fh, 01ch, 0c6h, 041h, 040h, 001h, 08bh
+    db 04fh, 024h, 08bh, 011h, 0ffh, 052h, 010h, 08bh, 04fh, 01ch, 033h, 0d2h, 085h, 0c0h, 00fh, 09fh
+    db 0c2h, 05fh, 05eh, 05dh, 0c6h, 041h, 040h, 000h, 05bh, 04ah, 023h, 0c2h, 083h, 0c4h, 00ch, 0c3h
+?update@AIHuntState@@UAE?AW4StateReturnType@@XZ ENDP
+_TEXT ENDS
+END

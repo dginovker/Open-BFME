23930862ee78ea1d66faa3b55bbe97895fb8ccab
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 05:25:02 +0000

    Recover ScriptActions doNamedFlash retail body

---
+++ Code/masm_dumps/ScriptActions_doNamedFlash_2EE7C0.asm
@@ -0,0 +1,19 @@
+.386
+.model flat
+
+; ?doNamedFlash@ScriptActions@@IAEXABVAsciiString@@HPBURGBColor@@@Z
+; Exact 124 retail bytes @ 0x002EE7C0
+_TEXT SEGMENT
+public ?doNamedFlash@ScriptActions@@IAEXABVAsciiString@@HPBURGBColor@@@Z
+?doNamedFlash@ScriptActions@@IAEXABVAsciiString@@HPBURGBColor@@@Z PROC
+    db 08Bh,00Dh,06Ch,007h,02Fh,001h,08Bh,054h,024h,004h,08Bh,001h,057h,052h,0FFh,050h
+    db 068h,08Bh,0F8h,085h,0FFh,074h,061h,08Bh,007h,053h,08Bh,0CFh,0FFh,050h,028h,08Bh
+    db 0D8h,085h,0DBh,074h,052h,08Bh,044h,024h,010h,085h,0C0h,07Eh,04Ah,08Dh,00Ch,080h
+    db 0B8h,089h,088h,088h,088h,0F7h,0E9h,003h,0D1h,08Bh,04Ch,024h,014h,0C1h,0FAh,003h
+    db 056h,08Bh,0F2h,0C1h,0EEh,01Fh,003h,0F2h,085h,0C9h,075h,019h,08Bh,0CFh,0E8h,08Dh
+    db 0B4h,0D1h,0FFh,089h,0B3h,060h,001h,000h,000h,05Eh,089h,083h,064h,001h,000h,000h
+    db 05Bh,05Fh,0C2h,00Ch,000h,0E8h,05Fh,0F1h,0D3h,0FFh,089h,0B3h,060h,001h,000h,000h
+    db 089h,083h,064h,001h,000h,000h,05Eh,05Bh,05Fh,0C2h,00Ch,000h
+?doNamedFlash@ScriptActions@@IAEXABVAsciiString@@HPBURGBColor@@@Z ENDP
+_TEXT ENDS
+END

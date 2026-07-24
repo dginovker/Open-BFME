da1b8a56d34dc75761ff5616866660a093b310e9
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 09:17:50 +0000

    Recover setUnignoreText retail body

---
+++ Code/masm_dumps/setUnignoreText_004EB100_packet274.asm
@@ -0,0 +1,33 @@
+.386
+.model flat
+
+; ?setUnignoreText@@YAXPAVWindowLayout@@VAsciiString@@H@Z
+; Exact 326 retail bytes @ 0x004EB100; certified packet 274
+; SHA-256 4c6c72a0bcae05f434c0f3e57f24aefb7cb1d9fe670cb081b50ef45be0c1e1cf
+_TEXT SEGMENT
+public ?setUnignoreText@@YAXPAVWindowLayout@@VAsciiString@@H@Z
+?setUnignoreText@@YAXPAVWindowLayout@@VAsciiString@@H@Z PROC
+    db 6Ah,0FFh,68h,0C8h,0BCh,02h,01h,64h,0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,83h,0ECh,08h,56h,0C7h,44h,24h,14h,00h,00h,00h
+    db 00h,0C7h,44h,24h,04h,00h,00h,00h,00h,8Bh,74h,24h,1Ch,8Dh,44h,24h
+    db 1Ch,50h,8Bh,0CEh,0C6h,44h,24h,18h,01h,0E8h,15h,0ECh,0B2h,0FFh,8Bh,00h
+    db 85h,0C0h,0C6h,44h,24h,14h,02h,74h,05h,83h,0C0h,08h,0EBh,05h,0B8h,8Bh
+    db 38h,07h,01h,83h,0C0h,06h,50h,51h,89h,64h,24h,10h,8Bh,0CCh,68h,84h
+    db 21h,10h,01h,0E8h,58h,0DAh,39h,00h,8Dh,4Ch,24h,0Ch,51h,0E8h,7Eh,0DEh
+    db 39h,00h,83h,0C4h,0Ch,8Dh,4Ch,24h,1Ch,0C6h,44h,24h,14h,01h,0E8h,0BDh
+    db 0C7h,39h,00h,8Bh,44h,24h,04h,85h,0C0h,74h,05h,83h,0C0h,08h,0EBh,05h
+    db 0B8h,8Bh,38h,07h,01h,8Bh,0Dh,00h,0D6h,2Eh,01h,50h,0E8h,36h,0FCh,0B4h
+    db 0FFh,8Bh,56h,08h,8Bh,0Dh,40h,1Bh,2Fh,01h,8Bh,31h,50h,52h,0FFh,96h
+    db 0DCh,00h,00h,00h,8Bh,0F0h,85h,0F6h,74h,5Dh,8Bh,0Dh,94h,71h,2Fh,01h
+    db 8Bh,44h,24h,24h,8Bh,11h,50h,0FFh,92h,2Ch,01h,00h,00h,84h,0C0h,75h
+    db 23h,51h,8Dh,54h,24h,24h,89h,64h,24h,0Ch,8Bh,0CCh,52h,0E8h,7Eh,0C9h
+    db 39h,00h,8Bh,0Dh,94h,71h,2Fh,01h,8Bh,01h,0FFh,90h,44h,01h,00h,00h
+    db 84h,0C0h,74h,23h,8Bh,0Dh,7Ch,14h,2Fh,01h,8Bh,11h,51h,8Bh,0C4h,89h
+    db 64h,24h,0Ch,6Ah,00h,68h,0CCh,21h,10h,01h,50h,0FFh,52h,28h,56h,0E8h
+    db 0DDh,72h,0B5h,0FFh,83h,0C4h,08h,8Dh,4Ch,24h,04h,0C6h,44h,24h,14h,00h
+    db 0E8h,1Bh,0C7h,39h,00h,8Dh,4Ch,24h,20h,0C7h,44h,24h,14h,0FFh,0FFh,0FFh
+    db 0FFh,0E8h,0Ah,0C7h,39h,00h,8Bh,4Ch,24h,0Ch,64h,89h,0Dh,00h,00h,00h
+    db 00h,5Eh,83h,0C4h,14h,0C3h
+?setUnignoreText@@YAXPAVWindowLayout@@VAsciiString@@H@Z ENDP
+_TEXT ENDS
+END

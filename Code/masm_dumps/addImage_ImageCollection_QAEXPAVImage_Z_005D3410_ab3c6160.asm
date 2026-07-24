f758db10401e896ee389927c345d42a08fc32c32
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 00:47:27 +0000

    Recover ImageCollection addImage

---
+++ Code/masm_dumps/addImage_ImageCollection_QAEXPAVImage_Z_005D3410_ab3c6160.asm
@@ -0,0 +1,20 @@
+.386
+.model flat
+
+; ?addImage@ImageCollection@@QAEXPAVImage@@@Z
+; Exact 133 retail bytes @ 0x005D3410; certified packet 107
+_TEXT SEGMENT
+public ?addImage@ImageCollection@@QAEXPAVImage@@@Z
+?addImage@ImageCollection@@QAEXPAVImage@@@Z PROC
+    db 06Ah,0FFh,068h,098h,0A9h,003h,001h,064h,0A1h,000h,000h,000h,000h,050h,064h,089h
+    db 025h,000h,000h,000h,000h,051h,056h,057h,08Bh,07Ch,024h,01Ch,08Dh,044h,024h,008h
+    db 08Bh,0F1h,050h,08Bh,0CFh,0E8h,0EEh,03Bh,0A7h,0FFh,08Bh,000h,085h,0C0h,0C7h,044h
+    db 024h,014h,000h,000h,000h,000h,074h,005h,083h,0C0h,008h,0EBh,005h,0B8h,08Bh,038h
+    db 007h,001h,08Bh,00Dh,000h,0D6h,02Eh,001h,050h,0E8h,058h,039h,0A3h,0FFh,08Dh,04Ch
+    db 024h,01Ch,051h,08Dh,04Eh,008h,089h,044h,024h,020h,0E8h,0BDh,070h,0A5h,0FFh,08Dh
+    db 04Ch,024h,008h,089h,038h,0C7h,044h,024h,014h,0FFh,0FFh,0FFh,0FFh,0E8h,0BEh,044h
+    db 02Bh,000h,08Bh,04Ch,024h,00Ch,05Fh,05Eh,064h,089h,00Dh,000h,000h,000h,000h,083h
+    db 0C4h,010h,0C2h,004h,000h
+?addImage@ImageCollection@@QAEXPAVImage@@@Z ENDP
+_TEXT ENDS
+END

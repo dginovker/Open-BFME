67b050a5fd3416a847ef4238e2146d191d499607
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 01:18:20 +0000

    Recover ScriptActions garrison building function

---
+++ Code/masm_dumps/doUnitGarrisonSpecificBuilding_ScriptActions_IAEXABVAsciiString_0_Z_00302570_c8425e05.asm
@@ -0,0 +1,21 @@
+.386
+.model flat
+
+; ?doUnitGarrisonSpecificBuilding@ScriptActions@@IAEXABVAsciiString@@0@Z
+; Exact 146 retail bytes @ 0x00302570; certified packet 121
+_TEXT SEGMENT
+public ?doUnitGarrisonSpecificBuilding@ScriptActions@@IAEXABVAsciiString@@0@Z
+?doUnitGarrisonSpecificBuilding@ScriptActions@@IAEXABVAsciiString@@0@Z PROC
+    db 8Bh,0Dh,6Ch,07h,2Fh,01h,8Bh,54h,24h,04h,8Bh,01h,53h,56h,57h,52h
+    db 0FFh,50h,68h,8Bh,0D8h,85h,0DBh,74h,73h,8Bh,44h,24h,14h,51h,89h,64h
+    db 24h,14h,8Bh,0CCh,50h,0E8h,0C6h,55h,58h,00h,8Bh,0Dh,6Ch,07h,2Fh,01h
+    db 8Bh,11h,0FFh,52h,6Ch,8Bh,0F0h,85h,0F6h,74h,51h,8Bh,8Eh,0FCh,01h,00h
+    db 00h,85h,0C9h,74h,47h,8Bh,01h,0FFh,90h,30h,01h,00h,00h,6Ah,07h,8Bh
+    db 0CEh,8Bh,0F8h,0E8h,57h,0FFh,0D2h,0FFh,84h,0C0h,74h,05h,66h,85h,0FFh,74h
+    db 16h,8Bh,0CBh,0E8h,4Ch,0E2h,0D1h,0FFh,8Bh,48h,24h,0BAh,01h,00h,00h,00h
+    db 0D3h,0E2h,66h,3Bh,0FAh,75h,15h,8Bh,83h,04h,02h,00h,00h,85h,0C0h,74h
+    db 0Bh,6Ah,01h,56h,8Dh,48h,20h,0E8h,0A5h,1Ch,0D4h,0FFh,5Fh,5Eh,5Bh,0C2h
+    db 08h,00h
+?doUnitGarrisonSpecificBuilding@ScriptActions@@IAEXABVAsciiString@@0@Z ENDP
+_TEXT ENDS
+END

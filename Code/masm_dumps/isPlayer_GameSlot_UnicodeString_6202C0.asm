dcd030065579ccb92382abdc9ff4cd2fa75b3b9e
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 09:46:05 +0000

    Recover GameSlot UnicodeString isPlayer

---
+++ Code/masm_dumps/isPlayer_GameSlot_UnicodeString_6202C0.asm
@@ -0,0 +1,15 @@
+.386
+.model flat
+
+; ?isPlayer@GameSlot@@QBE_NVUnicodeString@@@Z
+; Exact 58 retail bytes @ RVA 0x006202C0.
+_TEXT SEGMENT
+public ?isPlayer@GameSlot@@QBE_NVUnicodeString@@@Z
+?isPlayer@GameSlot@@QBE_NVUnicodeString@@@Z PROC
+    db 083h,079h,004h,005h,053h,075h,022h,08Dh,044h,024h,008h,050h,083h,0C1h,028h,0E8h
+    db 0CBh,05Dh,09Fh,0FFh,085h,0C0h,075h,011h,08Dh,04Ch,024h,008h,0B3h,001h,0E8h,0EDh
+    db 07Eh,026h,000h,08Ah,0C3h,05Bh,0C2h,004h,000h,08Dh,04Ch,024h,008h,032h,0DBh,0E8h
+    db 0DCh,07Eh,026h,000h,08Ah,0C3h,05Bh,0C2h,004h,000h
+?isPlayer@GameSlot@@QBE_NVUnicodeString@@@Z ENDP
+_TEXT ENDS
+END

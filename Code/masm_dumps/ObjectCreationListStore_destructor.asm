9c7842d9b9edf611a3d74c54ecdb0e339fc70ed3
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 08:46:13 +0000

    Recover ObjectCreationListStore destructor

---
+++ Code/masm_dumps/ObjectCreationListStore_destructor.asm
@@ -0,0 +1,23 @@
+.386
+.model flat
+; ??1ObjectCreationListStore@@UAE@XZ
+; Exact 207B @ 0x001DA720; certified constructor/vtable/wrapper identity
+_TEXT SEGMENT
+public ??1ObjectCreationListStore@@UAE@XZ
+??1ObjectCreationListStore@@UAE@XZ PROC
+    db 6Ah, 0FFh, 68h, 0EEh, 9Dh, 00h, 01h, 64h, 0A1h, 00h, 00h, 00h, 00h, 50h, 64h, 89h
+    db 25h, 00h, 00h, 00h, 00h, 51h, 56h, 8Bh, 0F1h, 57h, 89h, 74h, 24h, 08h, 0C7h, 06h
+    db 0DCh, 0F9h, 09h, 01h, 8Bh, 7Eh, 14h, 8Bh, 46h, 18h, 3Bh, 0F8h, 0C7h, 44h, 24h, 14h
+    db 02h, 00h, 00h, 00h, 74h, 16h, 8Bh, 0Fh, 85h, 0C9h, 74h, 06h, 8Bh, 01h, 6Ah, 01h
+    db 0FFh, 10h, 8Bh, 46h, 18h, 83h, 0C7h, 04h, 3Bh, 0F8h, 75h, 0EAh, 8Bh, 46h, 18h, 3Bh
+    db 0C0h, 8Bh, 4Eh, 14h, 75h, 04h, 8Bh, 0C1h, 0EBh, 12h, 8Bh, 0F8h, 2Bh, 0F8h, 57h, 50h
+    db 51h, 0FFh, 15h, 5Ch, 94h, 35h, 01h, 83h, 0C4h, 0Ch, 03h, 0C7h, 89h, 46h, 18h, 8Bh
+    db 4Eh, 14h, 85h, 0C9h, 0C6h, 44h, 24h, 14h, 01h, 74h, 27h, 8Bh, 46h, 1Ch, 2Bh, 0C1h
+    db 0C1h, 0F8h, 02h, 0C1h, 0E0h, 02h, 3Dh, 80h, 00h, 00h, 00h, 76h, 0Bh, 51h, 0E8h, 0FDh
+    db 76h, 6Ah, 00h, 83h, 0C4h, 04h, 0EBh, 0Ah, 50h, 51h, 0E8h, 31h, 3Eh, 65h, 00h, 83h
+    db 0C4h, 08h, 8Dh, 4Eh, 08h, 0C6h, 44h, 24h, 14h, 00h, 0E8h, 41h, 29h, 0E4h, 0FFh, 8Bh
+    db 0CEh, 0C7h, 44h, 24h, 14h, 0FFh, 0FFh, 0FFh, 0FFh, 0E8h, 62h, 72h, 7Ch, 00h, 8Bh, 4Ch
+    db 24h, 0Ch, 5Fh, 5Eh, 64h, 89h, 0Dh, 00h, 00h, 00h, 00h, 83h, 0C4h, 10h, 0C3h
+??1ObjectCreationListStore@@UAE@XZ ENDP
+_TEXT ENDS
+END

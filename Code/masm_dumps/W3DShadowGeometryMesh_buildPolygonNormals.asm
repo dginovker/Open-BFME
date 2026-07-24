04edb82ca8d37596a3e083f8ccfa1e1c41bd258a
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 08:26:24 +0000

    Recover W3DShadowGeometryMesh buildPolygonNormals

---
+++ Code/masm_dumps/W3DShadowGeometryMesh_buildPolygonNormals.asm
@@ -0,0 +1,38 @@
+.386
+.model flat
+
+; ?buildPolygonNormals@W3DShadowGeometryMesh@@QAEXXZ
+; Exact 209 retail bytes @ 0x007BD320.
+_TEXT SEGMENT
+public ?buildPolygonNormals@W3DShadowGeometryMesh@@QAEXXZ
+?buildPolygonNormals@W3DShadowGeometryMesh@@QAEXXZ PROC
+    db 6Ah,0FFh,68h,0CBh,2Ch,05h,01h,64h
+    db 0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,51h,56h,8Bh
+    db 0F1h,8Bh,46h,10h,85h,0C0h,0Fh,85h
+    db 9Dh,00h,00h,00h,8Ah,46h,30h,84h
+    db 0C0h,53h,55h,57h,74h,36h,8Bh,46h
+    db 1Ch,39h,05h,5Ch,6Fh,30h,01h,7Dh
+    db 23h,6Ah,00h,50h,0B9h,54h,6Fh,30h
+    db 01h,0E8h,18h,29h,88h,0FFh,84h,0C0h
+    db 74h,12h,0A1h,5Ch,6Fh,30h,01h,3Bh
+    db 05h,64h,6Fh,30h,01h,7Dh,05h,0A3h
+    db 64h,6Fh,30h,01h,8Bh,2Dh,58h,6Fh
+    db 30h,01h,0EBh,38h,8Bh,7Eh,1Ch,8Dh
+    db 04h,7Fh,0C1h,0E0h,02h,50h,0E8h,0DDh
+    db 4Bh,0Ch,00h,8Bh,0D8h,83h,0C4h,04h
+    db 89h,5Ch,24h,10h,85h,0DBh,0C7h,44h
+    db 24h,1Ch,00h,00h,00h,00h,74h,10h
+    db 68h,7Ch,67h,44h,00h,57h,6Ah,0Ch
+    db 53h,0E8h,0A6h,0DAh,84h,0FFh,0EBh,02h
+    db 33h,0DBh,8Bh,0EBh,8Bh,46h,1Ch,33h
+    db 0FFh,85h,0C0h,7Eh,16h,8Bh,0DDh,53h
+    db 57h,8Bh,0CEh,0E8h,29h,3Dh,85h,0FFh
+    db 8Bh,46h,1Ch,47h,83h,0C3h,0Ch,3Bh
+    db 0F8h,7Ch,0ECh,5Fh,89h,6Eh,10h,5Dh
+    db 5Bh,8Bh,4Ch,24h,08h,5Eh,64h,89h
+    db 0Dh,00h,00h,00h,00h,83h,0C4h,10h
+    db 0C3h
+?buildPolygonNormals@W3DShadowGeometryMesh@@QAEXXZ ENDP
+_TEXT ENDS
+END

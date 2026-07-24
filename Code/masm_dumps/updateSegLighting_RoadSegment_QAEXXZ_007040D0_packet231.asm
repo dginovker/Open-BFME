1e986fdc965a7c3360bd39cf6ff7c470fba593f5
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 06:57:40 +0000

    Recover RoadSegment updateSegLighting

---
+++ Code/masm_dumps/updateSegLighting_RoadSegment_QAEXXZ_007040D0_packet231.asm
@@ -0,0 +1,22 @@
+.386
+.model flat
+
+; ?updateSegLighting@RoadSegment@@QAEXXZ
+; Exact 161 retail bytes @ 0x007040D0
+_TEXT SEGMENT
+public ?updateSegLighting@RoadSegment@@QAEXXZ
+?updateSegLighting@RoadSegment@@QAEXXZ PROC
+    db 083h,0ECh,008h,053h,057h,08Bh,0F9h,08Bh,047h,058h,033h,0DBh,03Bh,0C3h,089h,05Ch
+    db 024h,00Ch,00Fh,08Eh,083h,000h,000h,000h,055h,056h,08Dh,09Bh,000h,000h,000h,000h
+    db 08Bh,047h,05Ch,0D9h,004h,003h,08Bh,00Dh,0E0h,07Fh,02Fh,001h,0D8h,00Dh,070h,05Ch
+    db 007h,001h,08Bh,091h,0F4h,02Fh,000h,000h,08Dh,02Ch,003h,08Bh,042h,010h,0DCh,005h
+    db 0F0h,0B3h,009h,001h,089h,044h,024h,010h,0E8h,01Bh,02Dh,02Fh,000h,0D9h,045h,004h
+    db 0D8h,00Dh,070h,05Ch,007h,001h,08Bh,0F0h,003h,074h,024h,010h,0DCh,005h,0F0h,0B3h
+    db 009h,001h,0E8h,001h,02Dh,02Fh,000h,08Bh,054h,024h,010h,08Bh,00Dh,0E0h,07Fh,02Fh
+    db 001h,003h,0C2h,050h,056h,0E8h,004h,06Fh,092h,0FFh,08Bh,04Fh,05Ch,00Dh,000h,000h
+    db 000h,0FFh,089h,044h,00Bh,00Ch,08Bh,044h,024h,014h,08Bh,04Fh,058h,040h,083h,0C3h
+    db 018h,03Bh,0C1h,089h,044h,024h,014h,07Ch,087h,05Eh,05Dh,05Fh,05Bh,083h,0C4h,008h
+    db 0C3h
+?updateSegLighting@RoadSegment@@QAEXXZ ENDP
+_TEXT ENDS
+END

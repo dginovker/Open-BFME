411074aa79f7b79fad4273263dec40774146f3f9
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 11:01:35 +0000

    Recover BitFlags 17 parse retail body

---
+++ Code/masm_dumps/parse_BitFlags_0BB_QAEXPAVINI_PAVAsciiString_Z_001EB490.asm
@@ -0,0 +1,32 @@
+.386
+.model flat
+
+; ?parse@?$BitFlags@$0BB@@@QAEXPAVINI@@PAVAsciiString@@@Z
+; Retail @ 0x001EB490 size 166
+_TEXT SEGMENT
+public ?parse@?$BitFlags@$0BB@@@QAEXPAVINI@@PAVAsciiString@@@Z
+?parse@?$BitFlags@$0BB@@@QAEXPAVINI@@PAVAsciiString@@@Z PROC
+    db 83h,0ECh,08h,55h,56h,57h,8Bh,7Ch
+    db 24h,1Ch,85h,0FFh,89h,4Ch,24h,10h
+    db 74h,07h,8Bh,0CFh,0E8h,97h,0C4h,69h
+    db 00h,8Bh,6Ch,24h,18h,6Ah,00h,8Bh
+    db 0CDh,0C6h,44h,24h,13h,00h,0C6h,44h
+    db 24h,20h,00h,0E8h,00h,55h,66h,00h
+    db 8Bh,0F0h,85h,0F6h,74h,67h,85h,0FFh
+    db 74h,3Ch,8Bh,07h,85h,0C0h,74h,15h
+    db 66h,83h,78h,04h,00h,74h,0Eh,6Ah
+    db 01h,68h,1Ch,0EDh,08h,01h,8Bh,0CFh
+    db 0E8h,7Bh,0C8h,69h,00h,85h,0F6h,74h
+    db 12h,8Bh,0C6h,8Dh,50h,01h,8Bh,0FFh
+    db 8Ah,08h,40h,84h,0C9h,75h,0F9h,2Bh
+    db 0C2h,0EBh,02h,33h,0C0h,50h,56h,8Bh
+    db 0CFh,0E8h,5Ah,0C8h,69h,00h,8Dh,44h
+    db 24h,1Ch,50h,8Dh,4Ch,24h,13h,51h
+    db 8Bh,4Ch,24h,18h,56h,0E8h,11h,0E5h
+    db 0E5h,0FFh,84h,0C0h,74h,0Fh,6Ah,00h
+    db 8Bh,0CDh,0E8h,99h,54h,66h,00h,8Bh
+    db 0F0h,85h,0F6h,75h,99h,5Fh,5Eh,5Dh
+    db 83h,0C4h,08h,0C2h,08h,00h
+?parse@?$BitFlags@$0BB@@@QAEXPAVINI@@PAVAsciiString@@@Z ENDP
+_TEXT ENDS
+END

077dc5f6244a2c669a3d8e8cbeed0d26a2a52f44
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 04:08:06 +0000

    Recover InGameUI messageColor retail body

---
+++ Code/masm_dumps/InGameUI_messageColor_RGBColor_43DEF0.asm
@@ -0,0 +1,12 @@
+.386
+.model flat
+
+; ?messageColor@InGameUI@@UAAXPBURGBColor@@VUnicodeString@@ZZ
+; Retail @ 0x0043DEF0 size 9
+_TEXT SEGMENT
+public ?messageColor@InGameUI@@UAAXPBURGBColor@@VUnicodeString@@ZZ
+?messageColor@InGameUI@@UAAXPBURGBColor@@VUnicodeString@@ZZ PROC
+    db 08Dh,04Ch,024h,00Ch,0E9h,0D7h,0A2h,044h,000h
+?messageColor@InGameUI@@UAAXPBURGBColor@@VUnicodeString@@ZZ ENDP
+_TEXT ENDS
+END

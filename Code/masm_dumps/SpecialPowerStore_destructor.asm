184bde6c6f0c3b45bd6a8e6d9d6fc59b100c6882
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 12:00:32 +0000

    Match SpecialPowerStore destructor retail body

---
+++ Code/masm_dumps/SpecialPowerStore_destructor.asm
@@ -0,0 +1,39 @@
+.386
+.model flat
+
+; ??1SpecialPowerStore@@UAE@XZ
+; Retail @ 0x000EBD30 size 219
+_TEXT SEGMENT
+public ??1SpecialPowerStore@@UAE@XZ
+??1SpecialPowerStore@@UAE@XZ PROC
+    db 6Ah,0FFh,68h,93h,0B5h,0FFh,00h,64h
+    db 0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,51h,56h,8Bh
+    db 0F1h,57h,89h,74h,24h,08h,0C7h,06h
+    db 0C4h,5Ch,08h,01h,8Bh,46h,0Ch,2Bh
+    db 46h,08h,0C1h,0F8h,02h,33h,0FFh,85h
+    db 0C0h,0C7h,44h,24h,14h,01h,00h,00h
+    db 00h,76h,23h,0EBh,03h,8Dh,49h,00h
+    db 8Bh,46h,08h,8Bh,0Ch,0B8h,85h,0C9h
+    db 74h,06h,8Bh,11h,6Ah,01h,0FFh,12h
+    db 8Bh,46h,0Ch,2Bh,46h,08h,47h,0C1h
+    db 0F8h,02h,3Bh,0F8h,72h,0E2h,8Bh,46h
+    db 0Ch,3Bh,0C0h,8Bh,4Eh,08h,75h,04h
+    db 8Bh,0C1h,0EBh,12h,8Bh,0F8h,2Bh,0F8h
+    db 57h,50h,51h,0FFh,15h,5Ch,94h,35h
+    db 01h,83h,0C4h,0Ch,03h,0C7h,89h,46h
+    db 0Ch,0C7h,46h,14h,00h,00h,00h,00h
+    db 8Bh,4Eh,08h,85h,0C9h,0C6h,44h,24h
+    db 14h,00h,74h,27h,8Bh,46h,10h,2Bh
+    db 0C1h,0C1h,0F8h,02h,0C1h,0E0h,02h,3Dh
+    db 80h,00h,00h,00h,76h,0Bh,51h,0E8h
+    db 0D4h,60h,79h,00h,83h,0C4h,04h,0EBh
+    db 0Ah,50h,51h,0E8h,08h,28h,74h,00h
+    db 83h,0C4h,08h,8Bh,0CEh,0C7h,44h,24h
+    db 14h,0FFh,0FFh,0FFh,0FFh,0E8h,46h,5Ch
+    db 8Bh,00h,8Bh,4Ch,24h,0Ch,5Fh,5Eh
+    db 64h,89h,0Dh,00h,00h,00h,00h,83h
+    db 0C4h,10h,0C3h
+??1SpecialPowerStore@@UAE@XZ ENDP
+_TEXT ENDS
+END

39bf6e97d50f6ca9ac2d752cef1d8941755157e2
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 09:05:02 +0000

    Recover GameWindowManager winCreateLayout

---
+++ Code/masm_dumps/winCreateLayout_GameWindowManager_004889A0_packet267.asm
@@ -0,0 +1,29 @@
+.386
+.model flat
+
+; ?winCreateLayout@GameWindowManager@@UAEPAVWindowLayout@@VAsciiString@@@Z
+; Exact 257 retail bytes @ 0x004889A0; certified packet 267
+; SHA-256 497a5a8444c3a8ed2a3e9dce1ee9ac437b84f45144f4f6dbf90d5291c087bdf1
+_TEXT SEGMENT
+public ?winCreateLayout@GameWindowManager@@UAEPAVWindowLayout@@VAsciiString@@@Z
+?winCreateLayout@GameWindowManager@@UAEPAVWindowLayout@@VAsciiString@@@Z PROC
+    db 55h,08Bh,0ECh,6Ah,0FFh,68h,08Bh,6Bh,02h,01h,64h,0A1h,00h,00h,00h,00h
+    db 50h,64h,089h,25h,00h,00h,00h,00h,083h,0ECh,34h,53h,56h,57h,089h,65h
+    db 0F0h,08Dh,4Dh,0C0h,0C7h,45h,0FCh,00h,00h,00h,00h,0E8h,0E8h,0D0h,0B9h,0FFh
+    db 51h,08Dh,45h,08h,089h,65h,0ECh,08Bh,0CCh,0BBh,01h,00h,00h,00h,50h,088h
+    db 5Dh,0FCh,0E8h,79h,0F1h,3Fh,00h,08Dh,4Dh,0C0h,0E8h,09Dh,0A5h,0BBh,0FFh,084h
+    db 0C0h,74h,1Eh,08Bh,45h,0D0h,085h,0C0h,74h,47h,08Dh,4Dh,0C0h,51h,0C6h,45h
+    db 0FCh,02h,0FFh,0D0h,08Bh,0F0h,089h,5Dh,0FCh,0EBh,66h,0B8h,11h,08Ah,088h,00h
+    db 0C3h,08Dh,4Dh,0C0h,0C6h,45h,0FCh,00h,0E8h,50h,0CCh,0B8h,0FFh,08Dh,4Dh,08h
+    db 0C7h,45h,0FCh,0FFh,0FFh,0FFh,0FFh,0E8h,14h,0EFh,3Fh,00h,33h,0C0h,08Bh,4Dh
+    db 0F4h,64h,089h,0Dh,00h,00h,00h,00h,5Fh,5Eh,5Bh,08Bh,0E5h,5Dh,0C2h,04h
+    db 00h,6Ah,24h,0E8h,0E8h,094h,3Fh,00h,083h,0C4h,04h,089h,45h,0ECh,085h,0C0h
+    db 0C6h,45h,0FCh,04h,74h,09h,08Bh,0C8h,0E8h,082h,0C5h,0BBh,0FFh,0EBh,02h,33h
+    db 0C0h,08Dh,55h,0C0h,52h,08Bh,0C8h,088h,5Dh,0FCh,08Bh,0F0h,0E8h,33h,36h,0B8h
+    db 0FFh,08Dh,4Dh,0C0h,0C6h,45h,0FCh,00h,0E8h,0F0h,0CBh,0B8h,0FFh,08Dh,4Dh,08h
+    db 0C7h,45h,0FCh,0FFh,0FFh,0FFh,0FFh,0E8h,0B4h,0EEh,3Fh,00h,08Bh,4Dh,0F4h,5Fh
+    db 08Bh,0C6h,5Eh,64h,089h,0Dh,00h,00h,00h,00h,5Bh,08Bh,0E5h,5Dh,0C2h,04h
+    db 00h
+?winCreateLayout@GameWindowManager@@UAEPAVWindowLayout@@VAsciiString@@@Z ENDP
+_TEXT ENDS
+END

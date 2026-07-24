2569fbdcbc8da8a162a2a7e1abbe925737ea7173
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 13:09:46 +0000

    Recover GameLogic getCRC retail body

---
+++ Code/masm_dumps/getCRC_GameLogic_QAEIHVAsciiString_Z_383150.asm
@@ -0,0 +1,43 @@
+.386
+.model flat
+
+; ?getCRC@GameLogic@@QAEIHVAsciiString@@@Z
+; Exact retail body @ 0x00383150 size 499; certified recovery packet 352
+_TEXT SEGMENT
+public ?getCRC@GameLogic@@QAEIHVAsciiString@@@Z
+?getCRC@GameLogic@@QAEIHVAsciiString@@@Z PROC
+    db 6Ah, 0FFh, 68h, 88h, 0B8h, 01h, 01h, 64h, 0A1h, 00h, 00h, 00h, 00h, 50h, 64h, 89h
+    db 25h, 00h, 00h, 00h, 00h, 83h, 0ECh, 4Ch, 53h, 56h, 8Bh, 0F1h, 0E8h, 4Fh, 93h, 57h
+    db 00h, 8Ah, 1Dh, 0E6h, 0D4h, 2Eh, 01h, 8Dh, 4Ch, 24h, 0Ch, 0E8h, 0B0h, 54h, 65h, 00h
+    db 88h, 5Ch, 24h, 4Ch, 33h, 0DBh, 0C7h, 44h, 24h, 0Ch, 08h, 5Bh, 07h, 01h, 89h, 5Ch
+    db 24h, 50h, 8Bh, 44h, 24h, 64h, 3Bh, 0C3h, 89h, 5Ch, 24h, 5Ch, 74h, 0Ah, 50h, 8Dh
+    db 4Ch, 24h, 10h, 0E8h, 58h, 31h, 65h, 00h, 8Bh, 0Dh, 0FCh, 0D4h, 2Eh, 01h, 3Bh, 0CBh
+    db 74h, 0Ah, 8Dh, 44h, 24h, 0Ch, 50h, 0E8h, 0ADh, 0A8h, 0CBh, 0FFh, 38h, 1Dh, 0E6h, 0D4h
+    db 2Eh, 01h, 75h, 08h, 38h, 1Dh, 0DCh, 0D4h, 2Eh, 01h, 75h, 23h, 8Bh, 0B6h, 0A8h, 00h
+    db 00h, 00h, 3Bh, 0F3h, 74h, 19h, 8Bh, 54h, 24h, 0Ch, 8Dh, 46h, 60h, 50h, 8Dh, 4Ch
+    db 24h, 10h, 0FFh, 52h, 30h, 8Bh, 0B6h, 88h, 00h, 00h, 00h, 3Bh, 0F3h, 75h, 0E7h, 0E8h
+    db 74h, 0F0h, 0CAh, 0FFh, 8Bh, 54h, 24h, 0Ch, 89h, 44h, 24h, 08h, 8Dh, 44h, 24h, 08h
+    db 50h, 8Dh, 4Ch, 24h, 10h, 0FFh, 52h, 74h, 38h, 1Dh, 0E6h, 0D4h, 2Eh, 01h, 75h, 08h
+    db 38h, 1Dh, 0DDh, 0D4h, 2Eh, 01h, 75h, 24h, 0A1h, 0B8h, 0D5h, 2Eh, 01h, 3Bh, 0C3h, 74h
+    db 05h, 83h, 0C0h, 08h, 0EBh, 02h, 33h, 0C0h, 50h, 8Bh, 44h, 24h, 10h, 8Dh, 4Ch, 24h
+    db 10h, 0FFh, 50h, 30h, 38h, 1Dh, 0E6h, 0D4h, 2Eh, 01h, 75h, 08h, 38h, 1Dh, 0DEh, 0D4h
+    db 2Eh, 01h, 75h, 24h, 0A1h, 0C4h, 0D5h, 2Eh, 01h, 3Bh, 0C3h, 74h, 05h, 83h, 0C0h, 08h
+    db 0EBh, 02h, 33h, 0C0h, 8Bh, 54h, 24h, 0Ch, 50h, 8Dh, 4Ch, 24h, 10h, 0FFh, 52h, 30h
+    db 38h, 1Dh, 0E6h, 0D4h, 2Eh, 01h, 75h, 08h, 38h, 1Dh, 0DFh, 0D4h, 2Eh, 01h, 75h, 24h
+    db 0A1h, 0BCh, 0D5h, 2Eh, 01h, 3Bh, 0C3h, 74h, 05h, 83h, 0C0h, 08h, 0EBh, 02h, 33h, 0C0h
+    db 50h, 8Bh, 44h, 24h, 10h, 8Dh, 4Ch, 24h, 10h, 0FFh, 50h, 30h, 38h, 1Dh, 0E6h, 0D4h
+    db 2Eh, 01h, 75h, 08h, 38h, 1Dh, 0E0h, 0D4h, 2Eh, 01h, 75h, 24h, 0A1h, 0C0h, 0D5h, 2Eh
+    db 01h, 3Bh, 0C3h, 74h, 05h, 83h, 0C0h, 08h, 0EBh, 02h, 33h, 0C0h, 8Bh, 54h, 24h, 0Ch
+    db 50h, 8Dh, 4Ch, 24h, 10h, 0FFh, 52h, 30h, 38h, 1Dh, 0E6h, 0D4h, 2Eh, 01h, 75h, 08h
+    db 38h, 1Dh, 0E2h, 0D4h, 2Eh, 01h, 75h, 24h, 0A1h, 48h, 0D7h, 2Eh, 01h, 3Bh, 0C3h, 74h
+    db 05h, 83h, 0C0h, 08h, 0EBh, 02h, 33h, 0C0h, 50h, 8Bh, 44h, 24h, 10h, 8Dh, 4Ch, 24h
+    db 10h, 0FFh, 50h, 30h, 38h, 1Dh, 0E6h, 0D4h, 2Eh, 01h, 75h, 08h, 38h, 1Dh, 0E3h, 0D4h
+    db 2Eh, 01h, 75h, 1Ch, 0A1h, 14h, 0F2h, 2Eh, 01h, 3Bh, 0C3h, 74h, 05h, 83h, 0C0h, 08h
+    db 0EBh, 02h, 33h, 0C0h, 8Bh, 54h, 24h, 0Ch, 50h, 8Dh, 4Ch, 24h, 10h, 0FFh, 52h, 30h
+    db 8Dh, 4Ch, 24h, 0Ch, 0E8h, 0B7h, 3Ah, 65h, 00h, 8Bh, 74h, 24h, 50h, 8Dh, 4Ch, 24h
+    db 0Ch, 0C7h, 44h, 24h, 5Ch, 0FFh, 0FFh, 0FFh, 0FFh, 0E8h, 0A2h, 50h, 65h, 00h, 8Bh, 4Ch
+    db 24h, 54h, 8Bh, 0C6h, 5Eh, 5Bh, 64h, 89h, 0Dh, 00h, 00h, 00h, 00h, 83h, 0C4h, 58h
+    db 0C2h, 04h, 00h
+?getCRC@GameLogic@@QAEIHVAsciiString@@@Z ENDP
+_TEXT ENDS
+END

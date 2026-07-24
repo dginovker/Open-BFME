cdc5b45d394ad2bf46799f9dd1819bd22291f5c3
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 09:09:16 +0000

    Recover CachedFileInputStream open retail body

---
+++ Code/masm_dumps/CachedFileInputStream_open_102A60.asm
@@ -0,0 +1,27 @@
+.386
+.model flat
+
+; ?open@CachedFileInputStream@@QAE_NVAsciiString@@@Z
+; Exact 253 retail bytes @ 0x00102A60
+_TEXT SEGMENT
+public ?open@CachedFileInputStream@@QAE_NVAsciiString@@@Z
+?open@CachedFileInputStream@@QAE_NVAsciiString@@@Z PROC
+    db 064h, 0A1h, 000h, 000h, 000h, 000h, 06Ah, 0FFh, 068h, 028h, 0C9h, 0FFh, 000h, 050h, 064h, 089h
+    db 025h, 000h, 000h, 000h, 000h, 053h, 055h, 056h, 057h, 08Bh, 0F1h, 08Bh, 044h, 024h, 020h, 033h
+    db 0DBh, 03Bh, 0C3h, 089h, 05Ch, 024h, 018h, 074h, 005h, 083h, 0C0h, 008h, 0EBh, 005h, 0B8h, 08Bh
+    db 038h, 007h, 001h, 08Bh, 00Dh, 048h, 0CBh, 034h, 001h, 06Ah, 041h, 050h, 0E8h, 0BFh, 05Dh, 08Ch
+    db 000h, 08Bh, 0F8h, 03Bh, 0FBh, 089h, 05Eh, 004h, 074h, 01Dh, 08Bh, 007h, 08Bh, 0CFh, 0FFh, 050h
+    db 02Ch, 03Bh, 0C3h, 089h, 046h, 004h, 074h, 00Ch, 08Bh, 017h, 08Bh, 0CFh, 0FFh, 052h, 034h, 089h
+    db 046h, 008h, 033h, 0FFh, 089h, 05Eh, 00Ch, 08Bh, 046h, 004h, 08Bh, 04Eh, 008h, 050h, 051h, 0E8h
+    db 0ACh, 0BEh, 071h, 000h, 083h, 0C4h, 008h, 084h, 0C0h, 074h, 047h, 08Bh, 056h, 004h, 08Bh, 046h
+    db 008h, 052h, 050h, 0E8h, 0E8h, 0BBh, 071h, 000h, 08Bh, 0D8h, 053h, 0E8h, 080h, 0F4h, 077h, 000h
+    db 08Bh, 04Eh, 004h, 08Bh, 056h, 008h, 053h, 08Bh, 0E8h, 055h, 051h, 052h, 0E8h, 08Fh, 0BDh, 071h
+    db 000h, 083h, 0C4h, 01Ch, 03Bh, 0C3h, 075h, 011h, 08Bh, 046h, 008h, 050h, 0E8h, 0DFh, 0F3h, 077h
+    db 000h, 089h, 06Eh, 008h, 089h, 05Eh, 004h, 0EBh, 006h, 055h, 0E8h, 0D1h, 0F3h, 077h, 000h, 083h
+    db 0C4h, 004h, 085h, 0FFh, 074h, 007h, 08Bh, 007h, 08Bh, 0CFh, 0FFh, 050h, 008h, 08Bh, 046h, 004h
+    db 085h, 0C0h, 08Dh, 04Ch, 024h, 020h, 00Fh, 095h, 0C3h, 0C7h, 044h, 024h, 018h, 0FFh, 0FFh, 0FFh
+    db 0FFh, 0E8h, 0FAh, 04Dh, 078h, 000h, 08Bh, 04Ch, 024h, 010h, 05Fh, 05Eh, 05Dh, 08Ah, 0C3h, 064h
+    db 089h, 00Dh, 000h, 000h, 000h, 000h, 05Bh, 083h, 0C4h, 00Ch, 0C2h, 004h, 000h
+?open@CachedFileInputStream@@QAE_NVAsciiString@@@Z ENDP
+_TEXT ENDS
+END

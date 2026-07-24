2a643b94b13e8685cf0c03f49b6a214cb7a67369
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 04:48:58 +0000

    Recover GadgetComboBoxSetText retail body

---
+++ Code/masm_dumps/GadgetComboBoxSetText_004B4E70_c668ce27.asm
@@ -0,0 +1,21 @@
+.386
+.model flat
+
+; ?GadgetComboBoxSetText@@YAXPAVGameWindow@@VUnicodeString@@@Z
+; Exact 157 retail bytes @ 0x004B4E70; certified packet 187
+_TEXT SEGMENT
+public ?GadgetComboBoxSetText@@YAXPAVGameWindow@@VUnicodeString@@@Z
+?GadgetComboBoxSetText@@YAXPAVGameWindow@@VUnicodeString@@@Z PROC
+    db 64h,0A1h,00h,00h,00h,00h,6Ah,0FFh,68h,00h,8Ch,02h,01h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,56h,8Bh,74h,24h,14h,85h,0F6h,0C7h,44h,24h,0Ch
+    db 00h,00h,00h,00h,74h,56h,8Bh,0CEh,0E8h,9Bh,16h,0B9h,0FFh,85h,0C0h,74h
+    db 07h,8Bh,40h,2Ch,85h,0C0h,75h,02h,33h,0C0h,6Ah,0FFh,50h,0E8h,51h,0C3h
+    db 0B4h,0FFh,83h,0C4h,04h,8Dh,44h,24h,1Ch,89h,64h,24h,18h,8Bh,0CCh,50h
+    db 0E8h,3Bh,35h,3Dh,00h,8Bh,0CEh,0C6h,44h,24h,10h,01h,0E8h,67h,16h,0B9h
+    db 0FFh,85h,0C0h,74h,07h,8Bh,40h,28h,85h,0C0h,75h,02h,33h,0C0h,50h,0C6h
+    db 44h,24h,14h,00h,0E8h,8Bh,79h,0B5h,0FFh,83h,0C4h,08h,8Dh,4Ch,24h,18h
+    db 0C7h,44h,24h,0Ch,0FFh,0FFh,0FFh,0FFh,0E8h,0D3h,32h,3Dh,00h,8Bh,4Ch,24h
+    db 04h,64h,89h,0Dh,00h,00h,00h,00h,5Eh,83h,0C4h,0Ch,0C3h
+?GadgetComboBoxSetText@@YAXPAVGameWindow@@VUnicodeString@@@Z ENDP
+_TEXT ENDS
+END

e77492d0b4322592f0ef708628f65239a0ee9acd
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 12:30:35 +0000

    Recover MainMenuScaleUpTransition update

---
+++ Code/masm_dumps/update_MainMenuScaleUpTransition_UAEXH_Z_0059E610_packet340.asm
@@ -0,0 +1,21 @@
+.386
+.model flat
+
+; ?update@MainMenuScaleUpTransition@@UAEXH@Z
+; Exact 150 retail bytes @ 0x0059E610; certified packet 340
+_TEXT SEGMENT
+public ?update@MainMenuScaleUpTransition@@UAEXH@Z
+?update@MainMenuScaleUpTransition@@UAEXH@Z PROC
+    db 56h,8bh,0f1h,8bh,46h,10h,57h,8bh,7ch,24h,0ch,3bh,0f8h,0c7h,46h,28h
+    db 0ffh,0ffh,0ffh,0ffh,7ch,7bh,8bh,4eh,14h,3bh,0f9h,7fh,74h,3bh,0f8h,75h
+    db 19h,8ah,46h,09h,84h,0c0h,75h,40h,8bh,46h,0ch,85h,0c0h,74h,39h,8bh
+    db 4eh,4ch,85h,0c9h,74h,32h,6ah,01h,0ebh,25h,3bh,0f9h,75h,2ah,8ah,46h
+    db 09h,84h,0c0h,74h,23h,8bh,4eh,0ch,85h,0c9h,74h,1ch,8bh,46h,4ch,85h
+    db 0c0h,74h,15h,6ah,01h,0e8h,0c0h,98h,0a8h,0ffh,8bh,4eh,4ch,6ah,00h,0e8h
+    db 0b6h,98h,0a8h,0ffh,0c6h,46h,08h,01h,3bh,7eh,10h,7eh,24h,3bh,7eh,14h
+    db 7dh,1fh,8bh,4eh,0ch,85h,0c9h,74h,07h,6ah,01h,0e8h,9ah,98h,0a8h,0ffh
+    db 8bh,4eh,4ch,85h,0c9h,74h,07h,6ah,01h,0e8h,8ch,98h,0a8h,0ffh,89h,7eh
+    db 28h,5fh,5eh,0c2h,04h,00h
+?update@MainMenuScaleUpTransition@@UAEXH@Z ENDP
+_TEXT ENDS
+END

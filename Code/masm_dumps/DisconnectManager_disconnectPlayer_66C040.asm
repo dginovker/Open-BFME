97000e5aab2ec7025e91763e86b01ba4d1234bc5
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 03:06:18 +0000

    Recover DisconnectManager disconnectPlayer retail body

---
+++ Code/masm_dumps/DisconnectManager_disconnectPlayer_66C040.asm
@@ -0,0 +1,26 @@
+.386
+.model flat
+
+; ?disconnectPlayer@DisconnectManager@@IAEXHPAVConnectionManager@@@Z
+; Certified exact body @ 0x0066C040 size 231.
+_TEXT SEGMENT
+public ?disconnectPlayer@DisconnectManager@@IAEXHPAVConnectionManager@@@Z
+?disconnectPlayer@DisconnectManager@@IAEXHPAVConnectionManager@@@Z PROC
+    db 64h, 0A1h, 00h, 00h, 00h, 00h, 6Ah, 0FFh, 68h, 18h, 48h, 04h, 01h, 50h, 64h, 89h
+    db 25h, 00h, 00h, 00h, 00h, 53h, 56h, 8Bh, 74h, 24h, 18h, 85h, 0F6h, 57h, 0Fh, 8Ch
+    db 0AFh, 00h, 00h, 00h, 83h, 0FEh, 08h, 0Fh, 8Dh, 0A6h, 00h, 00h, 00h, 8Bh, 0Dh, 8Ch
+    db 70h, 2Fh, 01h, 85h, 0C9h, 74h, 0Eh, 56h, 0E8h, 9Bh, 2Bh, 9Bh, 0FFh, 85h, 0C0h, 74h
+    db 04h, 0C6h, 40h, 40h, 01h, 8Bh, 5Ch, 24h, 20h, 8Bh, 0CBh, 0E8h, 01h, 0E2h, 9Dh, 0FFh
+    db 3Bh, 0F0h, 7Dh, 04h, 8Bh, 0FEh, 0EBh, 05h, 74h, 79h, 8Dh, 7Eh, 0FFh, 83h, 0FFh, 0FFh
+    db 74h, 71h, 56h, 8Dh, 44h, 24h, 20h, 50h, 8Bh, 0CBh, 0E8h, 0B3h, 69h, 9Ah, 0FFh, 56h
+    db 51h, 8Dh, 54h, 24h, 24h, 89h, 64h, 24h, 28h, 8Bh, 0CCh, 52h, 0C7h, 44h, 24h, 20h
+    db 00h, 00h, 00h, 00h, 0E8h, 37h, 0C3h, 21h, 00h, 8Bh, 0Dh, 2Ch, 0D6h, 2Eh, 01h, 0E8h
+    db 5Ch, 0CAh, 9Dh, 0FFh, 0A1h, 64h, 49h, 2Fh, 01h, 85h, 0C0h, 74h, 1Dh, 51h, 8Dh, 44h
+    db 24h, 20h, 89h, 64h, 24h, 24h, 8Bh, 0CCh, 50h, 0E8h, 12h, 0C3h, 21h, 00h, 8Bh, 0Dh
+    db 64h, 49h, 2Fh, 01h, 57h, 0E8h, 39h, 31h, 9Dh, 0FFh, 56h, 8Bh, 0CBh, 0E8h, 5Bh, 0CCh
+    db 9Bh, 0FFh, 8Dh, 4Ch, 24h, 1Ch, 0C7h, 44h, 24h, 14h, 0FFh, 0FFh, 0FFh, 0FFh, 0E8h, 0BDh
+    db 0C0h, 21h, 00h, 8Bh, 4Ch, 24h, 0Ch, 5Fh, 5Eh, 64h, 89h, 0Dh, 00h, 00h, 00h, 00h
+    db 5Bh, 83h, 0C4h, 0Ch, 0C2h, 08h, 00h
+?disconnectPlayer@DisconnectManager@@IAEXHPAVConnectionManager@@@Z ENDP
+_TEXT ENDS
+END

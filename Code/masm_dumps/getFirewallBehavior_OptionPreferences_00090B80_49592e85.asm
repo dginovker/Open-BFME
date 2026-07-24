b528e022d8ba81c23febaf5ebedfa303d7f1f86d
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 05:57:58 +0000

    Recover OptionPreferences firewall behavior getter

---
+++ Code/masm_dumps/getFirewallBehavior_OptionPreferences_00090B80_49592e85.asm
@@ -0,0 +1,24 @@
+.386
+.model flat
+
+; ?getFirewallBehavior@OptionPreferences@@QAEHXZ
+; Exact 100 retail bytes @ 0x00090B80; SHA-256 49592e8591aa5819fd94eaced4f32104556fe621dd21d36abf7981164d5d6479.
+_TEXT SEGMENT
+public ?getFirewallBehavior@OptionPreferences@@QAEHXZ
+?getFirewallBehavior@OptionPreferences@@QAEHXZ PROC
+    db 51h,56h,57h,8Bh,0F1h,68h,7Ch,0A1h
+    db 07h,01h,8Dh,4Ch,24h,0Ch,0E8h,2Dh
+    db 80h,7Fh,00h,8Dh,44h,24h,08h,83h
+    db 0C6h,04h,50h,8Bh,0CEh,0E8h,0Ah,0A3h
+    db 0F7h,0FFh,8Dh,4Ch,24h,08h,8Bh,0F8h
+    db 0E8h,93h,6Dh,7Fh,00h,3Bh,3Eh,75h
+    db 10h,8Bh,0Dh,0C8h,0D5h,2Eh,01h,8Bh
+    db 81h,14h,0Bh,00h,00h,5Fh,5Eh,59h
+    db 0C3h,8Bh,7Fh,14h,85h,0FFh,8Dh,47h
+    db 08h,75h,05h,0B8h,8Bh,38h,07h,01h
+    db 50h,0FFh,15h,84h,93h,35h,01h,83h
+    db 0C4h,04h,85h,0C0h,7Dh,02h,33h,0C0h
+    db 5Fh,5Eh,59h,0C3h
+?getFirewallBehavior@OptionPreferences@@QAEHXZ ENDP
+_TEXT ENDS
+END

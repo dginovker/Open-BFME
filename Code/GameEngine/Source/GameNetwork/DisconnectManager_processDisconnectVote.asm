c4db08c6fccc7fe2de0c1e077028e1905b6af9f7
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 08:17:38 +0000

    Recover DisconnectManager processDisconnectVote

---
+++ Code/GameEngine/Source/GameNetwork/DisconnectManager_processDisconnectVote.asm
@@ -0,0 +1,22 @@
+.386
+.model flat
+
+; ?processDisconnectVote@DisconnectManager@@IAEXPAVNetCommandMsg@@PAVConnectionManager@@@Z
+; Exact 87 retail bytes @ 0x0066BD10
+_TEXT SEGMENT
+public ?processDisconnectVote@DisconnectManager@@IAEXPAVNetCommandMsg@@PAVConnectionManager@@@Z
+?processDisconnectVote@DisconnectManager@@IAEXPAVNetCommandMsg@@PAVConnectionManager@@@Z PROC
+    db 53h,55h,8Bh,6Ch,24h,10h,56h,57h
+    db 8Bh,7Ch,24h,14h,8Bh,77h,0Ch,8Bh
+    db 0D9h,8Bh,0CDh,0E8h,69h,0E5h,9Dh,0FFh
+    db 3Bh,0F0h,7Ch,08h,75h,05h,83h,0CEh
+    db 0FFh,0EBh,01h,4Eh,55h,56h,8Bh,0CBh
+    db 0E8h,0Bh,0E9h,9Ch,0FFh,84h,0C0h,74h
+    db 1Fh,8Bh,47h,0Ch,55h,50h,8Bh,0CFh
+    db 0E8h,0D3h,81h,9Ah,0FFh,50h,8Bh,0CFh
+    db 0E8h,62h,9Bh,99h,0FFh,0Fh,0B6h,0C0h
+    db 50h,8Bh,0CBh,0E8h,4Dh,58h,9Bh,0FFh
+    db 5Fh,5Eh,5Dh,5Bh,0C2h,08h,00h
+?processDisconnectVote@DisconnectManager@@IAEXPAVNetCommandMsg@@PAVConnectionManager@@@Z ENDP
+_TEXT ENDS
+END

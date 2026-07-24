64d76c6f30e31c34e7d7b544dfbe848c20ea1801
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 12:22:07 +0000

    Recover WeaponStore newWeaponTemplate retail body

---
+++ Code/masm_dumps/newWeaponTemplate_WeaponStore_001E7F50_packet337.asm
@@ -0,0 +1,29 @@
+.386
+.model flat
+
+; ?newWeaponTemplate@WeaponStore@@IAEPAVWeaponTemplate@@VAsciiString@@@Z
+; Exact 261 retail bytes @ 0x001E7F50; certified packet 337
+; SHA-256 90f8376182dd2aab1ea9669ea2e5a32b0bacccef12f18d64a555cff615bea476
+_TEXT SEGMENT
+public ?newWeaponTemplate@WeaponStore@@IAEPAVWeaponTemplate@@VAsciiString@@@Z
+?newWeaponTemplate@WeaponStore@@IAEPAVWeaponTemplate@@VAsciiString@@@Z PROC
+    db 6Ah,0FFh,68h,0F3h,0A5h,00h,01h,64h,0A1h,00h,00h,00h,00h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,51h,57h,8Bh,0F9h,8Bh,44h,24h,18h,85h,0C0h,0C7h
+    db 44h,24h,10h,00h,00h,00h,00h,74h,07h,66h,83h,78h,04h,00h,75h,25h
+    db 8Dh,4Ch,24h,18h,0C7h,44h,24h,10h,0FFh,0FFh,0FFh,0FFh,0E8h,0AFh,0F9h,69h
+    db 00h,33h,0C0h,5Fh,8Bh,4Ch,24h,04h,64h,89h,0Dh,00h,00h,00h,00h,83h
+    db 0C4h,10h,0C2h,04h,00h,56h,68h,3Ch,05h,00h,00h,0E8h,80h,9Fh,69h,00h
+    db 83h,0C4h,04h,89h,44h,24h,08h,85h,0C0h,0C6h,44h,24h,14h,01h,74h,0Bh
+    db 8Bh,0C8h,0E8h,0A5h,0C5h,0E5h,0FFh,8Bh,0F0h,0EBh,02h,33h,0F6h,8Dh,44h,24h
+    db 1Ch,8Dh,4Eh,08h,50h,0C6h,44h,24h,18h,00h,89h,74h,24h,0Ch,0E8h,0ADh
+    db 0FCh,69h,00h,8Bh,44h,24h,1Ch,85h,0C0h,74h,05h,83h,0C0h,08h,0EBh,05h
+    db 0B8h,8Bh,38h,07h,01h,8Bh,0Dh,00h,0D6h,2Eh,01h,50h,0E8h,0D6h,2Dh,0E5h
+    db 0FFh,8Dh,4Fh,08h,89h,46h,0Ch,8Bh,41h,04h,3Bh,41h,08h,74h,0Ch,85h
+    db 0C0h,74h,02h,89h,30h,83h,41h,04h,04h,0EBh,14h,6Ah,01h,6Ah,01h,8Dh
+    db 54h,24h,24h,52h,8Dh,54h,24h,14h,52h,50h,0E8h,38h,0A0h,0E2h,0FFh,8Dh
+    db 4Ch,24h,1Ch,0C7h,44h,24h,14h,0FFh,0FFh,0FFh,0FFh,0E8h,00h,0F9h,69h,00h
+    db 8Bh,4Ch,24h,0Ch,8Bh,0C6h,5Eh,5Fh,64h,89h,0Dh,00h,00h,00h,00h,83h
+    db 0C4h,10h,0C2h,04h,00h
+?newWeaponTemplate@WeaponStore@@IAEPAVWeaponTemplate@@VAsciiString@@@Z ENDP
+_TEXT ENDS
+END

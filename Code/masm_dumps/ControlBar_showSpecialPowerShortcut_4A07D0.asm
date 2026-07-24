0b54cdd4945808f6c98cf7997581fef3e98cdd22
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 08:35:26 +0000

    Recover ControlBar showSpecialPowerShortcut

---
+++ Code/masm_dumps/ControlBar_showSpecialPowerShortcut_4A07D0.asm
@@ -0,0 +1,21 @@
+.386
+.model flat
+
+; ?showSpecialPowerShortcut@ControlBar@@QAEXXZ
+; Exact 153 retail bytes @ RVA 0x004A07D0; certified ControlBar member body
+_TEXT SEGMENT
+public ?showSpecialPowerShortcut@ControlBar@@QAEXXZ
+?showSpecialPowerShortcut@ControlBar@@QAEXXZ PROC
+    db 0A1h,06Ch,07h,02Fh,01h,056h,08Bh,0F1h,08Bh,088h,080h,070h,01h,00h,085h,0C9h
+    db 00Fh,08Dh,081h,00h,00h,00h,08Bh,086h,0FCh,00h,00h,00h,085h,0C0h,074h,077h
+    db 053h,08Dh,09Eh,0CCh,00h,00h,00h,085h,0DBh,074h,06Bh,0A1h,048h,0D7h,02Eh,01h
+    db 085h,0C0h,074h,062h,08Bh,048h,00Ch,085h,0C9h,074h,05Bh,08Bh,086h,0F4h,00h,00h
+    db 00h,057h,033h,0FFh,085h,0C0h,07Eh,04Dh,08Bh,00Bh,0E8h,019h,05Dh,0BAh,0FFh,085h
+    db 0C0h,075h,012h,08Bh,086h,0F4h,00h,00h,00h,047h,083h,0C3h,04h,03Bh,0F8h,07Ch
+    db 0E7h,05Fh,05Bh,05Eh,0C3h,08Bh,00Dh,048h,0D7h,02Eh,01h,08Bh,049h,00Ch,0E8h,0D9h
+    db 02Ah,0B8h,0FFh,085h,0C0h,074h,01Eh,08Bh,08Eh,0FCh,00h,00h,00h,06Ah,00h,0E8h
+    db 0D6h,076h,0B8h,0FFh,08Bh,015h,048h,0D7h,02Eh,01h,08Bh,042h,00Ch,050h,08Bh,0CEh
+    db 0E8h,030h,03Eh,0B8h,0FFh,05Fh,05Bh,05Eh,0C3h
+?showSpecialPowerShortcut@ControlBar@@QAEXXZ ENDP
+_TEXT ENDS
+END

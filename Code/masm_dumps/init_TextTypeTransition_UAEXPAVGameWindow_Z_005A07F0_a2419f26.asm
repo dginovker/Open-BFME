71801fe0064ada18b7ca8388eb0daacdbda92351
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 06:05:37 +0000

    Recover TextTypeTransition init retail body

---
+++ Code/masm_dumps/init_TextTypeTransition_UAEXPAVGameWindow_Z_005A07F0_a2419f26.asm
@@ -0,0 +1,28 @@
+.386
+.model flat
+
+; ?init@TextTypeTransition@@UAEXPAVGameWindow@@@Z
+; Exact 220 retail bytes @ 0x005A07F0 (SHA-256 a2419f2649de4939c78ca27f9528c905e6bb74f08cdfdd43de237dc8e943ef6f).
+; Identity: TextTypeTransition vtable 0x0110CCFC slot 1 resolves through ILT
+; 0x00412F53 to this body; adjacent slots resolve to its matched destructor
+; and update bodies at 0x005A0A50 and 0x005A0910.
+_TEXT SEGMENT
+public ?init@TextTypeTransition@@UAEXPAVGameWindow@@@Z
+?init@TextTypeTransition@@UAEXPAVGameWindow@@@Z PROC
+    db 64h,0A1h,00h,00h,00h,00h,6Ah,0FFh,68h,08h,85h,03h,01h,50h,64h,89h
+    db 25h,00h,00h,00h,00h,56h,8Bh,0F1h,8Bh,4Ch,24h,14h,85h,0C9h,57h,74h
+    db 20h,8Dh,46h,24h,50h,8Dh,56h,20h,52h,89h,4Eh,0Ch,0E8h,9Bh,66h,0A9h
+    db 0FFh,8Dh,46h,1Ch,50h,8Dh,4Eh,18h,51h,8Bh,4Eh,0Ch,0E8h,1Ah,0F1h,0A8h
+    db 0FFh,8Bh,46h,10h,8Bh,16h,50h,8Bh,0CEh,0C6h,46h,09h,00h,0FFh,52h,08h
+    db 0C6h,46h,08h,00h,0C6h,46h,09h,01h,8Bh,0Dh,0CCh,12h,2Fh,01h,8Bh,11h
+    db 0FFh,52h,24h,89h,46h,34h,8Bh,46h,0Ch,50h,8Dh,4Ch,24h,1Ch,51h,0E8h
+    db 0DFh,81h,0A7h,0FFh,83h,0C4h,08h,8Dh,7Eh,2Ch,50h,8Bh,0CFh,0C7h,44h,24h
+    db 14h,00h,00h,00h,00h,0E8h,0B6h,7Ch,2Eh,00h,8Dh,4Ch,24h,18h,0C7h,44h
+    db 24h,10h,0FFh,0FFh,0FFh,0FFh,0E8h,45h,79h,2Eh,00h,8Bh,3Fh,85h,0FFh,74h
+    db 06h,0Fh,0B7h,47h,04h,0EBh,02h,33h,0C0h,8Bh,4Eh,14h,3Bh,0C1h,7Dh,16h
+    db 89h,46h,04h,8Bh,4Ch,24h,08h,64h,89h,0Dh,00h,00h,00h,00h,5Fh,5Eh
+    db 83h,0C4h,0Ch,0C2h,04h,00h,89h,4Eh,04h,8Bh,4Ch,24h,08h,5Fh,64h,89h
+    db 0Dh,00h,00h,00h,00h,5Eh,83h,0C4h,0Ch,0C2h,04h,00h
+?init@TextTypeTransition@@UAEXPAVGameWindow@@@Z ENDP
+_TEXT ENDS
+END

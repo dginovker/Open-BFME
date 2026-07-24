01d35b43bbce18a480434265c9ac50cbbfe931f9
Open-BFME Fleet Publisher <open-bfme-fleet@localhost>
2026-07-24 07:02:20 +0000

    Recover AIMoveToState onEnter retail body

---
+++ Code/masm_dumps/AIMoveToState_onEnter_173470.asm
@@ -0,0 +1,32 @@
+.386
+.model flat
+
+; ?onEnter@AIMoveToState@@UAE?AW4StateReturnType@@XZ
+; Certified retail body @ 0x00173470 size 322.
+_TEXT SEGMENT
+public ?onEnter@AIMoveToState@@UAE?AW4StateReturnType@@XZ
+?onEnter@AIMoveToState@@UAE?AW4StateReturnType@@XZ PROC
+    db 051h, 0A0h, 039h, 002h, 02Fh, 001h, 084h, 0C0h, 056h, 057h, 08Bh, 0F1h, 074h, 017h, 0A1h, 0FCh
+    db 0D4h, 02Eh, 001h, 085h, 0C0h, 074h, 00Eh, 068h, 094h, 064h, 009h, 001h, 050h, 0E8h, 0E8h, 06Ch
+    db 0ECh, 0FFh, 083h, 0C4h, 008h, 08Bh, 04Eh, 01Ch, 0C6h, 046h, 04Ch, 001h, 08Bh, 041h, 010h, 08Bh
+    db 0B8h, 004h, 002h, 000h, 000h, 0E8h, 0C6h, 0B0h, 0E9h, 0FFh, 085h, 0C0h, 074h, 040h, 085h, 0FFh
+    db 074h, 03Ch, 08Bh, 04Eh, 01Ch, 053h, 0E8h, 0B5h, 0B0h, 0E9h, 0FFh, 08Bh, 058h, 074h, 08Bh, 0CFh
+    db 0E8h, 0A6h, 06Eh, 0EAh, 0FFh, 03Bh, 0D8h, 05Bh, 075h, 024h, 0A0h, 039h, 002h, 02Fh, 001h, 084h
+    db 0C0h, 074h, 017h, 0A1h, 0FCh, 0D4h, 02Eh, 001h, 085h, 0C0h, 074h, 00Eh, 068h, 070h, 08Fh, 009h
+    db 001h, 050h, 0E8h, 093h, 06Ch, 0ECh, 0FFh, 083h, 0C4h, 008h, 0C6h, 046h, 04Ch, 000h, 08Bh, 04Eh
+    db 01Ch, 0E8h, 07Ah, 0B0h, 0E9h, 0FFh, 085h, 0C0h, 08Bh, 04Eh, 01Ch, 00Fh, 084h, 08Fh, 000h, 000h
+    db 000h, 0E8h, 06Ah, 0B0h, 0E9h, 0FFh, 08Bh, 07Eh, 01Ch, 083h, 0C0h, 038h, 08Bh, 010h, 08Dh, 04Eh
+    db 024h, 089h, 011h, 08Bh, 050h, 004h, 089h, 051h, 004h, 08Bh, 040h, 008h, 089h, 041h, 008h, 08Bh
+    db 047h, 010h, 083h, 0C0h, 004h, 08Bh, 000h, 085h, 0C0h, 074h, 00Ch, 08Bh, 048h, 004h, 085h, 0C9h
+    db 074h, 005h, 0E8h, 084h, 0EDh, 0E8h, 0FFh, 0F7h, 080h, 0C8h, 000h, 000h, 000h, 000h, 000h, 000h
+    db 002h, 074h, 063h, 08Bh, 0CFh, 0E8h, 026h, 0B0h, 0E9h, 0FFh, 08Dh, 088h, 0ACh, 000h, 000h, 000h
+    db 0E8h, 0ABh, 0AAh, 070h, 000h, 0D8h, 00Dh, 03Ch, 053h, 007h, 001h, 08Bh, 04Eh, 01Ch, 0D9h, 054h
+    db 024h, 008h, 0D8h, 046h, 02Ch, 0D9h, 05Eh, 02Ch, 0E8h, 003h, 0B0h, 0E9h, 0FFh, 0D9h, 040h, 040h
+    db 0D8h, 05Eh, 02Ch, 0DFh, 0E0h, 0F6h, 0C4h, 005h, 07Ah, 02Ch, 0D9h, 044h, 024h, 008h, 05Fh, 0D8h
+    db 046h, 02Ch, 08Bh, 0CEh, 0D9h, 05Eh, 02Ch, 05Eh, 083h, 0C4h, 004h, 0E9h, 097h, 0E8h, 0EAh, 0FFh
+    db 083h, 0C1h, 024h, 08Bh, 001h, 08Dh, 056h, 024h, 089h, 002h, 08Bh, 041h, 004h, 089h, 042h, 004h
+    db 08Bh, 049h, 008h, 089h, 04Ah, 008h, 05Fh, 08Bh, 0CEh, 05Eh, 083h, 0C4h, 004h, 0E9h, 075h, 0E8h
+    db 0EAh, 0FFh
+?onEnter@AIMoveToState@@UAE?AW4StateReturnType@@XZ ENDP
+_TEXT ENDS
+END
